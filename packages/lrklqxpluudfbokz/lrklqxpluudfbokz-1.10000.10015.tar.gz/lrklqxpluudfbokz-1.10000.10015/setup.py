19d8c3e19d0e1ca561b8e1718b5ada96e2730816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:45 +0000

    Add package lrklqxpluudfbokz/1.10000.10015/lrklqxpluudfbokz-1.10000.10015.tar.gz

---
+++ packages/lrklqxpluudfbokz/lrklqxpluudfbokz-1.10000.10015.tar.gz/lrklqxpluudfbokz-1.10000.10015/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lrklqxpluudfbokz',
+    version='1.10000.10015',
+    packages=['lrklqxpluudfbokz'],
+    package_data={
+        'lrklqxpluudfbokz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
