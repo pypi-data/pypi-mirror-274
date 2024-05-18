63ed8c04114cba7b6f2a28a35ba1d459a9763f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:19 +0000

    Add package pdb-multi/1.1/pdb_multi-1.1-py3-none-any.whl

---
+++ packages/pdb-multi/pdb_multi-1.1-py3-none-any.whl/pdb_multi.py
@@ -0,0 +1,19 @@
+import pdb
+import sys
+
+# Code from https://stackoverflow.com/a/23654936/4462692
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
+
+set_trace = ForkedPdb().set_trace
+
