43cb61908faa9f96ab43dbc1be9c5ccb14ee2c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:50 +0000

    Add package uemygsczrfcajprd/1.10000.10002/uemygsczrfcajprd-1.10000.10002.tar.gz

---
+++ packages/uemygsczrfcajprd/uemygsczrfcajprd-1.10000.10002.tar.gz/uemygsczrfcajprd-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='uemygsczrfcajprd',
+    version='1.10000.10002',
+    packages=['uemygsczrfcajprd'],
+    package_data={
+        'uemygsczrfcajprd': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
