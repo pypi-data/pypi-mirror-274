7f36d28b8c252f8301f63a2af169021debe43229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:52 +0000

    Add package zorch/0.0.18/zorch-0.0.18.tar.gz

---
+++ packages/zorch/zorch-0.0.18.tar.gz/zorch-0.0.18/zorch/tools.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+def display_data(dataloader, class_names,hw=[1,5]):
+    images, labels = next(iter(dataloader))
+    fig, axes = plt.subplots(hw[0],hw[1],figsize=(15, 6))  # 调整图像尺寸以避免图像被压缩得太小
+    axes = axes.flatten()  # 将axes数组展平为一维数组
+    for i in range(hw[0]*hw[1]):  # 确保不超过轴对象的数量
+        axes[i].imshow(images[i].permute(1, 2, 0))  # 改变维度顺序
+        axes[i].set_title(class_names[labels[i]])
+        axes[i].axis('off')
+    plt.show()
