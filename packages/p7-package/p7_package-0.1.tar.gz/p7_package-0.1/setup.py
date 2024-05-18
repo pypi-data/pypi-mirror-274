b7310b9a61ed5f5821d31a0ba4523df9876dee21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:52 +0000

    Add package p7-package/0.1/p7_package-0.1.tar.gz

---
+++ packages/p7-package/p7_package-0.1.tar.gz/p7_package-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as f:
+#     description = f.read()
+
+setup(
+    name='p7_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+)
+
+
+# entry_points={
+#     "console_scripts" : [
+#         "algebrator = algebrator:testing",
+#     ],
+# },
+# long_description=description,
+# long_description_content_type="text/markdown"
