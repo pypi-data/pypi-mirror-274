4fc3119040b8450d612724c6e8938da2e2a5d9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:46 +0000

    Add package wgswceqeowcimdbh/1.10000.10001/wgswceqeowcimdbh-1.10000.10001.tar.gz

---
+++ packages/wgswceqeowcimdbh/wgswceqeowcimdbh-1.10000.10001.tar.gz/wgswceqeowcimdbh-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wgswceqeowcimdbh',
+    version='1.10000.10001',
+    packages=['wgswceqeowcimdbh'],
+    package_data={
+        'wgswceqeowcimdbh': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
