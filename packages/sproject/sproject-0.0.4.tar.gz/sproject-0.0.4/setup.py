1826ce8407d4e470750cd89efa97ebea6b9a1428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:21 +0000

    Add package sproject/0.0.4/sproject-0.0.4.tar.gz

---
+++ packages/sproject/sproject-0.0.4.tar.gz/sproject-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sproject',  # Replace with your desired package name
+    version='0.0.4',  # Version number (update for future releases)
+    description='Added new file encryption and decryption to secure your files.',
+    author='Grand Master',
+    packages=find_packages(),  # Automatically finds your package
+    install_requires=[]  # Add any external dependencies here (if needed)
+)
