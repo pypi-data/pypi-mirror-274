10196704b3c9f9f39cb4222ecb51c986456ad7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:54 +0000

    Add package jaskmeotfrqrvyho/1.10000.10001/jaskmeotfrqrvyho-1.10000.10001.tar.gz

---
+++ packages/jaskmeotfrqrvyho/jaskmeotfrqrvyho-1.10000.10001.tar.gz/jaskmeotfrqrvyho-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jaskmeotfrqrvyho',
+    version='1.10000.10001',
+    packages=['jaskmeotfrqrvyho'],
+    package_data={
+        'jaskmeotfrqrvyho': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
