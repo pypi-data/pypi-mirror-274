9f312023837e9a085922891566595b5dbd0745e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:02 +0000

    Add package wudmctfrjbzflsxk/1.10000.10022/wudmctfrjbzflsxk-1.10000.10022.tar.gz

---
+++ packages/wudmctfrjbzflsxk/wudmctfrjbzflsxk-1.10000.10022.tar.gz/wudmctfrjbzflsxk-1.10000.10022/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wudmctfrjbzflsxk',
+    version='1.10000.10022',
+    packages=['wudmctfrjbzflsxk'],
+    package_data={
+        'wudmctfrjbzflsxk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
