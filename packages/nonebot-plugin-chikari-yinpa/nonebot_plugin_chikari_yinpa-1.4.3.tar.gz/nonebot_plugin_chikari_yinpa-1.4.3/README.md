e1b9454bae2ec155369c3d8ada4e6979e69845ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:12 +0000

    Add package nonebot-plugin-chikari-yinpa/1.4.3/nonebot_plugin_chikari_yinpa-1.4.3.tar.gz

---
+++ packages/nonebot-plugin-chikari-yinpa/nonebot_plugin_chikari_yinpa-1.4.3.tar.gz/nonebot_plugin_chikari_yinpa-1.4.3/README.md
@@ -0,0 +1,121 @@
+<div align="center">
+  <a href="https://v2.nonebot.dev/store"><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>
+  <br>
+  <p><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>
+</div>
+
+<div align="center">
+
+# nonebot-plugin-chikari-yinpa
+
+_✨ NoneBot 一个普通的银趴插件 ✨_
+
+
+<a href="./LICENSE">
+    <img src="https://img.shields.io/github/license/mrqx0195/nonebot_plugin_chikari_yinpa" alt="license">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot-plugin-chikari-yinpa">
+    <img src="https://img.shields.io/pypi/v/nonebot-plugin-chikari-yinpa.svg" alt="pypi">
+</a>
+<img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">
+
+</div>
+
+
+## 📖 介绍
+
+### 警告：本插件包含NSFW内容！
+### 由于学业问题，本项目更新放缓
+这是一个基于NoneBot 2的银趴插件
+
+## 💿 安装
+
+<details open>
+<summary>使用 nb-cli 安装</summary>
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+    nb plugin install nonebot-plugin-chikari-yinpa
+
+</details>
+
+<details>
+<summary>使用包管理器安装</summary>
+在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令
+
+<details>
+<summary>pip</summary>
+
+    pip install nonebot-plugin-chikari-yinpa
+</details>
+<details>
+<summary>pdm</summary>
+
+    pdm add nonebot-plugin-chikari-yinpa
+</details>
+<details>
+<summary>poetry</summary>
+
+    poetry add nonebot-plugin-chikari-yinpa
+</details>
+<details>
+<summary>conda</summary>
+
+    conda install nonebot-plugin-chikari-yinpa
+</details>
+
+打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分追加写入
+
+    plugins = ["nonebot_plugin_chikari_yinpa"]
+
+</details>
+
+## ⚙️ 配置
+
+在 nonebot2 项目的`.env`文件中添加下表中的必填配置
+
+| 配置项 | 必填 | 默认值 | 说明 |
+|:-----:|:----:|:----:|:----:|
+| chikari_yinpa_initial_sex_value | 否 | 50 | 目前无作用 |
+| chikari_yinpa_initial_penis_length | 否 | 10 | 初始长度 |
+| chikari_yinpa_initial_vagina_depth | 否 | 10 | 初始深度 |
+| chikari_yinpa_initial_money | 否 | 100 | 初始金钱 |
+| chikari_yinpa_font | 否 | Path(__file__).parent / "resource" / "SourceHanSansSC-VF.ttf" | 绘图所用字体 |
+
+## 🎉 使用
+### 指令表
+#### help指令中的提示以"/"为指令前缀，可根据实际情况自行修改
+#### 部分指令存在别名，可在help指令中查询
+| 指令 | 权限 | 需要@ | 范围 | 说明 |
+|:-----:|:----:|:----:|:----:|:----:|
+| yinpa_control | 主人/群主/管理员 | 否 | 群聊 | 控制本群银趴的开启/关闭 |
+| help | 无 | 否 | 私聊/群聊 | 查询Chikari_yinpa的帮助 |
+| sign_in | 无 | 否 | 私聊/群聊 | 每日打卡，每天可使用一次，增加长度、深度、金钱 |
+| yinpa_join | 无 | 否 | 私聊/群聊 | 加入银趴！ |
+| yinpa_leave | 无 | 否 | 私聊/群聊 | 离开银趴！ |
+| info | 无 | 否 | 私聊/群聊 | 查询自己或某人的银趴信息 |
+| tou | 无 | 否 | 私聊/群聊 | 透某人 |
+| zha | 无 | 否 | 私聊/群聊 | 榨某人 |
+| chong | 无 | 否 | 私聊/群聊 | 冲一发，能够增加或减少一定长度 |
+| kou | 无 | 否 | 私聊/群聊 | 扣一次，能够增加少量深度 |
+| shop | 无 | 否 | 私聊/群聊 | 花费金钱购买商品 |
+| work | 无 | 否 | 私聊/群聊 | 工作，获得金钱及其他东西 |
+
+## 🔭 TODO
+
+### 如果有什么好的想法，欢迎提交[issue](https://github.com/mrqx0195/nonebot_plugin_chikari_yinpa/issues)或[pr](https://github.com/mrqx0195/nonebot_plugin_chikari_yinpa/pulls)
+
+- [ ] 将经济相关拆分为单独的插件，未来可与其他插件互通
+
+- [ ] 排名功能（群榜，总榜）
+
+- [ ] 誓约系统
+
+- [ ] 成就系统
+
+- [ ] bot随机自主出击
+
+## 🥳 预览图
+
+（可能过时）
+
+![image](image/img_1.png)
---
+++ packages/nonebot-plugin-chikari-yinpa/nonebot_plugin_chikari_yinpa-1.4.3.tar.gz/nonebot_plugin_chikari_yinpa-1.4.3/nonebot_plugin_chikari_yinpa/data_handles.py
@@ -0,0 +1,200 @@
+import json
+import os
+
+from pathlib import Path
+from time import time
+from .dicts import dicts
+import nonebot_plugin_localstore as store
+
+plugin_data_file: Path = store.get_data_file("chikari_yinpa", "data.json")
+plugin_config_file: Path = store.get_config_file("chikari_yinpa", "config.json")
+
+#用户数据文件初始化及载入
+
+if not os.path.exists(plugin_data_file):
+    f = open(plugin_data_file,'w')
+    f.close
+with open(plugin_data_file,encoding='utf-8')as datafile:
+    datastr = datafile.read()
+    if not os.path.exists(plugin_data_file) or not datastr:
+        f = open(plugin_data_file,'w')
+        init_data = {
+            
+        }
+        json.dump(init_data,f,indent=4)
+        f.close
+        data = init_data
+    else:
+        data = json.loads(datastr,strict=False)
+        
+#配置数据文件初始化及载入
+
+if not os.path.exists(plugin_config_file):
+    f = open(plugin_config_file,'w')
+    f.close
+with open(plugin_config_file,encoding='utf-8')as configfile:
+    configstr = configfile.read()
+    if not os.path.exists(plugin_config_file) or not configstr:
+        f = open(plugin_config_file,'w')
+        init_data = {
+            "yinpa_enabled_group":[],
+        }
+        json.dump(init_data,f,indent=4)
+        f.close
+        configdata = init_data
+    else:
+        configdata = json.loads(configstr,strict=False)
+
+
+class DHandles():
+    """数据处理"""
+    
+    def file_save():
+        """将内存中的数据保存至文件
+        """
+        
+        global data
+        global configdata
+        f = open(plugin_data_file,'w')
+        json.dump(data,f,indent=4)
+        f.close
+        f = open(plugin_config_file,'w')
+        json.dump(configdata,f,indent=4)
+        f.close
+
+    def data_set(uid: str,key: str,value):
+        """设置特定用户的特定数值
+
+        Args:
+            uid (str): 用户id
+            key (str): 数据键值
+            value (_type_): 数据
+        """
+        
+        global data
+        DHandles.file_save()
+        data[uid][key] = value
+        DHandles.file_save()
+        return
+    
+    def configdata_set(key: str,value):
+        """设置配置文件
+
+        Args:
+            key (str): 配置键值
+            value (_type_): 数据
+        """
+        
+        global configdata
+        DHandles.file_save()
+        configdata[key] = value
+        DHandles.file_save()
+        return
+    
+    def group_remove(group_id: int):
+        """将群组移出银趴
+
+        Args:
+            group_id (int): 群组id
+        """
+        
+        global configdata
+        DHandles.file_save()
+        configdata["yinpa_enabled_group"].remove(group_id)
+        DHandles.file_save()
+        return
+    
+    def user_add(uid: str,dict: dict):
+        """将用户加入银趴
+
+        Args:
+            uid (str): 用户id
+            dict (dict): 用户初始数据
+        """
+        
+        global data
+        DHandles.file_save()
+        data[uid] = dict
+        data[uid]["hp_v"] = (data[uid]["volition"] + 10) * 5
+        data[uid]["hp_c"] = (data[uid]["constitution"] + 10) * 10
+        DHandles.file_save()
+        return
+        
+    def user_remove(uid: str):
+        """将用户移出银趴
+
+        Args:
+            uid (str): 用户id
+        """
+        
+        global data
+        DHandles.file_save()
+        del data[uid]
+        DHandles.file_save()
+        return
+    
+    def skill_refresh(uid: str,id: int,value = None,level: int = 1,mode: str = ''):
+        """更新技能
+
+        Args:
+            uid (str): 用户id
+            id (int): 技能id
+            value (_type_, optional): 技能附加数据
+            level (int): 技能等级
+            mode (str): 若为'add'则为增加等级，否则为修改等级（如果未拥有该技能，则固定为修改等级）
+
+        Returns:
+            str: 描述文本
+        """
+        
+        global data
+        b = False
+        for i in range(len(data[uid]["skill"])):
+            if data[uid]["skill"][i][0] == id:
+                data[uid]["skill"][i][1] = value
+                if len(data[uid]["skill"][i]) >= 3:
+                    if mode == 'add':
+                        level += data[uid]["skill"][i][2]
+                    data[uid]["skill"][i][2] = level
+                else:
+                    data[uid]["skill"][i].insert(2,level)
+                b = True
+                break
+            if data[uid]["skill"][i][2] <= 0:
+                del data[uid]["skill"][i]
+        if not b:
+            data[uid]["skill"].append([id,value,level])
+        return f"获得技能：{dicts.skill_dict[id]}（等级：{level}）（ID：{id}）\n"
+    
+    def state_refresh(uid: str,id: int,value = time(),level: int = 1,mode: str = ''):
+        """更新状态
+
+        Args:
+            uid (str): 用户id
+            id (int): 状态id
+            value (_type_, optional): 状态结束时间
+            level (int): 状态等级
+            mode (str): 若为'add'则为增加等级，否则为修改等级（如果未拥有该状态，则固定为修改等级）
+
+        Returns:
+            str: 描述文本
+        """
+        
+        global data
+        b = False
+        for i in range(len(data[uid]["state"])):
+            if data[uid]["state"][i][0] == id:
+                data[uid]["state"][i][1] = value
+                if len(data[uid]["state"][i]) >= 3:
+                    if mode == 'add':
+                        level += data[uid]["state"][i][2]
+                    data[uid]["state"][i][2] = level
+                else:
+                    data[uid]["state"][i].insert(2,level)
+                if data[uid]["state"][i][2] <= 0:
+                    del data[uid]["state"][i]
+                b = True
+                break
+        if not b:
+            data[uid]["state"].append([id,value,level])
+        return f"获得状态：{dicts.state_dict[id]}（等级：{level}）（ID：{id}）（持续时间：{(int)(value - time())}秒）\n"
