4654239a9b5ce908047aca8e62b263c39467c4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:31 +0000

    Add package goto-conversion/0.3.1/goto_conversion-0.3.1-py3-none-any.whl

---
+++ packages/goto-conversion/goto_conversion-0.3.1-py3-none-any.whl/goto_conversion/__init__.py
@@ -0,0 +1,31 @@
+def goto_conversion(listOfOdds, total = 1, alpha = 1, beta = 1, eps = 1e-6, isAmericanOdds = False):
+
+    #Convert American Odds to Decimal Odds
+    if isAmericanOdds:
+        for i in range(len(listOfOdds)):
+            currOdds = listOfOdds[i]
+            isNegativeAmericanOdds = currOdds < 0
+            if isNegativeAmericanOdds:
+                currDecimalOdds = 1 + (100/(currOdds*-1))
+            else: #Is non-negative American Odds
+                currDecimalOdds = 1 + (currOdds/100)
+            listOfOdds[i] = currDecimalOdds
+
+    #Error Catchers
+    if len(listOfOdds) < 2:
+        raise ValueError('len(listOfOdds) must be >= 2')
+    if any(x < 1 for x in listOfOdds):
+        raise ValueError('All odds must be >= 1, set isAmericanOdds parameter to True if using American Odds')
+
+    #Computation
+    listOfProbabilities = [1/x for x in listOfOdds] #initialize probabilities using inverse odds
+    listOfSe = [pow((x-x**2)/((x**alpha)/beta),0.5) for x in listOfProbabilities] #compute the standard error (SE) for each probability
+    step = (sum(listOfProbabilities) - total)/sum(listOfSe) #compute how many steps of SE the probabilities should step back by
+    outputListOfProbabilities = [min(max(x - (y*step),eps),1) for x,y in zip(listOfProbabilities, listOfSe)]
+    return outputListOfProbabilities
+
+def zero_sum(listOfPrices, listOfVolumes):
+    listOfSe = [x**0.5 for x in listOfVolumes] #compute standard errors assuming standard deviation is same for all stocks
+    step = sum(listOfPrices)/sum(listOfSe)
+    outputListOfPrices = [x - (y*step) for x,y in zip(listOfPrices, listOfSe)]
+    return outputListOfPrices
