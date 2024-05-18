6ecd135bf1b842b440aadda10840707a597219bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:21 +0000

    Add package quool/5.0.13/quool-5.0.13-py3-none-any.whl

---
+++ packages/quool/quool-5.0.13-py3-none-any.whl/quool/__init__.py
@@ -0,0 +1,32 @@
+from .core import Table
+
+from .table import (
+    ItemTable,
+    DatetimeTable,
+    PanelTable,
+)
+
+from .record import (
+    RunRecorder,
+    TradeRecorder,
+    ProxyRecorder,
+)
+
+from .factor import (
+    Factor
+)
+
+from .util import (
+    Logger,
+    parse_commastr,
+    reduce_mem_usage,
+)
+
+
+DEBUG = 10
+INFO = 20
+WARNING = 30
+CRITICAL = 40
+
+
+__version__ = "5.0.13"
