c76cfb5ad886c05c4bdb04a10783525f609317e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:37 +0000

    Add package tcupprkggxuifmur/1.10000.10008/tcupprkggxuifmur-1.10000.10008.tar.gz

---
+++ packages/tcupprkggxuifmur/tcupprkggxuifmur-1.10000.10008.tar.gz/tcupprkggxuifmur-1.10000.10008/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tcupprkggxuifmur',
+    version='1.10000.10008',
+    packages=['tcupprkggxuifmur'],
+    package_data={
+        'tcupprkggxuifmur': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
