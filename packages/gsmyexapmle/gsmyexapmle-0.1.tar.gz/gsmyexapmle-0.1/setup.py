4d0ed79f91c77b9e8bdba69c1c211d622cbb7bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:52 +0000

    Add package gsmyexapmle/0.1/gsmyexapmle-0.1.tar.gz

---
+++ packages/gsmyexapmle/gsmyexapmle-0.1.tar.gz/gsmyexapmle-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as f:
+#     description = f.read()
+
+setup(
+    name='gsmyexapmle',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+)
+
+# entry_points={
+#     "console_scripts" : [
+#         "algebrator = algebrator:testing",
+#     ],
+# },
+# long_description=description,
+# long_description_content_type="text/markdown"
