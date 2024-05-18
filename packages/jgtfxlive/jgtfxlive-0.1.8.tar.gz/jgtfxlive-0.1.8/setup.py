ad5741c2d1c101e47197c4df7fbaad7b994f8951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:31 +0000

    Add package jgtfxlive/0.1.8/jgtfxlive-0.1.8.tar.gz

---
+++ packages/jgtfxlive/jgtfxlive-0.1.8.tar.gz/jgtfxlive-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jgtfxlive',
+    version='0.1.8',
+    author="GUillaume Isabelle",
+    author_email="jgi@jgwill.com",
+    url="https://github.com/jgwill/jgtfxlive",
+    description='A Python module for live chart data export',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'jgtfxlive = jgtfxlive.ptoLiveChartDataExport.LiveChartDataExport:main',
+            'jgtfxliveconf = jgtfxlive.config_generator:main'
+        ]
+    },
+    install_requires=[
+      'jgtutils',
+      'forexconnect',
+        # Add any dependencies required by your module here
+    ],
+)
