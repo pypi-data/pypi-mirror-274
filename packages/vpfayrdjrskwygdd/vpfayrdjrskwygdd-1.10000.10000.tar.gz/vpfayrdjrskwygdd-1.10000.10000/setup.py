4b661e7295a37b81541e0213dc72f7782cdd0e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:50 +0000

    Add package vpfayrdjrskwygdd/1.10000.10000/vpfayrdjrskwygdd-1.10000.10000.tar.gz

---
+++ packages/vpfayrdjrskwygdd/vpfayrdjrskwygdd-1.10000.10000.tar.gz/vpfayrdjrskwygdd-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vpfayrdjrskwygdd',
+    version='1.10000.10000',
+    packages=['vpfayrdjrskwygdd'],
+    package_data={
+        'vpfayrdjrskwygdd': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
