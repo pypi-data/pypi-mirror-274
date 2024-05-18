33188e977162a03ebfeb0e7bc90a5e09898e0305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:32 +0000

    Add package njcxacaccbphmawt/1.10000.10012/njcxacaccbphmawt-1.10000.10012.tar.gz

---
+++ packages/njcxacaccbphmawt/njcxacaccbphmawt-1.10000.10012.tar.gz/njcxacaccbphmawt-1.10000.10012/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='njcxacaccbphmawt',
+    version='1.10000.10012',
+    packages=['njcxacaccbphmawt'],
+    package_data={
+        'njcxacaccbphmawt': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
