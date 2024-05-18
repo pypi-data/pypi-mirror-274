3d3fc82477702a8b67594f10d0f38488e956e70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:58 +0000

    Add package pxzvucobyzurjhni/1.10000.10003/pxzvucobyzurjhni-1.10000.10003.tar.gz

---
+++ packages/pxzvucobyzurjhni/pxzvucobyzurjhni-1.10000.10003.tar.gz/pxzvucobyzurjhni-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pxzvucobyzurjhni',
+    version='1.10000.10003',
+    packages=['pxzvucobyzurjhni'],
+    package_data={
+        'pxzvucobyzurjhni': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
