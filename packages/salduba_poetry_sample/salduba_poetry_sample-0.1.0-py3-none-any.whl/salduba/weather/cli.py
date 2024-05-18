8076d112f2fb6c3f4715bd19365137ef2fd014c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:52 +0000

    Add package salduba_poetry_sample/0.1.0/salduba_poetry_sample-0.1.0-py3-none-any.whl

---
+++ packages/salduba_poetry_sample/salduba_poetry_sample-0.1.0-py3-none-any.whl/salduba/weather/cli.py
@@ -0,0 +1,34 @@
+import click
+import requests
+
+
+@click.command()
+@click.argument("metric", required=True)
+@click.option(
+  "--latitude", "-lat",
+  default=40.71,
+  type=float,
+  required=False,
+  help="latitude (in degrees)")
+@click.option(
+  "--longitude", "-lon",
+  default=-74.01,
+  type=float,
+  required=False,
+  help="longitude (in degrees)")
+def cli(metric: str, latitude: float, longitude: float) -> None:
+  r = requests.get(
+    'https://api.open-meteo.com/v1/forecast?latitude=' + str(latitude) +
+    '&longitude=' + str(longitude) +
+    '&current_weather=true')
+  if r.status_code == 200:
+    if metric in r.json()["current_weather"]:
+      print(r.json()['current_weather'][metric])
+    else:
+      print("Metric not supported!")
+  else:
+    print("Open-Meteo is down!")
+
+
+if __name__ == "__main__":
+  cli()
