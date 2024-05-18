bb78a3d7315ef73660e5aff5bca3abe82b27ea84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:40 +0000

    Add package kevin-toolbox-dev/1.3.5/kevin-toolbox-dev-1.3.5.tar.gz

---
+++ packages/kevin-toolbox-dev/kevin-toolbox-dev-1.3.5.tar.gz/kevin-toolbox-dev-1.3.5/kevin_toolbox/computer_science/algorithm/pareto_front/__init__.py
@@ -0,0 +1,1 @@
+from .get_pareto_points_idx import get_pareto_points_idx, Direction
---
+++ packages/kevin-toolbox-dev/kevin-toolbox-dev-1.3.5.tar.gz/kevin-toolbox-dev-1.3.5/kevin_toolbox/patches/for_os/__init__.py
@@ -0,0 +1,5 @@
+from .remove import remove
+from .copy import copy
+from .pack import pack
+from .unpack import unpack
+from .walk import walk, Path_Ignorer, Ignore_Scope
