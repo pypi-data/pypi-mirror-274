875ff699636f2c16ff6d27dbe924ddcbcd5901f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:47 +0000

    Add package pycwokcjdozgewia/1.10000.10003/pycwokcjdozgewia-1.10000.10003.tar.gz

---
+++ packages/pycwokcjdozgewia/pycwokcjdozgewia-1.10000.10003.tar.gz/pycwokcjdozgewia-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycwokcjdozgewia',
+    version='1.10000.10003',
+    packages=['pycwokcjdozgewia'],
+    package_data={
+        'pycwokcjdozgewia': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
