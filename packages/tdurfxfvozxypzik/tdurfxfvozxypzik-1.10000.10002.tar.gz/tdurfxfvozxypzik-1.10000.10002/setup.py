c3806e69367e920167f4cdec6c5617e5363dc339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:49 +0000

    Add package tdurfxfvozxypzik/1.10000.10002/tdurfxfvozxypzik-1.10000.10002.tar.gz

---
+++ packages/tdurfxfvozxypzik/tdurfxfvozxypzik-1.10000.10002.tar.gz/tdurfxfvozxypzik-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tdurfxfvozxypzik',
+    version='1.10000.10002',
+    packages=['tdurfxfvozxypzik'],
+    package_data={
+        'tdurfxfvozxypzik': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
