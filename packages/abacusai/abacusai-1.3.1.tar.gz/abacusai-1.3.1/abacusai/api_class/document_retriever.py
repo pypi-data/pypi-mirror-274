c25e31ee896583b7450d74c3efac8b350300fc44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 14:03:29 +0000

    Add package abacusai/1.3.1/abacusai-1.3.1.tar.gz

---
+++ packages/abacusai/abacusai-1.3.1.tar.gz/abacusai-1.3.1/abacusai/api_class/document_retriever.py
@@ -0,0 +1,32 @@
+import dataclasses
+
+from .abstract import ApiClass
+from .enums import VectorStoreTextEncoder
+
+
+@dataclasses.dataclass
+class VectorStoreConfig(ApiClass):
+    """
+    Configs for vector store indexing.
+
+    Args:
+        chunk_size (int): The size of text chunks in the vector store.
+        chunk_overlap_fraction (float): The fraction of overlap between chunks.
+        text_encoder (VectorStoreTextEncoder): Encoder used to index texts from the documents.
+        chunk_size_factors (list): Chunking data with multiple sizes. The specified list of factors are used to calculate more sizes, in addition to `chunk_size`.
+        score_multiplier_column (str): If provided, will use the values in this metadata column to modify the relevance score of returned chunks for all queries.
+        prune_vectors (bool): Transform vectors using SVD so that the average component of vectors in the corpus are removed.
+    """
+    chunk_size: int = dataclasses.field(default=None)
+    chunk_overlap_fraction: float = dataclasses.field(default=None)
+    text_encoder: VectorStoreTextEncoder = dataclasses.field(default=None)
+    chunk_size_factors: list = dataclasses.field(default=None)
+    score_multiplier_column: str = dataclasses.field(default=None)
+    prune_vectors: bool = dataclasses.field(default=None)
+
+
+@dataclasses.dataclass
+class DocumentRetrieverConfig(VectorStoreConfig):
+    """
+    Configs for document retriever. If any configuration value is not explicitly provided, Abacus.AI will automatically infer default values based on the data.
+    """
