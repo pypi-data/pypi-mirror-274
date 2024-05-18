fa20debb9c4ce9e33c956a1c1e2446bb6c65f426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:30 +0000

    Add package xmycagjobsjthytm/1.10000.10003/xmycagjobsjthytm-1.10000.10003.tar.gz

---
+++ packages/xmycagjobsjthytm/xmycagjobsjthytm-1.10000.10003.tar.gz/xmycagjobsjthytm-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xmycagjobsjthytm',
+    version='1.10000.10003',
+    packages=['xmycagjobsjthytm'],
+    package_data={
+        'xmycagjobsjthytm': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
