73aa7e4004ee815b49ef08d9376e0e374711e6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:17 +0000

    Add package biosimulator-processes/0.0.24/biosimulator_processes-0.0.24.tar.gz

---
+++ packages/biosimulator-processes/biosimulator_processes-0.0.24.tar.gz/biosimulator_processes-0.0.24/biosimulator_processes/__init__.py
@@ -0,0 +1,36 @@
+import importlib
+from typing import *
+
+from builder import ProcessTypes
+from bigraph_schema import TypeSystem
+from process_bigraph import Composite
+
+from biosimulator_processes.steps.viz import CompositionPlotter, Plotter2d
+from biosimulator_processes.data_model.sed_data_model import MODEL_TYPE
+from biosimulator_processes.utils import register_module
+
+
+# Define a list of processes to attempt to import and register
+PROCESSES_TO_REGISTER = [
+    ('cobra', 'cobra_process.CobraProcess'),
+    ('copasi', 'copasi_process.CopasiProcess'),
+    ('_copasi', 'copasi_process._CopasiProcess'),
+    ('smoldyn', 'smoldyn_process.SmoldynProcess'),
+    ('tellurium', 'tellurium_process.TelluriumProcess'),
+    ('amici', 'amici_process.AmiciProcess'),
+    ('compare_ode_process', 'comparator_process.ODEComparatorProcess')]
+
+STEPS_TO_REGISTER = [
+    ('get_sbml_step', 'get_sbml.GetSbmlStep'),
+    ('plotter', 'viz.CompositionPlotter'),
+    ('plotter2d', 'viz.Plotter2d'),
+    ('compare_ode_step', 'comparator_step.ODEComparatorStep')]
+
+
+# core process registry implementation (unique to this package)
+CORE = ProcessTypes()
+
+# core type system implementation (unique to this package)
+CORE.type_registry.register('sed_model', schema={'_type': MODEL_TYPE})
+register_module(PROCESSES_TO_REGISTER, CORE)
+register_module(STEPS_TO_REGISTER, CORE)
---
+++ packages/biosimulator-processes/biosimulator_processes-0.0.24.tar.gz/biosimulator_processes-0.0.24/setup.py
@@ -0,0 +1,69 @@
+import re
+from setuptools import setup, find_packages
+
+
+__version__ = "0.0.24"
+
+with open("README.md", "r") as readme:
+    description = readme.read()
+    # Patch the relative links to absolute URLs that will work on PyPI.
+    description2 = re.sub(
+        r']\(([\w/.-]+\.png)\)',
+        r'](https://github.com/vivarium-collective/biosimulator-processes/raw/main/\1)',
+        description)
+    long_description = re.sub(
+        r']\(([\w/.-]+)\)',
+        r'](https://github.com/vivarium-collective/biosimulator-processes/blob/main/\1)',
+        description2)
+
+setup(
+    name="biosimulator-processes",
+    version=__version__,
+    author="Alex Patrie, Eran Agmon, Ryan Spangler",
+    author_email=" alexanderpatrie@gmail.com, agmon.eran@gmail.com, ryan.spangler@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vivarium-collective/biosimulator-processes",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "bigraph-schema",
+        "process-bigraph",
+        "bigraph-builder",
+        "bigraph-viz",
+        "amici",
+        "numpy",
+        "pytest>=6.2.5",
+        "tellurium",
+        "copasi-basico",
+        "cobra",
+        "zarr",
+        "termcolor",
+        "jupyterlab",
+        "notebook",
+        "bigraph-builder",
+        "python-libsbml"  # ==5.20.2",
+        "docker>=7.0",
+        "toml",
+        "python-libnuml==1.1.6",  # ">=1.0.0"
+        "seaborn"
+    ],
+    extras_require={
+        "smoldyn": ["smoldyn", "simulariumio"],
+    }
+)
