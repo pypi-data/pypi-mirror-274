d972705155e571cb18826d5ae5dce4b519b1417b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:52 +0000

    Add package rklqnwnmetchsgfz/1.10000.10003/rklqnwnmetchsgfz-1.10000.10003.tar.gz

---
+++ packages/rklqnwnmetchsgfz/rklqnwnmetchsgfz-1.10000.10003.tar.gz/rklqnwnmetchsgfz-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rklqnwnmetchsgfz',
+    version='1.10000.10003',
+    packages=['rklqnwnmetchsgfz'],
+    package_data={
+        'rklqnwnmetchsgfz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
