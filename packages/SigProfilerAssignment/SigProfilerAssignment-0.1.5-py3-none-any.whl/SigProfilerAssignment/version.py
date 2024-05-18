ef366e74045d0c0d815415173277574655e2a036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:34 +0000

    Add package SigProfilerAssignment/0.1.5/SigProfilerAssignment-0.1.5-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.1.5-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.1.5'
+version = '0.1.5'
+Update = 'v0.1.5: Add text for stability and mutation count to the de novo signature plots.'
+
+    
+    
