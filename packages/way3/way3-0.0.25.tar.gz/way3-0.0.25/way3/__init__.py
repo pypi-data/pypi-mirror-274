a0a6738fb8a9e0c1273b00df4d25b96e7aba28ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:52 +0000

    Add package way3/0.0.25/way3-0.0.25.tar.gz

---
+++ packages/way3/way3-0.0.25.tar.gz/way3-0.0.25/way3/__init__.py
@@ -0,0 +1,24 @@
+name = "way3"
+
+# File OP
+from .file_op.create_file import create_file as create_file  # noqa: E402
+from .file_op.create_file import add_to_file_end as add_to_file_end  # noqa: E402
+from .file_op.create_file import delete_file as delete_file  # noqa: E402
+
+
+# File Find
+from .file_find.current_dir import get_current_dir as get_current_dir  # noqa: E402
+from .file_find.traverse_files_from_folder import parse_gitignore as parse_gitignore  # noqa: E402
+from .file_find.traverse_files_from_folder import is_gitignored as is_gitignored  # noqa: E402
+from .file_find.traverse_files_from_folder import (  # noqa: E402
+    get_files_in_directory as get_files_in_directory,
+)
+from .file_find.traverse_files_from_folder import (  # noqa: E402
+    get_directory_structure as get_directory_structure,
+)
+from .file_find.traverse_files_from_folder import (  # noqa: E402
+    get_directory_folder_list as get_directory_folder_list,
+)
+from .file_find.traverse_files_from_folder import (  # noqa: E402
+    get_directory_file_list as get_directory_file_list,
+)
