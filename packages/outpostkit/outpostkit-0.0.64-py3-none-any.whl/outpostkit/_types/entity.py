b114abe138bc4ac2217e06031db2e0600d5b450a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:04:04 +0000

    Add package outpostkit/0.0.64/outpostkit-0.0.64-py3-none-any.whl

---
+++ packages/outpostkit/outpostkit-0.0.64-py3-none-any.whl/outpostkit/_types/entity.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Literal, Optional
+
+ENTITY_TYPES = Literal["user", "team"]
+
+
+@dataclass
+class FollowEntity:
+    name: str
+    id: str
+    type: ENTITY_TYPES
+    avatarUrl: str
+    isFollowing: Optional[bool]
+
+
+@dataclass
+class HardwareInstanceDetails:
+    id: str
+    name: str
