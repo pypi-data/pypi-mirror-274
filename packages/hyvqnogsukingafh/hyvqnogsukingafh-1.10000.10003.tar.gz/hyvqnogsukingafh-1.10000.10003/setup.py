53250df81e556f244177628431569bf2e9a283e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:24 +0000

    Add package hyvqnogsukingafh/1.10000.10003/hyvqnogsukingafh-1.10000.10003.tar.gz

---
+++ packages/hyvqnogsukingafh/hyvqnogsukingafh-1.10000.10003.tar.gz/hyvqnogsukingafh-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hyvqnogsukingafh',
+    version='1.10000.10003',
+    packages=['hyvqnogsukingafh'],
+    package_data={
+        'hyvqnogsukingafh': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
