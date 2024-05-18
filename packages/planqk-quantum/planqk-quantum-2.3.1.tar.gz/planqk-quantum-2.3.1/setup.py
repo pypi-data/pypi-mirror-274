4df2b602be3b7204476d6d385563f8c978eacd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:43 +0000

    Add package planqk-quantum/2.3.1/planqk-quantum-2.3.1.tar.gz

---
+++ packages/planqk-quantum/planqk-quantum-2.3.1.tar.gz/planqk-quantum-2.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages
+
+with open('./README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('./requirements.txt', 'r') as fh:
+    requirements = fh.readlines()
+
+setup(
+    name='planqk-quantum',
+    version="2.3.1",
+    author='Anaqor AG',
+    author_email='info@anaqor.io',
+    url='https://github.com/planqk/planqk-quantum',
+    description='Python SDK for the PlanQK Platform',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_namespace_packages(include=['planqk', 'planqk.*']),
+    license='apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=requirements,
+)
