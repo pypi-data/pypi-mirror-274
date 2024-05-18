e7a4949066bcb33c47ba96d621d5e726f243a3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:26 +0000

    Add package zemsavfbrwoioqdd/1.10000.10006/zemsavfbrwoioqdd-1.10000.10006.tar.gz

---
+++ packages/zemsavfbrwoioqdd/zemsavfbrwoioqdd-1.10000.10006.tar.gz/zemsavfbrwoioqdd-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zemsavfbrwoioqdd',
+    version='1.10000.10006',
+    packages=['zemsavfbrwoioqdd'],
+    package_data={
+        'zemsavfbrwoioqdd': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
