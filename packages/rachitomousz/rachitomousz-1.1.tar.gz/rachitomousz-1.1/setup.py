9f8e3c3d62fad0a505e80f356eac64461bc6a76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:49 +0000

    Add package rachitomousz/1.1/rachitomousz-1.1.tar.gz

---
+++ packages/rachitomousz/rachitomousz-1.1.tar.gz/rachitomousz-1.1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'rachitomousz',
+    packages = ['rachitomousz'],
+    version = '1.1',
+    license='MIT',
+    description = 'rachitomousz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'Naistrai',
+    author_email = 'rizkynaistrai@gmail.com',
+    url = 'https://github.com/naistrai/rachitomousz',
+    download_url = 'https://github.com/naistrai/rachitomousz/archive/v_01.tar.gz',
+    project_urls={
+        'Documentation': 'https://github.com/naistrai/rachitomousz',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/naistrai/rachitomousz/',
+        'Tracker': 'https://github.com/naistrai/rachitomousz/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
+)
