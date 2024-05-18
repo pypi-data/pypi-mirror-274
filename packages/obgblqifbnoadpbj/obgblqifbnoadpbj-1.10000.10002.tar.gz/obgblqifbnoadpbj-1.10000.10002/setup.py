b24d35060d63e45b1a741b2629e3041a032b1fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:52 +0000

    Add package obgblqifbnoadpbj/1.10000.10002/obgblqifbnoadpbj-1.10000.10002.tar.gz

---
+++ packages/obgblqifbnoadpbj/obgblqifbnoadpbj-1.10000.10002.tar.gz/obgblqifbnoadpbj-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='obgblqifbnoadpbj',
+    version='1.10000.10002',
+    packages=['obgblqifbnoadpbj'],
+    package_data={
+        'obgblqifbnoadpbj': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
