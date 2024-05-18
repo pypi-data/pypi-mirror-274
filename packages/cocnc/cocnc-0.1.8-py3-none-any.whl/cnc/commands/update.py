9f0635280581a3f0d4e5de0a2aeac5519cd48400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 14:03:19 +0000

    Add package cocnc/0.1.8/cocnc-0.1.8-py3-none-any.whl

---
+++ packages/cocnc/cocnc-0.1.8-py3-none-any.whl/cnc/commands/update.py
@@ -0,0 +1,64 @@
+import typer
+import time
+from typing import List
+
+from cnc.models import DeployStageManager
+from cnc.models import BuildStageManager
+from .telemetry import send_event
+
+from cnc.logger import get_logger
+log = get_logger(__name__)
+
+
+app = typer.Typer()
+
+
+@app.command()
+def perform(
+    ctx: typer.Context,
+    environment_name: str,
+    services: List[str] = typer.Option(
+        [],
+        "--service",
+        help="Set for each service you want to build, if omitted will build all.",
+    ),
+    service_tags: List[str] = typer.Option(
+        [],
+        "--service-tag",
+        help="Set the tag to use for this service, default is 'latest'",
+    ),
+    collection_name: str = "",
+    cleanup: bool = True,
+    debug: bool = False,
+    generate: bool = True,
+):
+    start_time = time.time()
+    send_event("update.perform")
+    collection = ctx.obj.application.collection_by_name(collection_name)
+    if not collection:
+        log.error(f"No collection found for: {collection_name}")
+        raise typer.Exit(code=1)
+
+    environment = collection.environment_by_name(environment_name)
+    if not environment:
+        log.error(f"No environment found for: {environment_name}")
+        raise typer.Exit(code=1)
+
+    builder = BuildStageManager(environment, service_tags=service_tags)
+    builder.perform(
+        should_cleanup=cleanup,
+        should_regenerate_config=generate,
+        debug=debug,
+        service_names=services,
+    )
+
+    deployer = DeployStageManager(environment, service_tags=service_tags)
+    deployer.perform(
+        should_cleanup=cleanup,
+        should_regenerate_config=generate,
+        debug=debug,
+        service_names=services,
+    )
+
+    log.debug(f"All set updating for {services}/{service_tags} in {int(time.time() - start_time)} seconds")
+    raise typer.Exit()
