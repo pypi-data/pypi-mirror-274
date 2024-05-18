89908461bba77f5f33cec3a73e38bcb3d4e29852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:19 +0000

    Add package zjgmryppjsmpzadk/1.10000.10003/zjgmryppjsmpzadk-1.10000.10003.tar.gz

---
+++ packages/zjgmryppjsmpzadk/zjgmryppjsmpzadk-1.10000.10003.tar.gz/zjgmryppjsmpzadk-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zjgmryppjsmpzadk',
+    version='1.10000.10003',
+    packages=['zjgmryppjsmpzadk'],
+    package_data={
+        'zjgmryppjsmpzadk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
