19f78ebd7ba92da8116686520b62fea5c868a6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:40 +0000

    Add package umap-project/2.3.1/umap_project-2.3.1.tar.gz

---
+++ packages/umap-project/umap_project-2.3.1.tar.gz/umap_project-2.3.1/umap/static/umap/js/modules/global.js
@@ -0,0 +1,30 @@
+import URLs from './urls.js'
+import Browser from './browser.js'
+import Facets from './facets.js'
+import Caption from './caption.js'
+import { Panel, EditPanel, FullPanel } from './panel.js'
+import * as Utils from './utils.js'
+import { SCHEMA } from './schema.js'
+import { Request, ServerRequest, RequestError, HTTPError, NOKError } from './request.js'
+import Orderable from './orderable.js'
+
+// Import modules and export them to the global scope.
+// For the not yet module-compatible JS out there.
+
+window.U = {
+  URLs,
+  Request,
+  ServerRequest,
+  RequestError,
+  HTTPError,
+  NOKError,
+  Browser,
+  Facets,
+  Panel,
+  EditPanel,
+  FullPanel,
+  Utils,
+  SCHEMA,
+  Orderable,
+  Caption,
+}
