cc0b768a59ff40bdd59ce06ec3011994bb530182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:06 +0000

    Add package xplainable-client/1.2.4.post3/xplainable_client-1.2.4.post3.tar.gz

---
+++ packages/xplainable-client/xplainable_client-1.2.4.post3.tar.gz/xplainable_client-1.2.4.post3/setup.py
@@ -0,0 +1,31 @@
+import setuptools, modulefinder
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    description = f.read()
+
+with open("LICENSE", "r") as f:
+    license = f.read()
+
+setuptools.setup(
+    name="xplainable-client",
+    version="1.2.4.post3",
+    author="xplainable pty ltd",
+    author_email="contact@xplainable.io",
+    packages=["xplainable_client"],
+    description="The client for persisting and deploying models to Xplainable cloud.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license=license,
+    python_requires='>=3.8',
+    install_requires=[
+        "ipywidgets==8.0.6",
+        "numpy>=1.26",
+        "pandas>=1.5.2",
+        "pyperclip==1.8.2",
+        "Requests==2.31.0",
+        "scikit_learn",
+        "setuptools",
+        "urllib3==2.2.0",
+        "xplainable==1.2.4"
+    ]
+)
