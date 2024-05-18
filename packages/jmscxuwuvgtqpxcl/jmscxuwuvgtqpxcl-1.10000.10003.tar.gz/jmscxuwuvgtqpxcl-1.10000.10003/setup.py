27e955d9a96279c1e165284053ed4a7943289b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:49 +0000

    Add package jmscxuwuvgtqpxcl/1.10000.10003/jmscxuwuvgtqpxcl-1.10000.10003.tar.gz

---
+++ packages/jmscxuwuvgtqpxcl/jmscxuwuvgtqpxcl-1.10000.10003.tar.gz/jmscxuwuvgtqpxcl-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jmscxuwuvgtqpxcl',
+    version='1.10000.10003',
+    packages=['jmscxuwuvgtqpxcl'],
+    package_data={
+        'jmscxuwuvgtqpxcl': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
