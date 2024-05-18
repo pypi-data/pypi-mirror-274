ab1ef4c3cd56a9a8db454360e94087b223dc530a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:57 +0000

    Add package example-python-package-with-rust-backend/1.1.10/example_python_package_with_rust_backend-1.1.10-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/example-python-package-with-rust-backend/example_python_package_with_rust_backend-1.1.10-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl/example_python_package_with_rust_backend/__init__.py
@@ -0,0 +1,5 @@
+from .example_python_package_with_rust_backend import *
+
+__doc__ = example_python_package_with_rust_backend.__doc__
+if hasattr(example_python_package_with_rust_backend, "__all__"):
+    __all__ = example_python_package_with_rust_backend.__all__
