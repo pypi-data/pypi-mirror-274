4ef05b70269ec85de1d974d6924599cff9a89900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:48 +0000

    Add package tkmbpdxbagxuruyy/1.10000.10003/tkmbpdxbagxuruyy-1.10000.10003.tar.gz

---
+++ packages/tkmbpdxbagxuruyy/tkmbpdxbagxuruyy-1.10000.10003.tar.gz/tkmbpdxbagxuruyy-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tkmbpdxbagxuruyy',
+    version='1.10000.10003',
+    packages=['tkmbpdxbagxuruyy'],
+    package_data={
+        'tkmbpdxbagxuruyy': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
