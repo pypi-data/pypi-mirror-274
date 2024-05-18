e04a56c8cd541fa77c21dc626c9cc1b5fbfbd626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:40 +0000

    Add package osycrdrpqahpgsgp/1.10000.10004/osycrdrpqahpgsgp-1.10000.10004.tar.gz

---
+++ packages/osycrdrpqahpgsgp/osycrdrpqahpgsgp-1.10000.10004.tar.gz/osycrdrpqahpgsgp-1.10000.10004/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='osycrdrpqahpgsgp',
+    version='1.10000.10004',
+    packages=['osycrdrpqahpgsgp'],
+    package_data={
+        'osycrdrpqahpgsgp': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
