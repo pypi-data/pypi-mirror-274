1555a35b667afd6e5fb55dc06ba8ec45476fdc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 15:03:52 +0000

    Add package ntdgotv-langtools/0.2.0/ntdgotv_langtools-0.2.0.tar.gz

---
+++ packages/ntdgotv-langtools/ntdgotv_langtools-0.2.0.tar.gz/ntdgotv_langtools-0.2.0/ntdgotv_langtools/__init__.py
@@ -0,0 +1,5 @@
+from ntdgotv_langtools.pinyin.converter import pinyin_tone_nums_to_marks
+from ntdgotv_langtools.unicode.checks_char import is_standard_cjk_char
+from ntdgotv_langtools.unicode.checks_string import *
+from ntdgotv_langtools.unicode.ops_list import *
+from ntdgotv_langtools.unicode.ops_string import *
---
+++ packages/ntdgotv-langtools/ntdgotv_langtools-0.2.0.tar.gz/ntdgotv_langtools-0.2.0/setup.py
@@ -0,0 +1,30 @@
+import subprocess
+from setuptools import setup, find_packages
+
+_PACKAGE_NAME = "ntdgotv_langtools"
+
+
+subprocess.run(
+    [
+        "pipreqs",
+        "ntdggotv_langtools",
+        "--use-local",
+        "--savepath",
+        "requirements.txt",
+    ]
+)
+
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name=_PACKAGE_NAME,
+    version="0.2.0",
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="ntdGoTV",
+    author_email="ntdGoTV@gmail.com",
+    packages=find_packages(),
+)
