d0506b35ac65b04a5b65d290f879582266302ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:44 +0000

    Add package struqture-py/1.7.1/struqture_py-1.7.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/struqture-py/struqture_py-1.7.1-cp39-cp39-macosx_11_0_arm64.whl/struqture_py/__init__.py
@@ -0,0 +1,19 @@
+# Copyright © 2019-2023 HQS Quantum Simulations GmbH. All Rights Reserved.
+
+"""Struqture python interface
+
+`HQS Quantum Simulations <https://quantumsimulations.de>`_ package for representing physical operators.
+
+Copyright © 2021-2023 HQS Quantum Simulations GmbH. All Rights Reserved.
+
+.. autosummary::
+    :toctree: generated/
+
+    spins
+    bosons
+    fermions
+    mixed_systems
+
+"""
+from .struqture_py import *
+__license__ = "Apache-2.0"
