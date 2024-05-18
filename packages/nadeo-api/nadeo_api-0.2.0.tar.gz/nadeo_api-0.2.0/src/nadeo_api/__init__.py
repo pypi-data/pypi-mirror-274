60b2d323e9c2e983d3dbea5b5451e9b1457492ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:30 +0000

    Add package nadeo-api/0.2.0/nadeo_api-0.2.0.tar.gz

---
+++ packages/nadeo-api/nadeo_api-0.2.0.tar.gz/nadeo_api-0.2.0/src/nadeo_api/__init__.py
@@ -0,0 +1,10 @@
+'''
+| Author:   Ezio416
+| Created:  2024-05-07
+| Modified: 2024-05-17
+
+- A library to assist with accessing Nadeo's web services API and the public Trackmania API (OAuth2).
+'''
+
+__version__: tuple = 0, 2, 0
+v:           tuple = __version__
