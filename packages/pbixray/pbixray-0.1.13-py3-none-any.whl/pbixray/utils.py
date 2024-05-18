d50c19ebefef1077633dea53fc0ace8e5be5512f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 14:03:36 +0000

    Add package pbixray/0.1.13/pbixray-0.1.13-py3-none-any.whl

---
+++ packages/pbixray/pbixray-0.1.13-py3-none-any.whl/pbixray/utils.py
@@ -0,0 +1,24 @@
+from .abf.data_model import DataModel
+
+# ---------- CONSTANTS ----------
+AMO_PANDAS_TYPE_MAPPING = {
+    2: 'string',
+    6: 'int64',
+    8: 'float64',
+    9: 'datetime64[ns]',
+    10: 'decimal.Decimal',
+    11: 'bool',
+    17: 'bytes'
+}
+
+# ---------- UTILITY FUNCTIONS ----------
+def get_data_slice(data_model:DataModel, file_name:str) -> bytes:
+    """Gets a data slice based on a file name from the file log."""
+    file_ref = next((x for x in data_model.file_log if x['FileName'] == file_name), None)
+    if not file_ref:
+        raise ValueError(f"File reference not found for filename: {file_name}.")
+    # if error_code trim last 4 bytes
+    if data_model.error_code:
+        return data_model.decompressed_data[file_ref['m_cbOffsetHeader']:file_ref['m_cbOffsetHeader'] + file_ref['Size']-4]
+    else:
+        return data_model.decompressed_data[file_ref['m_cbOffsetHeader']:file_ref['m_cbOffsetHeader'] + file_ref['Size']]
