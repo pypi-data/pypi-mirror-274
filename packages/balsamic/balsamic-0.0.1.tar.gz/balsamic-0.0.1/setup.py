8c4debbde37009e41e40484fe6597763eea58fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:43 +0000

    Add package balsamic/0.0.1/balsamic-0.0.1.tar.gz

---
+++ packages/balsamic/balsamic-0.0.1.tar.gz/balsamic-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Send malicious pickles via requests or sockets'
+LONG_DESCRIPTION = 'A package that allows the user to easily send out malicious pickles, via web requests, or a malicious server or client(currently ipv4 only)'
+
+# Setting up
+setup(
+    name="balsamic",
+    version=VERSION,
+    author="Witchdoctor (malectrica)",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pickle', 'base64', 'requests', 'socket', 'argparse'],
+    keywords=['python', 'hack', 'pickle', 'serialization', 'security', 'sockets', 'web'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
