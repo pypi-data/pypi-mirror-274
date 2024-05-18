495e3eebf7b5b747be4ecf5d9d9b5cdd32d923d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:56 +0000

    Add package mypackagetest20240517/0.1.0/mypackagetest20240517-0.1.0.tar.gz

---
+++ packages/mypackagetest20240517/mypackagetest20240517-0.1.0.tar.gz/mypackagetest20240517-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='mypackagetest20240517',
+version='0.1.0',
+author='Isaac',
+description='Test package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.0',
+)
