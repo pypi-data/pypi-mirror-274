0f67ea859e375d70f7ff37d8b939290e587bddd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:03:14 +0000

    Add package catasta/0.2.2/catasta-0.2.2-py3-none-any.whl

---
+++ packages/catasta/catasta-0.2.2-py3-none-any.whl/catasta/models/__init__.py
@@ -0,0 +1,15 @@
+# regressors
+from .regressors.approximate_gp_regressor import ApproximateGPRegressor
+from .regressors.feedforward_regressor import FeedforwardRegressor
+from .regressors.transformer_regressor import TransformerRegressor
+from .regressors.transformer_fft_regressor import TransformerFFTRegressor
+from .regressors.mamba_regressor import MambaRegressor
+from .regressors.mamba_fft_regressor import MambaFFTRegressor
+
+# classifiers
+from .classifiers.feedforward_classifier import FeedforwardClassifier
+from .classifiers.transformer_classifier import TransformerClassifier
+from .classifiers.transformer_fft_classifier import TransformerFFTClassifier
+from .classifiers.mamba_classifier import MambaClassifier
+from .classifiers.mamba_fft_classifier import MambaFFTClassifier
+from .classifiers.cnn_classifier import CNNClassifier
