fafefb7e4038d40efebcb7beee2d7e8c296f657c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:56 +0000

    Add package zqlhufyrogsiuxya/1.10000.10001/zqlhufyrogsiuxya-1.10000.10001.tar.gz

---
+++ packages/zqlhufyrogsiuxya/zqlhufyrogsiuxya-1.10000.10001.tar.gz/zqlhufyrogsiuxya-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zqlhufyrogsiuxya',
+    version='1.10000.10001',
+    packages=['zqlhufyrogsiuxya'],
+    package_data={
+        'zqlhufyrogsiuxya': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
