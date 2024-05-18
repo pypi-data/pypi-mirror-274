83a976cfe5c7939b7f1a5cec4f97aef8819fff27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 14:03:24 +0000

    Add package balsamic/0.0.16/balsamic-0.0.16.tar.gz

---
+++ packages/balsamic/balsamic-0.0.16.tar.gz/balsamic-0.0.16/balsamic/__main__.py
@@ -0,0 +1,53 @@
+import balsamic
+import argparse
+if __name__ == "__main__":
+	#import argparse
+	import argparse
+
+	#defines parser function
+	def parser():
+		
+		#create base argument parser
+		parser = argparse.ArgumentParser(description="balsamic args")
+		subparse=parser.add_subparsers(dest="attack")
+
+		#create subparser for webreq attack
+		webreqparser=subparse.add_parser("webreq")
+		webreqparser.add_argument("-s","--schema",required=True, choices=["http","https"])
+		webreqparser.add_argument("-m","--method")
+		webreqparser.add_argument("-rh","--rhost",required=True)
+		webreqparser.add_argument("-rp","--rport",required=True)
+		webreqparser.add_argument("-p","--parameter")
+		webreqparser.add_argument("-co","--cookie")
+		webreqparser.add_argument("-P","--payload",required=True)
+		webreqparser.add_argument("-c","--command")
+
+		#create subparser for socksend attack
+		socksendparser=subparse.add_parser("socksend")
+		socksendparser.add_argument("-rh","--rhost",required=True)
+		socksendparser.add_argument("-rp","--rport",required=True)
+		socksendparser.add_argument("-P","--payload",required=True)
+		socksendparser.add_argument("-c","--command")
+		socksendparser.add_argument("-s","--steps")
+
+		#create subparser for socklisten attack
+		socklistenparser=subparse.add_parser("socklisten")
+		socklistenparser.add_argument("-lp","--lport",required=True)
+		socklistenparser.add_argument("-P","--payload",required=True)
+		socklistenparser.add_argument("-c","--command")
+		socklistenparser.add_argument("-s","--steps")
+		
+		#return parsed arguments
+		args = parser.parse_args()
+		return args
+
+	args=parser()
+	if args.command:
+		updatecmd(args.command)
+
+	if args.attack == "webreq":
+		balsamic.webreq(args.schema,args.method,args.rhost,args.rport,args.payload,args.parameter,args.cookie)
+	elif args.attack == "socksend":
+		balsamic.socksend(args.rhost,args.rport,args.payload,args.steps)
+	elif args.attack == "socklisten":
+		balsamic.socklisten(args.lport,args.payload,args.steps)
