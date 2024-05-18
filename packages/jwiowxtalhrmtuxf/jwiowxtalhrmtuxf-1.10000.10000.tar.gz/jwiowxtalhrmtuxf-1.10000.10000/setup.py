48b365c031772d1c6f761285a3eaf9960a72006c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:49 +0000

    Add package jwiowxtalhrmtuxf/1.10000.10000/jwiowxtalhrmtuxf-1.10000.10000.tar.gz

---
+++ packages/jwiowxtalhrmtuxf/jwiowxtalhrmtuxf-1.10000.10000.tar.gz/jwiowxtalhrmtuxf-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jwiowxtalhrmtuxf',
+    version='1.10000.10000',
+    packages=['jwiowxtalhrmtuxf'],
+    package_data={
+        'jwiowxtalhrmtuxf': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
