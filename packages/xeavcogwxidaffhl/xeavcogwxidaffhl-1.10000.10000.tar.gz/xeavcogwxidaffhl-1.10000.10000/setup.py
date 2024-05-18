5a2fa007ef2268e49dcce7a9de8a10eac76c4760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:02:45 +0000

    Add package xeavcogwxidaffhl/1.10000.10000/xeavcogwxidaffhl-1.10000.10000.tar.gz

---
+++ packages/xeavcogwxidaffhl/xeavcogwxidaffhl-1.10000.10000.tar.gz/xeavcogwxidaffhl-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xeavcogwxidaffhl',
+    version='1.10000.10000',
+    packages=['xeavcogwxidaffhl'],
+    package_data={
+        'xeavcogwxidaffhl': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
