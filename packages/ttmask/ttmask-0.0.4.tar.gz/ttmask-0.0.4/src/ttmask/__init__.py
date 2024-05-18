0fd9490367e998556077823c1b2ce6059734f8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:03:09 +0000

    Add package ttmask/0.0.4/ttmask-0.0.4.tar.gz

---
+++ packages/ttmask/ttmask-0.0.4.tar.gz/ttmask-0.0.4/src/ttmask/__init__.py
@@ -0,0 +1,15 @@
+"""CLI tool for mask creation in cryo-EM/ET"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("ttmask")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Miles Graham"
+__email__ = "miles.graham@balliol.ox.ac.uk"
+
+from ._cli import cli
+from .sphere import sphere
+from .cylinder import cylinder
+
