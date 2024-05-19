d42370d7e96f980f03d52c770cd6bad8ea966a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:32 +0000

    Add package wantagez/1.1/wantagez-1.1.tar.gz

---
+++ packages/wantagez/wantagez-1.1.tar.gz/wantagez-1.1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'wantagez',
+    packages = ['wantagez'],
+    version = '1.1',
+    license='MIT',
+    description = 'wantagez',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'Naistrai',
+    author_email = 'rizkynaistrai@gmail.com',
+    url = 'https://github.com/naistrai/wantagez',
+    download_url = 'https://github.com/naistrai/wantagez/archive/v_01.tar.gz',
+    project_urls={
+        'Documentation': 'https://github.com/naistrai/wantagez',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/naistrai/wantagez/',
+        'Tracker': 'https://github.com/naistrai/wantagez/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
+)
