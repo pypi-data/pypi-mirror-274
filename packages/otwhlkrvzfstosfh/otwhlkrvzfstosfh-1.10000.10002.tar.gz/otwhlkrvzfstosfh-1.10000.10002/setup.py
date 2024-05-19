29477a69cdcf5237fd6130f7824e485b2cdf0c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:26 +0000

    Add package otwhlkrvzfstosfh/1.10000.10002/otwhlkrvzfstosfh-1.10000.10002.tar.gz

---
+++ packages/otwhlkrvzfstosfh/otwhlkrvzfstosfh-1.10000.10002.tar.gz/otwhlkrvzfstosfh-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='otwhlkrvzfstosfh',
+    version='1.10000.10002',
+    packages=['otwhlkrvzfstosfh'],
+    package_data={
+        'otwhlkrvzfstosfh': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
