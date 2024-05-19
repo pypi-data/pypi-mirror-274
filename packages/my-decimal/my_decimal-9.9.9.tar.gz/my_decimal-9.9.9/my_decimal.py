172756d49c8f051d7382b90e1f1fa397967511fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:38 +0000

    Add package my-decimal/9.9.9/my_decimal-9.9.9.tar.gz

---
+++ packages/my-decimal/my_decimal-9.9.9.tar.gz/my_decimal-9.9.9/my_decimal.py
@@ -0,0 +1,3 @@
+def my_decimal(expression, long = 3):
+    return f'{expression:.{long}f}'
+
---
+++ packages/my-decimal/my_decimal-9.9.9.tar.gz/my_decimal-9.9.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_decimal',
+    version='9.9.9',
+    description='A small library for formatting decimals',
+    py_modules=['my_decimal'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+)
