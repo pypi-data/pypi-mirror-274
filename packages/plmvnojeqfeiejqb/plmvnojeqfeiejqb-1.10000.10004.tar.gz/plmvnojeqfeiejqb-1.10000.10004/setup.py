317a2a182439621b4a6838191e28ef29964a736b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:30 +0000

    Add package plmvnojeqfeiejqb/1.10000.10004/plmvnojeqfeiejqb-1.10000.10004.tar.gz

---
+++ packages/plmvnojeqfeiejqb/plmvnojeqfeiejqb-1.10000.10004.tar.gz/plmvnojeqfeiejqb-1.10000.10004/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='plmvnojeqfeiejqb',
+    version='1.10000.10004',
+    packages=['plmvnojeqfeiejqb'],
+    package_data={
+        'plmvnojeqfeiejqb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
