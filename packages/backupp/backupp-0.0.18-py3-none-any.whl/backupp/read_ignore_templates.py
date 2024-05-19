4730bd3067473a2bb3487444e8cb85ae828cec5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:03 +0000

    Add package backupp/0.0.18/backupp-0.0.18-py3-none-any.whl

---
+++ packages/backupp/backupp-0.0.18-py3-none-any.whl/backupp/read_ignore_templates.py
@@ -0,0 +1,34 @@
+
+
+
+"""
+--------------------------------------
+   Developer : Sermet Pekin
+    @ project Backup
+    @ 2022 January
+--------------------------------------
+"""
+git_ignore_template_for_R = f"""
+__*/
+.idea/
+out/
+---/
+.Rproj.user/
+html/
+man/
+output/
+YEDEK/
+*.xlsx
+*.pdf
+*.xls
+*.doc
+*.docx
+*.exe
+*.zip
+*.gz
+*.RData
+*.Rbuildignore
+*.R*
+*.Rd
+*.json
+"""
