85a1bd71c2a72516285632a31b357c905110be28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:09 +0000

    Add package MailMagician/0.3.0/MailMagician-0.3.0-py3-none-any.whl

---
+++ packages/MailMagician/MailMagician-0.3.0-py3-none-any.whl/mailmagician/__init__.py
@@ -0,0 +1,2 @@
+from mailmagician._mailbot import Mailbot 
+from ._smtp_server import print_smtp_server, SMTP_SERVER
---
+++ packages/MailMagician/MailMagician-0.3.0-py3-none-any.whl/mailmagician/_smtp_server.py
@@ -0,0 +1,10 @@
+
+SMTP_SERVER = {
+    'qq.com': "smtp.qq.com",
+    '126.com': 'smtp.126.com',
+    'gmail.com': "smtp.gmail.com",
+    'yeah.net': "smtp.yeah.net"
+}
+
+def print_smtp_server():
+    print(SMTP_SERVER)
