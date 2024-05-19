c6ba646af10968af6b12bc4c99744f00bddb01d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:27 +0000

    Add package Probability-Distribution-Devin/0.1/Probability-Distribution-Devin-0.1.tar.gz

---
+++ packages/Probability-Distribution-Devin/Probability-Distribution-Devin-0.1.tar.gz/Probability-Distribution-Devin-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Probability-Distribution-Devin',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='A Python package for Gaussian and Binomial probability distributions.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Devin',
+    author_email='devin@example.com',
+    url='https://github.com/Devin-Apps/Probability-Distribution-Devin',
+    install_requires=[
+        # List of dependencies, if any
+    ],
+)
