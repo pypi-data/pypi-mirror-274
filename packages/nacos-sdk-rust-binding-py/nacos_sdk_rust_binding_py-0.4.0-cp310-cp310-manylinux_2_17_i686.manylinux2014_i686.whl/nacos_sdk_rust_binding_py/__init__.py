4e0cf0d5e60a34f51d884d60eb0718d9152ba826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:34 +0000

    Add package nacos-sdk-rust-binding-py/0.4.0/nacos_sdk_rust_binding_py-0.4.0-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/nacos-sdk-rust-binding-py/nacos_sdk_rust_binding_py-0.4.0-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/nacos_sdk_rust_binding_py/__init__.py
@@ -0,0 +1,5 @@
+from .nacos_sdk_rust_binding_py import *
+
+__doc__ = nacos_sdk_rust_binding_py.__doc__
+if hasattr(nacos_sdk_rust_binding_py, "__all__"):
+    __all__ = nacos_sdk_rust_binding_py.__all__
