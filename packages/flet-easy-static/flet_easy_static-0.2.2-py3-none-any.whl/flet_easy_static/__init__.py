9953aaec1d6026f13226203efeb53e79f35f68f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:15 +0000

    Add package flet-easy-static/0.2.2/flet_easy_static-0.2.2-py3-none-any.whl

---
+++ packages/flet-easy-static/flet_easy_static-0.2.2-py3-none-any.whl/flet_easy_static/__init__.py
@@ -0,0 +1,13 @@
+from flet_easy_static.datasy import Datasy
+from flet_easy_static.extra import Redirect
+from flet_easy_static.extrasJwt import Algorithm, PemKey, SecretKey, encode_HS256, encode_RS256
+from flet_easy_static.fletEasy import FletEasy
+from flet_easy_static.inheritance import (
+    Keyboardsy,
+    Ref,
+    Resizesy,
+    ResponsiveControlsy,
+    Viewsy,
+)
+from flet_easy_static.jwt import EasyKey, decode, decode_async
+from flet_easy_static.pagesy import AddPagesy, Pagesy
