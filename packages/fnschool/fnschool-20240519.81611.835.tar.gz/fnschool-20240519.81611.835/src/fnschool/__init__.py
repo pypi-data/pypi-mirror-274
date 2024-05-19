03ab3ea9bc7ed55726d9330c836ba048421f8280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 15:02:58 +0000

    Add package fnschool/20240519.81611.835/fnschool-20240519.81611.835.tar.gz

---
+++ packages/fnschool/fnschool-20240519.81611.835.tar.gz/fnschool-20240519.81611.835/src/fnschool/__init__.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import argparse
+import random
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+from openpyxl import load_workbook
+from fnschool.app import *
+from fnschool.language import _
+from fnschool.fnprint import *
+from fnschool.path import *
+from fnschool.entry import *
+from fnschool.external import *
+
+
+__version__ = "20240519.81611.835"
+
+
+# The end.
