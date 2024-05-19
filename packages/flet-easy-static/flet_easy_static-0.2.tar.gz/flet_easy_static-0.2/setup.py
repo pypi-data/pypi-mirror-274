e87d17830043ccbe585728c4f5dff078a50ff4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:10 +0000

    Add package flet-easy-static/0.2/flet_easy_static-0.2.tar.gz

---
+++ packages/flet-easy-static/flet_easy_static-0.2.tar.gz/flet_easy_static-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='flet_easy_static',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'flet',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
