2f1321f8ce344dd7b8901561b6052c6b2979a94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 15:02:56 +0000

    Add package data-preprocessing-lib/0.1.0/data_preprocessing_lib-0.1.0.tar.gz

---
+++ packages/data-preprocessing-lib/data_preprocessing_lib-0.1.0.tar.gz/data_preprocessing_lib-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_preprocessing_lib',
+    version='0.1.0',
+    description='A easy peasy comprehensive library for data preprocessing tasks',
+    author='Mehdi Miraç ARAT, Latif Şimşek',
+    author_email='mehdimirac.arat@stu.fsm.edu.tr, latif.simsek@stu.fsm.edu.tr',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'nltk',
+    ],
+    python_requires='>=3.6',
+)
