3aa03b3f7383b8c8cdc4fcbd43853846c9a63edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:24 +0000

    Add package vmafdynlwobwewal/1.10000.10001/vmafdynlwobwewal-1.10000.10001.tar.gz

---
+++ packages/vmafdynlwobwewal/vmafdynlwobwewal-1.10000.10001.tar.gz/vmafdynlwobwewal-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vmafdynlwobwewal',
+    version='1.10000.10001',
+    packages=['vmafdynlwobwewal'],
+    package_data={
+        'vmafdynlwobwewal': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
