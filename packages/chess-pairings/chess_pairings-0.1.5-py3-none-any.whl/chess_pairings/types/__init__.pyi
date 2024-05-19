db2c4dca355080bf5f1925ee77d77e950cc0efe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 15:03:00 +0000

    Add package chess-pairings/0.1.5/chess_pairings-0.1.5-py3-none-any.whl

---
+++ packages/chess-pairings/chess_pairings-0.1.5-py3-none-any.whl/chess_pairings/types/__init__.pyi
@@ -0,0 +1,8 @@
+from .pairings import Result, Paired, Unpaired, Pairing, RoundPairings, GroupPairings, TournamentPairings
+from .ids import GameId, GroupId, RoundId, gameId, roundId, groupId
+from .mapping import GamesMapping
+
+__all__ = [
+  'Result', 'Paired', 'Unpaired', 'Pairing', 'RoundPairings', 'GroupPairings', 'TournamentPairings',
+  'GameId', 'GroupId', 'RoundId', 'gameId', 'roundId', 'groupId', 'GamesMapping',
+]
