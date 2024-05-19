d26595ca64c4740539c26d7ea86fdefabdf38828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:31 +0000

    Add package pop-sort/1.0/pop_sort-1.0.tar.gz

---
+++ packages/pop-sort/pop_sort-1.0.tar.gz/pop_sort-1.0/README.md
@@ -0,0 +1,2 @@
+# Popular sorting
+Library containing some of the most popular sorting algorithms to play around with. 
---
+++ packages/pop-sort/pop_sort-1.0.tar.gz/pop_sort-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pop_sort',
+    version='1.0',
+    description='Project for exploring popular sorting algorithms.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/MartynasGr/pop_sort',
+    author='Martynas Greičius',
+    author_email='martynas.greicius@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
