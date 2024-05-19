54f4f7db58ed53b35ecd1a31439c2b0cdc15b09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:03 +0000

    Add package RNAformer/1.3.1/rnaformer-1.3.1.tar.gz

---
+++ packages/RNAformer/rnaformer-1.3.1.tar.gz/rnaformer-1.3.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import shutil
+from distutils.core import setup
+
+from setuptools import find_packages
+
+
+setup(name='RNAformer',
+      version='1.2.0',
+      packages=find_packages(),
+      include_package_data=True,
+      package_dir={'': '.'},
+      dependency_links=[],
+      )
