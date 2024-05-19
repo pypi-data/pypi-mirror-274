af061dbc900b4c3469ea290f3d1861a8ca3c3b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:33 +0000

    Add package pnxehdqkyocawpec/1.10000.10006/pnxehdqkyocawpec-1.10000.10006.tar.gz

---
+++ packages/pnxehdqkyocawpec/pnxehdqkyocawpec-1.10000.10006.tar.gz/pnxehdqkyocawpec-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pnxehdqkyocawpec',
+    version='1.10000.10006',
+    packages=['pnxehdqkyocawpec'],
+    package_data={
+        'pnxehdqkyocawpec': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
