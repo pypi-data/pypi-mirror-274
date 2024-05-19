da332cc3d7f831df1c355001584a6c4b5306de0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:10 +0000

    Add package ooo-dev-tools/0.43.2/ooo_dev_tools-0.43.2.tar.gz

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.43.2.tar.gz/ooo_dev_tools-0.43.2/ooodev/loader/inst/options.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from dataclasses import dataclass
+import logging
+
+
+@dataclass(frozen=True)
+class Options:
+    """
+    Lo Load options
+
+    .. versionadded:: 0.6.10
+    """
+
+    verbose: bool = False
+    """Determines if various info is sent to console. Default ``False``"""
+
+    dynamic: bool = True
+    """
+    Determines if the script context is dynamic.
+
+    Also When loading a component via :py:meth:`LoInst.load_component() <ooodev.utils.inst.lo.LoInst.load_component>` It is recommended to set this value to ``False``.
+
+    If dynamic the script context is created with the current document; Otherwise, context is static.
+    Static context is useful when the script is only going to operate on a a single document.
+    There may be a performance gain when using static context.
+
+    Default ``True``
+    
+    .. versionadded:: 0.11.13
+    """
+    log_level: int = logging.INFO
+    """Logging level. Default ``logging.INFO``"""
+
+    lo_cache_size: int = 200
+    """Lo Instance cache size. Default ``200``, ``0`` or less means no caching. Normally you should not need to change this value. If you do, it should be a power of 2."""
