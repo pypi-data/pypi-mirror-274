ecd9a9cd508e8470fc880e5d00f2767051d4f4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:01 +0000

    Add package clean-alchemy/0.1.10/clean_alchemy-0.1.10-py3-none-any.whl

---
+++ packages/clean-alchemy/clean_alchemy-0.1.10-py3-none-any.whl/clean_alchemy/__init__.py
@@ -0,0 +1,7 @@
+from .base_dao import BaseDAO
+from .base_ent import BaseENT
+from .base_rpo import BaseRPO
+from .base_srv import BaseSRV
+from .base import get_db
+
+__all__ = ["BaseDAO", "BaseENT", "BaseRPO", "BaseSRV", "get_db"]
