884a386c794d4dc2b27073f77f4f9dee2d29e8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:02 +0000

    Add package iop-python/0.1.2/iop_python-0.1.2.tar.gz

---
+++ packages/iop-python/iop_python-0.1.2.tar.gz/iop_python-0.1.2/test.py
@@ -0,0 +1,22 @@
+import iop_python as iop
+from api import hydra
+
+wallet = hydra.HydraWallet()
+
+
+c = wallet.get_new_acc_on_vault('password')
+print(c)
+
+
+phrase = "blind market ability shoot topple round inmate pass lunch symbol average alpha party notice switch sea pass toy alien fuel pull angle weather sca"
+try:
+    wallet = iop.get_morpheus_vault(phrase,"pass")
+    print(wallet)
+
+except Exception as e:
+    if "VaultCouldNotBeUnwrapped" in str(e):
+        print("The vault could not be unwrapped.")
+    else:
+        print(f"A RuntimeError occurred: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
