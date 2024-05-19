2fe8d24e3de678fc019c054072b3c19a2a1512aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:48 +0000

    Add package Gewebehaken/0.4/Gewebehaken-0.4-py3-none-any.whl

---
+++ packages/Gewebehaken/Gewebehaken-0.4-py3-none-any.whl/gewebehaken/hooks/twitter.py
@@ -0,0 +1,53 @@
+"""
+Gewebehaken
+~~~~~~~~~~~
+
+Twitter_ web hooks
+
+These hooks are designed to work with requests sent via the Zapier_
+service. The corresponding "zaps" must be configured to send a JSON
+request body with at least the fields defined in the view functions
+below.
+
+.. _Twitter: https://twitter.com/
+.. _Zapier: https://zapier.com/
+
+:Copyright: 2015-2024 Jochen Kupperschmidt
+:License: MIT, see LICENSE for details.
+"""
+
+from blinker import signal
+from flask import Blueprint, request
+
+from ..util import get_all_or_400, log_incoming_request_data, respond_no_content
+
+
+twitter_followed = signal('twitter-followed')
+twitter_mentioned = signal('twitter-mentioned')
+
+
+blueprint = Blueprint('twitter', __name__, url_prefix='/twitter')
+
+
+@blueprint.route('/followed', methods=['POST'])
+@respond_no_content
+def followed():
+    data = request.get_json()
+    log_incoming_request_data(data)
+
+    keys = {'screen_name', 'name', 'target_account_screen_name'}
+    fields = get_all_or_400(data, keys)
+
+    twitter_followed.send(None, **fields)
+
+
+@blueprint.route('/mentioned', methods=['POST'])
+@respond_no_content
+def mentioned():
+    data = request.get_json()
+    log_incoming_request_data(data)
+
+    keys = {'screen_name', 'name', 'url', 'target_account_screen_name'}
+    fields = get_all_or_400(data, keys)
+
+    twitter_mentioned.send(None, **fields)
---
+++ packages/Gewebehaken/Gewebehaken-0.4-py3-none-any.whl/gewebehaken/util.py
@@ -0,0 +1,56 @@
+"""
+Gewebehaken
+~~~~~~~~~~~
+
+Utilities
+
+:Copyright: 2015-2024 Jochen Kupperschmidt
+:License: MIT, see LICENSE for details.
+"""
+
+from functools import wraps
+from pprint import pformat
+
+from flask import abort, current_app, Response
+
+
+def get_all_or_400(data, keys):
+    """Return a dictionary of the given keys mapped to their values
+    taken from the given dictionary.
+
+    If at least one of the keys is not found in the dictionary, a
+    response is sent with status code 400 and an error message
+    indicating the missing key.
+    """
+    return {key: get_or_400(data, key) for key in keys}
+
+
+def get_or_400(data, key):
+    """Return the value for the given key in the given dictionary
+
+    If the key is not found in the dictionary, a response is sent with
+    status code 400 and an error message indicating the missing key.
+    """
+    try:
+        return data[key]
+    except KeyError:
+        abort(400, 'Missing key: ' + key)
+
+
+def log_incoming_request_data(data):
+    delimiter_line = '-' * 40
+    log_message = delimiter_line + '\n' + pformat(data)
+    current_app.logger.warning(log_message)
+
+
+def respond_no_content(f):
+    """Decorate a callable so that a ``204 No Content`` response is
+    returned after it is executed.
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        f(*args, **kwargs)
+        return Response(status=204)
+
+    return wrapper
