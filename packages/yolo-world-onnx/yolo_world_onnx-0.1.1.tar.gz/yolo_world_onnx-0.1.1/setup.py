741c16d63be116e099246b9883758267e0de57a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:37 +0000

    Add package yolo-world-onnx/0.1.1/yolo_world_onnx-0.1.1.tar.gz

---
+++ packages/yolo-world-onnx/yolo_world_onnx-0.1.1.tar.gz/yolo_world_onnx-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="yolo-world-onnx",
+    version="0.1.1",
+    packages=["yolo_world_onnx"],
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "onnxruntime-gpu",
+        "torch>=1.7.0",
+        "torchvision>=0.8.0",
+        "clip-for-odlabel",
+    ],
+    author="Ziad-Algrafi",
+    author_email="ZiadAlgrafi@gmail.com",
+    description="ONNX-YOLO is a Python package for running inference on YOLO-WORLD open-vocabulary object detection models using ONNX runtime.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ziad-Algrafi/onnx-yolo",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.9",
+    setup_requires=["setuptools>=69.5.1", "wheel>=0.43.0"],
+    license="MIT",
+)
