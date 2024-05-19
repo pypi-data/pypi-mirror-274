e8df564973bd18189c2fc74d95baa21b99eacd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:18 +0000

    Add package fast-text-detect/0.0.2/fast_text_detect-0.0.2-py3-none-any.whl

---
+++ packages/fast-text-detect/fast_text_detect-0.0.2-py3-none-any.whl/fast_text_detect/__init__.py
@@ -0,0 +1,10 @@
+from .core import *
+from .detection import *
+from .layers import *
+from .predictor import *
+from .preprocessor import *
+from .textnet import *
+from .transforms import *
+from .utils import *
+
+__all__ = ["core", "detection", "layers", "predictor","preprocessor","textnet","transforms","utils"]
