67078aebd8be7fd14e59230694c0c09d8b91c1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:34 +0000

    Add package gitoptim/0.1.0.dev22/gitoptim-0.1.0.dev22.tar.gz

---
+++ packages/gitoptim/gitoptim-0.1.0.dev22.tar.gz/gitoptim-0.1.0.dev22/gitoptim/main.py
@@ -0,0 +1,21 @@
+import typer
+
+from gitoptim import commands
+from gitoptim.utils.cli import should_callback_execute
+
+app = typer.Typer(rich_markup_mode="rich")
+app.add_typer(commands.analyse.app, name="analyse")
+app.add_typer(commands.tag.app, name="tag")
+
+
+@app.callback()
+def main():
+    """
+    Gitoptim SDK
+    """
+
+    if not should_callback_execute():
+        return
+
+
+app.command(name="memlab")(commands.memlab)
