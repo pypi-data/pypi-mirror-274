b7ba7a2dacc17438cabf1dd40ecdcf5270552d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:28 +0000

    Add package brioqpwyupmfveul/1.10000.10003/brioqpwyupmfveul-1.10000.10003.tar.gz

---
+++ packages/brioqpwyupmfveul/brioqpwyupmfveul-1.10000.10003.tar.gz/brioqpwyupmfveul-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='brioqpwyupmfveul',
+    version='1.10000.10003',
+    packages=['brioqpwyupmfveul'],
+    package_data={
+        'brioqpwyupmfveul': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
