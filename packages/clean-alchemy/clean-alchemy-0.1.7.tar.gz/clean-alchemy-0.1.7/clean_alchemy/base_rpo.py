18efeec7cbbf130553341eb4be1ea090a89a57a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:12 +0000

    Add package clean-alchemy/0.1.7/clean-alchemy-0.1.7.tar.gz

---
+++ packages/clean-alchemy/clean-alchemy-0.1.7.tar.gz/clean-alchemy-0.1.7/clean_alchemy/base_rpo.py
@@ -0,0 +1,113 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, List, Type
+
+from sqlalchemy.orm import Session
+
+from clean_alchemy import BaseDAO, BaseENT
+from clean_alchemy.config import ENV
+
+DAO_TYPE = TypeVar("DAO_TYPE", bound=BaseDAO)
+ENT_TYPE = TypeVar("ENT_TYPE", bound=BaseENT)
+
+
+class BaseRPO(ABC, Generic[DAO_TYPE, ENT_TYPE]):
+    dao_class: Type[DAO_TYPE] = None
+    ent_class: Type[ENT_TYPE] = None
+
+    def __init__(cls, db_session: Session = None) -> None:
+        cls.db_session = db_session
+
+    def __init_subclass__(cls) -> None:
+        if cls.dao_class is None:
+            raise NotImplementedError("Must define a `dao_class` for a BaseRPO.")
+        if cls.ent_class is None:
+            raise NotImplementedError("Must define a `ent_class` for a BaseRPO.")
+
+    @abstractmethod
+    def _to_ent(self, dao: DAO_TYPE) -> ENT_TYPE:
+        pass
+
+    def _to_ents(self, daos: List[DAO_TYPE]) -> List[ENT_TYPE]:
+        return [self._to_ent(dao) for dao in daos]
+
+    @abstractmethod
+    def _to_dao(self, ent: ENT_TYPE) -> DAO_TYPE:
+        pass
+
+    def _to_daos(self, ents: List[ENT_TYPE]) -> List[DAO_TYPE]:
+        return [self._to_dao(ent) for ent in ents]
+
+    def _commit(self) -> None:
+        if ENV in ["production", "development"]:
+            self.db_session.commit()
+        else:
+            self.db_session.flush()
+
+    def _bulk_insert_mappings(self, mappings: List[dict]) -> None:
+        self.db_session.bulk_insert_mappings(
+            mapper=self.dao_class,
+            mappings=mappings,
+        )
+        self._commit()
+
+    def _to_mappings(self, ents: List[ENT_TYPE]) -> List[dict]:
+        daos = self._to_daos(ents=ents)
+        return [dao.__dict__.copy() for dao in daos]
+
+    def create_many(self, ents: List[ENT_TYPE]) -> List[ENT_TYPE]:
+        mappings = self._to_mappings(ents=ents)
+        self._bulk_insert_mappings(mappings=mappings)
+        return ents
+
+    def create(self, ent: ENT_TYPE) -> ENT_TYPE:
+        return self.create_many(ents=[ent]).pop()
+
+    def _filter_by_keys(self, keys: List[str]) -> List[DAO_TYPE]:
+        return (
+            self.db_session.query(self.dao_class)
+            .filter(
+                self.dao_class.key.in_(keys),
+                self.dao_class.archived.is_(False),
+            )
+            .all()
+        )
+
+    def retrieve_many(self, keys: List[str]) -> List[ENT_TYPE]:
+        daos = self._filter_by_keys(keys=keys)
+        return self._to_ents(daos=daos)
+
+    def retrieve(self, key: str) -> ENT_TYPE:
+        return self.retrieve_many(keys=[key]).pop()
+
+    def _filter_by_all(self) -> List[DAO_TYPE]:
+        return (
+            self.db_session.query(self.dao_class)
+            .filter(self.dao_class.archived.is_(False))
+            .all()
+        )
+
+    def retrieve_all(self) -> List[ENT_TYPE]:
+        daos = self._filter_by_all()
+        return self._to_ents(daos=daos)
+
+    def _bulk_update_mappings(self, mappings: List[dict]) -> None:
+        self.db_session.bulk_update_mappings(
+            mapper=self.dao_class,
+            mappings=mappings,
+        )
+        self._commit()
+
+    def update_many(self, ents: List[ENT_TYPE]) -> List[ENT_TYPE]:
+        mappings = self._to_mappings(ents=ents)
+        self._bulk_update_mappings(mappings=mappings)
+        return ents
+
+    def update(self, ent: ENT_TYPE) -> ENT_TYPE:
+        return self.update_many(ents=[ent]).pop()
+
+    def delete_many(self, keys: List[str]) -> None:
+        mappings = [{"key": key, "archived": True} for key in keys]
+        self._bulk_update_mappings(mappings=mappings)
+
+    def delete(self, key: str) -> None:
+        self.delete_many(keys=[key])
---
+++ packages/clean-alchemy/clean-alchemy-0.1.7.tar.gz/clean-alchemy-0.1.7/clean_alchemy/base_srv.py
@@ -0,0 +1,64 @@
+import string
+
+from abc import ABC, abstractmethod
+from random import sample
+from typing import Generic, List, Tuple, TypeVar
+
+from clean_alchemy import BaseRPO, BaseENT
+
+
+RPO_TYPE = TypeVar("RPO_TYPE", bound=BaseRPO)
+ENT_TYPE = TypeVar("ENT_TYPE", bound=BaseENT)
+
+
+class BaseSRV(ABC, Generic[RPO_TYPE, ENT_TYPE]):
+    def __init__(self, rpo: RPO_TYPE) -> None:
+        self.rpo = rpo
+
+    @staticmethod
+    def _generate_key(prefix: str = "", postfix: str = "", length: int = 24) -> str:
+        characters = string.ascii_letters + string.digits
+        return f"{prefix}{''.join(sample(characters, k=length))}{postfix}"
+
+    @abstractmethod
+    def _generate_metadata(self) -> dict:
+        pass
+
+    @abstractmethod
+    def _create_ent_from_doc(self, doc: dict) -> ENT_TYPE:
+        pass
+
+    @abstractmethod
+    def _update_ent_from_doc(self, ent: ENT_TYPE, doc: dict) -> ENT_TYPE:
+        pass
+
+    def create_many(self, docs: List[dict]) -> List[ENT_TYPE]:
+        ents = [self._create_ent_from_doc(doc=doc) for doc in docs]
+        return self.rpo.create_many(ents=ents)
+
+    def create(self, doc: dict) -> ENT_TYPE:
+        ent = self._create_ent_from_doc(doc=doc)
+        return self.rpo.create(ent=ent)
+
+    def retrieve(self, key: str) -> ENT_TYPE:
+        return self.rpo.retrieve(key=key)
+
+    def retrieve_many(self, keys: List[str]) -> List[ENT_TYPE]:
+        return self.rpo.retrieve_many(keys=keys)
+
+    def retrieve_all(self) -> List[ENT_TYPE]:
+        return self.rpo.retrieve_all()
+
+    def update_many(self, ents_docs: List[Tuple[ENT_TYPE, dict]]) -> List[ENT_TYPE]:
+        ents = [self._update_ent_from_doc(ent=ent, doc=doc) for ent, doc in ents_docs]
+        return self.rpo.update_many(ents=ents)
+
+    def update(self, ent: ENT_TYPE, doc: dict) -> ENT_TYPE:
+        ent = self._update_ent_from_doc(ent=ent, doc=doc)
+        return self.rpo.update(ent=ent)
+
+    def delete_many(self, keys: List[str]) -> List[ENT_TYPE]:
+        return self.rpo.delete_many(keys=keys)
+
+    def delete(self, key: str) -> ENT_TYPE:
+        return self.rpo.delete(key=key)
