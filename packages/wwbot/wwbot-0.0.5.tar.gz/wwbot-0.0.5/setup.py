1939ecd4bb65a68472e8885e2313a733ea03306f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 15:03:00 +0000

    Add package wwbot/0.0.5/wwbot-0.0.5.tar.gz

---
+++ packages/wwbot/wwbot-0.0.5.tar.gz/wwbot-0.0.5/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+setuptools.setup(
+  name="wwbot",
+  version="0.0.5",
+  author="tbbatbb",
+  author_email="20682299+tbbatbb@users.noreply.github.com",
+  description="A library for dealing with messages in WeWork",
+  url="https://github.com/tbbatbb/WeWorkBot",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Development Status :: 2 - Pre-Alpha",
+    "Framework :: Flask",
+    "Intended Audience :: Developers",
+    "License :: Free for non-commercial use",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Communications :: Chat",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+  ],
+)
