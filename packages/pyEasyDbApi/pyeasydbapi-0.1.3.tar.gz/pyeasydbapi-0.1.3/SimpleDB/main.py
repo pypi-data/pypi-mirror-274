d827ee7afa4fa22ec2461b198953f396b4c340c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:03:33 +0000

    Add package pyEasyDbApi/0.1.3/pyeasydbapi-0.1.3.tar.gz

---
+++ packages/pyEasyDbApi/pyeasydbapi-0.1.3.tar.gz/pyeasydbapi-0.1.3/SimpleDB/main.py
@@ -0,0 +1,155 @@
+import json
+import threading
+import sqlite3
+
+
+class Database:
+    def get(self, module: str, variable: str, default=None):
+        """Get value from database"""
+        raise NotImplementedError
+
+    def set(self, module: str, variable: str, value):
+        """Set key in database"""
+        raise NotImplementedError
+
+    def remove(self, module: str, variable: str):
+        """Remove key from database"""
+        raise NotImplementedError
+
+    def get_collection(self, module: str) -> dict:
+        """Get database for selected module"""
+        raise NotImplementedError
+
+    def close(self):
+        """Close the database"""
+        raise NotImplementedError
+
+    def add_user(self, user_id, name, args):
+        """Add user to database"""
+
+
+class SqliteDatabase(Database):
+    """
+    :param file: DataBase file
+    """
+
+    def __init__(self, file):
+        self._conn = sqlite3.connect(file, check_same_thread=False)
+        self._conn.row_factory = sqlite3.Row
+        self._cursor = self._conn.cursor()
+        self._lock = threading.Lock()
+
+    @staticmethod
+    def _parse_row(row: sqlite3.Row):
+        if row["type"] == "bool":
+            return row["val"] == "1"
+        elif row["type"] == "int":
+            return int(row["val"])
+        elif row["type"] == "str":
+            return row["val"]
+        else:
+            return json.loads(row["val"])
+
+    def _execute(self, module: str, *args, **kwargs) -> sqlite3.Cursor:
+        self._lock.acquire()
+        try:
+            return self._cursor.execute(*args, **kwargs)
+        except sqlite3.OperationalError as e:
+            if str(e).startswith("no such table"):
+                sql = f"""
+                CREATE TABLE IF NOT EXISTS '{module}' (
+                var TEXT UNIQUE NOT NULL,
+                val TEXT NOT NULL,
+                type TEXT NOT NULL
+                )
+                """
+                self._cursor.execute(sql)
+                self._conn.commit()
+                return self._cursor.execute(*args, **kwargs)
+            raise e from None
+        finally:
+            self._lock.release()
+
+    def get(self, module: str, variable: str, default=None):
+        sql = f"SELECT * FROM '{module}' WHERE var=:var"
+        cur = self._execute(module, sql, {"tabl": module, "var": variable})
+
+        row = cur.fetchone()
+        if row is None:
+            return default
+        else:
+            return self._parse_row(row)
+
+    def set(self, module: str, variable: str, value) -> bool:
+        sql = f"""
+        INSERT INTO '{module}' VALUES ( :var, :val, :type )
+        ON CONFLICT (var) DO
+        UPDATE SET val=:val, type=:type WHERE var=:var
+        """
+
+        if isinstance(value, bool):
+            val = "1" if value else "0"
+            typ = "bool"
+        elif isinstance(value, str):
+            val = value
+            typ = "str"
+        elif isinstance(value, int):
+            val = str(value)
+            typ = "int"
+        else:
+            val = json.dumps(value)
+            typ = "json"
+
+        self._execute(module, sql, {"var": variable, "val": val, "type": typ})
+        self._conn.commit()
+
+        return True
+
+    def remove(self, module: str, variable: str):
+        sql = f"DELETE FROM '{module}' WHERE var=:var"
+        self._execute(module, sql, {"var": variable})
+        self._conn.commit()
+
+    def get_collection(self, module: str) -> dict:
+        sql = f"SELECT * FROM '{module}'"
+        cur = self._execute(module, sql)
+
+        collection = {}
+        for row in cur:
+            collection[row["var"]] = self._parse_row(row)
+
+        return collection
+
+    def close(self):
+        self._conn.commit()
+        self._conn.close()
+
+    def _add_user(self, user_id):
+        if self.get('_users', user_id) is not None:
+            return False
+
+    def add_user(self, user_id, name, args: str = None):
+        """         :param user_id: user id
+                    :param args your vars
+                    :param name: user name
+        """
+
+        if args is None:
+            sql = f"INSERT INTO _users (id, name) VALUES ({id}, {name})"
+        else:
+            sql = f"INSERT INTO _users (id, name, {args}) VALUES ({id}, {name}, {args})"
+
+        sql_1 = (f"CREATE TABLE IF NOT EXISTS '_users' (user_id TEXT UNIQUE NOT NULL,"
+                 f" name TEXT NOT NULL, TEXT NOT NULL, {args})")
+
+        self._cursor.execute(sql_1)
+        self._conn.commit()
+
+        sql = f"SELECT * FROM _users WHERE user-_id={user_id}"
+        cur = self._cursor.execute(sql)
+
+        row = cur.fetchone()
+        if row is None:
+            self._cursor.execute(sql)
+            self._conn.commit()
+        return {'user': 'is table'}
