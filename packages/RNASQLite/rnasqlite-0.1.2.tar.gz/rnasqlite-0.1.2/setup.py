158b892e4c162d4ba0f59cb3473e2697c70bf8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:00 +0000

    Add package RNASQLite/0.1.2/rnasqlite-0.1.2.tar.gz

---
+++ packages/RNASQLite/rnasqlite-0.1.2.tar.gz/rnasqlite-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RNASQLite',
+    version='0.1.2',  # 버전을 업데이트합니다.
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'RNASQLite=RNASQLite.cli:main',
+        ],
+    },
+    description='A tool for managing RNA-Seq data with SQLite',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/RNASQLite',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
