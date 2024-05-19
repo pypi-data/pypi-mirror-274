ab48988868f6a5d8d54659397d2fe8e1ebecff57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:41 +0000

    Add package torchsense/0.0.5/torchsense-0.0.5.tar.gz

---
+++ packages/torchsense/torchsense-0.0.5.tar.gz/torchsense-0.0.5/torchsense/trainer/trainer.py
@@ -0,0 +1,25 @@
+from torchsense.models.lit_model import (LitRegressModel,
+                                         LitClassModel,
+                                         LitMultimodalModel)
+import lightning as L
+
+
+class Trainer:
+    def __init__(self, model, task="r", max_epochs=5, accelerator="auto"):
+        if task == "r":
+            self.model = LitRegressModel(model)
+        elif task == "c":
+            self.model = LitClassModel(model)
+        elif task == "m":
+            self.model = LitMultimodalModel(model)
+        else:
+            raise ValueError("task must be either 'r' or 'c'")
+        self.max_epochs = max_epochs
+        self.accelerator = accelerator
+
+    def fit(self, train_loader, val_loader):
+        trainer = L.Trainer(
+            accelerator=self.accelerator,
+            max_epochs=self.max_epochs,
+        )
+        trainer.fit(self.model, train_loader, val_loader)
