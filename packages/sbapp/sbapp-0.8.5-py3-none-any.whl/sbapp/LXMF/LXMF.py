1d855d72041b43eb992f60bf5aa680a5215e98cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:05 +0000

    Add package sbapp/0.8.5/sbapp-0.8.5-py3-none-any.whl

---
+++ packages/sbapp/sbapp-0.8.5-py3-none-any.whl/sbapp/LXMF/LXMF.py
@@ -0,0 +1,17 @@
+APP_NAME = "lxmf"
+
+# WARNING! These field specifiers are floating and not
+# yet final! Consider highly experiemental, and expect
+# them to change in the future! You have been warned :)
+
+FIELD_EMBEDDED_LXMS    = 0x01
+FIELD_TELEMETRY        = 0x02
+FIELD_TELEMETRY_STREAM = 0x03
+FIELD_ICON_APPEARANCE  = 0x04
+FIELD_FILE_ATTACHMENTS = 0x05
+FIELD_IMAGE            = 0x06
+FIELD_AUDIO            = 0x07
+FIELD_THREAD           = 0x08
+FIELD_COMMANDS         = 0x09
+FIELD_RESULTS          = 0x0A
+FIELD_GROUP            = 0x0B
