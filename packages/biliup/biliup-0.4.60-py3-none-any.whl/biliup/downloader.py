3d3e1c6b0ea0c2e0257d3708258170d90959c7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:03 +0000

    Add package biliup/0.4.60/biliup-0.4.60-py3-none-any.whl

---
+++ packages/biliup/biliup-0.4.60-py3-none-any.whl/biliup/downloader.py
@@ -0,0 +1,19 @@
+import logging
+import re
+
+from .engine.decorators import Plugin
+from .plugins import general
+
+logger = logging.getLogger('biliup')
+
+
+def download(fname, url, **kwargs):
+    pg = general.__plugin__(fname, url)
+    for plugin in Plugin.download_plugins:
+        if re.match(plugin.VALID_URL_BASE, url):
+            pg = plugin(fname, url)
+            for k in pg.__dict__:
+                if kwargs.get(k):
+                    pg.__dict__[k] = kwargs.get(k)
+            break
+    return pg.start()
