703c662cc44d0afdfec23e488f5e011d2431e4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:06 +0000

    Add package outpostkit/0.0.66/outpostkit-0.0.66.tar.gz

---
+++ packages/outpostkit/outpostkit-0.0.66.tar.gz/outpostkit-0.0.66/outpostkit/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.66"
+from outpostkit.client import Client as Client
+from outpostkit.endpoints import Endpoint as Endpoint
+from outpostkit.endpoints import Endpoints as Endpoints
+from outpostkit.finetuning import Finetunings as Finetunings
+from outpostkit.finetuning import FinetuningService as FinetuningService
+from outpostkit.repository import Repository as Repository
+from outpostkit.repository import RepositoryAtRef as RepositoryAtRef
+from outpostkit.team import Team as Team
+from outpostkit.user import User as User
