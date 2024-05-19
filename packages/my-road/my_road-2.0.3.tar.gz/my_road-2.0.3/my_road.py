6a4ad7cdba65d7650430eeac493a93cc6bb9ecaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:36 +0000

    Add package my-road/2.0.3/my_road-2.0.3.tar.gz

---
+++ packages/my-road/my_road-2.0.3.tar.gz/my_road-2.0.3/my_road.py
@@ -0,0 +1,26 @@
+__all__ = ['my_road']
+
+import time
+
+
+def cycle(speed=0.025):
+    string = '.'
+    a = 0
+    b = True
+    while b:
+        if a <= 15:
+            for i in range(1, 65 + 1):
+                print(f'{string * i:97}.{string * i:>97}')
+                a += 1
+                time.sleep(speed)
+                if a == 65:
+                    for i in range(65, 0, -1):
+                        print(f"{string * i:97}.{string * i:>97}")
+                        time.sleep(speed)
+                        if i == 1:
+                            b = False
+
+
+def my_road(num, speed):
+    for i in range(1, num + 1):
+        cycle(speed)
