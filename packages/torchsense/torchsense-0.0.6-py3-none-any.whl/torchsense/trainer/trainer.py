1743dd7f794d3abe3d67d33741d7f06e9220053a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:02 +0000

    Add package torchsense/0.0.6/torchsense-0.0.6-py3-none-any.whl

---
+++ packages/torchsense/torchsense-0.0.6-py3-none-any.whl/torchsense/trainer/trainer.py
@@ -0,0 +1,38 @@
+from torchsense.models.lit_model import (LitRegressModel,
+                                         LitClassModel,
+                                         LitMultimodalModel)
+import lightning as L
+import os
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
+
+CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "saved_models/base")
+
+
+class Trainer:
+    def __init__(self, model, task="r", precision="32", max_epochs=5, accelerator="auto"):
+        if task == "r":
+            self.model = LitRegressModel(model)
+        elif task == "c":
+            self.model = LitClassModel(model)
+        elif task == "m":
+            self.model = LitMultimodalModel(model)
+        else:
+            raise ValueError("task must be either 'r' or 'c'")
+        self.max_epochs = max_epochs
+        self.accelerator = accelerator
+        self.precision = precision
+
+    def fit(self, train_loader, val_loader):
+        trainer = L.Trainer(
+            default_root_dir=CHECKPOINT_PATH,
+            precision=self.precision,
+            accelerator=self.accelerator,
+            max_epochs=self.max_epochs,
+            callbacks=[
+                ModelCheckpoint(save_weights_only=True, mode="min", monitor="val_loss",
+                                save_last=True, save_top_k=3, filename='{epoch}-{val_loss:.2f}'),
+                # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer
+
+            ],
+        )
+        trainer.fit(self.model, train_loader, val_loader)
