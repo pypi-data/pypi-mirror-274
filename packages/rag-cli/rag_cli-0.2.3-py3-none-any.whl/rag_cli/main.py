ae4d6c29cbe0d9c826feecac8d8850cc92025fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:39 +0000

    Add package rag-cli/0.2.3/rag_cli-0.2.3-py3-none-any.whl

---
+++ packages/rag-cli/rag_cli-0.2.3-py3-none-any.whl/rag_cli/main.py
@@ -0,0 +1,30 @@
+import sys
+
+from rag_cli.cli import cli, list_of_floats
+from rag_cli.embedder import run_embedder
+from rag_cli.vector_store import run_vector_store
+
+
+def main() -> None:
+    args = cli()
+
+    if args.command in ["embed"]:
+        # If the file argument is not provided, read from stdin
+        if args.file == sys.stdin:
+            run_embedder(text=sys.stdin.read(), ollama_url=args.ollama_url)
+        else:
+            run_embedder(file=args.file, ollama_url=args.ollama_url)
+    elif args.command in ["vector-store"]:
+        embedding_input = args.embedding.read().strip()
+        embedding = list_of_floats(embedding_input)
+
+        run_vector_store(
+            qdrant_url=args.qdrant_url,
+            collection_name=args.collection_name,
+            embedding=embedding,
+            data=args.data,
+        )
+
+
+if __name__ == "__main__":
+    main()
