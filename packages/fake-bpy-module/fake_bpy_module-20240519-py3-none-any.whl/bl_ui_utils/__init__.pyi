055fe5e24a4dee2b97c49acef3fe0b3f4a7ea04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:46 +0000

    Add package fake-bpy-module/20240519/fake_bpy_module-20240519-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20240519-py3-none-any.whl/bl_ui_utils/__init__.pyi
@@ -0,0 +1,6 @@
+import typing
+from . import bug_report_url
+from . import layout
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
---
+++ packages/fake-bpy-module/fake_bpy_module-20240519-py3-none-any.whl/gpu_extras/__init__.pyi
@@ -0,0 +1,15 @@
+"""
+gpu_extras.batch.rst
+gpu_extras.presets.rst
+
+:maxdepth: 1
+:caption: Submodules
+
+"""
+
+import typing
+from . import batch
+from . import presets
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
