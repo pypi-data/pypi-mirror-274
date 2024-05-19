c0a1dbcaa91d7d7bcca46e57b4f33262e9bcc554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:04 +0000

    Add package my-road-c/1.0.0/my_road_c-1.0.0.tar.gz

---
+++ packages/my-road-c/my_road_c-1.0.0.tar.gz/my_road_c-1.0.0/my_road.py
@@ -0,0 +1,2 @@
+def my_decimal(expression):
+    return f'{expression:.3f}'
---
+++ packages/my-road-c/my_road_c-1.0.0.tar.gz/my_road_c-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_road_c',
+    version='1.0.0',
+    description='A small library for formatting decimals',
+    py_modules=['my_road'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    license='MIT',
+)
