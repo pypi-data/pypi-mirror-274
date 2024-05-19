2282b0a12b6c891cecffae57e889f169af57c265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:03:43 +0000

    Add package coherent.build/0.9.0/coherent_build-0.9.0.tar.gz

---
+++ packages/coherent.build/coherent_build-0.9.0.tar.gz/coherent_build-0.9.0/README.md
@@ -0,0 +1,5 @@
+Put your source in a repo and then build it with:
+
+```
+python -m coherent.build
+```
---
+++ packages/coherent.build/coherent_build-0.9.0.tar.gz/coherent_build-0.9.0/__main__.py
@@ -0,0 +1,27 @@
+import contextlib
+import importlib.resources
+import logging
+import pathlib
+import runpy
+
+
+@contextlib.contextmanager
+def write_pyproject():
+    path = pathlib.Path('pyproject.toml')
+    if path.exists():
+        yield
+        return
+    path.write_text(importlib.resources.files().joinpath('system.toml').read_text())
+    try:
+        yield
+    finally:
+        path.unlink()
+
+
+def run():
+    logging.basicConfig()
+    with write_pyproject():
+        runpy.run_module('build', run_name='__main__')
+
+
+__name__ == '__main__' and run()
