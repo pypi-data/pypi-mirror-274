87c43629ac28b40221a8a95b078453e34985c740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:47 +0000

    Add package xarizmi/0.2.2/xarizmi-0.2.2-py3-none-any.whl

---
+++ packages/xarizmi/xarizmi-0.2.2-py3-none-any.whl/xarizmi/candlestick.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from pydantic import NonNegativeFloat
+
+
+class Candlestick(BaseModel):
+    close: NonNegativeFloat
+    open: NonNegativeFloat
+    low: NonNegativeFloat
+    high: NonNegativeFloat
