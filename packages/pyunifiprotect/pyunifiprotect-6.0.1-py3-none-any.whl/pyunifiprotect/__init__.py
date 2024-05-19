c3d76e005a272b68314e547b9f7616c296ec7948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:13 +0000

    Add package pyunifiprotect/6.0.1/pyunifiprotect-6.0.1-py3-none-any.whl

---
+++ packages/pyunifiprotect/pyunifiprotect-6.0.1-py3-none-any.whl/pyunifiprotect/__init__.py
@@ -0,0 +1,13 @@
+"""Unofficial UniFi Protect Python API and Command Line Interface."""
+
+from __future__ import annotations
+
+from pyunifiprotect.api import ProtectApiClient
+from pyunifiprotect.exceptions import Invalid, NotAuthorized, NvrError
+
+__all__ = [
+    "Invalid",
+    "NotAuthorized",
+    "NvrError",
+    "ProtectApiClient",
+]
