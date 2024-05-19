e9deee6fa3d840c7705ebbd17b317a9ff930d81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:49 +0000

    Add package config-center/0.0.36/config_center-0.0.36.tar.gz

---
+++ packages/config-center/config_center-0.0.36.tar.gz/config_center-0.0.36/cfgc/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# # # # # # # # # # # # 
+"""
+ ╭───────────────────────────────────────╮  
+ │ __init__.py.py   2024/5/17-21:57
+ ╰───────────────────────────────────────╯ 
+ │ Description:
+    Default import conts
+"""  # [By: HuYw]
+
+# region |- Import -|
+from . import center
+from . import envs
+# endregion
