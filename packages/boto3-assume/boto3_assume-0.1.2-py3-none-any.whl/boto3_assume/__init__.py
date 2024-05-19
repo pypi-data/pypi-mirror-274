0978d417d1fe5485a2b391019e2c0e4d751599c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:36 +0000

    Add package boto3-assume/0.1.2/boto3_assume-0.1.2-py3-none-any.whl

---
+++ packages/boto3-assume/boto3_assume-0.1.2-py3-none-any.whl/boto3_assume/__init__.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+__version__ = "0.1.2"
+__all__ = []
+
+
+try:
+    from boto3_assume.core import assume_role_session
+    __all__.append("assume_role_session")
+except ModuleNotFoundError as error: # pragma: no cover
+    pass
+try:
+    from boto3_assume.aio_core import assume_role_aio_session
+    __all__.append("assume_role_aio_session")
+except ModuleNotFoundError as error: # pragma: no cover
+    pass
