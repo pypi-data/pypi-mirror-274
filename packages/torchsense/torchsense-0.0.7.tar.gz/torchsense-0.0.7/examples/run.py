4138a4b667607b978d5122180d71e407b152e96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:07 +0000

    Add package torchsense/0.0.7/torchsense-0.0.7.tar.gz

---
+++ packages/torchsense/torchsense-0.0.7.tar.gz/torchsense-0.0.7/examples/run.py
@@ -0,0 +1,44 @@
+import torch
+from torchsense.trainer import Trainer
+from torchsense.models.cnn4 import CNN4
+
+from torchsense.datasets.custom import SensorFolder
+from torch.utils.data import DataLoader
+from torchsense.models.gan_g import Generator
+from torchsense import transforms as T
+from torchaudio.transforms import MelScale, Spectrogram
+
+
+def train():
+    # data part
+    data_path = "data1"
+    transform1 = T.Compose([
+        T.ToTensor(),
+        T.Normalize(-1, 1),
+        Spectrogram(n_fft=512, hop_length=160, win_length=256, power=1),
+    ])
+    transform2 = T.Compose([
+        T.ToTensor(),
+        T.Interpolate(5000),
+        Spectrogram(n_fft=100, hop_length=10, win_length=100, power=1),
+    ])
+    data = SensorFolder(root=data_path,
+                        params=(["acc[2]", "mix_mic"], ["mic"]),
+                        transform=[transform2, transform1],
+                        target_transform=transform1)
+    train_set, test_set = data.train_test_split(0.5)
+    train_loader = DataLoader(train_set, batch_size=1, shuffle=True,
+                              drop_last=True, num_workers=0, pin_memory=True)
+    val_loader = DataLoader(test_set, batch_size=1, shuffle=False,
+                            drop_last=True, num_workers=0)
+
+    # model part
+    model = Generator()
+
+    # training part
+    trainer = Trainer(model, max_epochs=5, task="m")
+    trainer.fit(train_loader, val_loader)
+
+
+if __name__ == "__main__":
+    train()
