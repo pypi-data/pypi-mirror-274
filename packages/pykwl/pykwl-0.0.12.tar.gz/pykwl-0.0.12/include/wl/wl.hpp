b8bd46133e38ff4c139b5adde6b52741397eea58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:11 +0000

    Add package pykwl/0.0.12/pykwl-0.0.12.tar.gz

---
+++ packages/pykwl/pykwl-0.0.12.tar.gz/pykwl-0.0.12/include/wl/wl.hpp
@@ -0,0 +1,9 @@
+#ifndef WL_WL_HPP_
+#define WL_WL_HPP_
+
+#include "wl/details/graph.hpp"
+#include "wl/details/weisfeiler_leman.hpp"
+#include "wl/details/weisfeiler_leman_1d.hpp"
+#include "wl/details/weisfeiler_leman_2d.hpp"
+
+#endif
