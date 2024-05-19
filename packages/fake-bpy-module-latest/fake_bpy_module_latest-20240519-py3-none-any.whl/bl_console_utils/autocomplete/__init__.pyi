6027c7a7f0b86148789126abeaa662e5f8755b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:45 +0000

    Add package fake-bpy-module-latest/20240519/fake_bpy_module_latest-20240519-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240519-py3-none-any.whl/bl_console_utils/autocomplete/__init__.pyi
@@ -0,0 +1,8 @@
+import typing
+from . import complete_calltip
+from . import complete_import
+from . import complete_namespace
+from . import intellisense
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240519-py3-none-any.whl/bpy_restrict_state/__init__.pyi
@@ -0,0 +1,14 @@
+import typing
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+class RestrictBlend:
+    context: typing.Any
+    data: typing.Any
+
+class _RestrictContext:
+    preferences: typing.Any
+    window_manager: typing.Any
+
+class _RestrictData: ...
