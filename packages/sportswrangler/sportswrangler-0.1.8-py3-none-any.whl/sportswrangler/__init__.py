d41eb13a1df99a9d344ed938ede9120cdbf35ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:49 +0000

    Add package sportswrangler/0.1.8/sportswrangler-0.1.8-py3-none-any.whl

---
+++ packages/sportswrangler/sportswrangler-0.1.8-py3-none-any.whl/sportswrangler/__init__.py
@@ -0,0 +1,8 @@
+from .odds import *
+from .sports import *
+from .utils import enums
+from .global_configs import default_retry_config
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
