f13868614e1a40c61148386f489a6fb9cebc9ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:04:06 +0000

    Add package torrey/3.2.2.dev20240518180734/torrey-3.2.2.dev20240518180734.tar.gz

---
+++ packages/torrey/torrey-3.2.2.dev20240518180734.tar.gz/torrey-3.2.2.dev20240518180734/pinecone/plugins/example_plugin/__init__.py
@@ -0,0 +1,10 @@
+# from pinecone.interfaces import PineconePlugin, InstallablePlugin
+
+# class SubpackageAPlugin(PineconePlugin):
+#     def __init__(self, api_client_builder, config):
+#         print('Subpackage A Plugin initialized')
+
+#     def hello(self):
+#         print('Hello from Subpackage A Plugin')
+
+# __installables__ = [InstallablePlugin('Pinecone', 'subpackage_a', SubpackageAPlugin)]
