c49880f9b92aa3466fa862cf4010f623a6eb4f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:18 +0000

    Add package packaging-demo-bobo/0.0.2/packaging_demo_bobo-0.0.2.tar.gz

---
+++ packages/packaging-demo-bobo/packaging_demo_bobo-0.0.2.tar.gz/packaging_demo_bobo-0.0.2/packaging_demo/my_file.py
@@ -0,0 +1,10 @@
+import numpy as np
+from my_folder import A
+from my_folder.my_nested_file import CONSTANT as constant2
+
+from packaging_demo.my_other_file import CONSTANT
+
+MY_ARRAY = np.array([1, 2, 3])
+print(MY_ARRAY)
+print(CONSTANT)
+print(constant2)
