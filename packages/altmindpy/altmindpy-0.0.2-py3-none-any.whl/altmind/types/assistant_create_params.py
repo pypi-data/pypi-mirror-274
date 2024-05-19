ba458bb2cf60da36d5d3884c4f447ac58a82f31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 14:03:36 +0000

    Add package altmindpy/0.0.2/altmindpy-0.0.2-py3-none-any.whl

---
+++ packages/altmindpy/altmindpy-0.0.2-py3-none-any.whl/altmind/types/assistant_create_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["AssistantCreateParams"]
+
+
+class AssistantCreateParams(TypedDict, total=False):
+    model: Required[str]
+
+    assistant_metadata: object
+
+    description: Optional[str]
+
+    instructions: Optional[str]
+
+    name: Optional[str]
