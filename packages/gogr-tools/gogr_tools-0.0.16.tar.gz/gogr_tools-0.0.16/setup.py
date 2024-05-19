954b92e4ab312aa826d674a13bee86c4479f3dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:04:15 +0000

    Add package gogr-tools/0.0.16/gogr_tools-0.0.16.tar.gz

---
+++ packages/gogr-tools/gogr_tools-0.0.16.tar.gz/gogr_tools-0.0.16/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.16'
+DESCRIPTION = 'Package for personal use in Data Science'
+LONG_DESCRIPTION = 'Package for personal use in DataS Science, Machine Learning and Deep Learning'
+
+# Setting up
+setup(
+    name="gogr_tools",
+    version=VERSION,
+    author="Grzegorz Gomza",
+    author_email="<gomza.datascience@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['plotly', 'pandas', 'random', 'sys'],
+    keywords=['python', 'Data Science'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
