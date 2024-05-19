f815f85e55b8b1ae1676017460e99d85fcb98d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:03:34 +0000

    Add package pytest-md-report/0.6.2/pytest_md_report-0.6.2.tar.gz

---
+++ packages/pytest-md-report/pytest_md_report-0.6.2.tar.gz/pytest_md_report-0.6.2/setup.py
@@ -0,0 +1,88 @@
+import os.path
+from typing import Dict, Type
+
+import setuptools
+
+
+MODULE_NAME = "pytest-md-report"
+REPOSITORY_URL = f"https://github.com/thombashi/{MODULE_NAME:s}"
+REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info: Dict[str, str] = {}
+
+
+def get_release_command_class() -> Dict[str, Type[setuptools.Command]]:
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME.replace("-", "_"), "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with open("README.rst", encoding=ENCODING) as f:
+    LONG_DESCRIPTION = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    INSTALL_REQUIRES = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    TESTS_REQUIRES = [line.strip() for line in f if line.strip()]
+
+
+setuptools.setup(
+    name=MODULE_NAME,
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description="A pytest plugin to generate test outcomes reports with markdown table format.",
+    include_package_data=True,
+    keywords=["pytest", "plugin", "markdown"],
+    license=pkg_info["__license__"],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
+    package_data={MODULE_NAME: ["py.typed"]},
+    project_urls={
+        "Changlog": f"{REPOSITORY_URL:s}/releases",
+        "Source": REPOSITORY_URL,
+        "Tracker": f"{REPOSITORY_URL:s}/issues",
+    },
+    python_requires=">=3.7",
+    install_requires=INSTALL_REQUIRES,
+    extras_require={"test": TESTS_REQUIRES},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Typing :: Typed",
+    ],
+    cmdclass=get_release_command_class(),
+    zip_safe=False,
+    entry_points={
+        "pytest11": [
+            "pytest-md-report = pytest_md_report.plugin",
+        ]
+    },
+)
