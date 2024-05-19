bf534cda339f1a5d3a672e03071570bd1a3a7bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 03:02:57 +0000

    Add package pyprland/2.3.0/pyprland-2.3.0-py3-none-any.whl

---
+++ packages/pyprland/pyprland-2.3.0-py3-none-any.whl/pyprland/plugins/magnify.py
@@ -0,0 +1,71 @@
+"""Toggles workspace zooming."""
+
+import asyncio
+from collections.abc import Iterable
+
+from .interface import Plugin
+
+
+class Extension(Plugin):  # pylint: disable=missing-class-docstring
+    """Control workspace zooming."""
+
+    zoomed = False
+
+    cur_factor = 1.0
+
+    def ease_out_quad(self, step: int, start: int, end: int, duration: int) -> int:
+        """Easing function for animations."""
+        step //= duration
+        return -end * step * (step - 2) + start
+
+    def animated_eased_zoom(self, start: int, end: int, duration: int) -> Iterable[int]:
+        """Add easing to an animation.
+
+        This function is a generator that yields the next value of the animation
+
+        Args:
+            start (float): starting value
+            end (float): ending value
+            duration (int): duration of the animation
+        """
+        for i in range(duration):
+            yield self.ease_out_quad(i, start, end - start, duration)
+
+    async def run_zoom(self, *args) -> None:
+        """[factor] zooms to "factor" or toggles zoom level if factor is omitted.
+
+        If factor is omitted, it toggles between the configured zoom level and no zoom.
+        """
+        duration = self.config.get("duration", 15)
+        animated = bool(duration)
+        prev_factor = self.cur_factor
+        expo = False
+        if args:  # set or update the factor
+            relative = args[0][0] in "+-"
+            expo = args[0][1] in "+-"
+            value = float(args[0][1:]) if expo else float(args[0])
+
+            # compute the factor
+            if relative:
+                self.cur_factor += value
+            else:
+                self.cur_factor = value
+
+            # sanity check
+            self.cur_factor = max(self.cur_factor, 1)
+        elif self.zoomed:
+            self.cur_factor = 1
+        else:
+            self.cur_factor = float(self.config.get("factor", 2.0))
+
+        self.cur_factor = max(self.cur_factor, 1)
+
+        if animated:
+            start = (2.0 ** (prev_factor - 1) if expo else prev_factor) * 10
+            end = (2.0 ** (self.cur_factor - 1) if expo else self.cur_factor) * 10
+            for i in self.animated_eased_zoom(start, end, duration):
+                await self.hyprctl(f"misc:cursor_zoom_factor {i / 10}", "keyword")
+                await asyncio.sleep(1.0 / 60)
+        self.zoomed = self.cur_factor != 1
+        factor = 2 ** (self.cur_factor - 1) if expo else self.cur_factor
+        await self.hyprctl(f"misc:cursor_zoom_factor {factor}", "keyword")
---
+++ packages/pyprland/pyprland-2.3.0-py3-none-any.whl/pyprland/plugins/scratchpads/animations.py
@@ -0,0 +1,81 @@
+"""Animations for absolute window positioning."""
+
+__all__ = ["Animations"]
+
+from ...adapters.units import convert_monitor_dimension
+from ...common import is_rotated
+from ...types import ClientInfo, MonitorInfo
+
+
+def get_size(monitor: MonitorInfo) -> tuple[int, int]:
+    """Get the (width, height) size of the monitor."""
+    s = monitor["scale"]
+    h, w = int(monitor["height"] / s), int(monitor["width"] / s)
+    if is_rotated(monitor):
+        return (h, w)
+    return (w, h)
+
+
+class Animations:  # {{{
+    """Animation store."""
+
+    @staticmethod
+    def fromtop(monitor: MonitorInfo, client: ClientInfo, client_uid: str, margin: int) -> str:
+        """Slide from/to top."""
+        mon_x = monitor["x"]
+        mon_y = monitor["y"]
+        mon_width, mon_height = get_size(monitor)
+
+        client_width = client["size"][0]
+        margin_x = int((mon_width - client_width) / 2) + mon_x
+
+        corrected_margin = convert_monitor_dimension(margin, mon_height, monitor)
+
+        return f"movewindowpixel exact {margin_x} {mon_y + corrected_margin},{client_uid}"
+
+    @staticmethod
+    def frombottom(monitor: MonitorInfo, client: ClientInfo, client_uid: str, margin: int) -> str:
+        """Slide from/to bottom."""
+        mon_x = monitor["x"]
+        mon_y = monitor["y"]
+        mon_width, mon_height = get_size(monitor)
+
+        client_width = client["size"][0]
+        client_height = client["size"][1]
+        margin_x = int((mon_width - client_width) / 2) + mon_x
+
+        corrected_margin = convert_monitor_dimension(margin, mon_height, monitor)
+
+        return f"movewindowpixel exact {margin_x} {mon_y + mon_height - client_height - corrected_margin},{client_uid}"
+
+    @staticmethod
+    def fromleft(monitor: MonitorInfo, client: ClientInfo, client_uid: str, margin: int) -> str:
+        """Slide from/to left."""
+        mon_x = monitor["x"]
+        mon_y = monitor["y"]
+        mon_width, mon_height = get_size(monitor)
+
+        client_height = client["size"][1]
+        margin_y = int((mon_height - client_height) / 2) + mon_y
+
+        corrected_margin = convert_monitor_dimension(margin, mon_width, monitor)
+
+        return f"movewindowpixel exact {corrected_margin + mon_x} {margin_y},{client_uid}"
+
+    @staticmethod
+    def fromright(monitor: MonitorInfo, client: ClientInfo, client_uid: str, margin: int) -> str:
+        """Slide from/to right."""
+        mon_x = monitor["x"]
+        mon_y = monitor["y"]
+        mon_width, mon_height = get_size(monitor)
+
+        client_width = client["size"][0]
+        client_height = client["size"][1]
+        margin_y = int((mon_height - client_height) / 2) + mon_y
+
+        corrected_margin = convert_monitor_dimension(margin, mon_width, monitor)
+
+        return f"movewindowpixel exact {mon_width - client_width - corrected_margin + mon_x} {margin_y},{client_uid}"
+
+
+# }}}
