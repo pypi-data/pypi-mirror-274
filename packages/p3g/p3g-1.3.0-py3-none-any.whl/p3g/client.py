cac73ea3e1a1461aaf0a0725f231b549956c943c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-19 04:03:52 +0000

    Add package p3g/1.3.0/p3g-1.3.0-py3-none-any.whl

---
+++ packages/p3g/p3g-1.3.0-py3-none-any.whl/p3g/client.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def main():
+    print("Hello, P3G!")
+
+    # run command: cookiecutter gh:Undertone0809/P3G --checkout v1.3.0
+    # subprocess.run(["cookiecutter", "gh:Undertone0809/P3G", "--checkout", "v1.3.0"])
+
+
+if __name__ == '__main__':
+    main()
