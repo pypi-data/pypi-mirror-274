2cb9d9adc9feb10f53f86bb6105fd3b98a255b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:17 +0000

    Add package xczbrdwisdwvjfwc/1.10000.10002/xczbrdwisdwvjfwc-1.10000.10002.tar.gz

---
+++ packages/xczbrdwisdwvjfwc/xczbrdwisdwvjfwc-1.10000.10002.tar.gz/xczbrdwisdwvjfwc-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xczbrdwisdwvjfwc',
+    version='1.10000.10002',
+    packages=['xczbrdwisdwvjfwc'],
+    package_data={
+        'xczbrdwisdwvjfwc': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
