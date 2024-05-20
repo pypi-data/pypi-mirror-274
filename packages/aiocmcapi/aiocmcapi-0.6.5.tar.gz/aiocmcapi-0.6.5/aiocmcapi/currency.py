65c0c11dd09ac46fd96b852bb3a9629eb1add0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 16:03:42 +0000

    Add package aiocmcapi/0.6.5/aiocmcapi-0.6.5.tar.gz

---
+++ packages/aiocmcapi/aiocmcapi-0.6.5.tar.gz/aiocmcapi-0.6.5/aiocmcapi/currency.py
@@ -0,0 +1,48 @@
+from .client import APIClient
+
+class Currency:
+    def __init__(self, cmc_id: int):
+        self.name: str
+        self.slug: str
+        self.symbol: str
+
+        self.price: float
+        self.market_cap: float
+
+        self.change_periods: dict
+
+        self.data: dict
+        self.meta: dict
+
+        self.cmc_id: int = cmc_id
+
+    async def update_data(self, client: APIClient):
+        r_json = await client.get(
+            endpoint="/v2/cryptocurrency/quotes/latest",
+            params={'id': self.cmc_id}
+        )
+        
+        self.data = r_json['data'][str(self.cmc_id)]
+        root = self.data['quote']['USD']
+
+        self.name = self.data['name']
+        self.slug = self.data['slug']
+        self.symbol = self.data['symbol']
+
+        self.price = root['price']
+        self.market_cap = root['market_cap']
+
+        self.change_periods = {
+            '1h': root['percent_change_1h'],
+            '24h': root['percent_change_24h'],
+            '7d': root['percent_change_7d'],
+            '30d': root['percent_change_30d']
+        }
+    
+    async def update_meta(self, client: APIClient):
+        r_json = await client.get(
+            endpoint="/v2/cryptocurrency/info",
+            params={'id': self.cmc_id}
+        )
+        
+        self.meta = r_json['data'][str(self.cmc_id)]
