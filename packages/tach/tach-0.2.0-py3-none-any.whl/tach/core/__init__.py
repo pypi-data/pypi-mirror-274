9384d6d3393d728437db943a47d3f69c81407856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:54 +0000

    Add package tach/0.2.0/tach-0.2.0-py3-none-any.whl

---
+++ packages/tach/tach-0.2.0-py3-none-any.whl/tach/core/__init__.py
@@ -0,0 +1,10 @@
+from tach.core.config import PackageConfig, TagDependencyRules, ProjectConfig
+from tach.core.package import PackageNode, PackageTrie
+
+__all__ = [
+    "PackageConfig",
+    "TagDependencyRules",
+    "ProjectConfig",
+    "PackageNode",
+    "PackageTrie",
+]
