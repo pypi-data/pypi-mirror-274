dc122494c13945ca01e62af5c2f1138786a9afc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:49 +0000

    Add package xdmhcylxseklcbcx/1.10000.10002/xdmhcylxseklcbcx-1.10000.10002.tar.gz

---
+++ packages/xdmhcylxseklcbcx/xdmhcylxseklcbcx-1.10000.10002.tar.gz/xdmhcylxseklcbcx-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xdmhcylxseklcbcx',
+    version='1.10000.10002',
+    packages=['xdmhcylxseklcbcx'],
+    package_data={
+        'xdmhcylxseklcbcx': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
