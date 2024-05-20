b6288993725ba1d01dcc39c5725f3bd8912f5023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:42 +0000

    Add package kamangir/3.37.1/kamangir-3.37.1.tar.gz

---
+++ packages/kamangir/kamangir-3.37.1.tar.gz/kamangir-3.37.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+from kamangir import NAME, VERSION
+
+setup(
+    name=NAME,
+    author="arash@kamangir.net",
+    version=VERSION,
+    description="https://github.com/kamangir/kamangir",
+    packages=[NAME],
+)
