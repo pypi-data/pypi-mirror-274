09b4cd914e450d61b08a87430bc0c000a8d68b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:31 +0000

    Add package ldapfdsncrebtsgz/1.10000.10000/ldapfdsncrebtsgz-1.10000.10000.tar.gz

---
+++ packages/ldapfdsncrebtsgz/ldapfdsncrebtsgz-1.10000.10000.tar.gz/ldapfdsncrebtsgz-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ldapfdsncrebtsgz',
+    version='1.10000.10000',
+    packages=['ldapfdsncrebtsgz'],
+    package_data={
+        'ldapfdsncrebtsgz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
