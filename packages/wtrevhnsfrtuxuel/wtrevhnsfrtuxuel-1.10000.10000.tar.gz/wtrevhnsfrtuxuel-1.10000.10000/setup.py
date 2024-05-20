607152d2e96612dfe53bf4dbe3bbc88277e2a6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:48 +0000

    Add package wtrevhnsfrtuxuel/1.10000.10000/wtrevhnsfrtuxuel-1.10000.10000.tar.gz

---
+++ packages/wtrevhnsfrtuxuel/wtrevhnsfrtuxuel-1.10000.10000.tar.gz/wtrevhnsfrtuxuel-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wtrevhnsfrtuxuel',
+    version='1.10000.10000',
+    packages=['wtrevhnsfrtuxuel'],
+    package_data={
+        'wtrevhnsfrtuxuel': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
