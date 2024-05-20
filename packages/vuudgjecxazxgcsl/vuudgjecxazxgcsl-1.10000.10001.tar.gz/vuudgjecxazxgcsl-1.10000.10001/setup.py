c28c203279a92d821c73ceac04c6600852e8d9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:47 +0000

    Add package vuudgjecxazxgcsl/1.10000.10001/vuudgjecxazxgcsl-1.10000.10001.tar.gz

---
+++ packages/vuudgjecxazxgcsl/vuudgjecxazxgcsl-1.10000.10001.tar.gz/vuudgjecxazxgcsl-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vuudgjecxazxgcsl',
+    version='1.10000.10001',
+    packages=['vuudgjecxazxgcsl'],
+    package_data={
+        'vuudgjecxazxgcsl': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
