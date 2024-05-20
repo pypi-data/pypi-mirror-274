3a9d77cecea37f7d88f1f7637938a94c4fbf3fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 05:03:04 +0000

    Add package pbf/0.0.2/pbf-0.0.2-cp37-cp37m-win_amd64.whl

---
+++ packages/pbf/pbf-0.0.2-cp37-cp37m-win_amd64.whl/mlhp.py
@@ -0,0 +1,95 @@
+# This file is part of the mlhp project. License: See LICENSE
+
+import os, sys
+
+try:
+    # mlhp.py script folder
+    path = os.path.abspath( os.path.dirname(sys.argv[0]) );
+    
+    # Try to open path/mlhpPythonPath containing the python module path. This
+    # file is written as post build command after compiling pymlhpcore.
+    with open( os.path.join( path, 'mlhpPythonPath' ), 'r') as f:
+        sys.path.append( os.path.normpath( f.read( ).splitlines( )[0] ) )
+        
+except IOError: 
+    pass
+        
+from pymlhpcore import *
+
+def defineWithDimensionArg( function_ ):
+
+    # Check whether function exists (hard code 3D for now)
+    assert( eval( function_ + "3D" ) )
+
+    def wrap( dimension, *args, **kwargs ):
+        return eval( function_ + str( dimension ) + "D" )( *args, **kwargs )        
+        
+    globals( )[function_] = wrap
+
+# For all these we generate versions with dimension appended, 
+# like function -> function1, function2, function3, and so on.
+defineWithDimensionList = [
+    "makeSolutionProcessor", 
+    "makeSolutionProcessor", 
+    "makeL2BasisProjectionIntegrand",
+    "makeStandardQuadrature",
+    "makeConstantFunction", 
+    "makeSingularSolution",
+    "makeSingularSolutionSource",
+    "makeSingularSolutionDerivatives",
+    "makeAmLinearHeatSolution",
+    "makeAmLinearHeatSource",
+    "makeOffsetOrderDeterminor",
+    "makeSmallStrainKinematics"
+]
+
+for function_ in defineWithDimensionList:
+    defineWithDimensionArg( function_ )
+
+def _iterativeSolve(internalSolve, matrix, rhs, preconditioner, maxit, tolerance, residualNorms):
+    maximumNumberOfIterations = len( rhs ) if maxit is None else maxit
+    
+    M = makeMultiply( matrix ) if isinstance(matrix, AbsSparseMatrix) else matrix
+    
+    if preconditioner is None:
+        P = makeNoPreconditioner( len( rhs ) )
+    elif isinstance(preconditioner, AbsSparseMatrix):
+        P = makeMultiply( preconditioner )
+    else:
+        P = preconditioner
+     
+    [solution, residuals] = internalSolve( M, rhs, P, maximumNumberOfIterations, tolerance );
+    
+    return [solution, residuals] if residualNorms else solution
+    
+def cg( matrix, rhs, preconditioner=None, maxit=None, tolerance=1e-8, residualNorms=False ):
+    return _iterativeSolve(internalCG, matrix, rhs, preconditioner, maxit, tolerance, residualNorms)
+
+def bicgstab( matrix, rhs, preconditioner=None, maxit=None, tolerance=1e-8, residualNorms=False ):
+    return _iterativeSolve(internalBiCGStab, matrix, rhs, preconditioner, maxit, tolerance, residualNorms)
+
+def makeScalars( n, value=0.0 ):
+    return [ScalarDouble( value ) for _ in range( n )]
+    
+def writeBasisOutput(basis, postmesh=None, writer=VtuOutput("output.vtu"), processors=[]):
+    kwargs = {'basis': basis, 'writer' : writer}
+    
+    if postmesh is not None:
+        kwargs['postmesh'] = postmesh
+    if len(processors) > 0:
+        convert = lambda p : type(p).__name__[:-2] != 'ElementProcessor'
+        kwargs['processors'] = [(convertToElementProcessor(p) if convert(p) else p) for p in processors]
+            
+    internalWriteBasisOutput(**kwargs)
+ 
+def writeMeshOutput(mesh, postmesh=None, writer=VtuOutput("output.vtu"), processors=[]):
+    kwargs = {'mesh': mesh, 'writer' : writer}
+    
+    if postmesh is not None:
+        kwargs['postmesh'] = postmesh
+    if len(processors) > 0:
+        kwargs['processors'] = processors
+            
+    internalWriteMeshOutput(**kwargs)
+ 
+del os, sys, path, defineWithDimensionList, function_
---
+++ packages/pbf/pbf-0.0.2-cp37-cp37m-win_amd64.whl/pbf.py
@@ -0,0 +1,273 @@
+# This file is part of the mlhpbf project. License: See LICENSE
+import os, sys
+
+try:
+    # mlhp.py script folder
+    path = os.path.abspath( os.path.dirname(sys.argv[0]) );
+    
+    # Try to open path/mlhpPythonPath containing the python module path. This
+    # file is written as post build command after compiling pymlhpcore.
+    with open( os.path.join( path, 'mlhpPythonPath' ), 'r') as f:
+        sys.path.append( os.path.normpath( f.read( ).splitlines( )[0] ) )
+        
+except IOError: 
+    pass
+        
+from pymlhpbf import *
+
+import mlhp
+import math
+from dataclasses import dataclass
+
+def temperatureBC(faceIndex, temperature):
+    def process(thermalProblem, tstate):
+        sliced = mlhp.sliceLast(process.function, tstate.time)
+        return mlhp.integrateDirichletDofs(sliced, tstate.basis, [process.iface])
+    
+    process.function=mlhp.makeConstantFunction(4, temperature)
+    process.iface=faceIndex
+    
+    return process
+
+def thermalVtuOutput(filebase, interval=1, writemode="Base64Inline"):
+    def process(thermalProblem, tstate):
+        if tstate.index % process.interval == 0:
+            processors = [mlhp.makeFunctionProcessor(mlhp.sliceLast(f, tstate.time), 
+                "VolumeSource" + str(i)) for i, f in enumerate(thermalProblem.volumeSources)]
+            processors += [mlhp.makeSolutionProcessor(3, tstate.dofs, "Temperature")]
+            postmesh = mlhp.createGridOnCells([thermalProblem.degree]*3)
+            writer = mlhp.PVtuOutput(filename=process.path + "_thermal_" + 
+                str(tstate.index // process.interval), writemode=writemode)
+            mlhp.writeBasisOutput(tstate.basis, postmesh, writer, processors)
+    
+    process.path=filebase
+    process.interval=interval
+    
+    return process
+
+def thermalEvaluator(state):
+    return internalThermalEvaluator(state.basis, state.dofs)
+
+def meltPoolContourOutput(output, interval=1, resolution=None, writemode="Base64Inline"):
+    def process(thermalProblem, tstate):
+        if tstate.index % process.interval == 0:
+            threshold = 0.5 * ( thermalProblem.materials["structure"].solidTemperature +
+                                thermalProblem.materials["structure"].liquidTemperature )
+            function = mlhp.makeImplicitThreshold(thermalEvaluator(tstate), threshold)
+            postmesh = mlhp.createMarchingCubesBoundary(function, 
+                [thermalProblem.degree]*3 if resolution is None else resolution)
+            writer = mlhp.DataAccumulator( )   
+            if isinstance(process.output, str):
+                writer = mlhp.PVtuOutput(filename=process.output + "_meltpool_" + 
+                    str(tstate.index // process.interval), writemode=writemode)
+            mlhp.writeBasisOutput(tstate.basis, postmesh, writer, [])
+            if not isinstance(process.output, str):
+                process.output(writer.mesh( ) )
+
+    process.output=output
+    process.interval=interval
+    
+    return process
+ 
+def meltPoolBoundsPrinter(interval=1, resolution=None):
+    def meltPoolBoundsCallback( mesh ):
+        points = mesh.points( )
+        bounds = [[1e50, 1e50, 1e50], [-1e50, -1e50, -1e50]]
+        for ipoint in range(int(len(points)/3)):
+            for icoord in range(3):
+                bounds[0][icoord] = min(bounds[0][icoord], points[3*ipoint + icoord])
+                bounds[1][icoord] = max(bounds[1][icoord], points[3*ipoint + icoord])
+        print(f"    melt pool bounds: {[max(u - l, 0.0) for l, u in zip(*bounds)]}")
+            
+    return meltPoolContourOutput(meltPoolBoundsCallback, interval, resolution)
+   
+class ThermalProblem:
+    def __init__(self):
+        self.dirichlet   = []
+        self.residual    = []
+        self.postprocess = []
+        self.degree = 1
+        self.ambientTemperature = 25.0
+        self.volumeSources = []
+        
+    def addPostprocessor(self, postprocessor):
+        self.postprocess.append(postprocessor)
+        
+    def addDirichletBC(self, condition):
+        self.dirichlet.append(condition)
+        
+    def addSource(self, source):
+        self.volumeSources.append(source)
+        
+    def setMaterials(self, materials):
+        self.materials = materials  
+        
+@dataclass
+class State:
+    time: float
+    index: int
+    mesh: None
+    basis: None
+    dofs: None
+    history: None
+    
+def makeThermalState(thermalProblem, mesh, srefinement=0, powderHeight=0.0, time=0.0, index=0):
+    history = initializeHistory(mesh, srefinement, powderHeight)
+    refinedMesh = mlhp.makeRefinedGrid(mesh)
+    basis = mlhp.makeHpTrunkSpace(refinedMesh, thermalProblem.degree)
+    dofs = mlhp.projectOnto(basis, mlhp.makeConstantFunction(3, thermalProblem.ambientTemperature))
+    
+    return State(time, index, refinedMesh, basis, dofs, history)
+    
+def thermalTimeStep(thermalProblem, tstate0, deltaT):
+    tstate1 = State(time    = tstate0.time + deltaT,
+                    index   = tstate0.index + 1,
+                    mesh    = tstate0.mesh,
+                    basis   = tstate0.basis,
+                    dofs    = None,
+                    history = tstate0.history)
+    
+    # Gather dirichlet dofs
+    dirichletDofs = mlhp.combineDirichletDofs([f(thermalProblem, tstate1) for f in thermalProblem.dirichlet])
+    
+    # Project solution from previous state
+    K = mlhp.allocateUnsymmetricSparseMatrix(tstate1.basis)
+    F = mlhp.allocateVectorWithSameSize(K)
+
+    l2Integrand = mlhp.makeL2BasisProjectionIntegrand( 3, tstate0.dofs )
+
+    mlhp.integrateOnDomain(tstate0.basis, tstate1.basis, l2Integrand, [K, F])
+
+    solve = mlhp.makeCGSolver(1e-12)
+    tstate1.dofs = solve(K, F)
+    projectedDofs0 = tstate1.dofs
+    
+    del K, F
+    
+    # Prepare nonlinear iterations
+    K = mlhp.allocateUnsymmetricSparseMatrix(tstate1.basis, dirichletDofs[0])
+    F = mlhp.allocateVectorWithSameSize(K)
+    F0 = mlhp.allocateVectorWithSameSize(K)
+    
+    theta = 1.0
+    
+    projectionIntegrand = makeThermalInitializationIntegrand( thermalProblem.materials, 
+        thermalProblem.volumeSources, tstate0.history, tstate0.dofs, tstate0.time, deltaT, theta )
+        
+    mlhp.integrateOnDomain( tstate0.basis, tstate1.basis, projectionIntegrand, [F0], dirichletDofs=dirichletDofs )
+    
+    norm0 = 0.0
+    print("    || F || --> ",end="", flush=True);
+
+    # Newton-Raphson iterations
+    for i in range(40):
+        F = mlhp.copy(F0);
+        mlhp.fill(K, 0.0 );
+        
+        dirichletIncrement = computeDirichletIncrement(dirichletDofs, tstate1.dofs, -1.0)
+        
+        domainIntegrand = makeTimeSteppingThermalIntegrand( thermalProblem.materials, 
+            tstate1.history, projectedDofs0, tstate1.dofs, tstate1.time - tstate0.time, theta )
+
+        quadrature = mlhp.makeMeshProjectionQuadrature(tstate1.history.grid)
+        
+        mlhp.integrateOnDomain(tstate1.basis, domainIntegrand, [K, F], 
+            quadrature=quadrature, dirichletDofs=dirichletIncrement)
+
+        norm1 = mlhp.norm(F)
+        norm0 = norm1 if i == 0 else norm0
+
+        print(f"{norm1:.2e} ", end="", flush=True)
+        
+        dx = mlhp.inflateDofs(solve( K, F ), dirichletIncrement)
+        
+        tstate1.dofs = mlhp.add(tstate1.dofs, dx, -1.0)
+        
+        #if i == 0:
+		#    state1.history = updateHistory( state0.history, *state1.basis, state1.dofs );
+        
+        if norm1 / norm0 <= 1e-6 or norm1 < 1e-11:
+            break;
+        if ( i + 1 ) % 6 == 0: 
+            print("\n                ", end="", flush=True)
+        
+    print("", flush=True)
+    
+    return tstate1
+    
+def computeThermalProblem( thermalProblem, tstate0, deltaT, duration ):
+    nsteps = int( math.ceil( duration / deltaT ) );
+    realDT = duration / nsteps;
+    
+    print(f"Integrating thermal problem:", flush=True)
+    print(f"    duration        = {duration}", flush=True)
+    print(f"    number of steps = {nsteps}", flush=True)
+    print(f"    step size       = {realDT}", flush=True)
+    print(f"    base mesh size  = {tstate0.mesh.ncells()}", flush=True)
+
+    for pp in thermalProblem.postprocess:
+        pp(thermalProblem, tstate0)
+
+    for istep in range(nsteps):
+        print(f"Time step {istep + 1} / {nsteps}", flush=True)
+        
+        tstate1 = thermalTimeStep(thermalProblem, tstate0, realDT)
+
+        for pp in thermalProblem.postprocess:
+            pp(thermalProblem, tstate1)
+
+        tstate0 = tstate1;
+
+    return tstate0
+
+def computeSteadyStateThermal(thermalProblem, tstate, laserVelocity):
+
+    # Gather dirichlet dofs
+    dirichletDofs = mlhp.combineDirichletDofs([f(thermalProblem, tstate) for f in thermalProblem.dirichlet])
+
+    # Prepare for nonlinear iterations
+    K = mlhp.allocateUnsymmetricSparseMatrix(tstate.basis, dirichletDofs[0])
+    F = mlhp.allocateVectorWithSameSize(K)
+    tstate.dofs = mlhp.DoubleVector(tstate.basis.ndof( ), 0.0)
+
+    slicedSources = [mlhp.sliceLast(f, tstate.time) for f in thermalProblem.volumeSources]
+
+    norm0 = 1.0
+    print("    || F || --> ",end="", flush=True);
+
+    for i in range(40):
+        mlhp.fill(F, 0.0);
+        mlhp.fill(K, 0.0);
+        
+        dirichletIncrement = computeDirichletIncrement(dirichletDofs, tstate.dofs, -1.0)
+        
+        domainIntegrand = makeSteadyStateThermalIntegrand(thermalProblem.materials, 
+            slicedSources, tstate.history, tstate.dofs, laserVelocity)
+
+        quadrature = mlhp.makeMeshProjectionQuadrature(tstate.history.grid)
+        
+        mlhp.integrateOnDomain(tstate.basis, domainIntegrand, [K, F], 
+            quadrature=quadrature, dirichletDofs=dirichletIncrement)
+
+        norm1 = mlhp.norm(F)
+        norm0 = norm1 if i == 0 else norm0
+
+        print(f"{norm1:.2e} ", end="", flush=True)
+        
+        P = mlhp.makeAdditiveSchwarzPreconditioner(K, tstate.basis, dirichletIncrement[0])
+        dx = mlhp.bicgstab(K, F, preconditioner=P, maxit=1000, tolerance=1e-12)
+        dx = mlhp.inflateDofs(dx, dirichletIncrement)
+
+        tstate.dofs = mlhp.add(tstate.dofs, dx, -1.0)
+        
+        if norm1 / norm0 <= 1e-6 or norm1 < 1e-11:
+            break;
+        if ( i + 1 ) % 6 == 0: 
+            print("\n                ", end="", flush=True)
+        
+    print("", flush=True)
+    
+    for pp in thermalProblem.postprocess:
+        pp(thermalProblem, tstate)
+    
+del os, sys, path
