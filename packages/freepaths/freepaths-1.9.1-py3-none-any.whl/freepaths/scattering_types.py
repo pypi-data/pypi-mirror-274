23abf64659f3dd55c5ce680e0a36b45a66c2dbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:59 +0000

    Add package freepaths/1.9.1/freepaths-1.9.1-py3-none-any.whl

---
+++ packages/freepaths/freepaths-1.9.1-py3-none-any.whl/freepaths/scattering_types.py
@@ -0,0 +1,59 @@
+"""
+Module that provides types of phonon scattering that might occur.
+These scattering types are returned on each step
+so that higher level modules know what has happened.
+"""
+
+import enum
+
+
+class Scattering(enum.Enum):
+    """Possible scattering types"""
+    DIFFUSE = 1
+    SPECULAR = 2
+
+
+class ScatteringTypes:
+    """Phonon scattering types"""
+
+    def __init__(self):
+        """Initialize possible scattering type"""
+        self.holes = None
+        self.pillars = None
+        self.top_bottom = None
+        self.walls = None
+        self.internal = None
+        self.hot_side = None
+
+    @property
+    def is_diffuse(self):
+        """Is any of the scattering types diffuse?"""
+        return any([self.holes == Scattering.DIFFUSE,
+                    self.pillars == Scattering.DIFFUSE,
+                    self.top_bottom == Scattering.DIFFUSE,
+                    self.walls == Scattering.DIFFUSE,
+                    self.hot_side == Scattering.DIFFUSE])
+
+    @property
+    def is_internal(self):
+        """Is any of the scattering types diffuse?"""
+        return self.internal is not None
+
+    @property
+    def is_scattered(self):
+        """Has any of the scattering events occurred?"""
+        return any([self.holes,
+                    self.pillars,
+                    self.top_bottom,
+                    self.walls,
+                    self.internal,
+                    self.hot_side])
+
+    def reset(self):
+        """Reset all scattering types to None"""
+        self.holes = None
+        self.pillars = None
+        self.top_bottom = None
+        self.walls = None
+        self.internal = None
+        self.hot_side = None
