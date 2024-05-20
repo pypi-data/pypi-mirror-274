136236a524038fd4f1fdd74da084626e603cd54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:50 +0000

    Add package lwwpjkupzdaatanb/1.10000.10005/lwwpjkupzdaatanb-1.10000.10005.tar.gz

---
+++ packages/lwwpjkupzdaatanb/lwwpjkupzdaatanb-1.10000.10005.tar.gz/lwwpjkupzdaatanb-1.10000.10005/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lwwpjkupzdaatanb',
+    version='1.10000.10005',
+    packages=['lwwpjkupzdaatanb'],
+    package_data={
+        'lwwpjkupzdaatanb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
