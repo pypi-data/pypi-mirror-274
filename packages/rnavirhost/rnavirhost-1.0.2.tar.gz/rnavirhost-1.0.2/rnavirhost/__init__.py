7283576983f21b24d05000d6b653b91748260e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:04:01 +0000

    Add package rnavirhost/1.0.2/rnavirhost-1.0.2.tar.gz

---
+++ packages/rnavirhost/rnavirhost-1.0.2.tar.gz/rnavirhost-1.0.2/rnavirhost/__init__.py
@@ -0,0 +1,11 @@
+"""
+RNAVirHost: a machine learning-based method for predicting hosts of RNA viruses through viral genomes
+"""
+
+from rnavirhost.modules import (
+    classify_order,
+    predict,
+)
+from rnavirhost import rnavirhost
+
+__version__ = "1.0.2"
