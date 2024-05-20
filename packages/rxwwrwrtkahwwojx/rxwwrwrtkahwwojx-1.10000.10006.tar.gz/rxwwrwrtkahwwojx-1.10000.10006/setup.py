5e9a912a48c3088964a513b26f7a3a4a93d03ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:04:07 +0000

    Add package rxwwrwrtkahwwojx/1.10000.10006/rxwwrwrtkahwwojx-1.10000.10006.tar.gz

---
+++ packages/rxwwrwrtkahwwojx/rxwwrwrtkahwwojx-1.10000.10006.tar.gz/rxwwrwrtkahwwojx-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rxwwrwrtkahwwojx',
+    version='1.10000.10006',
+    packages=['rxwwrwrtkahwwojx'],
+    package_data={
+        'rxwwrwrtkahwwojx': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
