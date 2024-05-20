fefb4b79df1b896ce7c371fa921268408434698d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:38 +0000

    Add package Nara/0.2/Nara-0.2-py3-none-any.whl

---
+++ packages/Nara/Nara-0.2-py3-none-any.whl/Nara/__init__.py
@@ -0,0 +1,12 @@
+import collections
+import os
+import pdb
+try:
+    import pdbp  # (Pdb+) --- Python Debugger Plus
+except Exception:
+    pass
+import sys
+
+from Nara.nara.GenFunc import CreateFunc
+from Nara.nara.GenTemplate import CreateTemplate
+from Nara.nara.init import init
