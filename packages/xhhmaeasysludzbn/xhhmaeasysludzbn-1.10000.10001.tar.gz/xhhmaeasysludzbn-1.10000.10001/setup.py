778bdd7bdd2a766eaf5f5d35e6c44c507d99a02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:32 +0000

    Add package xhhmaeasysludzbn/1.10000.10001/xhhmaeasysludzbn-1.10000.10001.tar.gz

---
+++ packages/xhhmaeasysludzbn/xhhmaeasysludzbn-1.10000.10001.tar.gz/xhhmaeasysludzbn-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xhhmaeasysludzbn',
+    version='1.10000.10001',
+    packages=['xhhmaeasysludzbn'],
+    package_data={
+        'xhhmaeasysludzbn': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
