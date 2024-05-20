46731f2a7b9ad559fce72702a8cee53e0450eeaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:36 +0000

    Add package checkrepublic/0.1.1/checkrepublic-0.1.1.tar.gz

---
+++ packages/checkrepublic/checkrepublic-0.1.1.tar.gz/checkrepublic-0.1.1/README.md
@@ -0,0 +1,2 @@
+# Check Republic: A Python library for lightweight runtime invariant checking, efficient immutable value objects, and backwards-compatibility-centric, streaming serialization/deserialization.
+
---
+++ packages/checkrepublic/checkrepublic-0.1.1.tar.gz/checkrepublic-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="checkrepublic",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Nick Schrock",
+    description="A lightweight python library for runtime invariant checking.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/checkrepublic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
