8458cdf22949dbebed8e41fb7e7a9592eba7ffe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:04:05 +0000

    Add package pipy-test-xiaoqiang/1.1.0/pipy-test-xiaoqiang-1.1.0.tar.gz

---
+++ packages/pipy-test-xiaoqiang/pipy-test-xiaoqiang-1.1.0.tar.gz/pipy-test-xiaoqiang-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipy-test-xiaoqiang",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'click',  # 添加 click 作为依赖
+    ],
+    entry_points={
+        'console_scripts': [
+            'my_project_init=my_project.cli:init',  # 使用命令组前缀
+        ],
+    },
+    author="Ma Xiaoqiang",
+    author_email="851788096@qq.com",
+    description="A short description of your project",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/my_project",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+)
