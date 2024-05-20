f033233e17a7fd71c3b9148794cfb46be5139ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:53 +0000

    Add package ycwongrvrvecyvvn/1.10000.10000/ycwongrvrvecyvvn-1.10000.10000.tar.gz

---
+++ packages/ycwongrvrvecyvvn/ycwongrvrvecyvvn-1.10000.10000.tar.gz/ycwongrvrvecyvvn-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ycwongrvrvecyvvn',
+    version='1.10000.10000',
+    packages=['ycwongrvrvecyvvn'],
+    package_data={
+        'ycwongrvrvecyvvn': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
