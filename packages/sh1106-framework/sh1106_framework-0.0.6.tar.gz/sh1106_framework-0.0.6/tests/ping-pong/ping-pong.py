ac88d5829a9257228529a2c6f34656066c29de5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:50 +0000

    Add package sh1106-framework/0.0.6/sh1106_framework-0.0.6.tar.gz

---
+++ packages/sh1106-framework/sh1106_framework-0.0.6.tar.gz/sh1106_framework-0.0.6/tests/ping-pong/ping-pong.py
@@ -0,0 +1,20 @@
+from src.sh1106_framework.framework.states.state_manager import StateManager
+from sh1106_framework.sh1106_framework import SH1106Framework
+from ping import PingPage
+
+def __init():
+    SH1106Framework.register_font("default", "assets/default_font.json")
+    SH1106Framework.register_images("assets/icons.json")
+    
+    SH1106Framework.register_routes(
+        default_route="splash",
+        routes={
+            "splash": PingPage(StateManager)
+        }
+    )
+    
+    SH1106Framework.begin(port=1, address=0x3C)
+    pass
+    
+if __name__ == "__main__":
+    __init()
