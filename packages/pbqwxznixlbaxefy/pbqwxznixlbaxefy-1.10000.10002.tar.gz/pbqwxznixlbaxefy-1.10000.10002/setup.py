bde790859e61979f9817c6dc9957ffdd94d2cba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:04:02 +0000

    Add package pbqwxznixlbaxefy/1.10000.10002/pbqwxznixlbaxefy-1.10000.10002.tar.gz

---
+++ packages/pbqwxznixlbaxefy/pbqwxznixlbaxefy-1.10000.10002.tar.gz/pbqwxznixlbaxefy-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pbqwxznixlbaxefy',
+    version='1.10000.10002',
+    packages=['pbqwxznixlbaxefy'],
+    package_data={
+        'pbqwxznixlbaxefy': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
