39ea69985e6104faa680faf549baf7bb7a1f1a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:52 +0000

    Add package khonagdklfixvizi/1.10000.10000/khonagdklfixvizi-1.10000.10000.tar.gz

---
+++ packages/khonagdklfixvizi/khonagdklfixvizi-1.10000.10000.tar.gz/khonagdklfixvizi-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='khonagdklfixvizi',
+    version='1.10000.10000',
+    packages=['khonagdklfixvizi'],
+    package_data={
+        'khonagdklfixvizi': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
