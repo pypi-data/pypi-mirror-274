cd421846bfef1ba197d135db6c08c2d8613aab5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:41 +0000

    Add package DFPlayer/0.1.2/dfplayer-0.1.2.tar.gz

---
+++ packages/DFPlayer/dfplayer-0.1.2.tar.gz/dfplayer-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='DFPlayer',
+    packages=find_packages(exclude=['tests', 'assets']),
+    version='0.1.2',
+    description='Python3 library for the DFPlayer-mini',
+    author='Dianudi',
+    url="https://github.com/dianudi/dfplayer-py",
+    keywords=['dfplayer', 'dfplayer-mini', 'mp3-module'],
+    license='MIT',
+    requires=['pyserial'],
+)
