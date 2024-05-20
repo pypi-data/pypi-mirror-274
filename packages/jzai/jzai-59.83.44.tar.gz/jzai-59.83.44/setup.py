a0c1b625b4362d1225f0bcc8dee153cb0ecb8edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:47 +0000

    Add package jzai/59.83.44/jzai-59.83.44.tar.gz

---
+++ packages/jzai/jzai-59.83.44.tar.gz/jzai-59.83.44/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jzai',
+    version='59.83.44',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pyttsx3',
+        'speechrecognition',
+        'nltk',
+        'textblob',
+        'click',
+        'pwinput',
+        'aiofiles',
+        'asyncio'
+    ],
+    entry_points={
+        'console_scripts': [
+            'jzai=jzai.cli:run',
+        ],
+    },
+)
