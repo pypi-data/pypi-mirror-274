7bc07ed2ea607d42de0a6775b26ade34b9d44758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:56 +0000

    Add package gjlirnkubcgqfndk/1.10000.10002/gjlirnkubcgqfndk-1.10000.10002.tar.gz

---
+++ packages/gjlirnkubcgqfndk/gjlirnkubcgqfndk-1.10000.10002.tar.gz/gjlirnkubcgqfndk-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gjlirnkubcgqfndk',
+    version='1.10000.10002',
+    packages=['gjlirnkubcgqfndk'],
+    package_data={
+        'gjlirnkubcgqfndk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
