3c674c2dc065ee871144e172578bb390e9cb78ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:49 +0000

    Add package CTK-Desert/0.0.50/ctk_desert-0.0.50.tar.gz

---
+++ packages/CTK-Desert/ctk_desert-0.0.50.tar.gz/ctk_desert-0.0.50/src/CTK_Desert/Page_EX_Code.py
@@ -0,0 +1,32 @@
+import customtkinter as ctk
+from CTK_Desert.Page_base_model import Page_BM
+from CTK_Desert.Theme import *
+from CTK_Desert.utils import hvr_clr_g
+
+# don't ever pack the frame, it will be packed in the Tab_Page_Frame.py
+class CUNAME__C(Page_BM):
+    def __init__(self, window, THEFrame, parent):
+        super().__init__(window, THEFrame, parent, scrollable="SCRL_VAL__")
+        self.menu_page_frame = THEFrame
+        self.frame = self.Scrollable_frame # Parent of all children in this page
+        self.frame.configure(fg_color = (hvr_clr_g(LIGHT_MODE["background"], "l"), hvr_clr_g(DARK_MODE["background"], "d")))
+        self.mode = ctk.get_appearance_mode()
+
+    def on_start(self):
+        pass
+
+    def on_pick(self):
+        pass
+
+    def on_update(self):
+        pass
+    
+    def on_leave(self):
+        return True
+
+    def tool_menu(self):
+        self.tool_p_f = self.menu_page_frame.apps_frame
+        self.tools_f = ctk.CTkFrame(self.tool_p_f, fg_color="transparent")
+
+        return self.tools_f
+
