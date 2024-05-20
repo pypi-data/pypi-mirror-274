90c9cf1beb3c21afdaa11f75d736ca8abdb2ce1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:38 +0000

    Add package redis-director/0.6.0/redis_director-0.6.0-py3-none-any.whl

---
+++ packages/redis-director/redis_director-0.6.0-py3-none-any.whl/redis_director/constants.py
@@ -0,0 +1,29 @@
+INCREMENT_LUA_SCRIPT = """
+local key = KEYS[1]
+local member = ARGV[1]
+local increment = tonumber(ARGV[2])
+local minScore = tonumber(ARGV[4])
+local maxScore = tonumber(ARGV[5])
+
+local currentScore = redis.call('zscore', key, member)
+
+if not currentScore then
+    currentScore = tonumber(ARGV[3])
+else
+    currentScore = tonumber(currentScore)
+end
+
+local newScore = currentScore + increment
+
+if newScore < minScore then
+    newScore = minScore
+
+elseif newScore > maxScore then
+    newScore = maxScore
+
+end
+
+redis.call('zadd', key, newScore, member)
+
+return newScore
+"""
