a35bea1a6ea447934bd245fbeb31deb442a11a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:04:02 +0000

    Add package comet-toolbox/0.4.1/comet_toolbox-0.4.1.tar.gz

---
+++ packages/comet-toolbox/comet_toolbox-0.4.1.tar.gz/comet_toolbox-0.4.1/example_multiverse/universe_5.py
@@ -0,0 +1,14 @@
+import numpy as np
+import bct
+import comet
+
+print(f"Decision 1: 2")
+
+    
+# Load example data and calculate dFC + local efficiency
+ts = comet.data.load_example()
+dfc = comet.methods.SlidingWindow(ts, **{'windowsize': 29, 'shape': 'rectangular', 'std': 10.0, 'diagonal': 0, 'standardize': False, 'fisher_z': False, 'tril': False}).connectivity()
+dfc = dfc[0] if isinstance(dfc, tuple) else dfc #required as LeiDA returns multiple outputs
+    
+W = dfc[:,:,100]
+W_t = comet.graph.threshold(W, **{'type': 'density', 'threshold': 0.0, 'density': 0.5})
