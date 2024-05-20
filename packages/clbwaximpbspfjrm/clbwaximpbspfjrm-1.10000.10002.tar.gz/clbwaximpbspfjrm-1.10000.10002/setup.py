c74247aac9bb4616176dde296e430a9794e1f76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:20 +0000

    Add package clbwaximpbspfjrm/1.10000.10002/clbwaximpbspfjrm-1.10000.10002.tar.gz

---
+++ packages/clbwaximpbspfjrm/clbwaximpbspfjrm-1.10000.10002.tar.gz/clbwaximpbspfjrm-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='clbwaximpbspfjrm',
+    version='1.10000.10002',
+    packages=['clbwaximpbspfjrm'],
+    package_data={
+        'clbwaximpbspfjrm': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
