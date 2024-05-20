ebb37b3070588f2f320c02898eb04b6070a2e5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:18 +0000

    Add package SQLConn/0.0.12/sqlconn-0.0.12.tar.gz

---
+++ packages/SQLConn/sqlconn-0.0.12.tar.gz/sqlconn-0.0.12/SQLConn/__init__.py
@@ -0,0 +1,7 @@
+from .SQLConn import MSSQLConn
+from .SQLConn import MYSQLConn
+from .SQLConn import PostgresqlConn
+from .SQLConn import SQLiteConn
+from .SQLConn import SQLConn
+
+__all__ = ["MSSQLConn", "MYSQLConn", "PostgresqlConn", "SQLiteConn","SQLConn"]
