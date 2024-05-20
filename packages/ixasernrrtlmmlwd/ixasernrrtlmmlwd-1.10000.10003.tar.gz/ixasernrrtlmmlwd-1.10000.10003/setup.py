c0e593dc86ac9c471a310341df23a60e74489cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:58 +0000

    Add package ixasernrrtlmmlwd/1.10000.10003/ixasernrrtlmmlwd-1.10000.10003.tar.gz

---
+++ packages/ixasernrrtlmmlwd/ixasernrrtlmmlwd-1.10000.10003.tar.gz/ixasernrrtlmmlwd-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ixasernrrtlmmlwd',
+    version='1.10000.10003',
+    packages=['ixasernrrtlmmlwd'],
+    package_data={
+        'ixasernrrtlmmlwd': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
