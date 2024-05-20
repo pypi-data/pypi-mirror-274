2a76ab5941d803e89a8455cac0db360650d576e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:47 +0000

    Add package py92/1.0.3/py92-1.0.3-py3-none-any.whl

---
+++ packages/py92/py92-1.0.3-py3-none-any.whl/py92/__init__.py
@@ -0,0 +1,76 @@
+import py92.py92utils
+import py92.config
+
+py92utils = py92.py92utils.py92utils()
+
+
+def is_in(name):
+    return name in py92utils.is_in(name)
+
+
+def is_ins(names):
+    return py92utils.is_ins(names)
+
+
+def is_985(name):
+    return py92utils.is_985(name)
+
+
+def is_985s(names):
+    return py92utils.is_985s(names)
+
+
+def is_211(name):
+    return py92utils.is_211(name)
+
+
+def is_211s(names):
+    return py92utils.is_211s(names)
+
+
+def is_db1(name):
+    return py92utils.is_db1(name)
+
+
+def is_db1s(names):
+    return py92utils.is_db1s(names)
+
+
+def is_university(name):
+    return py92utils.is_university(name)
+
+
+def is_universitys(names):
+    return py92utils.is_universitys(names)
+
+
+def is_public(name):
+    return py92utils.is_public(name)
+
+
+def is_publics(names):
+    return py92utils.is_publics(names)
+
+
+def get_highest_label(name):
+    return py92utils.get_highest_label(name)
+
+
+def get_highest_labels(names):
+    return py92utils.get_highest_labels(names)
+
+
+def get_label(name):
+    return py92utils.get_label(name)
+
+
+def get_labels(names):
+    return py92utils.get_labels(names)
+
+
+def get_qs(name):
+    return py92utils.get_qs(name)
+
+
+def get_qss(names):
+    return py92utils.get_qss(names)
