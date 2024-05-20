465f807d5dfeb6926211348d8117c84cbca4eade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:33 +0000

    Add package yytqgfulvpbdkbjb/1.10000.10001/yytqgfulvpbdkbjb-1.10000.10001.tar.gz

---
+++ packages/yytqgfulvpbdkbjb/yytqgfulvpbdkbjb-1.10000.10001.tar.gz/yytqgfulvpbdkbjb-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='yytqgfulvpbdkbjb',
+    version='1.10000.10001',
+    packages=['yytqgfulvpbdkbjb'],
+    package_data={
+        'yytqgfulvpbdkbjb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
