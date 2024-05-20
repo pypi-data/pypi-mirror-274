f3f01033d47f367f2e0212986e4f6982eac435ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 16:03:35 +0000

    Add package arclet-entari/0.4.1/arclet_entari-0.4.1.tar.gz

---
+++ packages/arclet-entari/arclet_entari-0.4.1.tar.gz/arclet_entari-0.4.1/arclet/entari/core.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import asyncio
+from contextlib import suppress
+
+from arclet.letoderea import BaseAuxiliary, Contexts, EventSystem, Provider, ProviderFactory, global_providers
+from loguru import logger
+from satori.client import App
+from satori.client.account import Account
+from satori.client.session import Session
+from satori.config import Config
+from satori.model import Event
+
+from .event import MessageEvent, event_parse
+from .plugin import dispatchers
+from .session import ContextSession
+
+
+class SessionProvider(Provider[Session]):
+    async def __call__(self, context: Contexts):
+        if account := context.get("$account"):
+            return account.session
+
+
+class ContextSessionProvider(Provider[ContextSession]):
+    async def __call__(self, context: Contexts):
+        if "$origin_event" in context and "$account" in context:
+            return ContextSession(context["$account"], context["$origin_event"])
+
+
+global_providers.extend([SessionProvider(), ContextSessionProvider()])
+
+
+class Entari(App):
+    id = "entari.service"
+
+    def __init__(self, *configs: Config):
+        super().__init__(*configs)
+        self.event_system = EventSystem()
+        self.register(self.handle_event)
+        self._ref_tasks = set()
+        # self.lifecycle(self.handle_lifecycle)
+
+    def on(
+        self,
+        *events: type,
+        priority: int = 16,
+        auxiliaries: list[BaseAuxiliary] | None = None,
+        providers: list[Provider | type[Provider] | ProviderFactory | type[ProviderFactory]] | None = None,
+    ):
+        return self.event_system.on(*events, priority=priority, auxiliaries=auxiliaries, providers=providers)
+
+    def on_message(
+        self,
+        priority: int = 16,
+        auxiliaries: list[BaseAuxiliary] | None = None,
+        providers: list[Provider | type[Provider] | ProviderFactory | type[ProviderFactory]] | None = None,
+    ):
+        return self.event_system.on(
+            MessageEvent, priority=priority, auxiliaries=auxiliaries, providers=providers
+        )
+
+    async def handle_event(self, account: Account, event: Event):
+        async def event_parse_task(connection: Account, raw: Event):
+            loop = asyncio.get_running_loop()
+            with suppress(NotImplementedError):
+                ev = event_parse(connection, raw)
+                self.event_system.publish(ev)
+                for disp in dispatchers.values():
+                    if not disp.validate(ev):
+                        continue
+                    task = loop.create_task(disp.publish(ev))
+                    self._ref_tasks.add(task)
+                    task.add_done_callback(self._ref_tasks.discard)
+                return
+
+            logger.warning(f"received unsupported event {raw.type}: {raw}")
+
+        await event_parse_task(account, event)
+
+    # async def handle_lifecycle(self, account: Account, state: LoginStatus):
+    # if state == LoginStatus.ONLINE:
+    #     route = Selector().land(account.platform).account(account.self_id)
+    #     _account = SatoriAccount(route, self.protocol)
+    #     self.protocol.avilla.accounts[route] = AccountInfo(
+    #         route, _account, self.protocol, platform(account.platform)
+    #     )
+    #     self.protocol.avilla.broadcast.postEvent(AccountRegistered(self.protocol.avilla, _account))
+    #     self._accounts[account.identity] = _account
+    #     _account.client = account
+    # elif state == LoginStatus.CONNECT:
+    #     _account = self._accounts[account.identity]
+    #     self.protocol.avilla.broadcast.postEvent(AccountAvailable(self.protocol.avilla, _account))
+    #     _account.client = account
+    #     _account.status.enabled = True
+    # elif state == LoginStatus.DISCONNECT:
+    #     _account = self._accounts[account.identity]
+    #     _account.status.enabled = False
+    #     self.protocol.avilla.broadcast.postEvent(AccountUnavailable(self.protocol.avilla, _account))
+    # elif state == LoginStatus.OFFLINE:
+    #     _account = self._accounts[account.identity]
+    #     _account.status.enabled = False
+    #     self.protocol.avilla.broadcast.postEvent(AccountUnregistered(self.protocol.avilla, _account))
+    #     with suppress(KeyError):
+    #         del self._accounts[account.identity]
+    #         del self.protocol.avilla.accounts[_account.route]
