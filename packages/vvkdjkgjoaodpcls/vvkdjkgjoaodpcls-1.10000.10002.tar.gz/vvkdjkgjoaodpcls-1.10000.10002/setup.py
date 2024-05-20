324efdb383f55bdd0ebe56f65b1c38c97bf9474a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:55 +0000

    Add package vvkdjkgjoaodpcls/1.10000.10002/vvkdjkgjoaodpcls-1.10000.10002.tar.gz

---
+++ packages/vvkdjkgjoaodpcls/vvkdjkgjoaodpcls-1.10000.10002.tar.gz/vvkdjkgjoaodpcls-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vvkdjkgjoaodpcls',
+    version='1.10000.10002',
+    packages=['vvkdjkgjoaodpcls'],
+    package_data={
+        'vvkdjkgjoaodpcls': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
