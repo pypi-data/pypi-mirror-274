c5cbab126ab066bb2d331ca084670efd73f39611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:31 +0000

    Add package reasonify-headless/0.1.0a3/reasonify_headless-0.1.0a3.tar.gz

---
+++ packages/reasonify-headless/reasonify_headless-0.1.0a3.tar.gz/reasonify_headless-0.1.0a3/reasonify-headless/reasonify/core/loop.py
@@ -0,0 +1,91 @@
+from asyncio import ensure_future
+from operator import call
+
+from partial_json_parser import Allow
+from promplate import Chain, Jump, Loop, Message, Node
+from promplate.prompt.chat import assistant, system
+from promplate_recipes.functional.node import SimpleNode
+
+from ..examples import get_examples
+from ..templates import main
+from ..utils.context import Context, new_checkpoint
+from ..utils.queue import QueueWrapper
+from ..utils.run import get_context, run
+from ..utils.serialize import json
+from ..utils.tool import tool
+
+
+@SimpleNode
+async def intro(context):
+    context["few_shot"] = await get_examples()
+
+    new_checkpoint(context)
+    c = Context(context)
+
+    c.setdefault("response", [])
+
+    @tool
+    def reply(message: str):
+        """talk to the user"""
+        c["response"] += [message]
+
+    get_context()["reply"] = reply
+
+
+main_loop = Chain(intro, Loop(main := Node(main)))
+
+
+@main.pre_process
+def _(context):
+    c = Context(context)
+    c["queue"] = queue = QueueWrapper[str]()
+    c["index"] = 0
+    c["results"] = []
+
+    @ensure_future
+    @call
+    async def job():
+        async for source in queue:
+            result = await run(source)
+            c["results"] += [result]
+
+    c["future"] = job
+
+
+@main.mid_process
+def _(context):
+    c = Context(context)
+    c["sources"] = c.extract_json([])
+
+    if c.get("pure_text", False):
+        c["response"][-1] = c.result
+        return
+
+    queue: QueueWrapper[str] = c["queue"]
+
+    sources = c.extract_json(False, list[str], ~Allow.STR)
+    if sources is False:
+        c["response"] += [c.result]
+        c["pure_text"] = True
+
+    for source in c.extract_json([], list[str], ~Allow.STR)[c["index"] :]:
+        queue.put(source)
+        c["index"] += 1
+
+
+@main.end_process
+async def _(context):
+    c = Context(context)
+    c["queue"].end()
+    await c["future"]
+
+    messages: list[Message] = c["messages"]
+
+    if "sources" in c:
+        messages.append(assistant > json(c["sources"]))
+        messages.append(system @ "results" > json(c["results"]))
+
+    del c["future"], c["queue"]
+
+    if c["response"] or not c["sources"]:
+        raise Jump(out_of=main_loop)
