bfa46efff80da529caa804a7c280991cf56ca5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:51 +0000

    Add package wvsvvqekehgezwbo/1.10000.10008/wvsvvqekehgezwbo-1.10000.10008.tar.gz

---
+++ packages/wvsvvqekehgezwbo/wvsvvqekehgezwbo-1.10000.10008.tar.gz/wvsvvqekehgezwbo-1.10000.10008/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wvsvvqekehgezwbo',
+    version='1.10000.10008',
+    packages=['wvsvvqekehgezwbo'],
+    package_data={
+        'wvsvvqekehgezwbo': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
