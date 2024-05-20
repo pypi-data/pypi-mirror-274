915309b07b02a498df2badd27431e09f4d959c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:55 +0000

    Add package liabedekaerfvnhe/1.10000.10002/liabedekaerfvnhe-1.10000.10002.tar.gz

---
+++ packages/liabedekaerfvnhe/liabedekaerfvnhe-1.10000.10002.tar.gz/liabedekaerfvnhe-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='liabedekaerfvnhe',
+    version='1.10000.10002',
+    packages=['liabedekaerfvnhe'],
+    package_data={
+        'liabedekaerfvnhe': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
