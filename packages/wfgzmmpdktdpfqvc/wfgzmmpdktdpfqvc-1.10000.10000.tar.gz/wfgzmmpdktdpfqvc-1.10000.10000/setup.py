c05fa900199c5728f207be7ff8b44fd82c77f9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:51 +0000

    Add package wfgzmmpdktdpfqvc/1.10000.10000/wfgzmmpdktdpfqvc-1.10000.10000.tar.gz

---
+++ packages/wfgzmmpdktdpfqvc/wfgzmmpdktdpfqvc-1.10000.10000.tar.gz/wfgzmmpdktdpfqvc-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wfgzmmpdktdpfqvc',
+    version='1.10000.10000',
+    packages=['wfgzmmpdktdpfqvc'],
+    package_data={
+        'wfgzmmpdktdpfqvc': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
