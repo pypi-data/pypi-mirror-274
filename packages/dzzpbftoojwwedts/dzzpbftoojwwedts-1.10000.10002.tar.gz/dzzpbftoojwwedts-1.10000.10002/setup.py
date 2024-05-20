dcae08f9ff7762dd684f4f1b5e44b8902b250af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:58 +0000

    Add package dzzpbftoojwwedts/1.10000.10002/dzzpbftoojwwedts-1.10000.10002.tar.gz

---
+++ packages/dzzpbftoojwwedts/dzzpbftoojwwedts-1.10000.10002.tar.gz/dzzpbftoojwwedts-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dzzpbftoojwwedts',
+    version='1.10000.10002',
+    packages=['dzzpbftoojwwedts'],
+    package_data={
+        'dzzpbftoojwwedts': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
