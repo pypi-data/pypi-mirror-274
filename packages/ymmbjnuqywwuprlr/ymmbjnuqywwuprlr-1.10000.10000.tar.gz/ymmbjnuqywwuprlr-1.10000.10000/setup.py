40668458554144110aeb5ec607a641ac9fb7f6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:24 +0000

    Add package ymmbjnuqywwuprlr/1.10000.10000/ymmbjnuqywwuprlr-1.10000.10000.tar.gz

---
+++ packages/ymmbjnuqywwuprlr/ymmbjnuqywwuprlr-1.10000.10000.tar.gz/ymmbjnuqywwuprlr-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ymmbjnuqywwuprlr',
+    version='1.10000.10000',
+    packages=['ymmbjnuqywwuprlr'],
+    package_data={
+        'ymmbjnuqywwuprlr': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
