aac1420ce99f2aa9b9da0caa6b2bed3b59eeb031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:37 +0000

    Add package rjdikuqfrrrranka/1.10000.10000/rjdikuqfrrrranka-1.10000.10000.tar.gz

---
+++ packages/rjdikuqfrrrranka/rjdikuqfrrrranka-1.10000.10000.tar.gz/rjdikuqfrrrranka-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rjdikuqfrrrranka',
+    version='1.10000.10000',
+    packages=['rjdikuqfrrrranka'],
+    package_data={
+        'rjdikuqfrrrranka': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
