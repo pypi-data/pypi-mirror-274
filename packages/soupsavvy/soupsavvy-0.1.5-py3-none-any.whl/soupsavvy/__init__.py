d71ee54206b8f1bd8bd7c329959dbe9fe491d714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:42 +0000

    Add package soupsavvy/0.1.5/soupsavvy-0.1.5-py3-none-any.whl

---
+++ packages/soupsavvy/soupsavvy-0.1.5-py3-none-any.whl/soupsavvy/__init__.py
@@ -0,0 +1,20 @@
+from .tags import (
+    AnyTagSelector,
+    AttributeSelector,
+    DescendantCombinator,
+    PatternSelector,
+    SelectorList,
+    TagSelector,
+)
+
+__version__ = "0.1.5"
+__author__ = "sewcio543"
+
+__all__ = [
+    "AnyTagSelector",
+    "AttributeSelector",
+    "TagSelector",
+    "PatternSelector",
+    "SelectorList",
+    "DescendantCombinator",
+]
