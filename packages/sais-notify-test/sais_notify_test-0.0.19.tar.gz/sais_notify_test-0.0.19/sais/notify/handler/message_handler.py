58d5c73452d7e48e45d4914fdb3e84234ccf717b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 16:03:46 +0000

    Add package sais-notify-test/0.0.19/sais_notify_test-0.0.19.tar.gz

---
+++ packages/sais-notify-test/sais_notify_test-0.0.19.tar.gz/sais_notify_test-0.0.19/sais/notify/handler/message_handler.py
@@ -0,0 +1,48 @@
+import logging
+
+from sais.notify.auth.auth_info import EnvVarCredentialsProvider
+from sais.notify.clients.notify_client import NotifyClient
+from sais.notify.config import const
+from sais.notify.config.const import ENDPOINT, NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU, \
+    NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU_GROUP, LOGGER_NAME
+from sais.notify.model.message_model import NotificationRequest, MessageModel
+from sais.notify.types import NotifyType
+
+logger = logging.getLogger(LOGGER_NAME)
+
+
+class MessageHandler(object):
+    def __init__(self, auth_provider: EnvVarCredentialsProvider):
+        self.auth_provider = auth_provider
+
+    def send_notification(self, notify_type: NotifyType, to: str, message: str) -> bool:
+        request = NotificationRequest(notify_type=notify_type, to=to, message=message)
+        request.model_dump()
+        if notify_type in (NotifyType.FEISHU_USER_TEXT, NotifyType.FEISHU_USER_RICH_TEXT,
+                           NotifyType.FEISHU_GROUP_TEXT, NotifyType.FEISHU_GROUP_MESSAGE_CARD):
+            return self.__send_notification_feishu(notify_type, to, message)
+        else:
+            logger.error(f'not support notify type: {notify_type}')
+        return False
+
+    def __send_notification_feishu(self, notify_type: NotifyType, to: str, message: str) -> bool:
+        client = NotifyClient(ENDPOINT, self.auth_provider)
+        message_model = MessageModel(
+            message=message,
+            to=to,
+            type={
+                NotifyType.FEISHU_USER_TEXT: NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU,
+                NotifyType.FEISHU_USER_RICH_TEXT: NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU,
+                NotifyType.FEISHU_GROUP_TEXT: NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU_GROUP,
+                NotifyType.FEISHU_GROUP_MESSAGE_CARD: NOTIFY_SERVICE_NOTIFY_TYPE_FEISHU_GROUP,
+            }.get(notify_type, None)
+        )
+
+        robot_name = const.SAIS_ROBOT_NAME if notify_type == NotifyType.FEISHU_USER_RICH_TEXT else None
+        message_model.robot_name = robot_name
+
+        if message_model.type is None:
+            logger.error(f'not support notify type: {notify_type}')
+            return False
+
+        return client.send_notification_feishu(message_model)
