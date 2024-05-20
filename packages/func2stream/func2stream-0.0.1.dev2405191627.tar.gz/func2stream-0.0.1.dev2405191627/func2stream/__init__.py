3200574dcba23267ed3b6c72ea35d432cab8db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:24 +0000

    Add package func2stream/0.0.1.dev2405191627/func2stream-0.0.1.dev2405191627.tar.gz

---
+++ packages/func2stream/func2stream-0.0.1.dev2405191627.tar.gz/func2stream-0.0.1.dev2405191627/func2stream/__init__.py
@@ -0,0 +1,11 @@
+from core import Pipeline, Element, MapReduce, build_ctx, from_ctx
+from video import VideoSource
+
+__all__ = [
+    'Pipeline',
+    'VideoSource',
+    'Element',
+    'MapReduce',
+    'build_ctx',
+    'from_ctx'
+]
