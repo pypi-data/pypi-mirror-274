4ed12cc6093ab3a406408cd9b7a22cc292b43b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:49 +0000

    Add package sfbuymbbpknnbaum/1.10000.10004/sfbuymbbpknnbaum-1.10000.10004.tar.gz

---
+++ packages/sfbuymbbpknnbaum/sfbuymbbpknnbaum-1.10000.10004.tar.gz/sfbuymbbpknnbaum-1.10000.10004/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sfbuymbbpknnbaum',
+    version='1.10000.10004',
+    packages=['sfbuymbbpknnbaum'],
+    package_data={
+        'sfbuymbbpknnbaum': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
