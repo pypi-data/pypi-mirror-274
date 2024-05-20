a9539325c6b9375d303b5836ed581c10f5603638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:42 +0000

    Add package kjzuurvlmuukeqeu/1.10000.10000/kjzuurvlmuukeqeu-1.10000.10000.tar.gz

---
+++ packages/kjzuurvlmuukeqeu/kjzuurvlmuukeqeu-1.10000.10000.tar.gz/kjzuurvlmuukeqeu-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kjzuurvlmuukeqeu',
+    version='1.10000.10000',
+    packages=['kjzuurvlmuukeqeu'],
+    package_data={
+        'kjzuurvlmuukeqeu': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
