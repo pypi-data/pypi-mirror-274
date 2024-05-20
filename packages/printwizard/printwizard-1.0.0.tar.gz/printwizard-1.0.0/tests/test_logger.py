1abce97efd69f2cd1313370b8dd31b723ffda56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:21 +0000

    Add package printwizard/1.0.0/printwizard-1.0.0.tar.gz

---
+++ packages/printwizard/printwizard-1.0.0.tar.gz/printwizard-1.0.0/tests/test_logger.py
@@ -0,0 +1,15 @@
+from printwizard import Logger
+
+
+logger = Logger({
+    "include_timestamp": False,
+})
+
+
+# set_wizard_config(
+#     {
+#         "include_timestamp": False,
+#     }
+# )
+
+logger.success("Success Statement")
