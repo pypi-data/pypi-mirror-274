02283f512aea53553161cc67473818928ff7ddc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:47 +0000

    Add package ramses_rf/0.31.21/ramses_rf-0.31.21-py3-none-any.whl

---
+++ packages/ramses_rf/ramses_rf-0.31.21-py3-none-any.whl/ramses_rf/version.py
@@ -0,0 +1,4 @@
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser (application layer)."""
+
+__version__ = "0.31.21"
+VERSION = __version__
---
+++ packages/ramses_rf/ramses_rf-0.31.21-py3-none-any.whl/ramses_tx/version.py
@@ -0,0 +1,4 @@
+"""RAMSES RF - a RAMSES-II protocol decoder & analyser (transport layer)."""
+
+__version__ = "0.31.21"
+VERSION = __version__
