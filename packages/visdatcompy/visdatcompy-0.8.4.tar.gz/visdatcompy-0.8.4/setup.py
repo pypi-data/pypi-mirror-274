b84b17641fa0b17b93058a8573ca574cb1fdc732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:45 +0000

    Add package visdatcompy/0.8.4/visdatcompy-0.8.4.tar.gz

---
+++ packages/visdatcompy/visdatcompy-0.8.4.tar.gz/visdatcompy-0.8.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="visdatcompy",
+    version="0.8.4",
+    python_requires=">=3.12",
+    author="cls, Reape4er, Adg1r",
+    author_email="ferjenkill@gmail.com",
+    description="Библиотека для сравнения визуальных наборов данных",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cloudysock/visdatcompy",
+    packages=find_packages(),
+    install_requires=requirements,
+)
---
+++ packages/visdatcompy/visdatcompy-0.8.4.tar.gz/visdatcompy-0.8.4/visdatcompy/__init__.py
@@ -0,0 +1,5 @@
+from visdatcompy.feature_extractor import *
+from visdatcompy.hash import *
+from visdatcompy.image_handler import *
+from visdatcompy.metrics import *
+from visdatcompy.visdatcompare import *
