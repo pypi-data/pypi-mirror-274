9d72986f3e72e0c78d1fc80e79fab07b14af74eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:43 +0000

    Add package reverse-life/0.0.1/reverse_life-0.0.1.tar.gz

---
+++ packages/reverse-life/reverse_life-0.0.1.tar.gz/reverse_life-0.0.1/README.md
@@ -0,0 +1,23 @@
+# 使用说明
+
+```python
+import sys  
+sys.path.append('D:/Roche/code/roche_db')
+
+from roche_db.data_processing import map2db
+
+
+output_path = 'test_data/temp.xlsx' # output可以是一个新文件，可以是原来的DB
+
+input_path = '../data/MD/22年9月-23年8月产品注册数据-众成数科-20230920.xlsx' # input是MD的数据
+map2db(input_path, output_path, '常规审批')
+
+# input_path = '../data/MD/创新2022.xlsx'
+# map2db(input_path, output_path, '创新审批')
+
+# input_path = '../data/MD/优先审批_20230919173353_1595699578026229761.xls'
+# map2db(input_path, output_path, '优先审批')
+
+# input_path = '../data/MD/创新审批_20230907_MD.xls'
+# map2db(input_path, output_path, '创新审批')
+```
---
+++ packages/reverse-life/reverse_life-0.0.1.tar.gz/reverse_life-0.0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/17 01:12
+# @Name    : setup.py
+# @Author  : yanlee
+
+
+
+import setuptools
+import shutil
+
+# 删除dist/目录
+shutil.rmtree('dist', ignore_errors=True)
+
+setuptools.setup(
+    name="reverse_life",
+    version="0.0.1",
+    author="yanjlee",
+    author_email="yanjlee@163.com",  # 作者邮箱
+    description="reverse tools",  # 模块简介
+    install_requires=[
+        'requests',
+        'faker',
+        'execjs',
+        'loguru',
+        'base64',
+        'hashlib',
+        'Crypto',
+    ],
+    long_description=open(r'readme.md', encoding='utf-8').read(),  # 读取readme自述文件
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanjlee/reverse_life",  # 模块github地址
+    packages=setuptools.find_packages(),     # 自动列出项目下的包
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",   # 开源许可证
+        "Operating System :: OS Independent",      # 这里的定义是系统无关（全平台兼容），如果你的包只能在部分特定系统上运行，需要修改。
+    ],
+)
