c9d88704eb92ce45413ba53664b3ae445578a755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:11 +0000

    Add package lsx/0.3.5/lsx-0.3.5.tar.gz

---
+++ packages/lsx/lsx-0.3.5.tar.gz/lsx-0.3.5/lsx/np.py
@@ -0,0 +1,46 @@
+"""Image Library Functions"""
+
+import os
+
+
+__all__ = list(globals())
+
+
+def imread(file):
+    import cv2
+    import numpy as np
+    return cv2.imdecode(np.fromfile(file, np.uint8), -1)
+
+
+def imwrite(file, img):
+    import cv2
+    cv2.imencode('.jpg', img)[1].tofile(file)
+
+
+def imshow(img, delay=50, title=''): # for test
+    import cv2
+    cv2.imshow('', img)
+    cv2.setWindowTitle('', title)
+    cv2.waitKey(delay)
+
+
+def imsave(file): # for test
+    import cv2
+    folder = os.path.splitext(file)[0]
+    os.makedirs(folder, exist_ok=True)
+    for n, img in enumerate(imiter(file)):
+        cv2.imwrite('%s/img_%04d.png' % (folder, n), img)
+
+
+def imiter(file_or_id):
+    import cv2
+    cap = cv2.VideoCapture(file_or_id)
+    while cap.isOpened():
+        ret, img = cap.read()
+        if not ret:
+            cap.release()
+            return
+        yield img
+
+
+__all__ = [k for k in globals() if k not in __all__]
