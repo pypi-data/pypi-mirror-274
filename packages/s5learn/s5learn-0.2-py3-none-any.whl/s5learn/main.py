a26a096b368c6ea9ed37692a3ac205153e25e83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:47 +0000

    Add package s5learn/0.2/s5learn-0.2-py3-none-any.whl

---
+++ packages/s5learn/s5learn-0.2-py3-none-any.whl/s5learn/main.py
@@ -0,0 +1,12 @@
+import os
+
+def prints():
+    with open("example.txt", "r") as file:
+        # Read the contents of the file
+        file_contents = file.read()
+        # Print the contents
+        print(file_contents)
+    print("Lets do this sindikuku X zeez X bur X mi X te X lu")
+
+
+prints()
