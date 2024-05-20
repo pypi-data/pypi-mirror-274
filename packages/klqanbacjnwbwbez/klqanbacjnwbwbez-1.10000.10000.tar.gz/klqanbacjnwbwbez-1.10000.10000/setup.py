741c380b247ac5581300c6ca3a0cf8e7e41f8f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:48 +0000

    Add package klqanbacjnwbwbez/1.10000.10000/klqanbacjnwbwbez-1.10000.10000.tar.gz

---
+++ packages/klqanbacjnwbwbez/klqanbacjnwbwbez-1.10000.10000.tar.gz/klqanbacjnwbwbez-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='klqanbacjnwbwbez',
+    version='1.10000.10000',
+    packages=['klqanbacjnwbwbez'],
+    package_data={
+        'klqanbacjnwbwbez': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
