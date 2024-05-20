2175860dd70cd766ac4efda10c10d65e54953a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:34 +0000

    Add package ogijinsczpfvpyjr/1.10000.10001/ogijinsczpfvpyjr-1.10000.10001.tar.gz

---
+++ packages/ogijinsczpfvpyjr/ogijinsczpfvpyjr-1.10000.10001.tar.gz/ogijinsczpfvpyjr-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ogijinsczpfvpyjr',
+    version='1.10000.10001',
+    packages=['ogijinsczpfvpyjr'],
+    package_data={
+        'ogijinsczpfvpyjr': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
