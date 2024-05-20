ec45abb393ece68d8e03be1e837b0332c7478fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:37 +0000

    Add package Flask-App-Install/0.0.1/Flask_App_Install-0.0.1-py3-none-any.whl

---
+++ packages/Flask-App-Install/Flask_App_Install-0.0.1-py3-none-any.whl/Flask_App_Install-0.0.1.data/scripts/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the directory of the currently executing script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Set the PYTHONPATH to include the script directory
+export PYTHONPATH="${PYTHONPATH}:${SCRIPT_DIR}"
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Run the Python script
+python writefile_app.py
+
+# Deactivate the virtual environment
+deactivate
