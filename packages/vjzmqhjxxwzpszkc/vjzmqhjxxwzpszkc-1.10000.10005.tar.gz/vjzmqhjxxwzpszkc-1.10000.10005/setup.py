1b3ec7ef0ec0b561f8a4ee975ff8670dd0b11d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:38 +0000

    Add package vjzmqhjxxwzpszkc/1.10000.10005/vjzmqhjxxwzpszkc-1.10000.10005.tar.gz

---
+++ packages/vjzmqhjxxwzpszkc/vjzmqhjxxwzpszkc-1.10000.10005.tar.gz/vjzmqhjxxwzpszkc-1.10000.10005/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vjzmqhjxxwzpszkc',
+    version='1.10000.10005',
+    packages=['vjzmqhjxxwzpszkc'],
+    package_data={
+        'vjzmqhjxxwzpszkc': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
