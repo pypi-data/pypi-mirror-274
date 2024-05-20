4cbe61e38077fe855405e4cf337e1e30b4dd19d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:04:16 +0000

    Add package pygamepal/0.6.3/pygamepal-0.6.3.tar.gz

---
+++ packages/pygamepal/pygamepal-0.6.3.tar.gz/pygamepal-0.6.3/src/pygamepal/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.6.3"
+__author__ = 'Rik Cross'
+__license__ = 'MIT'
+
+import pygame
+
+from .game import *
+from .scene import *
+from .sprite import *
+
+from .input import *
+from .camera import *
+from .spriteImage import *
+from .collider import *
+from .trigger import *
+from .button import *
+from .particles import *
+from .transition import *
+
+from .drawText import *
+from .spriteTextureList import *
+from .splitTexture import *
+from .flatten import *
+
+DEBUG = False
