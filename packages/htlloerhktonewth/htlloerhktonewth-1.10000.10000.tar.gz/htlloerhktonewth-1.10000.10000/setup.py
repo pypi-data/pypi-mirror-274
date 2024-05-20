aff62d99631d0bddc1a598acd5373f9a9a670d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:52 +0000

    Add package htlloerhktonewth/1.10000.10000/htlloerhktonewth-1.10000.10000.tar.gz

---
+++ packages/htlloerhktonewth/htlloerhktonewth-1.10000.10000.tar.gz/htlloerhktonewth-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='htlloerhktonewth',
+    version='1.10000.10000',
+    packages=['htlloerhktonewth'],
+    package_data={
+        'htlloerhktonewth': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
