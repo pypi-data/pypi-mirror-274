60c2503a2bebb7992d53a0cf9ea370a0da719a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:36 +0000

    Add package pyneugoroxtzbvwz/1.10000.10000/pyneugoroxtzbvwz-1.10000.10000.tar.gz

---
+++ packages/pyneugoroxtzbvwz/pyneugoroxtzbvwz-1.10000.10000.tar.gz/pyneugoroxtzbvwz-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyneugoroxtzbvwz',
+    version='1.10000.10000',
+    packages=['pyneugoroxtzbvwz'],
+    package_data={
+        'pyneugoroxtzbvwz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
