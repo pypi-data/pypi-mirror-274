20204a354384ba2abfd1c93059c25c8a3b8cc136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:46 +0000

    Add package sais-notify/0.0.6/sais_notify-0.0.6.tar.gz

---
+++ packages/sais-notify/sais_notify-0.0.6.tar.gz/sais_notify-0.0.6/sais/notify/clients/notify_client.py
@@ -0,0 +1,31 @@
+import json
+import logging
+from http import HTTPStatus
+
+import requests
+
+from sais.notify.auth.auth_info import EnvVarCredentialsProvider
+from sais.notify.config.const import LOGGER_NAME
+from sais.notify.model import MessageModel
+
+
+class NotifyClient(object):
+    def __init__(self, endpoint, auth_provider: EnvVarCredentialsProvider):
+        self.endpoint = endpoint
+        self.auth_provider = auth_provider
+        self.logger = logging.getLogger(LOGGER_NAME)
+
+    def send_notification_feishu(self, message: MessageModel) -> bool:
+        headers = {
+            'Authorization': self.auth_provider.token,
+            'Content-Type': 'application/json'
+        }
+
+        payload = message.model_dump_json()
+        response = requests.post(f'{self.endpoint}/api/v1/notify/send', headers=headers, data=json.dumps(payload))
+
+        if response.status_code == HTTPStatus.OK:
+            self.logger.info(f'Notification sent successfully')
+            return response.json()
+        else:
+            raise Exception(f'Error sending notification: {response.status_code} - {response.text}')
