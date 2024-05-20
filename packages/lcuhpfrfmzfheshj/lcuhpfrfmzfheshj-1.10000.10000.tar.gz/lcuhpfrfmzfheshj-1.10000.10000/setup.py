40c3930b23cf611ad46446642bf82beab46108cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:56 +0000

    Add package lcuhpfrfmzfheshj/1.10000.10000/lcuhpfrfmzfheshj-1.10000.10000.tar.gz

---
+++ packages/lcuhpfrfmzfheshj/lcuhpfrfmzfheshj-1.10000.10000.tar.gz/lcuhpfrfmzfheshj-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lcuhpfrfmzfheshj',
+    version='1.10000.10000',
+    packages=['lcuhpfrfmzfheshj'],
+    package_data={
+        'lcuhpfrfmzfheshj': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
