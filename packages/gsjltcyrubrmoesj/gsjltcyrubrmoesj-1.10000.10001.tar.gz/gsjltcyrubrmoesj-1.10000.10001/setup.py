ee7950f236986b088dd480d8ee9efd0e5e4e747b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:49 +0000

    Add package gsjltcyrubrmoesj/1.10000.10001/gsjltcyrubrmoesj-1.10000.10001.tar.gz

---
+++ packages/gsjltcyrubrmoesj/gsjltcyrubrmoesj-1.10000.10001.tar.gz/gsjltcyrubrmoesj-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gsjltcyrubrmoesj',
+    version='1.10000.10001',
+    packages=['gsjltcyrubrmoesj'],
+    package_data={
+        'gsjltcyrubrmoesj': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
