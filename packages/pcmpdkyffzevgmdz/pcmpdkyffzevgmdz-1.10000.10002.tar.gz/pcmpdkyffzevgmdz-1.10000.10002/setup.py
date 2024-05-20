c1d1293dfe90fe010823a8299169117033ed5632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:24 +0000

    Add package pcmpdkyffzevgmdz/1.10000.10002/pcmpdkyffzevgmdz-1.10000.10002.tar.gz

---
+++ packages/pcmpdkyffzevgmdz/pcmpdkyffzevgmdz-1.10000.10002.tar.gz/pcmpdkyffzevgmdz-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pcmpdkyffzevgmdz',
+    version='1.10000.10002',
+    packages=['pcmpdkyffzevgmdz'],
+    package_data={
+        'pcmpdkyffzevgmdz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
