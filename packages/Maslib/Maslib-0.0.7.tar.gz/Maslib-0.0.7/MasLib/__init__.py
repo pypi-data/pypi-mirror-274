9b33968a2f11404623b6dd801ccc2520549446e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:36 +0000

    Add package Maslib/0.0.7/Maslib-0.0.7.tar.gz

---
+++ packages/Maslib/Maslib-0.0.7.tar.gz/Maslib-0.0.7/MasLib/__init__.py
@@ -0,0 +1,4 @@
+from .encoding import number_encode_features
+from .correlation import compute_phik_matrix, plot_phik_correlation_matrix
+from .regression import gb_regression, rf_regression, lr_regression, catboost_regression, cross_val_evaluate
+from .grid_search_optimization import grid_search_gb_boost, grid_search_rf, grid_search_lr, grid_search_catboost
