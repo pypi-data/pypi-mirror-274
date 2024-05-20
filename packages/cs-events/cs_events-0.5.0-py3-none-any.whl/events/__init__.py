505f0cb2f8082c04ddf882e87d6da817288e93e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:25 +0000

    Add package cs-events/0.5.0/cs_events-0.5.0-py3-none-any.whl

---
+++ packages/cs-events/cs_events-0.5.0-py3-none-any.whl/events/__init__.py
@@ -0,0 +1,28 @@
+"""
+Provides C#-style event handling mechanism.
+"""
+
+from ._collections import EventHandlerCollection, EventHandlerDict, EventHandlerList
+from ._common import Delegate, void
+from ._field import AsyncEvent, AsyncEventHandler, Event, EventHandler
+from ._property import async_event, event
+from ._utils import event_key, events
+
+
+__all__ = [
+    "async_event",
+    "AsyncEvent",
+    "AsyncEventHandler",
+    "Delegate",
+    "event_key",
+    "event",
+    "Event",
+    "EventHandler",
+    "EventHandlerCollection",
+    "EventHandlerDict",
+    "EventHandlerList",
+    "events",
+    "void",
+]
+__author__ = "Daniel Jeong"
+__version__ = "0.5.0"
