928963be2be74d3eb071c7ed5cbdea9d1d21b75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:04:12 +0000

    Add package vsujdzqqdnpcdjvq/1.10000.10009/vsujdzqqdnpcdjvq-1.10000.10009.tar.gz

---
+++ packages/vsujdzqqdnpcdjvq/vsujdzqqdnpcdjvq-1.10000.10009.tar.gz/vsujdzqqdnpcdjvq-1.10000.10009/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vsujdzqqdnpcdjvq',
+    version='1.10000.10009',
+    packages=['vsujdzqqdnpcdjvq'],
+    package_data={
+        'vsujdzqqdnpcdjvq': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
