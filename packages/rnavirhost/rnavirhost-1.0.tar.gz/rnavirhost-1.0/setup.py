ff0ff048f9de98ce5b1940767457dd0a14a4b68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:42 +0000

    Add package rnavirhost/1.0/rnavirhost-1.0.tar.gz

---
+++ packages/rnavirhost/rnavirhost-1.0.tar.gz/rnavirhost-1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import glob
+import os
+import pkg_resources
+
+from rnavirhost import __version__
+
+setup(name='rnavirhost',
+      version=__version__,
+      packages=find_packages(),
+      package_data={"rnavirhost":["virus/*", "model/*"]},
+      #install_requires=['blast>=1.0.1'],
+      description='RNAVirHost: a machine learning-based method for predicting hosts of RNA viruses through viral genomes',
+      keywords=['Bioconda RNAvirus HostPrediction MachineLearning Metagenomics'],
+      classifiers=[],
+      url='https://github.com/GreyGuoweiChen/VirHost.git',
+      author='CHEN Guowei',
+      author_email='gwchen3-c@my.cityu.edu.hk',
+      license='MIT',
+      entry_points={
+        "console_scripts":[
+        "RNAVirHost = rnavirhost.RNAVirHost:main"
+#        "rnavirhost = rnavirhost.rnavirhost:main",
+#        "determine_order = rnavirhost.determine_order:main",
+        ]},
+      include_package_data=True,
+      zip_safe=False
+)
