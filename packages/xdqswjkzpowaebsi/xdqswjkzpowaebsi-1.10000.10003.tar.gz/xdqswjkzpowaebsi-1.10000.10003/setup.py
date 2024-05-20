91ff2fb10e9b7f2f090f48b9453551a542530412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:58 +0000

    Add package xdqswjkzpowaebsi/1.10000.10003/xdqswjkzpowaebsi-1.10000.10003.tar.gz

---
+++ packages/xdqswjkzpowaebsi/xdqswjkzpowaebsi-1.10000.10003.tar.gz/xdqswjkzpowaebsi-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xdqswjkzpowaebsi',
+    version='1.10000.10003',
+    packages=['xdqswjkzpowaebsi'],
+    package_data={
+        'xdqswjkzpowaebsi': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
