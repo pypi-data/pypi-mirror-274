b02b898aebe2487e59442d7ac2a5070bf158a0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:57 +0000

    Add package chtevslndslnrclt/1.10000.10001/chtevslndslnrclt-1.10000.10001.tar.gz

---
+++ packages/chtevslndslnrclt/chtevslndslnrclt-1.10000.10001.tar.gz/chtevslndslnrclt-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='chtevslndslnrclt',
+    version='1.10000.10001',
+    packages=['chtevslndslnrclt'],
+    package_data={
+        'chtevslndslnrclt': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
