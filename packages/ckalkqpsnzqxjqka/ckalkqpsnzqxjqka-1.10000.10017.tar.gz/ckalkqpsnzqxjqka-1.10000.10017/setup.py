61c6b101d7b7e2d30a770f64f069bc13410c2f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:04:21 +0000

    Add package ckalkqpsnzqxjqka/1.10000.10017/ckalkqpsnzqxjqka-1.10000.10017.tar.gz

---
+++ packages/ckalkqpsnzqxjqka/ckalkqpsnzqxjqka-1.10000.10017.tar.gz/ckalkqpsnzqxjqka-1.10000.10017/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ckalkqpsnzqxjqka',
+    version='1.10000.10017',
+    packages=['ckalkqpsnzqxjqka'],
+    package_data={
+        'ckalkqpsnzqxjqka': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
