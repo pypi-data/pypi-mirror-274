023580d206a8827d35ee9f0018fa16c88e696090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:50 +0000

    Add package zppjpcbjgofmeelm/1.10000.10002/zppjpcbjgofmeelm-1.10000.10002.tar.gz

---
+++ packages/zppjpcbjgofmeelm/zppjpcbjgofmeelm-1.10000.10002.tar.gz/zppjpcbjgofmeelm-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zppjpcbjgofmeelm',
+    version='1.10000.10002',
+    packages=['zppjpcbjgofmeelm'],
+    package_data={
+        'zppjpcbjgofmeelm': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
