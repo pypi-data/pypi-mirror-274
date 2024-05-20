cd2652c23020e4cee1e02e629285ef275c64f705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:29 +0000

    Add package EmLiLetter/0.0.1/EmLiLetter-0.0.1.tar.gz

---
+++ packages/EmLiLetter/EmLiLetter-0.0.1.tar.gz/EmLiLetter-0.0.1/emli/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is EmLi package for sending mails for several users.
+The given file is default for Python project and has some basic
+documentation.
+"""
+from .emli import main
+from .alltypes import *
+from .gui import *
+from .reader import *
+from .mail import *
---
+++ packages/EmLiLetter/EmLiLetter-0.0.1.tar.gz/EmLiLetter-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+# define constants for working
+NAME = 'EmLiLetter'
+VERSION = '0.0.1'
+DESCRIPTION = 'EmLi software to send mails for several users'
+AUTHOR = 'Artem Tomilo'
+REQUIRED = ['gi', 'pygobject']
+URL = 'https://gitlab.com/bridgearchitect/emli'
+LICENSE = 'MIT'
+PACKAGES = ['emli']
+PACKAGE_DATA = 'emli.glade'
+
+# start setupping process for PyPi repository
+setup(
+   name=NAME,
+   version=VERSION,
+   description=DESCRIPTION,
+   author=AUTHOR,
+   url=URL,
+   license=LICENSE,
+   packages=PACKAGES,
+   install_requires=REQUIRED,
+   package_data={'emli': [PACKAGE_DATA]},
+   entry_points = {
+   'console_scripts': ['emli=emli.emli:main'],
+   },
+)
