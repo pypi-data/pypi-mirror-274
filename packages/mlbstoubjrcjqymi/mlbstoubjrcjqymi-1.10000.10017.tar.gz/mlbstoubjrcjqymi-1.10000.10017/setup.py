bbf2d0a1f4afdfe85ec61f38a1afe0b6be7671e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:51 +0000

    Add package mlbstoubjrcjqymi/1.10000.10017/mlbstoubjrcjqymi-1.10000.10017.tar.gz

---
+++ packages/mlbstoubjrcjqymi/mlbstoubjrcjqymi-1.10000.10017.tar.gz/mlbstoubjrcjqymi-1.10000.10017/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlbstoubjrcjqymi',
+    version='1.10000.10017',
+    packages=['mlbstoubjrcjqymi'],
+    package_data={
+        'mlbstoubjrcjqymi': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
