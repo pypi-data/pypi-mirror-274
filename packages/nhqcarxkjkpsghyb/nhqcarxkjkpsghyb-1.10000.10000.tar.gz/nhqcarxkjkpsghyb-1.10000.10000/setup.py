bb0d40868f70eae67561eaa3435f0743025fb9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:58 +0000

    Add package nhqcarxkjkpsghyb/1.10000.10000/nhqcarxkjkpsghyb-1.10000.10000.tar.gz

---
+++ packages/nhqcarxkjkpsghyb/nhqcarxkjkpsghyb-1.10000.10000.tar.gz/nhqcarxkjkpsghyb-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nhqcarxkjkpsghyb',
+    version='1.10000.10000',
+    packages=['nhqcarxkjkpsghyb'],
+    package_data={
+        'nhqcarxkjkpsghyb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
