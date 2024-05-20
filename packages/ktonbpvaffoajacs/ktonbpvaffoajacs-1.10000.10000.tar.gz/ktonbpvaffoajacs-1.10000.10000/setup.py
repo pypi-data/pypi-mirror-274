d07bff5046931a33914c2168f3c1e91aa7a61622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:49 +0000

    Add package ktonbpvaffoajacs/1.10000.10000/ktonbpvaffoajacs-1.10000.10000.tar.gz

---
+++ packages/ktonbpvaffoajacs/ktonbpvaffoajacs-1.10000.10000.tar.gz/ktonbpvaffoajacs-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ktonbpvaffoajacs',
+    version='1.10000.10000',
+    packages=['ktonbpvaffoajacs'],
+    package_data={
+        'ktonbpvaffoajacs': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
