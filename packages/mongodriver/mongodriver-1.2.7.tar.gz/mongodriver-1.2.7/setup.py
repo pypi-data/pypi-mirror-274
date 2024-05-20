a4ab4dd1d310f7bd567b116cfe7a830222d07fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:04:15 +0000

    Add package mongodriver/1.2.7/mongodriver-1.2.7.tar.gz

---
+++ packages/mongodriver/mongodriver-1.2.7.tar.gz/mongodriver-1.2.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mongodriver',
+    version='1.2.7',
+    license='MIT',
+    author="Jake Strouse",
+    author_email='jstrouse@meh.llc',
+    packages=find_packages('src'),
+    url='https://github.com/jakestrouse00/mongodriver',
+    package_dir={'': 'src'},
+    keywords='mongodb',
+    install_requires=[
+        'pymongo==3.12.0',
+        'pymongo[srv]==3.12.0',
+        'motor==3.2.0',
+        'motor[srv]==3.2.0'
+    ],
+    description='Object-oriented interactions with MongoDB',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+
+)
