5f7657a2f435f6087c32450a6e3966d87fdb35fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:44 +0000

    Add package mliumudyhiqlvqcu/1.10000.10001/mliumudyhiqlvqcu-1.10000.10001.tar.gz

---
+++ packages/mliumudyhiqlvqcu/mliumudyhiqlvqcu-1.10000.10001.tar.gz/mliumudyhiqlvqcu-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mliumudyhiqlvqcu',
+    version='1.10000.10001',
+    packages=['mliumudyhiqlvqcu'],
+    package_data={
+        'mliumudyhiqlvqcu': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
