9d1a9b620994545e3df6a51743c1ad9a5e80b857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:52 +0000

    Add package liumqjyadmznbhco/1.10000.10000/liumqjyadmznbhco-1.10000.10000.tar.gz

---
+++ packages/liumqjyadmznbhco/liumqjyadmznbhco-1.10000.10000.tar.gz/liumqjyadmznbhco-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='liumqjyadmznbhco',
+    version='1.10000.10000',
+    packages=['liumqjyadmznbhco'],
+    package_data={
+        'liumqjyadmznbhco': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
