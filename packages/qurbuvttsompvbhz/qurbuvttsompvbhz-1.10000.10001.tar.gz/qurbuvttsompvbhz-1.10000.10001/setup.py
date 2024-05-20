d66b1ec6f60b99c04eb072aaeb807e363584f1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:42 +0000

    Add package qurbuvttsompvbhz/1.10000.10001/qurbuvttsompvbhz-1.10000.10001.tar.gz

---
+++ packages/qurbuvttsompvbhz/qurbuvttsompvbhz-1.10000.10001.tar.gz/qurbuvttsompvbhz-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qurbuvttsompvbhz',
+    version='1.10000.10001',
+    packages=['qurbuvttsompvbhz'],
+    package_data={
+        'qurbuvttsompvbhz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
