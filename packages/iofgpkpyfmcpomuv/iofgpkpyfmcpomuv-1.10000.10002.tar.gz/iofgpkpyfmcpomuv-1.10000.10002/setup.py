ee81c39a02771106331442ed1689959b41b69bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:19 +0000

    Add package iofgpkpyfmcpomuv/1.10000.10002/iofgpkpyfmcpomuv-1.10000.10002.tar.gz

---
+++ packages/iofgpkpyfmcpomuv/iofgpkpyfmcpomuv-1.10000.10002.tar.gz/iofgpkpyfmcpomuv-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='iofgpkpyfmcpomuv',
+    version='1.10000.10002',
+    packages=['iofgpkpyfmcpomuv'],
+    package_data={
+        'iofgpkpyfmcpomuv': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
