aacae3f79bf89e52a6da43ebd8bb11c39e16f368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:49 +0000

    Add package nupmzytokurznqla/1.10000.10005/nupmzytokurznqla-1.10000.10005.tar.gz

---
+++ packages/nupmzytokurznqla/nupmzytokurznqla-1.10000.10005.tar.gz/nupmzytokurznqla-1.10000.10005/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nupmzytokurznqla',
+    version='1.10000.10005',
+    packages=['nupmzytokurznqla'],
+    package_data={
+        'nupmzytokurznqla': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
