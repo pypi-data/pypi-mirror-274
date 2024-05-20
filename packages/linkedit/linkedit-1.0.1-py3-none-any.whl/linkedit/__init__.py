f8f2f966623f4fdf1bdcf550e3374688960f2939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:04:14 +0000

    Add package linkedit/1.0.1/linkedit-1.0.1-py3-none-any.whl

---
+++ packages/linkedit/linkedit-1.0.1-py3-none-any.whl/linkedit/__init__.py
@@ -0,0 +1,20 @@
+"""Sophisticate Linked List"""
+
+from .linkedit import (
+    singlyLinkedList,
+    sll,
+    doublyLinkedList,
+    dll,
+    orthogonalLinkedList,
+    oll,
+)
+
+
+__all__ = [
+    "singlyLinkedList",
+    "sll",
+    "doublyLinkedList",
+    "dll",
+    "orthogonalLinkedList",
+    "oll",
+]
