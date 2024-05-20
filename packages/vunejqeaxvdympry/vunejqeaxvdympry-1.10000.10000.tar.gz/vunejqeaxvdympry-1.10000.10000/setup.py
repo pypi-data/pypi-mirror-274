efeb64c2f93b7b6dd3201ba9f9c786f1e52255ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:48 +0000

    Add package vunejqeaxvdympry/1.10000.10000/vunejqeaxvdympry-1.10000.10000.tar.gz

---
+++ packages/vunejqeaxvdympry/vunejqeaxvdympry-1.10000.10000.tar.gz/vunejqeaxvdympry-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vunejqeaxvdympry',
+    version='1.10000.10000',
+    packages=['vunejqeaxvdympry'],
+    package_data={
+        'vunejqeaxvdympry': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
