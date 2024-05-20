2e91f0adcf3df75f7f1f86e780473d467af159f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:48 +0000

    Add package eras/0.1.0/eras-0.1.0.tar.gz

---
+++ packages/eras/eras-0.1.0.tar.gz/eras-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eras',
+    version='0.1.0',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'openai==1.26.0',  # Specify the version here
+        'keyboard==0.13.5',  # Specify the version here
+        # Add other dependencies with their versions here
+    ],
+    entry_points={
+        'console_scripts': [
+            'eras=main:main',
+        ],
+    },
+    author='Jason McAffee',
+    author_email='jasonlmcaffee@gmail.com',
+    description='A terminal command library that provides a Natural Language Interface for running shell commands.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jasonmcaffee/eras',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
