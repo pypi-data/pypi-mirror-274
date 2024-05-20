c6950726bc5348a23cef13811f5affd91c3016d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:04:02 +0000

    Add package alzihiddlcxljozz/1.10000.10008/alzihiddlcxljozz-1.10000.10008.tar.gz

---
+++ packages/alzihiddlcxljozz/alzihiddlcxljozz-1.10000.10008.tar.gz/alzihiddlcxljozz-1.10000.10008/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='alzihiddlcxljozz',
+    version='1.10000.10008',
+    packages=['alzihiddlcxljozz'],
+    package_data={
+        'alzihiddlcxljozz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
