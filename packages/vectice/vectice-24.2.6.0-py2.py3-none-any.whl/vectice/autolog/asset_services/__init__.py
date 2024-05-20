3b7076cad3043c2e2c5237cfc84c2817c0038728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:46 +0000

    Add package vectice/24.2.6.0/vectice-24.2.6.0-py2.py3-none-any.whl

---
+++ packages/vectice/vectice-24.2.6.0-py2.py3-none-any.whl/vectice/autolog/asset_services/__init__.py
@@ -0,0 +1,27 @@
+from vectice.autolog.asset_services.catboost_service import AutologCatboostService
+from vectice.autolog.asset_services.keras_service import AutologKerasService
+from vectice.autolog.asset_services.lightgbm_service import AutologLightgbmService
+from vectice.autolog.asset_services.pandas_service import AutologPandasService
+from vectice.autolog.asset_services.pyspark_service import AutologPysparkService
+from vectice.autolog.asset_services.pytorch_service import AutologPytorchService
+from vectice.autolog.asset_services.sklearn_service import AutologSklearnService
+from vectice.autolog.asset_services.vectice_asset_service import (
+    AutologVecticeAssetService,
+    TVecticeObjects,
+    VecticeObjectClasses,
+    VecticeObjectTypes,
+)
+
+__all__ = [
+    "AutologPandasService",
+    "AutologPysparkService",
+    "AutologPytorchService",
+    "AutologCatboostService",
+    "AutologKerasService",
+    "AutologLightgbmService",
+    "AutologSklearnService",
+    "AutologVecticeAssetService",
+    "TVecticeObjects",
+    "VecticeObjectTypes",
+    "VecticeObjectClasses",
+]
