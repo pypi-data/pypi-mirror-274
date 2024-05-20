99c1048e3dcc08fd8f5b9f131a941f1207140f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:44 +0000

    Add package stable-diffusion-cpp-python/0.1.6/stable_diffusion_cpp_python-0.1.6.tar.gz

---
+++ packages/stable-diffusion-cpp-python/stable_diffusion_cpp_python-0.1.6.tar.gz/stable_diffusion_cpp_python-0.1.6/tests/test_img2img.py
@@ -0,0 +1,27 @@
+from stable_diffusion_cpp import StableDiffusion
+
+MODEL_PATH = "C:\\stable-diffusion\\turbovisionxlSuperFastXLBasedOnNew_tvxlV431Bakedvae.q8_0.gguf"
+
+INPUT_IMAGE_PATH = "assets\\input.png"
+
+stable_diffusion = StableDiffusion(model_path=MODEL_PATH)
+
+
+def callback(step: int, steps: int, time: float):
+    print("Completed step: {} of {}".format(step, steps))
+
+
+try:
+    # Generate images
+    images = stable_diffusion.img_to_img(
+        prompt="blue eyes",
+        image=INPUT_IMAGE_PATH,
+        strength=0.4,
+        progress_callback=callback,
+    )
+    # Save images
+    for i, image in enumerate(images):
+        image.save(f"output_img2img_{i}.png")
+
+except Exception as e:
+    print("Test - img2img failed: ", e)
