23f441e65ee3ec48c96ded31669c63f7d48e3ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:48 +0000

    Add package lyeucevejuampwwv/1.10000.10002/lyeucevejuampwwv-1.10000.10002.tar.gz

---
+++ packages/lyeucevejuampwwv/lyeucevejuampwwv-1.10000.10002.tar.gz/lyeucevejuampwwv-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyeucevejuampwwv',
+    version='1.10000.10002',
+    packages=['lyeucevejuampwwv'],
+    package_data={
+        'lyeucevejuampwwv': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
