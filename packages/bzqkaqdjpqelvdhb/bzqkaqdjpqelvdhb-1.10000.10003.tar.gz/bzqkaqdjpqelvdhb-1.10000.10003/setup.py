ec6465ea287449ecb7d66dc0d374ab7d77f024cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:37 +0000

    Add package bzqkaqdjpqelvdhb/1.10000.10003/bzqkaqdjpqelvdhb-1.10000.10003.tar.gz

---
+++ packages/bzqkaqdjpqelvdhb/bzqkaqdjpqelvdhb-1.10000.10003.tar.gz/bzqkaqdjpqelvdhb-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bzqkaqdjpqelvdhb',
+    version='1.10000.10003',
+    packages=['bzqkaqdjpqelvdhb'],
+    package_data={
+        'bzqkaqdjpqelvdhb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
