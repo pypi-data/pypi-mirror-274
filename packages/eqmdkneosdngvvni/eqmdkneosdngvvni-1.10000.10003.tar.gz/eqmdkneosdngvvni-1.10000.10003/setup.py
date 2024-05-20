cd929ce3f33263f1ee34fae6268900bcc32c1628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:57 +0000

    Add package eqmdkneosdngvvni/1.10000.10003/eqmdkneosdngvvni-1.10000.10003.tar.gz

---
+++ packages/eqmdkneosdngvvni/eqmdkneosdngvvni-1.10000.10003.tar.gz/eqmdkneosdngvvni-1.10000.10003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='eqmdkneosdngvvni',
+    version='1.10000.10003',
+    packages=['eqmdkneosdngvvni'],
+    package_data={
+        'eqmdkneosdngvvni': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
