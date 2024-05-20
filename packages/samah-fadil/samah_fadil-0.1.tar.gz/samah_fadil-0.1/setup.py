0e64df499bff95a3cf727f282d5325146fe81ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 16:03:38 +0000

    Add package samah-fadil/0.1/samah_fadil-0.1.tar.gz

---
+++ packages/samah-fadil/samah_fadil-0.1.tar.gz/samah_fadil-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup (
+    name = 'samah_fadil',
+    version = '0.1',
+    author = 'samah_fadil',
+    description = 'simple Python library for dragging files ',
+    packages = setuptools.find_packages(),
+    classifiers =[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
