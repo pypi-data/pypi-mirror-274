5d1a680c25e06e49188ea83553d22f9520c0b917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:50 +0000

    Add package hsrrnxbjozijqyhc/1.10000.10000/hsrrnxbjozijqyhc-1.10000.10000.tar.gz

---
+++ packages/hsrrnxbjozijqyhc/hsrrnxbjozijqyhc-1.10000.10000.tar.gz/hsrrnxbjozijqyhc-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hsrrnxbjozijqyhc',
+    version='1.10000.10000',
+    packages=['hsrrnxbjozijqyhc'],
+    package_data={
+        'hsrrnxbjozijqyhc': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
