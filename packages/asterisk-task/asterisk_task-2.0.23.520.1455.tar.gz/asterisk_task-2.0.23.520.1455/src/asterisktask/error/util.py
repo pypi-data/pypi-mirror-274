6097fb17199eb732e5d9912524e42298eb0887fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:34 +0000

    Add package asterisk-task/2.0.23.520.1455/asterisk_task-2.0.23.520.1455.tar.gz

---
+++ packages/asterisk-task/asterisk_task-2.0.23.520.1455.tar.gz/asterisk_task-2.0.23.520.1455/src/asterisktask/error/util.py
@@ -0,0 +1,20 @@
+'''
+一些工具类的错误
+'''
+
+class EmailNotValidError(Exception):
+    '''
+    email地址无效
+    '''
+    pass
+class InvalidEncriptionKey(Exception):
+    '''
+    加密算法的key为字母数字，其他的字符均不支持
+    '''
+    pass
+
+class InvalidTypeError(Exception):
+    '''
+    错误的数据类型，例如需要int型，实际输入为float
+    '''
+    pass
