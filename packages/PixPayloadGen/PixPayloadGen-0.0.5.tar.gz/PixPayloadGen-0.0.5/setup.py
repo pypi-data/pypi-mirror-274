109feb89980ed7457e7e41f028351df14d3add7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:18 +0000

    Add package PixPayloadGen/0.0.5/PixPayloadGen-0.0.5.tar.gz

---
+++ packages/PixPayloadGen/PixPayloadGen-0.0.5.tar.gz/PixPayloadGen-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import codecs
+
+with codecs.open("README.md", "r", "utf-8") as fh:
+        readme = fh.read()
+
+setup(name='PixPayloadGen',
+    version='0.0.5',
+    license='MIT License',
+    author='REIZINHODEV',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='edleyteixeira@hotmail.com',
+    keywords='pix payload gerar',
+    description=u'Gerar Payload pix com QRCODE',
+    packages=['PixPayloadGen'],
+    install_requires=['crcmod', 'qrcode[pil]'],)
