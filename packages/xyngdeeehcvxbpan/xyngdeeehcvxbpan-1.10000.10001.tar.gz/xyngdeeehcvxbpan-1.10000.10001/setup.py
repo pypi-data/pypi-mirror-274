1a36f302534ae13e8f65ac118c4a3c96665033b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:04:16 +0000

    Add package xyngdeeehcvxbpan/1.10000.10001/xyngdeeehcvxbpan-1.10000.10001.tar.gz

---
+++ packages/xyngdeeehcvxbpan/xyngdeeehcvxbpan-1.10000.10001.tar.gz/xyngdeeehcvxbpan-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xyngdeeehcvxbpan',
+    version='1.10000.10001',
+    packages=['xyngdeeehcvxbpan'],
+    package_data={
+        'xyngdeeehcvxbpan': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
