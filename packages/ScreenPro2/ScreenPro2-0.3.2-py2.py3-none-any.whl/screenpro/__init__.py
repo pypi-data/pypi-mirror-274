f9f8a035bf4641b0c3e8c147f01cf4cd4a1f0fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 05:03:03 +0000

    Add package ScreenPro2/0.3.2/ScreenPro2-0.3.2-py2.py3-none-any.whl

---
+++ packages/ScreenPro2/ScreenPro2-0.3.2-py2.py3-none-any.whl/screenpro/__init__.py
@@ -0,0 +1,11 @@
+from . import plotting as pl
+from . import phenoscore as ps
+from . import utils
+from . import ngs
+from . import load
+from .ngs import Counter
+from .assays import PooledScreens, GImaps
+
+__version__ = "0.3.2"
+__author__ = "Abe Arab"
+__email__ = 'abea@arcinstitute.org' # "abarbiology@gmail.com"
