21d4d7742a49903ace1ce1145cb0749724bd5caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:27 +0000

    Add package costa2019/1.2.0/costa2019-1.2.0-py3-none-any.whl

---
+++ packages/costa2019/costa2019-1.2.0-py3-none-any.whl/costa2019/__init__.py
@@ -0,0 +1,14 @@
+"""
+Data from canopy and soil thermal patterns in vineyards
+"""
+# {# pkglts, src
+# FYEO
+# #}
+# {# pkglts, version, after src
+from . import version
+
+__version__ = version.__version__
+# #}
+# {# pkglts, glabdata, after version
+from .info import *
+# #}
