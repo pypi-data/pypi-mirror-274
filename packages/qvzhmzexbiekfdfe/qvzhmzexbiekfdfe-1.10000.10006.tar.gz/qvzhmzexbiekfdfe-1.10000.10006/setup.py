24b82934f25cb9f3f7d86b1a98671f5c49e4bb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:04:02 +0000

    Add package qvzhmzexbiekfdfe/1.10000.10006/qvzhmzexbiekfdfe-1.10000.10006.tar.gz

---
+++ packages/qvzhmzexbiekfdfe/qvzhmzexbiekfdfe-1.10000.10006.tar.gz/qvzhmzexbiekfdfe-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qvzhmzexbiekfdfe',
+    version='1.10000.10006',
+    packages=['qvzhmzexbiekfdfe'],
+    package_data={
+        'qvzhmzexbiekfdfe': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
