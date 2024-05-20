ff44c1927ae84b2c225e6889de094c171067d86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:52 +0000

    Add package hwnxurxciymiyulb/1.10000.10001/hwnxurxciymiyulb-1.10000.10001.tar.gz

---
+++ packages/hwnxurxciymiyulb/hwnxurxciymiyulb-1.10000.10001.tar.gz/hwnxurxciymiyulb-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hwnxurxciymiyulb',
+    version='1.10000.10001',
+    packages=['hwnxurxciymiyulb'],
+    package_data={
+        'hwnxurxciymiyulb': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
