a88a532271e9033c6ee7c744c4eba15f6467dd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:35 +0000

    Add package mongodriver/1.3.1/mongodriver-1.3.1-py3-none-any.whl

---
+++ packages/mongodriver/mongodriver-1.3.1-py3-none-any.whl/mongodriver/__init__.py
@@ -0,0 +1,3 @@
+from .mongodriver import Driver, Document, Variable
+from .async_mongodriver import Driver as AsyncDriver, Document as AsyncDocument, Variable as AsyncVariable
+__version__ = "1.3.1"
