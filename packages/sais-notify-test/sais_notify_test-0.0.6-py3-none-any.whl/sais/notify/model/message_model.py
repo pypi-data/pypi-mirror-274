9baa5fa3d312b88c205e95697d01e287ec6eb20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:49 +0000

    Add package sais-notify-test/0.0.6/sais_notify_test-0.0.6-py3-none-any.whl

---
+++ packages/sais-notify-test/sais_notify_test-0.0.6-py3-none-any.whl/sais/notify/model/message_model.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from sais.notify.types import NotifyType
+
+
+class NotificationRequest(BaseModel):
+    notify_type: NotifyType
+    to: str
+    message: str
+
+
+class MessageModel(BaseModel):
+    message: str
+    to: str
+    type: str
+    robot_name: Optional[str] = Field(None, alias='robotName', description='Robot name field')
