5c8675df3cd96d8704d01cc7618dc413db997339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:25 +0000

    Add package kgtrvxwbozasowar/1.10000.10017/kgtrvxwbozasowar-1.10000.10017.tar.gz

---
+++ packages/kgtrvxwbozasowar/kgtrvxwbozasowar-1.10000.10017.tar.gz/kgtrvxwbozasowar-1.10000.10017/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kgtrvxwbozasowar',
+    version='1.10000.10017',
+    packages=['kgtrvxwbozasowar'],
+    package_data={
+        'kgtrvxwbozasowar': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
