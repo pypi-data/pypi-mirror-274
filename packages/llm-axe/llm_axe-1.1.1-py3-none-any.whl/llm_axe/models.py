60594345f605c623aaa21110ea000b46b34b4c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:36 +0000

    Add package llm-axe/1.1.1/llm_axe-1.1.1-py3-none-any.whl

---
+++ packages/llm-axe/llm_axe-1.1.1-py3-none-any.whl/llm_axe/models.py
@@ -0,0 +1,16 @@
+from ollama import Client
+
+class OllamaChat():
+    def __init__(self, host:str="http://localhost:11434", model:str=None):
+
+        if model is None:
+            raise ValueError('''You must provide a model to use OllamaChat. 
+                                example: OllamaChat(model='llama3:instruct')''')
+
+        self._host = host
+        self._model = model
+        self._ollama = Client(host)
+
+    def ask(self, prompts:list, format:str="", temperature:float=0.8):
+        return self._ollama.chat(model=self._model, messages=prompts, format=format, options={"temperature": temperature})["message"]["content"]        
+
