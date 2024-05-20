1523cd9344edc2cb30ae449bc2860e39eca0f2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:04:10 +0000

    Add package pwimtfapifyfakuk/1.10000.10000/pwimtfapifyfakuk-1.10000.10000.tar.gz

---
+++ packages/pwimtfapifyfakuk/pwimtfapifyfakuk-1.10000.10000.tar.gz/pwimtfapifyfakuk-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pwimtfapifyfakuk',
+    version='1.10000.10000',
+    packages=['pwimtfapifyfakuk'],
+    package_data={
+        'pwimtfapifyfakuk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
