7922edebb8795efe9b89b0972318a31fe7158c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:38 +0000

    Add package ferfereh/1.120.1/ferfereh-1.120.1.tar.gz

---
+++ packages/ferfereh/ferfereh-1.120.1.tar.gz/ferfereh-1.120.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+from ferfereh import NAME, VERSION, DESCRIPTION
+
+setup(
+    name=NAME,
+    author="kamangir",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+)
