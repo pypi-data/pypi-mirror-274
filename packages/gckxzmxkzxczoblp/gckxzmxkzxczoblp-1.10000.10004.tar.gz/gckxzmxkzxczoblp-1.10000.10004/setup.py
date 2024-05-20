03fb8b71af0d0f165a10de416e2cd3308004c1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:56 +0000

    Add package gckxzmxkzxczoblp/1.10000.10004/gckxzmxkzxczoblp-1.10000.10004.tar.gz

---
+++ packages/gckxzmxkzxczoblp/gckxzmxkzxczoblp-1.10000.10004.tar.gz/gckxzmxkzxczoblp-1.10000.10004/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gckxzmxkzxczoblp',
+    version='1.10000.10004',
+    packages=['gckxzmxkzxczoblp'],
+    package_data={
+        'gckxzmxkzxczoblp': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
