23a24bd1238c62b0126521ad358b11bc14b1d1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:39 +0000

    Add package uudhmjlcsneexxnr/1.10000.10007/uudhmjlcsneexxnr-1.10000.10007.tar.gz

---
+++ packages/uudhmjlcsneexxnr/uudhmjlcsneexxnr-1.10000.10007.tar.gz/uudhmjlcsneexxnr-1.10000.10007/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='uudhmjlcsneexxnr',
+    version='1.10000.10007',
+    packages=['uudhmjlcsneexxnr'],
+    package_data={
+        'uudhmjlcsneexxnr': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
