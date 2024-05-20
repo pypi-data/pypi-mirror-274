a298d4c041f80310dee5a1639d5e23a4dc007917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 03:03:57 +0000

    Add package oybrokombbtbcspk/1.10000.10002/oybrokombbtbcspk-1.10000.10002.tar.gz

---
+++ packages/oybrokombbtbcspk/oybrokombbtbcspk-1.10000.10002.tar.gz/oybrokombbtbcspk-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='oybrokombbtbcspk',
+    version='1.10000.10002',
+    packages=['oybrokombbtbcspk'],
+    package_data={
+        'oybrokombbtbcspk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
