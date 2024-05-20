4dd2f4b9c351e79133e89275ccdf8d002c2324fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 15:03:55 +0000

    Add package techgram/1.0.13/techgram-1.0.13-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/techgram/techgram-1.0.13-cp310-cp310-manylinux2014_x86_64.whl/pyrogram/storage/mongo.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from pymongo.errors import InvalidURI
+
+
+class MongoDB:
+    def __init__(self, mongoUrl, mongoName):
+        self.url = mongoUrl
+        self.name = mongoName
+
+        if self.url:
+            try:
+                self.mDb = MongoClient(self.url)
+                if self.name:
+                    self._mongo = self.mDb[self.name]
+                else:
+                    self._mongo = self.mDb["AyiinUserbot"]
+            except InvalidURI:
+                pass
+
+    @property
+    def mongo(self):
+        return self._mongo
+
+    def collection(self, name: str):
+        return self._mongo[name]
