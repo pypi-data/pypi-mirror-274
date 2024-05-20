45ad9b9c2941f0bddf234a7bb5ea6b20a274c494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:29 +0000

    Add package s5learn/0.8/s5learn-0.8.tar.gz

---
+++ packages/s5learn/s5learn-0.8.tar.gz/s5learn-0.8/s5learn/__init__.py
@@ -0,0 +1,10 @@
+from .main import prints
+from .preprocessing.mnv2 import mnv2
+from .preprocessing.idp import idp
+from .preprocessing.m5 import m5
+from .preprocessing.mnv2_dp import mnv2_dp
+from .preprocessing.mnv2_dv import mnv2_dv
+from .preprocessing.ms import ms
+from .preprocessing.mv2_train import mv2_train
+from .preprocessing.t5 import t5
+
