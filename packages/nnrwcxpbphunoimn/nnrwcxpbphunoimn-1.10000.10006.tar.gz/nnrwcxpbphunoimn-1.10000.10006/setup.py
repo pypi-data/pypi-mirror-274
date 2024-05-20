5241fc1e176dfb8dedf3e90064e5a7ab03c901c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:41 +0000

    Add package nnrwcxpbphunoimn/1.10000.10006/nnrwcxpbphunoimn-1.10000.10006.tar.gz

---
+++ packages/nnrwcxpbphunoimn/nnrwcxpbphunoimn-1.10000.10006.tar.gz/nnrwcxpbphunoimn-1.10000.10006/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nnrwcxpbphunoimn',
+    version='1.10000.10006',
+    packages=['nnrwcxpbphunoimn'],
+    package_data={
+        'nnrwcxpbphunoimn': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
