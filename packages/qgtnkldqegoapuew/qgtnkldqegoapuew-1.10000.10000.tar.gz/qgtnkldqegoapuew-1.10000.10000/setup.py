9026da859b64fa2de494d949e672050ab527d3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 04:03:49 +0000

    Add package qgtnkldqegoapuew/1.10000.10000/qgtnkldqegoapuew-1.10000.10000.tar.gz

---
+++ packages/qgtnkldqegoapuew/qgtnkldqegoapuew-1.10000.10000.tar.gz/qgtnkldqegoapuew-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qgtnkldqegoapuew',
+    version='1.10000.10000',
+    packages=['qgtnkldqegoapuew'],
+    package_data={
+        'qgtnkldqegoapuew': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
