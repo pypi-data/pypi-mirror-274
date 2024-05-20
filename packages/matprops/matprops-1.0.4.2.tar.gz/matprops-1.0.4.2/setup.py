10ebc2301d5cc4aaee1123e0c3c214b6c49112cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-20 14:03:35 +0000

    Add package matprops/1.0.4.2/matprops-1.0.4.2.tar.gz

---
+++ packages/matprops/matprops-1.0.4.2.tar.gz/matprops-1.0.4.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='matprops',
+    version='1.0.4.2',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords="matplotlib, visualization, proportional charts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy >= 1.22.0",
+        "pandas >= 1.3.5",
+        "matplotlib >= 3.5.1"
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 6.2.5"
+        ]
+    },
+    url='https://github.com/shammeer-s/matprops',
+    author='Mohammed Shammeer',
+    author_email='mohammedshammeer.s@gmail.com',
+    description='Python library written on top of matplotlib library for customizable proportional charts'
+)
