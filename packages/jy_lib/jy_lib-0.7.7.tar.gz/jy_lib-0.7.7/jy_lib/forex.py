698980d13fe47589531199a5cd4d1d3403c28807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:22 +0000

    Add package jy_lib/0.7.7/jy_lib-0.7.7.tar.gz

---
+++ packages/jy_lib/jy_lib-0.7.7.tar.gz/jy_lib-0.7.7/jy_lib/forex.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""外汇基础货币枚举"""
+import enum
+
+
+class Currency:
+    AUD = enum.auto()
+    BRL = enum.auto()
+    CAD = enum.auto()
+    CHF = enum.auto()
+    CNH = enum.auto()
+    CZK = enum.auto()
+    DKK = enum.auto()
+    EUR = enum.auto()
+    GBP = enum.auto()
+    HKD = enum.auto()
+    JPY = enum.auto()
+    MXN = enum.auto()
+    NOK = enum.auto()
+    NZD = enum.auto()
+    PLN = enum.auto()
+    RUB = enum.auto()
+    SEK = enum.auto()
+    SGD = enum.auto()
+    TRY = enum.auto()
+    USD = enum.auto()
+    XAG = enum.auto()
+    XAU = enum.auto()
+    XPD = enum.auto()
+    XPT = enum.auto()
+    ZAR = enum.auto()
