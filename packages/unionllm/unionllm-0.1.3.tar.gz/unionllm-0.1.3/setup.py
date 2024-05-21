26e756de12ee6b1f5d491057c8e438ce19436cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:41 +0000

    Add package unionllm/0.1.3/unionllm-0.1.3.tar.gz

---
+++ packages/unionllm/unionllm-0.1.3.tar.gz/unionllm-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unionllm',
+    version='0.1.3',
+    packages=find_packages(),
+    license='MIT',
+    description='A Python library for unified access to Chinese domestic large language models.',
+    author='everfly',
+    author_email='tagriver@gmail.com',
+    url='https://github.com/EvalsOne/UnionLLM',
+    install_requires=[
+        'openai',
+        'pydantic',
+        'zhipuai',
+        'tenacity',
+        'dashscope',
+        'websocket_client',
+        'requests',
+        'pytest',
+        'litellm'
+    ],
+)
