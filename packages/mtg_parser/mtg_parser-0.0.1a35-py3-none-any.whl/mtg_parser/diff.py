834fe8128d7f0b8ee4cb923ac6dc47eadd05d4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 06:04:12 +0000

    Add package mtg_parser/0.0.1a35/mtg_parser-0.0.1a35-py3-none-any.whl

---
+++ packages/mtg_parser/mtg_parser-0.0.1a35-py3-none-any.whl/mtg_parser/diff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'diff',
+]
+
+
+def diff(deck1, deck2, differences_only=False):
+    deckset1 = set(card.name for card in deck1)
+    deckset2 = set(card.name for card in deck2)
+
+    result = {}
+    result['deck1 - deck2'] = deckset1.difference(deckset2)
+    if not differences_only:
+        result['deck1 x deck2'] = deckset1.intersection(deckset2)
+    result['deck2 - deck1'] = deckset2.difference(deckset1)
+    return result
