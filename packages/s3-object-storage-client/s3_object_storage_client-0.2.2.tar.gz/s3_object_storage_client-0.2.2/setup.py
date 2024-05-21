4e42af4e5f67c4bfa2e7e1379cdd087403e76bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:19 +0000

    Add package s3-object-storage-client/0.2.2/s3_object_storage_client-0.2.2.tar.gz

---
+++ packages/s3-object-storage-client/s3_object_storage_client-0.2.2.tar.gz/s3_object_storage_client-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="s3_object_storage_client",
+    version="0.2.2",
+    keywords=["pip", "py_s3", "s3", "ceph", "minio"],
+    description="s3 using tool",
+    long_description="s3 protocol using tool",
+    license="MIT Licence",
+    url="https://github.com/susufqx/pyCeph",
+    author="susufqx",
+    author_email="jiangsulirui@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "aioboto3",
+        "boto3",
+        "urllib3",
+    ]
+)
