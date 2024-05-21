ecc2f5954a166d3a0d36bee77974a709db13c9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:04:12 +0000

    Add package blueness/3.5.1/blueness-3.5.1.tar.gz

---
+++ packages/blueness/blueness-3.5.1.tar.gz/blueness-3.5.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+
+from blueness import NAME, VERSION, DESCRIPTION
+from blueness.pypi import get_long_description, get_requirements
+
+
+setup(
+    name=NAME,
+    author="arash@kamangir.net",
+    author_email="arash@kamangir.net",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=get_long_description(__file__),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kamangir/blue-plugin",
+    packages=[NAME],
+    package_data={
+        NAME: ["config.env"],
+    },
+    # install_requires=get_requirements(__file__),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Unix Shell",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    license="Public Domain",
+)
