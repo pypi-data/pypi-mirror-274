9e676cc49f83c63e688595e0b53aa75df5a16dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 06:03:58 +0000

    Add package EQL-NN/2.1.0/eql_nn-2.1.0.tar.gz

---
+++ packages/EQL-NN/eql_nn-2.1.0.tar.gz/eql_nn-2.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EQL-NN",
+    version="2.1.0", # MAJOR.MINOR.MAINTENANCE
+    author="Kristof Pusztai",
+    author_email="kpusztai@berkeley.edu",
+    description="A Tensorflow implementation of the Equation Learning Based Neural Network Model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KristofPusztai/EQL",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tensorflow>=2.11',
+        'numpy',
+        'sympy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
