3105046e2871c9f650ef76204766cb82580b4c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:56 +0000

    Add package permission-manager-drf/0.2.1/permission_manager_drf-0.2.1-py3-none-any.whl

---
+++ packages/permission-manager-drf/permission_manager_drf-0.2.1-py3-none-any.whl/permission_manager_drf/utils.py
@@ -0,0 +1,63 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import Model
+from permission_manager import BasePermissionManager
+from rest_framework.viewsets import GenericViewSet
+
+
+def get_permission_manager(
+    *,
+    view: GenericViewSet,
+    instance: Model = None,
+    cache: bool = False,
+) -> BasePermissionManager:
+    """Get a permission manager instance from a view.
+
+    Args:
+        view: The DRF view from which to get the permission manager.
+        instance: The model instance (optional).
+        cache (bool): Whether to enable caching in the permission manager.
+
+    Returns:
+        BasePermissionManager: An instance of the permission manager.
+
+    Raises:
+        ImproperlyConfigured: If the view does not have a way to determine
+            the permission manager.
+    """
+    if view_manager_class := getattr(view, 'get_permission_manager', None):
+        manager_class = view_manager_class()
+    elif view_manager_class := getattr(view, 'permission_manager', None):
+        manager_class = view_manager_class
+    elif (queryset := getattr(view, 'queryset', None)) is not None:
+        manager_class = get_permission_manager_class_for_model(queryset.model)
+    elif model := getattr(view, 'model', None):
+        manager_class = get_permission_manager_class_for_model(model)
+    else:
+        msg = (
+            "You must define the 'get_permission_manager' method, "
+            "or the 'permission_manager' attribute, or the 'model' "
+            'attribute in the view.'
+        )
+        raise ImproperlyConfigured(msg)
+
+    return manager_class(
+        user=view.request.user,
+        instance=instance,
+        cache=cache,
+        **getattr(view, 'get_permission_manager_context', {}),
+    )
+
+
+def get_permission_manager_class_for_model(
+    model: Model,
+) -> type[BasePermissionManager]:
+    """Get a permission manager class for a given model.
+
+    Args:
+        model: The Django model for which to get the permission manager class.
+
+    Returns:
+        type[BasePermissionManager]: The permission manager class associated
+            with the model.
+    """
+    return model.permission_manager
