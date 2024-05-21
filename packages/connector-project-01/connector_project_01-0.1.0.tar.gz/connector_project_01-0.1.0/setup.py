b17d23f156a33f4c151216113be636650b192376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:38 +0000

    Add package connector-project-01/0.1.0/connector_project_01-0.1.0.tar.gz

---
+++ packages/connector-project-01/connector_project_01-0.1.0.tar.gz/connector_project_01-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='connector_project_01',
+    version='0.1.0',
+    description='A brief description of your project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Chandani7250/CONNECTOR',
+    author='Chandani Kumari',
+    author_email='chandani@chandani.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here.
+        # e.g. 'numpy>=1.18.0', 'requests>=2.23.0',
+    ],
+    python_requires='>=3.6',
+)
