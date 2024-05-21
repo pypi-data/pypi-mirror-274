d0970bac2629b1e3947c3b1870dacfa1faa12b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:05:06 +0000

    Add package hyrule/0.6.0/hyrule-0.6.0.tar.gz

---
+++ packages/hyrule/hyrule-0.6.0.tar.gz/hyrule-0.6.0/hyrule/__init__.py
@@ -0,0 +1,11 @@
+# We use an `__init__.py` instead of `__init__.hy` so that importing
+# from Python works even if `hy` hasn't been imported yet.
+
+__version__ = '0.6.0'
+
+import hy
+hy.macros.require('hyrule.hy_init',
+   # The Python equivalent of `(require hyrule.hy-init *)`
+   None, assignments = 'ALL', prefix = '')
+hy.macros.require_reader('hyrule.hy_init', None, assignments = 'ALL')
+from hyrule.hy_init import *
