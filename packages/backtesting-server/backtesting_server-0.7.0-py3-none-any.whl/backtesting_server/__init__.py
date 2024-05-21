0fcab4bb6ba8310b09098390bcb5fefb6c326c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:04:45 +0000

    Add package backtesting-server/0.7.0/backtesting_server-0.7.0-py3-none-any.whl

---
+++ packages/backtesting-server/backtesting_server-0.7.0-py3-none-any.whl/backtesting_server/__init__.py
@@ -0,0 +1,9 @@
+'''
+Package to interact with MySQL server, recording results of the backtesting.
+
+Created on Tuesday 9th April 2024.
+@author: Harry New
+
+'''
+
+from .main import BacktestingServer, InstrumentGroup, HistoricalPriceGap
