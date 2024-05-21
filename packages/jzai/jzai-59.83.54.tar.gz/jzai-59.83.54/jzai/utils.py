c0a6cc7e90ec656a562a0f9339650b9e5729489f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 15:04:14 +0000

    Add package jzai/59.83.54/jzai-59.83.54.tar.gz

---
+++ packages/jzai/jzai-59.83.54.tar.gz/jzai-59.83.54/jzai/utils.py
@@ -0,0 +1,9 @@
+# jzai/utils.py
+
+import hashlib
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def check_password(stored_password, provided_password):
+    return stored_password == hash_password(provided_password)
