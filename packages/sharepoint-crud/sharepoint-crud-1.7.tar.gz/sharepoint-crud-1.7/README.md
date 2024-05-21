2d6a2c12ad1f96b4839123506da50362c2adf08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:04:47 +0000

    Add package sharepoint-crud/1.7/sharepoint-crud-1.7.tar.gz

---
+++ packages/sharepoint-crud/sharepoint-crud-1.7.tar.gz/sharepoint-crud-1.7/README.md
@@ -0,0 +1,93 @@
+# sharepoint-crud
+
+![GitHub repo size](https://img.shields.io/github/repo-size/almemanuel/sharepoint-crud?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/almemanuel/sharepoint-crud?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/almemanuel/sharepoint-crud?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/almemanuel/sharepoint-crud?style=for-the-badge)
+![Bitbucket open pull requests](https://img.shields.io/bitbucket/pr-raw/almemanuel/sharepoint-crud?style=for-the-badge)
+
+
+> Solution to integrate SharePoint with your Python script
+
+### Improvement
+
+This project is under development, so if you have any suggestions, please let me know:
+
+- [x] Get file content
+- [ ] Search for a file
+- [x] Upload file
+- [ ] Edit file
+- [ ] Delete a file
+
+## 💻 Requeriments
+Before you begin, ensure you have met the following requirements:
+- you have installed a `python 3.8` or higher
+
+## 🚀 Install sharepoint-crud
+- run the command: `pip install sharepoint-crud` to activate the virtual environment
+- voilà!
+
+## ☕ Run
+To run sharepoint-crud, you need a sharepoint account. You can instanciate an object with the following parameters:
+
+```python
+from sharepoint-crud import SharePointCrud
+
+sharepoint = SharePointCrud(
+  'https://your-site.sharepoint.com/sites/your-library',
+  'your-email@your-domain.com',
+  'your-password'
+)
+
+```
+
+Actually, is possible download and upload file. An example:
+
+```python
+file_content = sharepoint.get_file('your/folder/to/your/file', 'your-file.file_format')
+
+```
+An example:
+I have a file `example.csv` in the folder `my_library/Shared Files/my_folder` on SharePoint:
+```python
+from sharepoint-crud import SharePointCrud
+
+file_content = SharePointCrud(
+  'https://your-site.sharepoint.com/sites/your-library',
+  'your-email@your-domain.com',
+  'your-password'
+).get_file('my_library/Shared Files/my_folder', 'example.csv')
+```
+
+## 📫 Contributing
+
+To contribute to sharepoint-crud, follow these steps:
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and confirm them: `git commit -m '<commit_message>'`
+4. Send to the original branch: `git push origin <project_name> <location>`
+5. Create the pull request.
+
+Alternatively, consult the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 🤝 Colaborators
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/almemanuel" title="Emanuel's GitHub">
+        <img src="https://avatars.githubusercontent.com/u/40006289?v=4" width="100px;" alt="Emanuel's profile photo"/><br>
+        <sub>
+          <b>Emanuel Almeida</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+## 📝 Licença
+
+This project is under license. See the file [LICENÇA](LICENSE.md) for more details.
+
+[⬆ Turn to top](#sharepoint-crud)
+<br>
---
+++ packages/sharepoint-crud/sharepoint-crud-1.7.tar.gz/sharepoint-crud-1.7/sharepoint_crud/sharepoint.py
@@ -0,0 +1,108 @@
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.files.file import File
+from office365.sharepoint.files.file_creation_information import FileCreationInformation
+import os
+
+class SharePointCrud():
+    """Class for interacting with SharePoint.
+    """
+    def __init__(self, site, username, password):
+        self._site = site
+        self._username = username
+        self._password = password
+
+
+    @property
+    def site(self):
+        return self._site
+
+
+    @site.setter
+    def site(self, site):
+        self._site = site
+
+
+    @property
+    def username(self):
+        return self._username
+
+
+    @username.setter
+    def username(self, username):
+        self._username = username
+
+
+    @property
+    def password(self):
+        return self._password
+
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+
+    def _auth(self) -> ClientContext:
+        """Method for authenticating the user.
+
+        Returns:
+            ClientContext: context for the user.
+        """
+        conn = ClientContext(self._site).with_credentials(UserCredential(self._username, self._password))
+        return conn
+
+
+    def get_files_list(self, folder_name:str) -> list:
+        """Method for getting the list of files in a folder.
+
+        Args:
+            folder_name (str): The name of the folder.
+
+        Returns:
+            list: List of files in the folder.
+        """
+        conn = self._auth()
+        target_folder_url = f"{folder_name}"
+        root_folder = conn.web.get_folder_by_server_relative_url(target_folder_url)
+        root_folder.expand(['Files', 'Folders']).get().execute_query()
+        return root_folder.files
+
+
+    def get_file(self, folder_name:str, file_name:str) -> bytes:
+        """Method for getting the content of a file.
+
+        Args:
+            folder_name (str): the name of the folder.
+            file_name (str): the name of the file.
+
+        Returns:
+            bytes: the content of the file.
+        """
+        conn = self._auth()
+        file_url = f'/sites/{folder_name}/{file_name}'
+        file = File.open_binary(conn, file_url)
+        return file.content
+
+
+    def upload_file(self, file:str, folder_name:str, overwrite:bool=False) -> any:
+        """Method for uploading a file to a folder.
+
+        Args:
+            file (str): the filename from the file to be uploaded.
+            folder_name (str): the name of the folder on SharePoint.
+            overwrite (bool, optional): whether to overwrite the file if it already exists. Defaults to False.
+
+        Returns:
+            any: the response from the server.
+        """
+        conn = self._auth()
+        target_folder_url = f'/sites/{folder_name}'
+        input(target_folder_url)
+        file_info = FileCreationInformation()
+        file_info.content = open(file, 'rb').read()
+        file_info.url = os.path.basename(file)
+        file_info.overwrite = overwrite
+        target_folder = conn.web.get_folder_by_server_relative_url(target_folder_url)
+        response = target_folder.files.add(file_info).execute_query()
+        return response
