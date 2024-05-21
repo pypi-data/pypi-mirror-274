2c83d38285a0af7536c2f345806d9b15d304e391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:05:02 +0000

    Add package libcsv/1.0.1/libcsv-1.0.1.tar.gz

---
+++ packages/libcsv/libcsv-1.0.1.tar.gz/libcsv-1.0.1/README.md
@@ -0,0 +1,7 @@
+# libCSV - a library for reading and writing CSV tables in Python
+
+Install using pip.
+
+Functions:
+libcsv.openTable(nameOfTable)
+libcsv.writetable(nameOfTable, writeOptions)
---
+++ packages/libcsv/libcsv-1.0.1.tar.gz/libcsv-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libcsv',
+    version='1.0.1',
+    author='Marko Camandioti',
+    author_email='camandiotimarko@gmail.com',
+    description='Library for working with csv files using Python.',
+    packages=["libcsv"]
+)
