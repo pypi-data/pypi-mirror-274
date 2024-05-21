2e7dd3ba0684f7aa7981b96fbc2184ca472f6a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:04:29 +0000

    Add package ImageObject/1.15/ImageObject-1.15.tar.gz

---
+++ packages/ImageObject/ImageObject-1.15.tar.gz/ImageObject-1.15/setup.py
@@ -0,0 +1,34 @@
+﻿import pybind11
+from distutils.core import setup, Extension
+from distutils import ccompiler
+from setuptools import find_packages
+
+cpp_args = [ ]
+print("Compiler is: ", ccompiler.get_default_compiler())
+sfc_module = Extension(
+    'ImageObject',
+    sources=['ImageObject.cpp','pybind11.cpp'],
+  
+    include_dirs=[
+      pybind11.get_include(False),
+      pybind11.get_include(True ),
+    ],
+    language='c++',
+    extra_compile_args=cpp_args)
+
+
+setup(
+    name='ImageObject',
+    version='1.15',
+    description='Python package for image object operations',
+    ext_modules=[sfc_module],
+    author='Lei Wang',
+    author_email='leiwang@lsu.edu',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: C++',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
