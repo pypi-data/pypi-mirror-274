2ca7b7b29107582e48fcddcec8cf05d3ad24aee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:59 +0000

    Add package hugr/0.2.1/hugr-0.2.1.tar.gz

---
+++ packages/hugr/hugr-0.2.1.tar.gz/hugr-0.2.1/src/hugr/__init__.py
@@ -0,0 +1,10 @@
+"""`hugr` is a Python package for the Quantinuum HUGR common
+representation.
+"""
+
+__version__ = "0.2.0a1"
+
+
+def it_works() -> str:
+    """Return a string to confirm that the package is installed and working."""
+    return "It works!"
