3984f66bb0daee505ef92d35eb4f53b5f58c5a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:25 +0000

    Add package gotranx/0.3.7/gotranx-0.3.7.tar.gz

---
+++ packages/gotranx/gotranx-0.3.7.tar.gz/gotranx-0.3.7/src/gotranx/schemes.py
@@ -0,0 +1,176 @@
+from __future__ import annotations
+import typing
+import sympy
+from enum import Enum
+
+from structlog import get_logger
+
+from . import atoms
+from .ode import ODE
+from . import sympytools
+
+logger = get_logger()
+
+
+def default_printer(
+    lhs: sympy.Symbol | sympy.IndexedBase,
+    rhs: sympy.Expr,
+    use_variable_prefix: bool = False,
+) -> str:
+    from sympy.codegen.ast import Assignment
+    from sympy.printing import pycode
+
+    return pycode(Assignment(lhs, rhs))
+
+
+class printer_func(typing.Protocol):
+    def __call__(
+        self,
+        lhs: sympy.Symbol | sympy.IndexedBase,
+        rhs: sympy.Expr,
+        use_variable_prefix: bool = False,
+    ) -> str: ...
+
+
+class scheme_func(typing.Protocol):
+    def __call__(
+        self,
+        ode: ODE,
+        dt: sympy.Symbol,
+        name: str = "values",
+        printer: printer_func = default_printer,
+        remove_unused: bool = False,
+    ) -> list[str]: ...
+
+
+class Scheme(str, Enum):
+    forward_explicit_euler = "forward_explicit_euler"
+    forward_generalized_rush_larsen = "forward_generalized_rush_larsen"
+
+
+def get_scheme(scheme: str) -> scheme_func:
+    """Get the scheme function from a string"""
+    if scheme in ["forward_euler", "forward_explicit_euler", "euler", "explicit_euler"]:
+        return forward_explicit_euler
+    elif scheme in ["forward_generalized_rush_larsen", "generalized_rush_larsen"]:
+        return forward_generalized_rush_larsen
+    else:
+        raise ValueError(f"Unknown scheme {scheme}")
+
+
+def fraction_numerator_is_nonzero(expr):
+    """Perform a very cheap check to detect if a fraction is definitely non-zero."""
+
+    if isinstance(expr, sympy.Pow):
+        # check if the expression is on the form a**-1
+        a, b = expr.args
+        if b is sympy.S.NegativeOne:
+            return True
+        else:
+            # we won't do any further checks
+            return False
+    elif isinstance(expr, sympy.Mul):
+        # check if all factors are non-zero
+        args = expr.args
+        certainly_nonzero_args = []
+        potentially_nonzero_args = []
+        for e in args:
+            if len(e.free_symbols) == 0 and e.is_nonzero:
+                certainly_nonzero_args.append(e)
+            else:
+                potentially_nonzero_args.append(e)
+
+        if len(potentially_nonzero_args) == 0:
+            # all factors are certainly nonzero
+            return True
+
+        # check all potentially non-zero factors
+        for e in potentially_nonzero_args:
+            if not fraction_numerator_is_nonzero(e):
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def forward_explicit_euler(
+    ode: ODE,
+    dt: sympy.Symbol,
+    name: str = "values",
+    printer: printer_func = default_printer,
+    remove_unused: bool = False,
+) -> list[str]:
+    """Generate forward Euler equations for the ODE"""
+    eqs = []
+    values = sympy.IndexedBase(name, shape=(len(ode.state_derivatives),))
+    i = 0
+    for x in ode.sorted_assignments(remove_unused=remove_unused):
+        eqs.append(printer(x.symbol, x.expr, use_variable_prefix=True))
+        if isinstance(x, atoms.StateDerivative):
+            eqs.append(
+                printer(
+                    values[i],
+                    x.state.symbol + dt * x.symbol,
+                )
+            )
+
+            i += 1
+
+    return eqs
+
+
+def forward_generalized_rush_larsen(
+    ode: ODE,
+    dt: sympy.Symbol,
+    name: str = "values",
+    printer: printer_func = default_printer,
+    remove_unused: bool = False,
+    delta=1e-8,
+) -> list[str]:
+    """Generate forward Generalized Rush Larsen equations for the ODE"""
+    eqs = []
+    values = sympy.IndexedBase(name, shape=(len(ode.state_derivatives),))
+    i = 0
+    for x in ode.sorted_assignments(remove_unused=remove_unused):
+        eqs.append(printer(x.symbol, x.expr, use_variable_prefix=True))
+
+        if not isinstance(x, atoms.StateDerivative):
+            continue
+
+        expr_diff = x.expr.diff(x.state.symbol)
+
+        if expr_diff.is_zero:
+            # Use forward Euler
+            eqs.append(
+                printer(
+                    values[i],
+                    x.state.symbol + dt * x.symbol,
+                )
+            )
+            i += 1
+            continue
+
+        linearized_name = x.name + "_linearized"
+        linearized = sympy.Symbol(linearized_name)
+        eqs.append(printer(linearized, expr_diff, use_variable_prefix=True))
+
+        need_zero_div_check = not fraction_numerator_is_nonzero(expr_diff)
+        if not need_zero_div_check:
+            logger.debug(f"{linearized_name} cannot be zero. Skipping zero division check")
+
+        RL_term = x.symbol / linearized * (sympy.exp(linearized * dt) - 1)
+        if need_zero_div_check:
+            RL_term = sympytools.Conditional(
+                abs(linearized) > delta,
+                RL_term,
+                dt * x.symbol,
+            )
+        eqs.append(
+            printer(
+                values[i],
+                x.state.symbol + RL_term,
+            )
+        )
+        i += 1
+    return eqs
---
+++ packages/gotranx/gotranx-0.3.7.tar.gz/gotranx-0.3.7/src/gotranx/transformer.py
@@ -0,0 +1,233 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import NamedTuple
+from typing import Type
+from typing import TypeVar
+
+import lark
+
+from . import atoms
+from . import exceptions
+from . import ode_component
+from . import units
+
+T = TypeVar("T", atoms.State, atoms.Parameter)
+
+
+class LarkODE(NamedTuple):
+    components: tuple[ode_component.Component, ...]
+    comments: tuple[atoms.Comment, ...]
+
+
+def remove_quotes(s: str) -> str:
+    return s.replace("'", "").replace('"', "")
+
+
+def get_unit_and_comment_from_assignment(
+    s: lark.Tree,
+) -> tuple[str | None, atoms.Comment | None]:
+    if len(s.children) >= 3:
+        potential_unit = s.children[2]
+        if isinstance(potential_unit, atoms.Comment):
+            try:
+                unit = units.ureg(potential_unit.text)
+            except (units.pint.UndefinedUnitError, AttributeError):
+                # Not a proper unit so it's a comment
+                return None, atoms.Comment(potential_unit.text)
+            else:
+                if isinstance(unit, units.pint.Quantity):
+                    # It's a proper unit
+                    return potential_unit.text, None
+
+                # It is something else, so it's a comment
+                return None, atoms.Comment(potential_unit.text)
+
+        else:
+            return None, None
+
+    return None, None
+
+
+def find_assignments(
+    s,
+    components: tuple[str, ...],
+) -> list[atoms.Assignment]:
+    if isinstance(s, lark.Tree):
+        unit_str, comment = get_unit_and_comment_from_assignment(s)
+        return [
+            atoms.Assignment(
+                name=str(s.children[0]),
+                value=atoms.Expression(tree=s.children[1]),
+                components=tuple(components),
+                unit_str=unit_str,
+                comment=comment,
+            ),
+        ]
+
+    return []
+
+
+def find_components(
+    s: list[None | lark.tree.Tree | lark.lexer.Token],
+) -> tuple[int, tuple[str, ...]]:
+    i = 0
+    components = []
+
+    while (
+        isinstance(s[i], lark.lexer.Token)
+        and hasattr(s[i], "type")
+        and s[i].type == "COMPONENT_NAME"  # type:ignore
+    ):
+        components.append(remove_quotes(str(s[i])))
+        i += 1
+
+    # If no components are found, add an empty string
+    if len(components) == 0:
+        components.append("")
+    return i, tuple(components)
+
+
+def lark_list_to_parameters(
+    s: list[None | lark.tree.Tree | lark.lexer.Token],
+    cls: Type[T],
+) -> tuple[T, ...]:
+    i, components = find_components(s)
+    return tuple(
+        [
+            tree2parameter(p, components=components, cls=cls)
+            for p in s[i:]
+            if isinstance(p, lark.Tree)
+        ],
+    )
+
+
+def tree2parameter(
+    s: lark.Tree,
+    components: tuple[str, ...],
+    cls: Type[T],
+) -> T:
+    from .expressions import build_expression
+
+    if s.data == "param":
+        return cls(
+            name=str(s.children[0]),
+            value=build_expression(s.children[1]),
+            components=tuple(components),
+        )
+    elif s.data == "scalarparam":
+        unit = None
+        if s.children[2] is not None:
+            unit = remove_quotes(str(s.children[2]))
+
+        desc = None
+        if s.children[3] is not None:
+            desc = remove_quotes(str(s.children[3]))
+
+        return cls(
+            name=str(s.children[0]),
+            value=build_expression(s.children[1]),
+            components=tuple(components),
+            unit_str=unit,
+            description=desc,
+        )
+    else:
+        raise exceptions.UnknownTreeTypeError(datatype=s.data, atom="Parameter")
+
+
+class TreeToODE(lark.Transformer):
+    def _call_userfunc(self, tree, new_children=None):  # pragma: nocover
+        # Assumes tree is already transformed
+        children = new_children if new_children is not None else tree.children
+        try:
+            f = getattr(self, tree.data)
+        except AttributeError:
+            return self.__default__(tree.data, children, tree.meta)
+        else:
+            try:
+                wrapper = getattr(f, "visit_wrapper", None)
+                if wrapper is not None:
+                    return f.visit_wrapper(f, tree.data, children, tree.meta)
+                else:
+                    return f(children)
+            except lark.GrammarError:
+                raise
+
+    def _call_userfunc_token(self, token):  # pragma: nocover
+        try:
+            f = getattr(self, token.type)
+        except AttributeError:
+            return self.__default_token__(token)
+        else:
+            try:
+                return f(token)
+            except lark.GrammarError:
+                raise
+
+    def comment(self, s):
+        return atoms.Comment(" ".join(map(str.lstrip, map(lambda x: x.lstrip("#"), map(str, s)))))
+
+    def states(self, s: list[None | lark.tree.Tree | lark.lexer.Token]) -> tuple[atoms.State, ...]:
+        return lark_list_to_parameters(s, cls=atoms.State)
+
+    def parameters(
+        self, s: list[None | lark.tree.Tree | lark.lexer.Token]
+    ) -> tuple[atoms.Parameter, ...]:
+        return lark_list_to_parameters(s, cls=atoms.Parameter)
+
+    def expressions(self, s) -> tuple[atoms.Assignment, ...]:
+        i, components = find_components(s)
+
+        assignments = []
+
+        for si in s[i:]:
+            assignments.extend(find_assignments(si, components=components))
+
+        return tuple(assignments)
+
+    def ode(self, s) -> LarkODE:
+        # FIXME: Could use Enum here
+        mapping = {
+            atoms.Parameter: "parameters",
+            atoms.Assignment: "assignments",
+            atoms.State: "states",
+        }
+
+        components: dict[str, dict[str, set[atoms.Atom]]] = defaultdict(
+            lambda: {atom: set() for atom in mapping.values()},
+        )
+
+        # breakpoint()
+
+        comments = []
+        for line in s:  # Each line in the block
+            if isinstance(line, atoms.Comment):
+                comments.append(line)
+                continue
+            if isinstance(line, str):
+                assert line.strip() == "", f"Ivalid line {line!r}"
+                # Skip empty lines
+                continue
+
+            for atom in line:  # State, Parameters or Assignment
+                for component in atom.components:
+                    components[component][mapping[type(atom)]].add(atom)
+
+        # Make sets frozen
+        frozen_components: dict[str, dict[str, frozenset[atoms.Atom]]] = {}
+        for component_name, component_values in components.items():
+            frozen_components[component_name] = {}
+            for atom_name, atom_values in component_values.items():
+                frozen_components[component_name][atom_name] = frozenset(atom_values)
+
+        # FIXME: Need to somehow tell the type checker that each of the inner dictionaries
+        # are actually of the correct type.
+        return LarkODE(
+            components=tuple(
+                [
+                    ode_component.Component(name=key, **value)  # type: ignore
+                    for key, value in frozen_components.items()
+                ],
+            ),
+            comments=tuple(comments),
+        )
