34d179069068a44ad7f136cbb531b431258be213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:04:08 +0000

    Add package imas-tools/0.4.3/imas_tools-0.4.3.tar.gz

---
+++ packages/imas-tools/imas_tools-0.4.3.tar.gz/imas_tools-0.4.3/imas_tools/story/adapter.py
@@ -0,0 +1,15 @@
+from .gakuen_parser import parse_messages
+
+
+def gakuen_txt_to_sc_csv(gakuen_txt: str, txt_name_without_ext: str) -> str:
+    parsed = parse_messages(gakuen_txt)
+    sc_csv = "id,name,text,trans\n"
+    for line in parsed:
+        if line["__tag__"] == "message":
+            sc_csv += f"0000000000000,{line['name']},{line['text']},\n"
+        if line["__tag__"] == "choicegroup":
+            for choice in line["choices"]:
+                sc_csv += f"select,,{choice['text']},\n"
+    sc_csv += f"info,{txt_name_without_ext}.txt,,\n"
+    sc_csv += f"译者,,,\n"
+    return sc_csv
