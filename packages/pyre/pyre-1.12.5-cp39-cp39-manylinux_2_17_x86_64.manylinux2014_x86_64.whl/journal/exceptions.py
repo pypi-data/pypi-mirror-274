313830aca074fbdb9c502e1a94d91915f62ed861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:23 +0000

    Add package pyre/1.12.5/pyre-1.12.5-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyre/pyre-1.12.5-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/journal/exceptions.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# the base exception from the framework
+from pyre.framework.exceptions import PyreError
+
+
+# all local exceptions derive from this
+class JournalError(PyreError):
+    """
+    Base class for all journal errors
+
+    Useful when you are trying to catch all journal errors
+    """
+
+
+# raised by firewalls
+class FirewallError(JournalError):
+    """
+    Exception raised when firewalls fire
+    """
+
+    # public data
+    description = "firewall breached; aborting..."
+
+    # metamethods
+    def __init__(self, channel, **kwds):
+        # chain up
+        super().__init__(locator=channel.locator, **kwds)
+        # save the channel
+        self.firewall = channel
+        # all done
+        return
+
+
+# raised by debug channels that are marked fatal
+class DebugError(JournalError):
+    """
+    Exception raised when fatal debug channels fire
+    """
+
+    # public data
+    description = "aborting..."
+
+    # metamethods
+    def __init__(self, channel, **kwds):
+        # chain up
+        super().__init__(locator=channel.locator, **kwds)
+        # save the channel
+        self.debug = channel
+        # all done
+        return
+
+
+# raised by error channels
+class ApplicationError(JournalError):
+    """
+    Exception raised when an application error is encountered
+    """
+
+    # public data
+    description = "application error; aborting..."
+
+    # metamethods
+    def __init__(self, channel, **kwds):
+        # chain up
+        super().__init__(locator=channel.locator, **kwds)
+        # save the channel
+        self.error = channel
+        # all done
+        return
+
+
+# end of file
---
+++ packages/pyre/pyre-1.12.5-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyre-1.12.5.data/data/include/pyre/pyre.h
@@ -0,0 +1,19 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2024 all rights reserved
+
+// code guard
+#if !defined(pyre_h)
+#define pyre_h
+
+// support
+#include "timers.h"
+#include "memory.h"
+#include "grid.h"
+
+#endif
+
+// end of file
