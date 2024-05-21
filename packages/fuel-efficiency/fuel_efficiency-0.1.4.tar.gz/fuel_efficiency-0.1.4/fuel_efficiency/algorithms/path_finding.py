a6f30a335c1d63415c75a0a1c7fd8a95596d5c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:24 +0000

    Add package fuel-efficiency/0.1.4/fuel_efficiency-0.1.4.tar.gz

---
+++ packages/fuel-efficiency/fuel_efficiency-0.1.4.tar.gz/fuel_efficiency-0.1.4/fuel_efficiency/algorithms/path_finding.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+from fuel_efficiency.entities.node import Node
+
+
+class PathfindingStrategy(ABC):
+    @abstractmethod
+    def find_path(grid: List[List[Node]], start: Node, end: Node):
+        pass   # pragma: no cover
+
+    @abstractmethod
+    def get_neighbors(grid: List[List[Node]], node: Node) -> List[Node]:
+        pass   # pragma: no cover
+
+    @abstractmethod
+    def calculate_distance(node1: Node, node2: Node) -> float:
+        pass   # pragma: no cover
+
+    @abstractmethod
+    def calculate_fuel_consumption(
+        grid: List[List[Node]], start: Node, end: Node
+    ) -> Tuple[float, List[Node]]:
+        pass   # pragma: no cover
