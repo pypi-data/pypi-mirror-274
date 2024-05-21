b44f9d5c90bdc7033f0c1e55c425fe3ab8985cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:49 +0000

    Add package django-custom-jsonfield/1.2/django_custom_jsonfield-1.2-py3-none-any.whl

---
+++ packages/django-custom-jsonfield/django_custom_jsonfield-1.2-py3-none-any.whl/django_custom_jsonfield/rest_framework/serializers.py
@@ -0,0 +1,24 @@
+import jsonschema
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+
+class CustomJSONField(serializers.JSONField):
+    default_error_messages = {
+        "invalid_data": _("Value does not match the JSON schema."),
+    }
+
+    def __init__(self, schema: dict, **kwargs):
+        self.schema = schema
+        super().__init__(**kwargs)
+        self.validators.append(self._validate_data)
+
+    def _validate_data(self, value):
+        """Validate value against JSON schema."""
+        try:
+            jsonschema.validate(value, self.schema)
+        except jsonschema.exceptions.ValidationError:
+            raise serializers.ValidationError(
+                self.error_messages["invalid_data"],
+                code="invalid_data",
+            )
