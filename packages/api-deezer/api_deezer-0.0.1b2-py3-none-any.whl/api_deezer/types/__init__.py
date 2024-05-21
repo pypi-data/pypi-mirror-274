02cdd3ef815cc52442c47f8dd6918e4d4373d1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:30 +0000

    Add package api-deezer/0.0.1b2/api_deezer-0.0.1b2-py3-none-any.whl

---
+++ packages/api-deezer/api_deezer-0.0.1b2-py3-none-any.whl/api_deezer/types/__init__.py
@@ -0,0 +1,19 @@
+from .track import Track
+from .album import Album
+from .chart import Chart
+
+from .artist import (
+	Artist, Artist_TOP
+)
+
+from .playlist import Playlist
+
+
+__all__ = (
+	'Track',
+	'Album',
+	'Chart',
+	'Artist',
+	'Artist_TOP',
+	'Playlist'
+)
