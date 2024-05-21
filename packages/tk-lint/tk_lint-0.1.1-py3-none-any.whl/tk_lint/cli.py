e262608bd10858a2b1781a4a2165730eca37b25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:04:04 +0000

    Add package tk-lint/0.1.1/tk_lint-0.1.1-py3-none-any.whl

---
+++ packages/tk-lint/tk_lint-0.1.1-py3-none-any.whl/tk_lint/cli.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+
+import click
+
+
+@click.group()
+def tk_lint_cli():
+    print("Running tk_lint")
+
+
+@tk_lint_cli.command()
+@click.argument("paths", required=False, nargs=-1)
+def ruff(paths):
+    if len(paths) == 0:
+        paths = ['.']
+
+    relative_dir = os.path.dirname(__file__)
+    toml_config_path = relative_dir + "/config/ruff.toml"
+    process = subprocess.Popen(["ruff", "check", " ".join(paths), "--config", toml_config_path])
+    process.communicate()
+    sys.exit(process.returncode)
