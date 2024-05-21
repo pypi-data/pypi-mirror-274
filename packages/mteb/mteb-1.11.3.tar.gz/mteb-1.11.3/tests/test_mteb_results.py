72b4f266f8a566540519be8e0558c4007f37ff06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:42 +0000

    Add package mteb/1.11.3/mteb-1.11.3.tar.gz

---
+++ packages/mteb/mteb-1.11.3.tar.gz/mteb-1.11.3/tests/test_mteb_results.py
@@ -0,0 +1,87 @@
+from importlib.metadata import version
+
+import mteb
+from mteb import AbsTask
+from mteb.abstasks.MTEBResults import MTEBResults
+
+
+class DummyTask(AbsTask):
+    superseeded_by = "newer_task"
+    metadata = mteb.TaskMetadata(
+        name="dummy_task",
+        description="dummy task for testing",
+        dataset={"revision": "1.0", "path": "dummy_dataset"},
+        type="Classification",
+        category="p2p",
+        eval_langs={
+            "en-de": ["eng-Latn", "deu-Latn"],
+            "en-fr": ["eng-Latn", "fra-Latn"],
+        },
+        main_score="main_score",
+        eval_splits=["train"],
+        domains=[],
+        text_creation="created",
+        reference="https://www.noreference.com",
+        date=("2024-05-02", "2024-05-03"),
+        form=[],
+        task_subtypes=[],
+        license="mit",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        bibtex_citation="",
+        avg_character_length={},
+        n_samples={},
+    )
+
+    def evaluate(self, model, split: str = "test"):
+        pass
+
+    def _evaluate_subset(self, **kwargs):
+        pass
+
+
+def test_mteb_results():
+    """Test MTEBResults class (this is the same as the example in the docstring)"""
+    scores = {
+        "train": {
+            "en-de": {
+                "main_score": 0.5,
+            },
+            "en-fr": {
+                "main_score": 0.6,
+            },
+        },
+    }
+
+    evaluation_time = 100
+
+    mteb_results = MTEBResults.from_task_results(
+        task=DummyTask(), scores=scores, evaluation_time=evaluation_time
+    )
+
+    assert mteb_results.get_score() == 0.55
+    assert mteb_results.get_score(languages=["eng"]) == 0.55
+    assert mteb_results.get_score(languages=["fra"]) == 0.6
+    dict_repr = {
+        "dataset_revision": "1.0",
+        "task_name": "dummy_task",
+        "mteb_version": version("mteb"),
+        "evaluation_time": 100,
+        "kg_co2_emissions": None,
+        "scores": {
+            "train": [
+                {
+                    "main_score": 0.5,
+                    "hf_subset": "en-de",
+                    "languages": ["eng-Latn", "deu-Latn"],
+                },
+                {
+                    "main_score": 0.6,
+                    "hf_subset": "en-fr",
+                    "languages": ["eng-Latn", "fra-Latn"],
+                },
+            ]
+        },
+    }
+    assert mteb_results.to_dict() == dict_repr
