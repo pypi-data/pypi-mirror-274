1129d023ab03ded76a33a48d62e7e37522552cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:18 +0000

    Add package streamlit-tree-select2/0.0.7/streamlit_tree_select2-0.0.7.tar.gz

---
+++ packages/streamlit-tree-select2/streamlit_tree_select2-0.0.7.tar.gz/streamlit_tree_select2-0.0.7/streamlit_tree_select/frontend/build/precache-manifest.fb2c8a642427119e778be31dfb78a6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a537187c8c91b78214b5f29aa4ccffbf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f79c29af28bbdec1d89d",
+    "url": "./static/js/2.d51cccdd.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.d51cccdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c927fb65ecd354944c",
+    "url": "./static/js/main.9bf9af58.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
