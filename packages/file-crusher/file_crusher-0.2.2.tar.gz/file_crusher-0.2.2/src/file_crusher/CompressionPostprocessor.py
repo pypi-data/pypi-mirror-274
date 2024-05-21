8c982eaeb2b1648b6413288f7041422d2c810323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:24 +0000

    Add package file-crusher/0.2.2/file_crusher-0.2.2.tar.gz

---
+++ packages/file-crusher/file_crusher-0.2.2.tar.gz/file_crusher-0.2.2/src/file_crusher/CompressionPostprocessor.py
@@ -0,0 +1,12 @@
+import os.path
+import re
+
+
+class CompressionPostprocessor:
+    __compressor_name: str
+
+    def __init__(self, compressor_name: str):
+        self.__compressor_name = compressor_name
+
+    def postprocess(self, source_file: str, destination_file: str) -> None:
+        print(f"** - Compressed Files with {self.__compressor_name}")
