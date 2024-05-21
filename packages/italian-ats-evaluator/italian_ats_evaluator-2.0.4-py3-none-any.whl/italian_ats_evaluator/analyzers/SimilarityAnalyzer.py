8ac64809130eaec2e1c022392767263f8b6e7721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:55 +0000

    Add package italian-ats-evaluator/2.0.4/italian_ats_evaluator-2.0.4-py3-none-any.whl

---
+++ packages/italian-ats-evaluator/italian_ats_evaluator-2.0.4-py3-none-any.whl/italian_ats_evaluator/analyzers/SimilarityAnalyzer.py
@@ -0,0 +1,16 @@
+from ..utils import nlp_utils
+from sentence_transformers import SentenceTransformer, util
+
+
+class SimilarityAnalyzer:
+
+    def __init__(self, reference_text: str, simplified_text: str):
+        model = nlp_utils.get_sentence_transformers_model()
+
+        embeddings1 = model.encode([reference_text], convert_to_numpy=True)
+        embeddings2 = model.encode([simplified_text], convert_to_numpy=True)
+        cosine_scores = util.cos_sim(embeddings1, embeddings2)
+
+        self.semantic_similarity = float(cosine_scores[0][0]) * 100.0
+
+
