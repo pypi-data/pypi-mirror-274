ef3c9a3777e04667abd610d4f0c5e0cc810902ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:41 +0000

    Add package yamlvault/0.2.0/yamlvault-0.2.0.tar.gz

---
+++ packages/yamlvault/yamlvault-0.2.0.tar.gz/yamlvault-0.2.0/src/yamlvault/constants.py
@@ -0,0 +1,9 @@
+CONTAINS_ENCRYPTED_TAGS = "yaml contains encrypted tags"
+CONTAINS_UNENCRYPTED_TAGS = "yaml contains unencrypted tags"
+
+
+ANSIBLE_VAULT_ENCRYPTION_METHOD = 'ansible-vault'
+
+VALID_ENCRYPTION_METHODS = [
+    ANSIBLE_VAULT_ENCRYPTION_METHOD
+]
