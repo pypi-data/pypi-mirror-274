eb7c9cedd344c29bff5dfd62a9e878ac227fa9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:22 +0000

    Add package biogeoloc/0.0.1/biogeoloc-0.0.1.tar.gz

---
+++ packages/biogeoloc/biogeoloc-0.0.1.tar.gz/biogeoloc-0.0.1/biogeoloc/versions.py
@@ -0,0 +1,11 @@
+def get_versions():
+    return versions[0]["number"]
+
+versions = [
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. initial version.",
+        ],
+    },
+]
