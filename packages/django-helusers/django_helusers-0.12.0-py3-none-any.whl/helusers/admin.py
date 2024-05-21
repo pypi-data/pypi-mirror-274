e4c380d42e0ead2ac5c3ca6869ccc8696d62367c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:38 +0000

    Add package django-helusers/0.12.0/django_helusers-0.12.0-py3-none-any.whl

---
+++ packages/django-helusers/django_helusers-0.12.0-py3-none-any.whl/helusers/admin.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.contrib import admin
+
+from .models import ADGroup, ADGroupMapping, OIDCBackChannelLogoutEvent
+
+
+@admin.register(ADGroupMapping)
+class ADGroupMappingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ADGroup)
+
+
+if getattr(settings, "HELUSERS_BACK_CHANNEL_LOGOUT_ENABLED", False):
+
+    @admin.register(OIDCBackChannelLogoutEvent)
+    class OIDCBackChannelLogoutEventAdmin(admin.ModelAdmin):
+        list_display = ["iss", "sub", "sid", "created_at"]
+        readonly_fields = ["created_at"]
+        search_fields = ["iss", "sub", "sid"]
