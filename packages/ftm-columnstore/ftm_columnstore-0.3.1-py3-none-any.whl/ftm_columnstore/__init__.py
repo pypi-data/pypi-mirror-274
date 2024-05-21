61cf3a90e0cb2e97fd2a9c37120d1c824b9867e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:06:35 +0000

    Add package ftm-columnstore/0.3.1/ftm_columnstore-0.3.1-py3-none-any.whl

---
+++ packages/ftm-columnstore/ftm_columnstore-0.3.1-py3-none-any.whl/ftm_columnstore/__init__.py
@@ -0,0 +1,21 @@
+import logging
+import warnings
+
+import nomenklatura.settings
+import numpy as np
+
+from ftm_columnstore.engine import get_engine
+from ftm_columnstore.store import get_store
+
+# shut up
+logging.getLogger("clickhouse_driver.columns.service").setLevel(logging.ERROR)
+logging.getLogger("numpy.core.fromnumeric").setLevel(logging.ERROR)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+
+# FIXME sqlalchemy monkey patch not working
+nomenklatura.settings.DB_URL = "sqlite:///:memory:"
+
+__version__ = "0.3.1"
+
+__all__ = ["get_engine", "get_store"]
---
+++ packages/ftm-columnstore/ftm_columnstore-0.3.1-py3-none-any.whl/ftm_columnstore/settings.py
@@ -0,0 +1,16 @@
+import os
+
+VERSION = "0.3.1"
+
+
+def get_env(name, default=None):
+    value = os.environ.get(name)
+    if value is not None:
+        return str(value)
+    if default is not None:
+        return str(default)
+
+
+DATABASE_URI = get_env("DATABASE_URI", "clickhouse://localhost/default")
+LOG_LEVEL = get_env("LOG_LEVEL", "INFO")
+BULK_WRITE_SIZE = int(get_env("BULK_WRITE_SIZE", 100_000))
