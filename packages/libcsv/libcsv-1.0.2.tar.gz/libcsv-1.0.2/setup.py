98f3b2d4f8f0bf01ef96b8531155314fbc6876df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:05:03 +0000

    Add package libcsv/1.0.2/libcsv-1.0.2.tar.gz

---
+++ packages/libcsv/libcsv-1.0.2.tar.gz/libcsv-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libcsv',
+    version='1.0.2',
+    author='Marko Camandioti',
+    author_email='camandiotimarko@gmail.com',
+    description='Library for working with csv files using Python.',
+    long_description=open("README.md").read(),
+    packages=["libcsv"]
+)
