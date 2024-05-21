3ff4100f177338bf1554aa33d14989a3d7b34bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:40 +0000

    Add package my-scrcpy/0.1/my_scrcpy-0.1.tar.gz

---
+++ packages/my-scrcpy/my_scrcpy-0.1.tar.gz/my_scrcpy-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_scrcpy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Các package phụ thuộc nếu có
+    description='Screen copy android',
+    author='NamHV4',
+    author_email='namhoang2110@gmail.com',
+    url='https://github.com/NamHV4/scrcpy_namhv4',  # URL đến repo của bạn
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
