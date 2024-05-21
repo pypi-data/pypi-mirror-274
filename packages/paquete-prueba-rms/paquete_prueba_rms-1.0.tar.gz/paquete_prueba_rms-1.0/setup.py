b420022e5e1b86165d056ae872c4c4b583ef4a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 15:03:59 +0000

    Add package paquete-prueba-rms/1.0/paquete_prueba_rms-1.0.tar.gz

---
+++ packages/paquete-prueba-rms/paquete_prueba_rms-1.0.tar.gz/paquete_prueba_rms-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup (
+    name='paquete_prueba_rms',
+    version='1.0',
+    description='paquete de suma y resta',
+    author='Richard Makiya',
+    packages=["paquete1"] 
+    )
