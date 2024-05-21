fcc38bcb6194d61bed3977ae335bf85bcb758afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:38 +0000

    Add package jupyterlab-new-launcher/0.3.3/jupyterlab_new_launcher-0.3.3.tar.gz

---
+++ packages/jupyterlab-new-launcher/jupyterlab_new_launcher-0.3.3.tar.gz/jupyterlab_new_launcher-0.3.3/src/database.ts
@@ -0,0 +1,138 @@
+import { IStateDB } from '@jupyterlab/statedb';
+import { ReadonlyPartialJSONValue, PromiseDelegate } from '@lumino/coreutils';
+import { Signal } from '@lumino/signaling';
+import { ILauncher } from '@jupyterlab/launcher';
+import {
+  ILastUsedDatabase,
+  IFavoritesDatabase,
+  ILauncherDatabase
+} from './types';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+abstract class Database<V extends ReadonlyPartialJSONValue, K> {
+  ready: Promise<void>;
+  constructor(options: { stateDB: IStateDB; fetchInterval: number }) {
+    this._stateDB = options.stateDB;
+    const ready = new PromiseDelegate<void>();
+    this.ready = ready.promise;
+    this._updateDB().then(() => ready.resolve());
+    window.setInterval(this._updateDB, options.fetchInterval);
+  }
+  protected _get(item: K) {
+    if (!this._db) {
+      console.error('Database is not ready!');
+      return null;
+    }
+    return this._db[this._itemKey(item)];
+  }
+  protected async _set(item: K, value: V) {
+    const db = await this._fetch();
+    this._db = db;
+    db[this._itemKey(item)] = value;
+    await this._stateDB.save(this._stateDBKey, db);
+  }
+  private _updateDB = async () => {
+    const db = await this._fetch();
+    this._db = db;
+  };
+  private async _fetch(): Promise<Record<string, V>> {
+    let db = (await this._stateDB.fetch(this._stateDBKey)) as
+      | Record<string, V>
+      | undefined;
+    if (typeof db === 'undefined') {
+      // retry once: sometimes state DB does not load up on first try
+      db = (await this._stateDB.fetch(this._stateDBKey)) as
+        | Record<string, V>
+        | undefined;
+    }
+    if (typeof db === 'undefined') {
+      return {};
+    }
+    return db;
+  }
+  protected abstract _itemKey(item: K): string;
+  protected abstract _stateDBKey: string;
+  private _db: Record<string, V> | null = null;
+  private _stateDB: IStateDB;
+}
+
+export abstract class ItemDatabase<
+  V extends ReadonlyPartialJSONValue
+> extends Database<V, ILauncher.IItemOptions> {
+  protected _itemKey(item: ILauncher.IItemOptions): string {
+    return item.command + '_' + JSON.stringify(item.args);
+  }
+}
+
+export class LastUsedDatabase
+  extends ItemDatabase<string>
+  implements ILastUsedDatabase
+{
+  protected _stateDBKey = 'new-launcher:last-used';
+
+  get(item: ILauncher.IItemOptions) {
+    const date = super._get(item);
+    return date ? new Date(date) : null;
+  }
+
+  async recordAsUsedNow(item: ILauncher.IItemOptions) {
+    await this.recordAsUsed(item, new Date());
+  }
+
+  async recordAsUsed(item: ILauncher.IItemOptions, date: Date) {
+    await this._set(item, date.toUTCString());
+    this._changed.emit();
+  }
+
+  get changed() {
+    return this._changed;
+  }
+
+  private _changed = new Signal<LastUsedDatabase, void>(this);
+}
+
+export class FavoritesDatabase
+  extends ItemDatabase<boolean>
+  implements IFavoritesDatabase
+{
+  protected _stateDBKey = 'new-launcher:favorites';
+
+  get(item: ILauncher.IItemOptions) {
+    return super._get(item) ?? null;
+  }
+
+  async set(item: ILauncher.IItemOptions, isFavourite: boolean) {
+    await this._set(item, isFavourite);
+    this._changed.emit();
+  }
+
+  get changed() {
+    return this._changed;
+  }
+
+  private _changed = new Signal<FavoritesDatabase, void>(this);
+}
+
+/**
+ * Initialization data for the jupyterlab-new-launcher extension.
+ */
+export const databasePlugin: JupyterFrontEndPlugin<ILauncherDatabase> = {
+  id: 'jupyterlab-new-launcher:database',
+  description: 'A redesigned JupyterLab launcher databases',
+  provides: ILauncherDatabase,
+  autoStart: true,
+  requires: [IStateDB],
+  activate: (app: JupyterFrontEnd, stateDB: IStateDB) => {
+    const databaseOptions = {
+      stateDB,
+      fetchInterval: 10000
+    };
+    return {
+      lastUsed: new LastUsedDatabase(databaseOptions),
+      favorites: new FavoritesDatabase(databaseOptions)
+    };
+  }
+};
---
+++ packages/jupyterlab-new-launcher/jupyterlab_new_launcher-0.3.3.tar.gz/jupyterlab_new_launcher-0.3.3/src/launcher.tsx
@@ -0,0 +1,342 @@
+// Copyright (c) Nebari Development Team.
+// Distributed under the terms of the Modified BSD License.
+import type { CommandRegistry } from '@lumino/commands';
+import type { ISignal } from '@lumino/signaling';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { ILauncher, Launcher } from '@jupyterlab/launcher';
+import { TranslationBundle } from '@jupyterlab/translation';
+import {
+  FilterBox,
+  notebookIcon,
+  consoleIcon
+} from '@jupyterlab/ui-components';
+import * as React from 'react';
+import {
+  IItem,
+  IKernelItem,
+  ILastUsedDatabase,
+  IFavoritesDatabase,
+  ISettingsLayout
+} from './types';
+import { fileIcon, starIcon } from './icons';
+import { Item } from './item';
+import { KernelTable } from './components/table';
+import { CollapsibleSection } from './components/section';
+import { TypeCard } from './components/card';
+
+function LauncherBody(props: {
+  trans: TranslationBundle;
+  cwd: string;
+  typeItems: IItem[];
+  notebookItems: IKernelItem[];
+  consoleItems: IKernelItem[];
+  otherItems: IItem[];
+  commands: CommandRegistry;
+  settings: ISettingRegistry.ISettings;
+  favouritesChanged: ISignal<IFavoritesDatabase, void>;
+  lastUsedChanged: ISignal<ILastUsedDatabase, void>;
+}): React.ReactElement {
+  const { trans, cwd, typeItems, otherItems, favouritesChanged } = props;
+  const [query, updateQuery] = React.useState<string>('');
+  const [, forceUpdate] = React.useReducer(x => x + 1, 0);
+  const [showStarred, updateShowStarred] = React.useState<
+    ISettingsLayout['starredSection']
+  >(
+    props.settings.composite.starredSection as ISettingsLayout['starredSection']
+  );
+
+  const [searchAll, updateSearchAll] = React.useState<
+    ISettingsLayout['searchAllSections']
+  >(
+    props.settings.composite
+      .searchAllSections as ISettingsLayout['searchAllSections']
+  );
+
+  const syncSettings = () => {
+    const newStarred = props.settings.composite
+      .starredSection as ISettingsLayout['starredSection'];
+    if (showStarred !== newStarred) {
+      updateShowStarred(newStarred);
+    }
+    const newSearchAll = props.settings.composite
+      .searchAllSections as ISettingsLayout['searchAllSections'];
+    if (searchAll !== newSearchAll) {
+      updateSearchAll(newSearchAll);
+    }
+  };
+
+  React.useEffect(() => {
+    props.settings.changed.connect(syncSettings);
+    return () => {
+      props.settings.changed.disconnect(syncSettings);
+    };
+  });
+
+  if (favouritesChanged) {
+    const updateIfNeeded = () => {
+      if (showStarred) {
+        forceUpdate();
+      }
+    };
+    React.useEffect(() => {
+      favouritesChanged.connect(updateIfNeeded);
+      return () => {
+        favouritesChanged.disconnect(updateIfNeeded);
+      };
+    });
+  }
+
+  const metadataAvailable = new Set<string>();
+  for (const item of props.notebookItems) {
+    const kernelMetadata = item.metadata?.kernel;
+    if (!kernelMetadata) {
+      continue;
+    }
+    for (const key of Object.keys(kernelMetadata)) {
+      metadataAvailable.add(key);
+    }
+  }
+
+  const starred = [...props.notebookItems, ...props.consoleItems].filter(
+    item => item.starred
+  );
+
+  const startCollapsed = props.settings.composite
+    .collapsedSections as ISettingsLayout['collapsedSections'];
+
+  return (
+    <div className="jp-LauncherBody">
+      <div className="jp-NewLauncher-TopBar">
+        <div className="jp-Launcher-cwd">
+          <h3>
+            {trans.__('Current folder:')} <code>{cwd ? cwd : '/'}</code>
+          </h3>
+        </div>
+        <div className="jp-NewLauncher-OtherItems">
+          {otherItems.map(item => (
+            <TypeCard item={item} trans={trans} />
+          ))}
+        </div>
+      </div>
+      {searchAll ? (
+        <div className="jp-Launcher-searchBox">
+          <FilterBox
+            placeholder={trans.__('Filter')}
+            updateFilter={(_, query) => {
+              updateQuery(query ?? '');
+            }}
+            initialQuery={''}
+            useFuzzyFilter={false}
+          />
+        </div>
+      ) : null}
+      <CollapsibleSection
+        className="jp-Launcher-openByType"
+        title={trans.__('Create Empty')}
+        icon={fileIcon}
+        open={startCollapsed['create-empty'] !== 'collapsed'}
+      >
+        {typeItems
+          .filter(
+            item =>
+              !query ||
+              item.label.toLowerCase().indexOf(query.toLowerCase()) !== -1
+          )
+          .map(item => (
+            <TypeCard item={item} trans={trans} />
+          ))}
+      </CollapsibleSection>
+      {showStarred ? (
+        <CollapsibleSection
+          className="jp-Launcher-openByKernel"
+          title={trans.__('Starred')}
+          icon={starIcon}
+          open={startCollapsed['starred'] !== 'collapsed'}
+        >
+          {starred.length > 0 ? (
+            <KernelTable
+              items={starred}
+              commands={props.commands}
+              showSearchBox={!searchAll}
+              showWidgetType={true}
+              query={query}
+              settings={props.settings}
+              trans={trans}
+              onClick={item => item.execute()}
+              favouritesChanged={props.favouritesChanged}
+              lastUsedChanged={props.lastUsedChanged}
+            />
+          ) : (
+            'No starred items'
+          )}
+        </CollapsibleSection>
+      ) : null}
+      <CollapsibleSection
+        className="jp-Launcher-openByKernel jp-Launcher-launchNotebook"
+        title={trans.__('Launch New Notebook')}
+        icon={notebookIcon}
+        open={startCollapsed['launch-notebook'] !== 'collapsed'}
+      >
+        <KernelTable
+          items={props.notebookItems}
+          commands={props.commands}
+          showSearchBox={!searchAll}
+          query={query}
+          settings={props.settings}
+          trans={trans}
+          onClick={item => item.execute()}
+          favouritesChanged={props.favouritesChanged}
+          lastUsedChanged={props.lastUsedChanged}
+        />
+      </CollapsibleSection>
+      <CollapsibleSection
+        className="jp-Launcher-openByKernel jp-Launcher-launchConsole"
+        title={trans.__('Launch New Console')}
+        icon={consoleIcon}
+        open={startCollapsed['launch-console'] !== 'collapsed'}
+      >
+        <KernelTable
+          items={props.consoleItems}
+          commands={props.commands}
+          showSearchBox={!searchAll}
+          query={query}
+          settings={props.settings}
+          trans={trans}
+          onClick={item => item.execute()}
+          favouritesChanged={props.favouritesChanged}
+          lastUsedChanged={props.lastUsedChanged}
+        />
+      </CollapsibleSection>
+    </div>
+  );
+}
+
+export namespace NewLauncher {
+  export interface IOptions extends ILauncher.IOptions {
+    lastUsedDatabase: ILastUsedDatabase;
+    favoritesDatabase: IFavoritesDatabase;
+    settings: ISettingRegistry.ISettings;
+  }
+}
+
+const SERVER_PROXY_COMMAND = 'server-proxy:open';
+
+export class NewLauncher extends Launcher {
+  constructor(options: NewLauncher.IOptions) {
+    super(options);
+    this.commands = options.commands;
+    this.trans = this.translator.load('jupyterlab-new-launcher');
+    this._lastUsedDatabase = options.lastUsedDatabase;
+    this._favoritesDatabase = options.favoritesDatabase;
+    this._settings = options.settings;
+  }
+  private _lastUsedDatabase: ILastUsedDatabase;
+  private _favoritesDatabase: IFavoritesDatabase;
+  trans: TranslationBundle;
+
+  renderCommand = (item: ILauncher.IItemOptions): IItem => {
+    return new Item({
+      item,
+      cwd: this.cwd,
+      commands: this.commands,
+      lastUsedDatabase: this._lastUsedDatabase,
+      favoritesDatabase: this._favoritesDatabase
+    });
+  };
+
+  renderKernelCommand = (item: ILauncher.IItemOptions): IItem => {
+    // note: do not use spread syntax here or object attributes will get frozen
+    return this.renderCommand(item);
+  };
+
+  /**
+   * Render the launcher to virtual DOM nodes.
+   */
+  protected render(): React.ReactElement<any> | null {
+    // Bail if there is no model.
+    if (!this.model) {
+      return null;
+    }
+
+    const trans = this.trans;
+    const items = [...this.model.items()];
+
+    const notebookCategory = trans.__('Notebook');
+    const consoleCategory = trans.__('Console');
+    const kernelCategories = [notebookCategory, consoleCategory];
+
+    const otherCommands = this._settings.composite
+      .utilityCommands as ISettingsLayout['utilityCommands'];
+
+    const otherItems = items
+      .filter(item => otherCommands.includes(item.command))
+      .map(this.renderCommand);
+
+    // TODO: maybe better to filter out everything from default lab and re-populate the kernel categories manually to get more metadata?
+    const nonKernelItems = items.filter(
+      item =>
+        ((!item.category || !kernelCategories.includes(item.category)) &&
+          !otherCommands.includes(item.command)) ||
+        item.command === SERVER_PROXY_COMMAND
+    );
+    const rankOverrides = {
+      'terminal:create-new': 3, // TODO: replace with terminal which asks for environment choice?
+      'fileeditor:create-new': 6,
+      'fileeditor:create-new-markdown-file': 5
+    };
+    for (const item of nonKernelItems) {
+      if (item.command in rankOverrides) {
+        item.rank = rankOverrides[item.command as keyof typeof rankOverrides];
+      }
+    }
+    const typeCommands = [
+      {
+        command: 'notebook:create-new',
+        rank: 1
+      },
+      {
+        command: 'console:create',
+        rank: 4
+      },
+      ...nonKernelItems
+    ].sort((a, b) => (a?.rank ?? 0) - (b?.rank ?? 0));
+
+    const notebookItems = items
+      .filter(
+        item =>
+          item.category &&
+          item.category === notebookCategory &&
+          item.command !== SERVER_PROXY_COMMAND
+      )
+      .map(this.renderKernelCommand);
+
+    const consoleItems = items
+      .filter(
+        item =>
+          item.category &&
+          item.category === consoleCategory &&
+          item.command !== SERVER_PROXY_COMMAND
+      )
+      .map(this.renderKernelCommand);
+
+    // TODO: only create items once or if changed; dispose of them too
+    const typeItems: IItem[] = typeCommands.map(this.renderCommand);
+
+    return (
+      <LauncherBody
+        trans={this.trans}
+        cwd={this.cwd}
+        commands={this.commands}
+        typeItems={typeItems}
+        notebookItems={notebookItems}
+        consoleItems={consoleItems}
+        otherItems={otherItems}
+        settings={this._settings}
+        favouritesChanged={this._favoritesDatabase.changed}
+        lastUsedChanged={this._lastUsedDatabase.changed}
+      />
+    );
+  }
+  protected commands: CommandRegistry;
+  private _settings: ISettingRegistry.ISettings;
+}
