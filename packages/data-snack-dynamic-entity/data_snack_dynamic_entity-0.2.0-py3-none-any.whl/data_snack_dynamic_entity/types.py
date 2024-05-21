1ec71152a20c66125f8c131412ea470cdd6ff07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:42 +0000

    Add package data-snack-dynamic-entity/0.2.0/data_snack_dynamic_entity-0.2.0-py3-none-any.whl

---
+++ packages/data-snack-dynamic-entity/data_snack_dynamic_entity-0.2.0-py3-none-any.whl/data_snack_dynamic_entity/types.py
@@ -0,0 +1,17 @@
+from typing import TypedDict, Dict, Any
+
+
+class FieldSchema(TypedDict):
+    type: str
+    key: bool
+    excluded: bool
+    optional: bool
+    default: Any
+
+
+class EntitySchema(TypedDict):
+    properties: Dict[str, FieldSchema]
+    version: int
+
+
+EntityTemplates = Dict[str, EntitySchema]
