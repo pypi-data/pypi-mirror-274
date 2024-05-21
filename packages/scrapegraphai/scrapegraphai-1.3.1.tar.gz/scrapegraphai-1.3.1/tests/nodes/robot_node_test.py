0458706d800d295b0e3d7737776f8923a7f0fe12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:01 +0000

    Add package scrapegraphai/1.3.1/scrapegraphai-1.3.1.tar.gz

---
+++ packages/scrapegraphai/scrapegraphai-1.3.1.tar.gz/scrapegraphai-1.3.1/tests/nodes/robot_node_test.py
@@ -0,0 +1,57 @@
+"""
+Module for testinh robot_node
+"""
+import pytest
+from scrapegraphai.models import Ollama
+from scrapegraphai.nodes import RobotsNode
+
+
+@pytest.fixture
+def setup():
+    """
+    setup
+    """
+    # ************************************************
+    # Define the configuration for the graph
+    # ************************************************
+
+    graph_config = {
+        "llm": {
+            "model": "ollama/llama3",
+            "temperature": 0,
+            "streaming": True
+        },
+    }
+
+    # ************************************************
+    # Define the node
+    # ************************************************
+
+    llm_model = Ollama(graph_config["llm"])
+
+    robots_node = RobotsNode(
+        input="url",
+        output=["is_scrapable"],
+        node_config={"llm": llm_model,
+                     "headless": False
+                     }
+    )
+
+    return robots_node
+
+# ************************************************
+# Test the node
+# ************************************************
+
+
+def test_robots_node(setup):
+    """
+    Run the tests
+    """
+    state = {
+        "url": "https://twitter.com/home"
+    }
+
+    result = setup.execute(state)
+
+    assert result is not None
