f77bcec26fe18f6e543e835480d4187fa1b73d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:04:41 +0000

    Add package ezsmtp/0.0.0/ezsmtp-0.0.0.tar.gz

---
+++ packages/ezsmtp/ezsmtp-0.0.0.tar.gz/ezsmtp-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='ezsmtp',
+version='0.0.0',
+author='rephy',
+author_email='rapmanayon@gmail.com',
+description='Makes sending emails via SMTP so much easier!',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3'
+)
