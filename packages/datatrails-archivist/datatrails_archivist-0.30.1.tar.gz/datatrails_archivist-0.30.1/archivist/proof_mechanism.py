e332d12b5ee564125dc3d35c6aace3c70ae5fa49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 17:03:34 +0000

    Add package datatrails-archivist/0.30.1/datatrails_archivist-0.30.1.tar.gz

---
+++ packages/datatrails-archivist/datatrails_archivist-0.30.1.tar.gz/datatrails_archivist-0.30.1/archivist/proof_mechanism.py
@@ -0,0 +1,21 @@
+"""Archivist Proof Mechanism
+
+   Enumerated type that allows user to select the proof_mechanism option when
+   creating an asset.
+
+"""
+
+# pylint: disable=unused-private-member
+
+from enum import Enum
+
+
+class ProofMechanism(Enum):
+    """Enumerate proof mechanism options"""
+
+    # previously used but now removed
+    __RESERVED = 1
+    #: Assets and events are proven using a hash of the originator's evidence
+    SIMPLE_HASH = 2
+    #: Assets and events are proven using a merkle log hash of the originator's evidence
+    MERKLE_LOG = 3
