3a35923edeaf149b36e28d9f099833de21cedf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 06:04:03 +0000

    Add package noveler/0.1.3/noveler-0.1.3-py3-none-any.whl

---
+++ packages/noveler/noveler-0.1.3-py3-none-any.whl/noveler/controllers/__init__.py
@@ -0,0 +1,23 @@
+from noveler.controllers.BaseController import BaseController
+from noveler.controllers.ActivityController import ActivityController
+from noveler.controllers.AssistantController import AssistantController
+from noveler.controllers.AuthorController import AuthorController
+from noveler.controllers.BibliographyController import BibliographyController
+from noveler.controllers.ChapterController import ChapterController
+from noveler.controllers.CharacterController import CharacterController
+from noveler.controllers.EventController import EventController
+from noveler.controllers.ExportController import ExportController
+from noveler.controllers.ImageController import ImageController
+from noveler.controllers.LinkController import LinkController
+from noveler.controllers.LocationController import LocationController
+from noveler.controllers.NoteController import NoteController
+from noveler.controllers.OllamaModelController import OllamaModelController
+from noveler.controllers.SceneController import SceneController
+from noveler.controllers.StoryController import StoryController
+from noveler.controllers.SubmissionController import SubmissionController
+from noveler.controllers.UserController import UserController
+
+
+
+
+
