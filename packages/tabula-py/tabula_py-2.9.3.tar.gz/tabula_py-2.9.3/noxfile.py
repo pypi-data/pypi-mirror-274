7c6054798db9ad9ca09b48c2901c6f460dac0eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:03:59 +0000

    Add package tabula-py/2.9.3/tabula_py-2.9.3.tar.gz

---
+++ packages/tabula-py/tabula_py-2.9.3.tar.gz/tabula_py-2.9.3/noxfile.py
@@ -0,0 +1,50 @@
+import nox
+
+
+@nox.session
+def lint(session):
+    lint_tools = [
+        "black",
+        "isort",
+        "flake8",
+        "mypy",
+        "types-setuptools",
+        "Flake8-pyproject",
+    ]
+    targets = ["tabula", "tests", "noxfile.py"]
+    session.install(*lint_tools)
+    session.run("flake8", *targets)
+    session.run("black", "--diff", "--check", *targets)
+    session.run("isort", "--check-only", *targets)
+    session.run("mypy", *targets)
+
+
+@nox.session
+@nox.parametrize(
+    "python,jpype",
+    [
+        ("3.8", True),
+        ("3.9", True),
+        ("3.10", True),
+        ("3.11", True),
+        ("3.12", False),
+        ("3.12", True),
+    ],
+)
+def tests(session, jpype):
+    if jpype:
+        tests_with_jpype(session)
+    else:
+        tests_without_jpype(session)
+
+
+def tests_without_jpype(session):
+    session.install(".[test]")
+    session.run("pytest", "-v", "tests/test_read_pdf_table.py")
+
+
+def tests_with_jpype(session):
+    session.install(".[jpype,test]")
+    session.run("pytest", "-v", "tests/test_read_pdf_table.py")
+    session.run("pytest", "-v", "tests/test_read_pdf_jar_path.py")
+    session.run("pytest", "-v", "tests/test_read_pdf_silent.py")
