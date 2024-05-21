5e8f384238f5470703e676ea1ab098fb21db84c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 17:03:33 +0000

    Add package sleepecg/0.5.8/sleepecg-0.5.8.tar.gz

---
+++ packages/sleepecg/sleepecg-0.5.8.tar.gz/sleepecg-0.5.8/tests/test_config.py
@@ -0,0 +1,62 @@
+# © SleepECG developers
+#
+# License: BSD (3-clause)
+
+"""Tests for the configuration interface."""
+
+import pytest
+import sleepecg.config
+from sleepecg import get_config, set_config
+
+
+@pytest.fixture(autouse=True)
+def temp_test_config(tmp_path):
+    """Create, use and delete a temporary user config file for testing."""
+    # setup
+    user_config_path_backup = sleepecg.config._USER_CONFIG_PATH
+    sleepecg.config._USER_CONFIG_PATH = tmp_path / "testconfig.yml"
+
+    # execute test
+    yield
+
+    # cleanup
+    sleepecg.config._USER_CONFIG_PATH = user_config_path_backup
+
+
+def test_get_set_del_config():
+    """Test setting, getting and deleting a setting."""
+    assert get_config("data_dir") == "~/.sleepecg/datasets"
+    assert get_config("classifiers_dir") == "~/.sleepecg/classifiers"
+    set_config(data_dir="some_dir")
+    assert get_config("data_dir") == "some_dir"
+    assert get_config("classifiers_dir") == "~/.sleepecg/classifiers"
+    set_config(data_dir=None)
+    assert get_config("data_dir") == "~/.sleepecg/datasets"
+    assert get_config("classifiers_dir") == "~/.sleepecg/classifiers"
+
+
+def test_set_invalid_config():
+    """Test trying to set an invalid configuration key."""
+    with pytest.raises(ValueError, match="Trying to set invalid config key:"):
+        set_config(invalid_key="some_value")
+
+
+def test_get_invalid_config():
+    """Test trying to get an invalid configuration key."""
+    with pytest.raises(ValueError, match="Trying to get invalid config key:"):
+        get_config("invalid_key")
+
+
+def test_get_all_config():
+    """Test trying to get all configuration values as a dict."""
+    all_config = get_config()
+    assert isinstance(all_config, dict)
+
+
+def test_invalid_key_in_user_config_file():
+    """Test writing an invalid key to the user config file."""
+    with open(sleepecg.config._USER_CONFIG_PATH, "w") as user_config_file:
+        user_config_file.write("invalid_key: 3")
+
+    with pytest.raises(ValueError, match=r"Invalid key\(s\) found .* {'invalid_key'}"):
+        get_config("data_dir")
---
+++ packages/sleepecg/sleepecg-0.5.8.tar.gz/sleepecg-0.5.8/tests/test_heartbeats.py
@@ -0,0 +1,55 @@
+# © SleepECG developers
+#
+# License: BSD (3-clause)
+
+"""Tests for heartbeat detection and detector evaluation."""
+
+from sys import version_info
+
+import numpy as np
+import pytest
+from sleepecg import compare_heartbeats, detect_heartbeats, read_mitdb
+
+
+def test_compare_heartbeats():
+    """Test heartbeat comparison results."""
+    detection = np.array([20, 33, 43, 53, 73])
+    annotation = np.array([20, 34, 58, 75, 99])
+    max_distance = 3
+
+    TP, FP, FN = compare_heartbeats(detection, annotation, max_distance)
+
+    assert np.all(TP == np.array([20, 33, 73]))
+    assert np.all(FP == np.array([43, 53]))
+    assert np.all(FN == np.array([58, 99]))
+
+
+@pytest.fixture(scope="session")
+def mitdb_234_MLII():
+    """Fetch record for detector tests."""
+    return next(read_mitdb(records_pattern="234"))
+
+
+@pytest.mark.parametrize(
+    "backend",
+    [
+        "c",
+        pytest.param(
+            "numba",
+            marks=pytest.mark.skipif(
+                version_info >= (3, 12), reason="Numba does not support Python 3.12 yet"
+            ),
+        ),
+        "python",
+    ],
+)
+def test_detect_heartbeats(mitdb_234_MLII, backend):
+    """Test heartbeat detection on mitdb:234:MLII."""
+    record = mitdb_234_MLII
+    detection = detect_heartbeats(record.ecg, record.fs, backend=backend)
+    TP, FP, FN = compare_heartbeats(detection, record.annotation, int(record.fs / 10))
+
+    # Changes in the heartbeat detector should not lead to worse results!
+    assert len(TP) >= 2750
+    assert len(FP) <= 3
+    assert len(FN) == 0
