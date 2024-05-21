7479b8a3fc656c598da495082636f60054844105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:23 +0000

    Add package api-deezer-full/0.0.1b37/api_deezer_full-0.0.1b37.tar.gz

---
+++ packages/api-deezer-full/api_deezer_full-0.0.1b37.tar.gz/api_deezer_full-0.0.1b37/api_deezer_full/__init__.py
@@ -0,0 +1,13 @@
+from .gw import API_GW
+from .pipe import API_PIPE
+from .media import API_Media
+from .mobile import API_Mobile
+
+
+__all__ = (
+	'API_GW', 'API_PIPE',
+	'API_Media', 'API_Mobile'
+)
+
+
+VERSION = '0.0.1'
---
+++ packages/api-deezer-full/api_deezer_full-0.0.1b37.tar.gz/api_deezer_full-0.0.1b37/api_deezer_full/gw/decorators/__init__.py
@@ -0,0 +1,5 @@
+from .login import check_login
+
+__all__ = (
+	'check_login',
+)
