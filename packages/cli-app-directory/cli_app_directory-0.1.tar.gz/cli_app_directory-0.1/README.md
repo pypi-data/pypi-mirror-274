ea5323a332485912ce5a292e87437108919a1ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:42 +0000

    Add package cli-app-directory/0.1/cli_app_directory-0.1.tar.gz

---
+++ packages/cli-app-directory/cli_app_directory-0.1.tar.gz/cli_app_directory-0.1/README.md
@@ -0,0 +1,6 @@
+Install the Package:
+
+Use the Function:
+from homedir import home_dir
+
+home_dir()
---
+++ packages/cli-app-directory/cli_app_directory-0.1.tar.gz/cli_app_directory-0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+DESCRIPTION = 'Sets up a basic CLI application to print the Home Directory and use inputs to change the cuurent directory or make a new directory '
+LONG_DESCRIPTION = 'A package that sets up a basic CLI App to work and nvigate through the home directories.'
+
+# Setting up
+setup(
+    name="cli_app_directory",
+    version=VERSION,
+    author="kunaalg",
+    author_email="<kunaal@runcode.in>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['datetime', 'requests'],
+    include_package_data=True,
+    keywords=['python', 'Directory', 'App', 'Home', 'Make', 'Home Directory'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
