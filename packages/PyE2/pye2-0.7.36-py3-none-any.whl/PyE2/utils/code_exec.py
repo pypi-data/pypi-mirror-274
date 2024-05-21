9f4f56deedb285d835e5481646d47d85abb3445d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:40 +0000

    Add package PyE2/0.7.36/pye2-0.7.36-py3-none-any.whl

---
+++ packages/PyE2/pye2-0.7.36-py3-none-any.whl/PyE2/utils/code_exec.py
@@ -0,0 +1,28 @@
+import zlib
+import sys
+import base64
+
+
+def code_to_base64(plain_code, verbose=False, compress=True, code_checker_callback=None):
+  if verbose:
+    print("Processing:\n{}".format(plain_code), color='y')
+  errors = None
+  if code_checker_callback is not None:
+    errors = code_checker_callback(plain_code)
+  if errors is not None:
+    print("Cannot serialize code due to: '{}'".format(errors), color='r')
+    return None
+  l_i = len(plain_code)
+  l_c = -1
+  b_code = bytes(plain_code, 'utf-8')
+  if compress:
+    b_code = zlib.compress(b_code, level=9)
+    l_c = sys.getsizeof(b_code)
+  b_encoded = base64.b64encode(b_code)
+  str_encoded = b_encoded.decode('utf-8')
+  l_b64 = len(str_encoded)
+  if verbose:
+    print("Code checking and serialization suceeded. Initial/Compress/B64: {}/{}/{}".format(
+        l_i, l_c, l_b64), color='g'
+    )
+  return str_encoded
