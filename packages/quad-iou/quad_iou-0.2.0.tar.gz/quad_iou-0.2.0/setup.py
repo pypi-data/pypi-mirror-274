5929c7ddb3dd1a863d98ec31079eac84431ed688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:21 +0000

    Add package quad-iou/0.2.0/quad_iou-0.2.0.tar.gz

---
+++ packages/quad-iou/quad_iou-0.2.0.tar.gz/quad_iou-0.2.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
+import torch
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+ext_modules = [
+    CppExtension("quad_iou_cpu", [
+        "src/core/quad_iou_tensors_bind_cpu.cpp",
+        "src/core/quad_iou_tensors.cpp",
+    ],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-fopenmp']
+    )
+]
+
+if torch.cuda.is_available():
+    ext_modules.append(
+        CUDAExtension('quad_iou_cuda', [
+            'src/core/quad_iou_tensors_bind_cuda.cpp',
+            'src/core/quad_iou_tensors.cu',
+        ])
+    )
+
+setup(
+    name="quad_iou",
+    version="0.2.0",
+    author="Irakli Salia",
+    author_email="irakli.salia854@gmail.com",
+    description="Torch extension for calculating IoU(Intersection over Union) for quadrilaterals(MxN)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Isalia20/quad-iou-torch",
+    package_dir={'': 'src'},
+    packages=find_packages(where="src"),
+    ext_modules=ext_modules,
+    cmdclass={
+        'build_ext': BuildExtension
+    },
+    python_requires='>=3.9',
+)
---
+++ packages/quad-iou/quad_iou-0.2.0.tar.gz/quad_iou-0.2.0/src/core/allPoints.h
@@ -0,0 +1,28 @@
+#define MAX_INTERSECTION_POINTS 8
+#define MAX_INSIDE_POINTS 8
+#define MAX_ALL_POINTS 16
+#include <torch/extension.h>
+#ifdef __CUDACC__
+#include <cuda_runtime.h>
+#define HOST_DEVICE __host__ __device__
+#define PRAGMA_UNROLL _Pragma("unroll")
+#else
+#define HOST_DEVICE
+#define PRAGMA_UNROLL
+#endif
+
+template <typename scalar_t>
+HOST_DEVICE inline void fillArrayWithInfinity(scalar_t points[][2], int num_points){
+    PRAGMA_UNROLL
+    for (int i = 0; i < num_points; i++){
+        points[i][0] = INFINITY;
+        points[i][1] = INFINITY;
+    }
+}
+
+namespace allPoints {
+    template <typename scalar_t>
+    HOST_DEVICE inline void fillPointsWithInfinity(scalar_t all_points[MAX_ALL_POINTS][2]){
+        fillArrayWithInfinity(all_points, MAX_ALL_POINTS);
+    }
+}
