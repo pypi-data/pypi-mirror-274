6ff695b56edeff65b73eb67937d2595c66ed2b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:58 +0000

    Add package xspace-dl/0.1.23/xspace-dl-0.1.23.tar.gz

---
+++ packages/xspace-dl/xspace-dl-0.1.23.tar.gz/xspace-dl-0.1.23/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xspace-dl',  # Updated package name
+    version='0.1.23',  # Increment the version number
+    author='RoseBabaGanoush',
+    author_email='hello@rosebabaganoush.com',
+    description='A python module to download Twitter spaces',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RoseBabaganoush/xspace-dl',  # Update with your repository URL
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'mutagen',  # Add mutagen to dependencies
+    ],
+    entry_points={
+        'console_scripts': [
+            'xspace-dl=xspace.main:main',  # Adjust as per your CLI entry point
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
