2d0cdacda396bfa35a611304f8d259c6bb1b5c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:19 +0000

    Add package ellis-django-views/0.1/ellis_django_views-0.1.tar.gz

---
+++ packages/ellis-django-views/ellis_django_views-0.1.tar.gz/ellis_django_views-0.1/ellis_django_views/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+def image_directory(self, image_name):
+    return f'images/{self.id}/{image_name}'
+
+class TestImageModel(models.Model):
+    image = models.ImageField(upload_to=image_directory)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            saved_image = self.image
+            self.image = None
+            super(TestImageModel, self).save(*args, **kwargs)
+            self.image = saved_image
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+        super(TestImageModel, self).save(*args, **kwargs)
