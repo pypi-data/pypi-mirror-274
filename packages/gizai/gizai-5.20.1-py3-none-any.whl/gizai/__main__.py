ccd163bf68a638b836542d9b3f49e337af081f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 06:03:57 +0000

    Add package gizai/5.20.1/gizai-5.20.1-py3-none-any.whl

---
+++ packages/gizai/gizai-5.20.1-py3-none-any.whl/gizai/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+from gizai import NAME, VERSION
+from gizai.logger import logger
+
+
+parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+parser.add_argument(
+    "task",
+    type=str,
+    help="version",
+)
+args = parser.parse_args()
+
+success = False
+if args.task == "version":
+    print(f"{NAME}-{VERSION}")
+    success = True
+else:
+    logger.error(f"-{NAME}: {args.task}: command not found.")
+
+if not success:
+    logger.error(f"-{NAME}: {args.task}: failed.")
