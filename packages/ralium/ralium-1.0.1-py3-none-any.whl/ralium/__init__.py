f8d767cc4c4758570825435ea3eb1ec0e2301609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:04:14 +0000

    Add package ralium/1.0.1/ralium-1.0.1-py3-none-any.whl

---
+++ packages/ralium/ralium-1.0.1-py3-none-any.whl/ralium/__init__.py
@@ -0,0 +1,17 @@
+from .element import HTMLElement
+from .webpage import WebHook, create_namespace
+from .errors import DisableWarnings
+from .config import WindowConfig
+from .window import Window
+from .setup import setup
+from ._util import __version__
+
+from . import errors
+from . import api
+
+from .builtins import (
+    get_window_port,
+    get_window_path,
+    redirect,
+    shutdown
+)
---
+++ packages/ralium/ralium-1.0.1-py3-none-any.whl/ralium/setup.pyi
@@ -0,0 +1,26 @@
+from ralium._util import (
+    FilePathStr,
+    DirPathStr,
+    Any
+)
+
+HTML_FILE_EXTENSIONS: list[str]
+FILE_EXTENSIONS: list[str]
+
+def _exe_str_error(name: str, var: Any) -> Exception: ...
+def _add_data_arg(src: FilePathStr, dst: FilePathStr) -> str: ...
+
+def collect_webfolder(webfolder: DirPathStr) -> dict[FilePathStr]: ...
+
+def setup(
+    pyfile: FilePathStr,
+    name: str | None = None,
+    icon: FilePathStr | None = None,
+    bundle: bool = False,
+    webfolder: DirPathStr | None = None,
+    warnings: bool = False,
+    onefile: bool = True,
+    noconsole: bool = True,
+    bundle_dist: DirPathStr | None = None,
+    pyi_args: list[str] | None = None
+) -> None: ...
