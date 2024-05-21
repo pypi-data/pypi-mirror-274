a32e0876d1db4964c6795dcf9063d63d387d3cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:04:20 +0000

    Add package blueness/3.9.1/blueness-3.9.1.tar.gz

---
+++ packages/blueness/blueness-3.9.1.tar.gz/blueness-3.9.1/setup.py
@@ -0,0 +1,14 @@
+from blueness import NAME, VERSION, DESCRIPTION
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.argparse",
+        f"{NAME}.pypi",
+    ],
+)
