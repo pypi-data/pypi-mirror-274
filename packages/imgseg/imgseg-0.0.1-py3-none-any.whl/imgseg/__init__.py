1427f855927cd77713730816ef6d9fe24b154b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:20 +0000

    Add package imgseg/0.0.1/imgseg-0.0.1-py3-none-any.whl

---
+++ packages/imgseg/imgseg-0.0.1-py3-none-any.whl/imgseg/__init__.py
@@ -0,0 +1,7 @@
+# segline/__init__.py
+
+# If you want to make specific modules or functions available when the package is imported,
+# you can import them here. For example:
+
+from .run import process_image
+
