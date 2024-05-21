7fbbaf866d6bc1c0113cdfb47d2d4f6d3c62ec40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 06:03:58 +0000

    Add package nora-lib/0.0.5.dev0/nora_lib-0.0.5.dev0.tar.gz

---
+++ packages/nora-lib/nora_lib-0.0.5.dev0.tar.gz/nora_lib-0.0.5.dev0/nora_lib/interactions/interactions_service.py
@@ -0,0 +1,146 @@
+from datetime import datetime
+import requests
+from typing import List, Optional
+
+from nora_lib.interactions.models import (
+    Event,
+    EventType,
+    Message,
+    ReturnedMessage,
+    ThreadRelationsResponse,
+    ThreadForkEventData,
+    thread_message_lookup_request,
+)
+
+
+class InteractionsService:
+    """
+    Service which saves interactions to the Interactions API
+    """
+
+    def __init__(self, base_url, timeout, token):
+        self.base_url = base_url
+        self.timeout = timeout
+        self.headers = {"Authorization": f"Bearer {token}"}
+
+    def save_message(self, message: Message) -> None:
+        """Save a message to the Interactions API"""
+        message_url = f"{self.base_url}/interaction/v1/message"
+        response = requests.post(
+            message_url,
+            json=message.model_dump(),
+            headers=self.headers,
+            timeout=int(self.timeout),
+        )
+        response.raise_for_status()
+
+    def save_event(self, event: Event) -> None:
+        """Save an event to the Interactions API"""
+        event_url = f"{self.base_url}/interaction/v1/event"
+        response = requests.post(
+            event_url,
+            json=event.model_dump(),
+            headers=self.headers,
+            timeout=int(self.timeout),
+        )
+        response.raise_for_status()
+
+    def get_message(self, message_id: str) -> ReturnedMessage:
+        """Fetch a message from the Interactions API"""
+        message_url = f"{self.base_url}/interaction/v1/search/message"
+        request_body = {
+            "id": message_id,
+        }
+        response = requests.post(
+            message_url,
+            json=request_body,
+            headers=self.headers,
+            timeout=int(self.timeout),
+        )
+        response.raise_for_status()
+        res_dict = response.json()["message"]
+
+        return ReturnedMessage.model_validate(res_dict)
+
+    def fetch_thread_messages_and_events_for_message(
+        self, message_id: str, event_type: str
+    ) -> ThreadRelationsResponse:
+        """Fetch messages and associated events from the same thread as provided messagev id"""
+        message_url = f"{self.base_url}/interaction/v1/search/message"
+        request_body = thread_message_lookup_request(message_id, event_type=event_type)
+        response = requests.post(
+            message_url,
+            json=request_body,
+            headers=self.headers,
+            timeout=int(self.timeout),
+        )
+        response.raise_for_status()
+        json_response = response.json()
+
+        return ThreadRelationsResponse.model_validate(
+            json_response.get("message", {}).get("thread", {})
+        )
+
+    def fetch_messages_and_events_for_thread(
+        self,
+        thread_id: str,
+        event_type: Optional[str] = None,
+        min_timestamp: Optional[str] = None,
+    ) -> dict:
+        """Fetch messages and events for the thread containing a given message from the Interactions API"""
+        THREAD_SEARCH_URL = f"{self.base_url}/interaction/v1/search/thread"
+        request_body = {
+            "id": thread_id,
+            "relations": {
+                "messages": (
+                    {"filter": {"min_timestamp": min_timestamp}}
+                    if min_timestamp
+                    else {}
+                ),
+                "events": {"filter": {"type": event_type}} if event_type else {},
+            },
+        }
+
+        response = requests.post(
+            THREAD_SEARCH_URL,
+            json=request_body,
+            headers=self.headers,
+            timeout=int(self.timeout),
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def fetch_messages_and_events_for_forked_thread(
+        self, message_id: str, event_type: str
+    ) -> List[ReturnedMessage]:
+        """Build a history of messages for a given message including associated events.
+        This includes messages from pre-forked threads."""
+        returned_messages: List[ReturnedMessage] = []
+
+        messages_for_thread: ThreadRelationsResponse = (
+            self.fetch_thread_messages_and_events_for_message(message_id, event_type)
+        )
+        if messages_for_thread.messages:
+            returned_messages.extend(messages_for_thread.messages)
+
+        # Lookup any thread_fork events (conversation across surfaces)
+        thread_fork_events = self.fetch_messages_and_events_for_thread(
+            messages_for_thread.thread_id, EventType.THREAD_FORK.value
+        )
+        for forked_thread_event in thread_fork_events.get("thread", {}).get(
+            "events", []
+        ):
+            event_data = ThreadForkEventData.model_validate(
+                forked_thread_event.get("data", {})
+            )
+            forked_thread: ThreadRelationsResponse = (
+                self.fetch_thread_messages_and_events_for_message(
+                    event_data.previous_message_id, event_type
+                )
+            )
+            if forked_thread.messages:
+                returned_messages.extend(forked_thread.messages)
+
+        returned_messages.sort(key=lambda x: datetime.fromisoformat(x.ts))
+
+        return returned_messages
---
+++ packages/nora-lib/nora_lib-0.0.5.dev0.tar.gz/nora_lib-0.0.5.dev0/nora_lib/interactions/models.py
@@ -0,0 +1,148 @@
+"""
+Model for interactions to be sent to the interactions service.
+"""
+
+from datetime import datetime
+from enum import Enum
+from typing import Dict, List, Optional, Tuple
+from uuid import UUID
+
+from pydantic import BaseModel, Field, field_serializer, ConfigDict
+
+
+class Surface(str, Enum):
+    SLACK = "Slack"
+    WEB = "NoraWebapp"
+
+
+class Annotation(BaseModel):
+    # Need this config to stringify numeric values in attributes.
+    # Otherwise, we'll get 'Input should be a valid string' error.
+    model_config = ConfigDict(coerce_numbers_to_str=True)
+
+    tag: str
+    span: Tuple[int, int]
+    attributes: Optional[Dict[str, str]] = None
+
+
+class Message(BaseModel):
+    message_id: str
+    actor_id: UUID
+    text: str
+    thread_id: Optional[str]
+    channel_id: str
+    surface: Surface
+    ts: datetime
+    annotations: Optional[List[Annotation]] = None
+
+    @field_serializer("actor_id")
+    def serialize_actor_id(self, actor_id: UUID):
+        return str(actor_id)
+
+    @field_serializer("ts")
+    def serialize_ts(self, ts: datetime):
+        return ts.isoformat()
+
+
+class EventType(str, Enum):
+    """Event types for the interactions service"""
+
+    AGENT_CONTEXT = "agent:message_context"
+    THREAD_FORK = "thread_fork"
+
+
+class Event(BaseModel):
+    """event object to be sent to the interactions service; requires association with a message, thread or channel id"""
+
+    type: str
+    actor_id: UUID = Field(
+        description="identifies actor writing the event to the interaction service"
+    )
+    timestamp: datetime
+    text: Optional[str] = None
+    data: Optional[dict] = Field(default_factory=dict)
+    message_id: Optional[str] = None
+    thread_id: Optional[str] = None
+    channel_id: Optional[str] = None
+
+    @field_serializer("actor_id")
+    def serialize_actor_id(self, actor_id: UUID):
+        return str(actor_id)
+
+    @field_serializer("timestamp")
+    def serialize_timestamp(self, timestamp: datetime):
+        return timestamp.isoformat()
+
+
+class ReturnedMessage(BaseModel):
+    """Message format returned by interaction service"""
+
+    message_id: str
+    actor_id: str
+    text: str
+    ts: str
+    annotated_text: Optional[str] = None
+    events: Optional[List[dict]] = None
+
+
+class AgentMessageData(BaseModel):
+    """capture requests to and responses from tools within Events"""
+
+    message_data: dict  # dict of agent/tool request/response format
+    data_sender_actor_id: Optional[str] = None  # agent sending the data
+    virtual_thread_id: Optional[str] = None  # tool-provided thread
+    tool_call_id: Optional[str] = None  # llm-provided thread
+    tool_name: Optional[str] = None  # llm identifier for tool
+
+
+class ReturnedAgentContextEvent(BaseModel):
+    """Event format returned by interaction service for agent context events"""
+
+    actor_id: str  # agent that saved this context
+    timestamp: str
+    data: AgentMessageData
+    type: str
+
+
+class ReturnedAgentContextMessage(BaseModel):
+    """Message format returned by interaction service for search by thread"""
+
+    message_id: str
+    actor_id: str
+    text: str
+    ts: str
+    annotated_text: Optional[str] = None
+    events: Optional[List[ReturnedAgentContextEvent]] = None
+
+
+class ThreadForkEventData(BaseModel):
+    """Event data for a thread fork event"""
+
+    previous_message_id: str
+
+
+class ThreadRelationsResponse(BaseModel):
+    """Thread format returned by interaction service for thread relations in a search response"""
+
+    thread_id: str
+    events: Optional[List[Event]] = None  # events associated only with the thread
+    messages: Optional[List[ReturnedMessage]] = (
+        None  # includes events associated with each message
+    )
+
+
+def thread_message_lookup_request(message_id: str, event_type: str) -> dict:
+    """retrieve messages and events for the thread associated with a message"""
+    return {
+        "id": message_id,
+        "relations": {
+            "thread": {
+                "relations": {
+                    "messages": {
+                        "relations": {"events": {"filter": {"type": event_type}}},
+                        "apply_annotations_from_actors": ["*"],
+                    },
+                }
+            }
+        },
+    }
