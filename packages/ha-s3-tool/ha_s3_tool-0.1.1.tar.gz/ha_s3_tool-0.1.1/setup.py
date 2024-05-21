738df69974fa71bdca94a086da018b99cb5e4d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:04:54 +0000

    Add package ha-s3-tool/0.1.1/ha_s3_tool-0.1.1.tar.gz

---
+++ packages/ha-s3-tool/ha_s3_tool-0.1.1.tar.gz/ha_s3_tool-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys 
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        if 'install' in sys.argv:
+            subprocess.call([sys.executable, 'post_install.py'])
+
+setup(
+    name="ha-s3-tool",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "s3-upload=s3_tool.s3_tool:upload_command",
+            "s3-download=s3_tool.s3_tool:download_command",
+        ],
+    },
+    author="Manuel Rodriguez Ladron de Guevara",
+    author_email="manuelrodriguez@higharc.com",
+    description="A tool for uploading and downloading files and folders to/from S3.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/manuelladron/s3_tool",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
