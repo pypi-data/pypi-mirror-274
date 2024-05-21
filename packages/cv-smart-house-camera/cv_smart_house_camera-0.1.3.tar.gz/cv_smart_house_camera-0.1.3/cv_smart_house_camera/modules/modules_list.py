fd8adde2ebe3c25a878f122338f893be757345d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 05:04:53 +0000

    Add package cv-smart-house-camera/0.1.3/cv_smart_house_camera-0.1.3.tar.gz

---
+++ packages/cv-smart-house-camera/cv_smart_house_camera-0.1.3.tar.gz/cv_smart_house_camera-0.1.3/cv_smart_house_camera/modules/modules_list.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def test_module_processing(frame, frame_number):
+	center_x = frame.shape[1] // 2
+	center_y = frame.shape[0] // 2
+
+	# Draw a circle at the center
+	radius = 50
+	color = (0, 255, 0)  # BGR color (here, green)
+	thickness = 2
+	cv2.circle(frame, (center_x, center_y), radius, color, thickness)
+	return {"frame": frame}
+
+
+
+
+def original_frame_processing(frame, frame_number):
+	return
+
+modules = [
+{ "name": "Original Frame", "package_name": "original", "processing": original_frame_processing }
+]
