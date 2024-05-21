10c196db2a7d874bc7514d02f4257c9624069586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:27 +0000

    Add package drf-bulk-editing/0.1.4/drf-bulk-editing-0.1.4.tar.gz

---
+++ packages/drf-bulk-editing/drf-bulk-editing-0.1.4.tar.gz/drf-bulk-editing-0.1.4/README.md
@@ -0,0 +1,8 @@
+# JFrog Security Holding Package
+
+This is a security holding package, a placeholder for a deleted package susceptible to hijacking.
+
+This is a completely empty package.
+
+For inquiries: [sec_reporting@service.jfrog.com](mailto:sec_reporting@service.jfrog.com)
+
---
+++ packages/drf-bulk-editing/drf-bulk-editing-0.1.4.tar.gz/drf-bulk-editing-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='drf-bulk-editing',
+    version='0.1.4',
+    packages=['drf-bulk-editing'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
