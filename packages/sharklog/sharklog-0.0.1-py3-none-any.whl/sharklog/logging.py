73f916ccc6cc42ba86fe2e3166a734b7d5b8daca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 15:04:13 +0000

    Add package sharklog/0.0.1/sharklog-0.0.1-py3-none-any.whl

---
+++ packages/sharklog/sharklog-0.0.1-py3-none-any.whl/sharklog/logging.py
@@ -0,0 +1,27 @@
+import logging
+from logging import Logger
+import inspect
+
+
+
+
+
+
+
+def getLogger() -> Logger:
+    pass
+
+
+
+def debug() -> None:
+    pass
+
+
+def info() -> None:
+    pass
+
+def warning() -> None:
+    pass
+
+def error() -> None:
+    pass
