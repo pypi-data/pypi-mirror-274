9dc9dd76ddf4736cdb7c5d17be7e8f59134f5dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:23 +0000

    Add package pyre/1.12.5/pyre-1.12.5-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/pyre/pyre-1.12.5-cp312-cp312-macosx_10_9_x86_64.whl/journal/Informational.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# superclasses
+from .Channel import Channel
+
+
+# the implementation of the debug channel
+class Informational(Channel, active=True, fatal=False):
+    """
+    Informational channels are used for communicating application progress to users
+    """
+
+
+    # types
+    from .exceptions import ApplicationError
+
+
+    # implementation details
+    def record(self):
+        """
+        Commit my payload to the journal
+        """
+        # hunt down my device and record the entry
+        self.device.alert(entry=self.entry)
+        # all done
+        return self
+
+
+    # constants
+    severity = "info"              # the channel severity
+    fatalError = ApplicationError  # the exception i raise when i'm fatal
+
+
+# end of file
