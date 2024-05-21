44ffeb6418297416d798d47bdc49b74d10de4e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:56 +0000

    Add package slot-can/0.1.0/slot_can-0.1.0.tar.gz

---
+++ packages/slot-can/slot_can-0.1.0.tar.gz/slot_can-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slot_can',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
