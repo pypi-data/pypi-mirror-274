f7bc897dd6e3839cbd712b301fa18f10441b5a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 15:05:06 +0000

    Add package taskara/0.1.86/taskara-0.1.86.tar.gz

---
+++ packages/taskara/taskara-0.1.86.tar.gz/taskara-0.1.86/taskara/db/models.py
@@ -0,0 +1,47 @@
+import time
+
+from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class TaskRecord(Base):
+    __tablename__ = "tasks"
+
+    id = Column(String, primary_key=True)
+    owner_id = Column(String, nullable=True)
+    description = Column(String, nullable=False)
+    max_steps = Column(Integer, nullable=False, default=30)
+    device = Column(String, nullable=True)
+    device_type = Column(String, nullable=True)
+    assigned_to = Column(String, nullable=True)
+    assigned_type = Column(String, nullable=True)
+    status = Column(String, nullable=False)
+    created = Column(Float, nullable=False)
+    started = Column(Float, nullable=False, default=0.0)
+    completed = Column(Float, nullable=False, default=0.0)
+    error = Column(String, nullable=True)
+    output = Column(String, nullable=True)
+    threads = Column(String, nullable=False)
+    prompts = Column(String, nullable=True)
+    parameters = Column(String, nullable=True)
+    version = Column(String, nullable=True)
+    tags = Column(String, nullable=True)
+    labels = Column(String, nullable=True)
+    episode_id = Column(String, nullable=True)
+
+
+class TrackerRecord(Base):
+    __tablename__ = "trackers"
+
+    id = Column(String, primary_key=True)
+    name = Column(String, unique=True, index=True)
+    runtime_name = Column(String)
+    runtime_config = Column(String)
+    status = Column(String)
+    port = Column(Integer)
+    owner_id = Column(String, nullable=True)
+    labels = Column(String, nullable=True)
+    created = Column(Float, default=time.time)
+    updated = Column(Float, default=time.time)
