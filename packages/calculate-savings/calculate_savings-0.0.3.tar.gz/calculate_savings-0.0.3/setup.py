db1dcd0c52444fa87c1f418c34b99411921a6c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:05:05 +0000

    Add package calculate-savings/0.0.3/calculate_savings-0.0.3.tar.gz

---
+++ packages/calculate-savings/calculate_savings-0.0.3.tar.gz/calculate_savings-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="calculate_savings",
+    version="0.0.3",
+    author="YukiTakenaka",
+    author_email="s2222024@stu.musashino-u.ac.jp",
+    description="How much can you save in 10 years by predicting your salary?",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/takenakayuuki0901/calculate_savings.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/takenakayuuki0901/calculate_savings.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=["calculate_savings"],
+    packages=setuptools.find_packages(where="sre"),
+    include_package_data=True,
+    package_data={"calculate_savings": ["data/*.csv"]},
+    python_requires=">=3.7",
+    entry_points={
+        "console _scripts": ["calculate_savings = calculate_savings.main:main"]
+    },
+)
