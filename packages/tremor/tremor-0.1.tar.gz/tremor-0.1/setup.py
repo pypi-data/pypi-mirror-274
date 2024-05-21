d06fde795eb8d4fa4e311ebce18aa3a900322ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 15:03:49 +0000

    Add package tremor/0.1/tremor-0.1.tar.gz

---
+++ packages/tremor/tremor-0.1.tar.gz/tremor-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tremor',
+    version=0.1,
+    description='Placeholder for private GA package',
+    author='Brenainn Moushall',
+    author_email='brenainn.moushall@ga.gov.au',
+)
