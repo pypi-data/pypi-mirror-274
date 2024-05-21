a812b800f60eb6d6d2dbfca0ab8b37e6e8b4d461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 03:03:51 +0000

    Add package mmcore/0.26.0/mmcore-0.26.0.tar.gz

---
+++ packages/mmcore/mmcore-0.26.0.tar.gz/mmcore-0.26.0/README.md
@@ -0,0 +1,21 @@
+[![Docker](https://github.com/contextmachine/mmcore/actions/workflows/docker-publish.yml/badge.svg)](https://github.com/contextmachine/mmcore/actions/workflows/docker-publish.yml)
+[![Build documentation](https://github.com/contextmachine/mmcore/actions/workflows/build-docs.yml/badge.svg)](https://github.com/contextmachine/mmcore/actions/workflows/build-docs.yml)
+[![poetry-build](https://github.com/contextmachine/mmcore/actions/workflows/poetry-build.yml/badge.svg)](https://github.com/contextmachine/mmcore/actions/workflows/poetry-build.yml)
+# mmcore
+
+
+## Base Docker Image
+Pull image
+```bash
+docker pull ghcr.io/contextmachine/mmcore:latest
+```
+Using in Dockerfile
+```dockerfile
+FROM ghcr.io/contextmachine/mmcore:latest
+```
+## Install with pip
+```bash
+python -m pip install git+https://github.com/contextmachine/mmcore.git
+```
+
+---
---
+++ packages/mmcore/mmcore-0.26.0.tar.gz/mmcore-0.26.0/build.py
@@ -0,0 +1,59 @@
+import sys
+
+if sys.platform.startswith('win'):
+    import pyMSVC
+
+    environment = pyMSVC.setup_environment()
+    print(environment)
+
+import setuptools
+import numpy
+# rest of setup code here
+from setuptools import Extension, Distribution
+from setuptools.command.build_ext import build_ext
+
+from setuptools.command.build_py import build_py
+from Cython.Build import cythonize
+
+compile_args = ["-O3"]
+link_args = []
+include_dirs = [numpy.get_include()]
+
+extensions = [
+    Extension(
+        "mmcore.geom.vec.vec_speedups",
+        ["mmcore/geom/vec/vec_speedups.pyx"],
+        extra_compile_args=compile_args,
+        extra_link_args=link_args,
+        include_dirs=include_dirs
+
+    ), Extension(
+        "mmcore.geom.curves.deboor",
+        ["mmcore/geom/curves/deboor.pyx"],
+        extra_compile_args=compile_args,
+        extra_link_args=link_args,
+        include_dirs=[numpy.get_include(), 'mmcore/geom/curves']
+
+    )
+
+
+]
+#Extension(
+#        "mmcore.geom.mesh.mesh",
+#        ["mmcore/geom/mesh/mesh.pyx"],
+#        extra_compile_args=compile_args,
+#        extra_link_args=link_args,
+#        include_dirs=[numpy.get_include(), 'mmcore/geom/mesh']
+
+#    ),
+ext_modules = cythonize(extensions, include_path=[numpy.get_include(), 'mmcore/cmmcore'])
+dist = Distribution({"ext_modules": ext_modules})
+cmd = build_ext(dist)
+cmd.ensure_finalized()
+cmd.run()
+
+import os, shutil
+
+for output in cmd.get_outputs():
+    relative_extension = os.path.relpath(output, cmd.build_lib)
+    shutil.copyfile(output, relative_extension)
