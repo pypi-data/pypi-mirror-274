37026aea7a5fb1c1e2ba58bc8311ce4c0d3c4c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:03:20 +0000

    Add package scrcpy-namhv4/0.1/scrcpy_namhv4-0.1.tar.gz

---
+++ packages/scrcpy-namhv4/scrcpy_namhv4-0.1.tar.gz/scrcpy_namhv4-0.1/README.md
@@ -0,0 +1,8 @@
+# My Module
+
+This is a simple hello world module.
+
+## Installation
+
+```bash
+pip install my_module
---
+++ packages/scrcpy-namhv4/scrcpy_namhv4-0.1.tar.gz/scrcpy_namhv4-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrcpy_namhv4',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Các package phụ thuộc nếu có
+    description='A simple hello world module',
+    author='Your Name',
+    author_email='namhoang2110@gmail.com',
+    url='https://github.com/NamHV4/scrcpy_namhv4',  # URL đến repo của bạn
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
