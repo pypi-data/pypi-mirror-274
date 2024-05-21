c12005c3da148b3ba7634dba26e61b1b5064b426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:56 +0000

    Add package lk-logger/5.7.3/lk_logger-5.7.3-py3-none-any.whl

---
+++ packages/lk-logger/lk_logger-5.7.3-py3-none-any.whl/lk_logger/console.py
@@ -0,0 +1,43 @@
+from typing import Any
+
+from rich.console import Console as BaseConsole
+
+
+class Console(BaseConsole):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        if self._color_system is None:
+            try:
+                # in rich version >= 12.5, the color system is changed to be a
+                # contant integer.
+                from rich.console import ColorSystem
+                self._color_system = ColorSystem.STANDARD
+            except:
+                # the older version is using a string.
+                self._color_system = 'standard'
+        
+        # TODO (width):
+        #   if width longer than default, use single line style; otherwise
+        #   split sourcemap and message into different lines.
+        pass
+    
+    def print(
+        self, *objects: Any, soft_wrap: bool = True, **kwargs
+    ) -> None:
+        from .message_builder import MessageStruct
+        # pop incompatible arguments
+        # bg: streamlit passes `file` to `sys.stdout` which causes an error here.
+        kwargs.pop('file', None)
+        # kwargs.pop('flush', None)
+        if len(objects) == 1 and isinstance(objects[0], MessageStruct):
+            super().print(
+                objects[0].text, overflow='fold', soft_wrap=soft_wrap, **kwargs
+            )
+        else:
+            super().print(
+                *objects, overflow='fold', soft_wrap=soft_wrap, **kwargs
+            )
+
+
+console = Console()
