21d55cd9d65361ed76496c2e7b654ca55278bd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 16:04:51 +0000

    Add package xbox-python-api/0.1.3/xbox_python_api-0.1.3-py3-none-any.whl

---
+++ packages/xbox-python-api/xbox_python_api-0.1.3-py3-none-any.whl/xpa/classes/XUID_PRESENCE.py
@@ -0,0 +1,7 @@
+class XUID_PRESENCE:
+    def __init__(self, state, last_seen_device_type, last_seen_title_id, last_seen_title_name, last_seen_timestamp):
+        self.state = state
+        self.last_seen_device_type = last_seen_device_type
+        self.last_seen_title_id = last_seen_title_id
+        self.last_seen_title_name = last_seen_title_name
+        self.last_seen_timestamp = last_seen_timestamp
