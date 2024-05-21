98cd961b0da745c17d056167af306c3406d5bf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 04:04:07 +0000

    Add package amplpy/0.13.4b0/amplpy-0.13.4b0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/amplpy/amplpy-0.13.4b0-cp310-cp310-macosx_10_9_x86_64.whl/amplpy/amplpython/cppinterface/include/ampl/ep/error_information.h
@@ -0,0 +1,46 @@
+#ifndef AMPL_ERROR_INFORMATION_H_
+#define AMPL_ERROR_INFORMATION_H_
+
+#include <cstring>
+
+#include "ampl/amplexception.h"
+#include "ampl/ep/declarations.h"
+
+namespace ampl {
+namespace internal {
+/*
+C++ exception types
+*/
+namespace error {
+enum Type {
+  OK = 0,
+  AMPLException,
+  LicenseException,
+  FileIOException,
+  UnsupportedOperationException,
+  InvalidSubscriptException,
+  SyntaxErrorException,
+  NoDataException,
+  Logic_Error,
+  Runtime_Error,
+  Invalid_Argument,
+  Out_Of_Range,
+  Std_Exception,
+  PresolveException,
+  InfeasibilityException
+
+};
+}
+
+struct ErrorInformation {
+  error::Type errorType;
+  char *message;
+  int line;
+  int offset;
+  char *source;
+  ErrorInformation() : errorType(error::OK), message(NULL), source(NULL) {}
+};
+}  // namespace internal
+}  // namespace ampl
+
+#endif  // AMPL_ERROR_INFORMATION_H_
