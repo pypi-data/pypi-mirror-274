9566f03ac5b3b325a68035958aeefda9df039c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-21 14:03:55 +0000

    Add package juriscraper/2.6.2/juriscraper-2.6.2-py27-none-any.whl

---
+++ packages/juriscraper/juriscraper-2.6.2-py27-none-any.whl/juriscraper/opinions/united_states/state/nmctapp.py
@@ -0,0 +1,8 @@
+from juriscraper.opinions.united_states.state import nm
+
+
+class Site(nm.Site):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.court_id = self.__module__
+        self.url = f"https://nmonesource.com/nmos/nmca/en/{self.year}/nav_date.do?iframe=true"
