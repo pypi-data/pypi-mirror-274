8ebe1382c0dc4d4b2669125c6adb5e6e564d307c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:14 +0000

    Add package parsee-core/0.1.4.34/parsee_core-0.1.4.34-py3-none-any.whl

---
+++ packages/parsee-core/parsee_core-0.1.4.34-py3-none-any.whl/parsee/extraction/models/llm_models/prompts.py
@@ -0,0 +1,27 @@
+from typing import *
+
+from parsee.datasets.dataset_dataclasses import DatasetRow
+from parsee.extraction.extractor_dataclasses import Base64Image
+
+
+class Prompt:
+
+    def __init__(self, description: Optional[str], main_task: str, additional_info: Optional[str] = None, full_example: Optional[str] = None, available_data: Optional[Union[str, List[Base64Image]]] = None):
+
+        self.description = f"{description} \n" if description is not None else ""
+        self.main_task = main_task
+        self.additional_info = f"{additional_info} \n" if additional_info is not None else ""
+        self.full_example = f"{full_example} \n" if full_example is not None else ""
+        self.available_data = available_data if available_data is not None else ""
+
+    def __str__(self) -> str:
+        return f'''{self.description}
+
+                    {self.main_task} \n
+                    
+                    {self.additional_info}
+
+                    {self.full_example}
+                    
+                    {self.available_data if isinstance(self.available_data, str) else ''}
+                    '''
---
+++ packages/parsee-core/parsee_core-0.1.4.34-py3-none-any.whl/parsee/storage/interfaces.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from typing import *
+
+from parsee.extraction.extractor_elements import StandardDocumentFormat, FinalOutputTableColumn, ParseeAnswer
+from parsee.utils.enums import DocumentType
+from parsee.templates.job_template import JobTemplate
+from parsee.extraction.extractor_dataclasses import ParseeBucket
+from parsee.extraction.models.model_dataclasses import MlModelSpecification
+from parsee.datasets.readers.interfaces import ModelReader
+from parsee.storage.vector_stores.interfaces import VectorStore
+from parsee.extraction.extractor_elements import ExtractedEl
+from parsee.converters.image_creation import ImageCreator
+
+
+class StorageManager:
+
+    vector_store: VectorStore
+    image_creator: ImageCreator
+
+    def __init__(self, vector_store: VectorStore, image_creator: ImageCreator):
+        self.vector_store = vector_store
+        self.image_creator = image_creator
+
+    def db_values_template(self, job_template: JobTemplate, strict: bool) -> JobTemplate:
+        raise NotImplementedError
+
+    def log_expense(self, service: str, amount: Decimal, class_id: str):
+        raise NotImplementedError
+
+    def get_available_models(self) -> List[MlModelSpecification]:
+        raise NotImplementedError
