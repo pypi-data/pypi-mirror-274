a3e8da4c177a221e39a32543c2be6155feca637c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:55 +0000

    Add package dzidb/1.0.12/dzidb-1.0.12-py3-none-any.whl

---
+++ packages/dzidb/dzidb-1.0.12-py3-none-any.whl/dzidb/exceptions.py
@@ -0,0 +1,50 @@
+
+__all__ = [
+    'BaseError', 'MuxError', 'MuxReplyError', 'MuxVersionError', 'MuxServiceError', 'ServiceError',
+    'SocketError', 'DownloadError', 'DeveloperImageError',
+    'IPAError'
+]
+
+from _proto import UsbmuxReplyCode
+
+
+class BaseError(OSError):
+    pass
+
+
+class MuxError(BaseError):
+    """ Mutex error """
+    pass
+
+
+class MuxReplyError(MuxError):
+    def __init__(self, number: int):
+        self.reply_code = UsbmuxReplyCode(number)
+        super().__init__(self.reply_code)
+
+
+class MuxVersionError(MuxError):
+    """ usbmuxd version not match """
+
+
+class ServiceError(MuxError):
+    """ Service error """
+
+
+class MuxServiceError(ServiceError):
+    pass
+
+
+class SocketError(MuxError):
+    """ Socket timeout error """
+
+
+class IPAError(BaseError):
+    """ IPA error """
+
+
+class DownloadError(BaseError):
+    """ Download error """
+
+class DeveloperImageError(BaseError):
+    """ Developer image error """
