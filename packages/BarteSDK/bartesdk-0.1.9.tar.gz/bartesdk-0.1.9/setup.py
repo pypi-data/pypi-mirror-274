6c48c89c702613925756ba4112e1328c477f210e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:25 +0000

    Add package BarteSDK/0.1.9/bartesdk-0.1.9.tar.gz

---
+++ packages/BarteSDK/bartesdk-0.1.9.tar.gz/bartesdk-0.1.9/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bartesdk',
+    version='0.1.9',
+    packages=find_packages(where='bartesdk'),
+    package_dir={'': 'bartesdk'},
+    install_requires=[
+        'requests',
+    ],
+    author='Engenharia de Plataforma da Barte',
+    author_email='devops@barte.com',
+    description='SDK para interação com a API da Barte.',
+    license='MIT',
+    keywords='barte pay sdk',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+)
