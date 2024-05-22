118b2928cba6ee445b8b71c806cbd6f9feabf060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:44 +0000

    Add package smart-terminal/0.0.1/smart_terminal-0.0.1.tar.gz

---
+++ packages/smart-terminal/smart_terminal-0.0.1.tar.gz/smart_terminal-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smart_terminal',
+    version='0.0.1',
+    author='Aditya Kakarla',
+    author_email='adityak0523@gmail.com',
+    description='The copilot for running terminal commands.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            "copilot = smart_terminal.cli_tool:cli"
+        ]
+    },
+    install_requires=[
+        'click',
+        'ollama',
+        'click_default_group'
+    ]
+)
