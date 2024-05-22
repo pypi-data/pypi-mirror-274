42d96aff956b24d0881b223e7f184cc9b5cedfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:12 +0000

    Add package bandicoincurrency/0.0.5/bandicoincurrency-0.0.5.tar.gz

---
+++ packages/bandicoincurrency/bandicoincurrency-0.0.5.tar.gz/bandicoincurrency-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bandicoincurrency",
+    version="0.0.5",
+    author="cbt1",
+    description="Bandicoin scratch project wrapper for external payments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
