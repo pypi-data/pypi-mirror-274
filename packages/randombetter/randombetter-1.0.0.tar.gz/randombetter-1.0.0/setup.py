b4770acceba386352e8b2a107c20f89b4ab86cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:05:04 +0000

    Add package randombetter/1.0.0/randombetter-1.0.0.tar.gz

---
+++ packages/randombetter/randombetter-1.0.0.tar.gz/randombetter-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='randombetter',
+    version='1.0.0',
+    author='Marek',
+    author_email='marekciganik0603@gmail.com',
+    description='test package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
