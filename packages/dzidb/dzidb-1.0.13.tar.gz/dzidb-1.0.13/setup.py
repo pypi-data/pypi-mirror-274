e7870efe22c82a0b6abdf69136470130ccfd8602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:20 +0000

    Add package dzidb/1.0.13/dzidb-1.0.13.tar.gz

---
+++ packages/dzidb/dzidb-1.0.13.tar.gz/dzidb-1.0.13/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license_text = fh.read()
+
+setuptools.setup(
+    name="dzidb",
+    version="1.0.13",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A custom adb-like tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/dzidb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # 示例：MIT License
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'dzidb = dzidb.__main__:main',
+        ],
+    },
+    license=license_text,  # 添加许可证信息
+)
