01562f3dfa2e5089d887d0f58c046176e2eacfa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 17:04:36 +0000

    Add package g3projects/0.2.4/g3projects-0.2.4.tar.gz

---
+++ packages/g3projects/g3projects-0.2.4.tar.gz/g3projects-0.2.4/src/g3projects/utils/__init__.py
@@ -0,0 +1,14 @@
+from ._utils import (
+    format_template,
+    combine_str,
+    remove_multiple_newlines,
+    write_to_file
+)
+
+
+__all__ = [
+    'format_template',
+    'combine_str',
+    'remove_multiple_newlines',
+    'write_to_file'
+    ]
