ab2f2c5b7fc773bf562ec8ad8005d0d288027e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:22 +0000

    Add package EAACommander/0.2/EAACommander-0.2.tar.gz

---
+++ packages/EAACommander/EAACommander-0.2.tar.gz/EAACommander-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EAACommander',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'art',
+    ],
+    entry_points={
+        'console_scripts': [
+            'eaacommander=EAACommander.cli_client:main',
+        ],
+    },
+    author='Marko Žnidar',
+    author_email='marko.znidar@gmail.com',
+    description='A tool for Electronically Assisted Astronomy (EAA) enthusiasts',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MarkoZnidar/EAACommander/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
