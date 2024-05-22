91f7bb209ba5ec2bc5b1bc05d81e9dcb6c1a1db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:10 +0000

    Add package drf-serializer-dumps/1.0.4/drf_serializer_dumps-1.0.4.tar.gz

---
+++ packages/drf-serializer-dumps/drf_serializer_dumps-1.0.4.tar.gz/drf_serializer_dumps-1.0.4/tests/serializers.py
@@ -0,0 +1,47 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
+from tests.models import Person
+
+
+class PersonCars(serializers.Serializer):
+    car_name = serializers.CharField()
+    car_price = serializers.IntegerField()
+
+
+class PersonHouse(serializers.Serializer):
+    address = serializers.CharField()
+    cfield = serializers.ChoiceField(choices=())
+
+
+class PersonSerializer1(serializers.Serializer):
+    name = serializers.CharField()
+    age = serializers.IntegerField()
+    birthday = serializers.DateTimeField()
+
+    field_without_annotation = serializers.SerializerMethodField()
+
+    def get_field_without_annotation(self, obj: object):  # type: ignore[no-untyped-def]
+        return 'Hello world!'
+
+    height = serializers.SerializerMethodField()
+
+    def get_height(self, obj: object) -> int:
+        return 184
+
+    weight = serializers.SerializerMethodField()
+
+    @extend_schema_field(OpenApiTypes.INT)
+    def get_weight(self, obj):  # type: ignore[no-untyped-def]
+        return 78
+
+    cars = PersonCars(many=True)
+
+    house = PersonHouse()
+
+
+class PersonSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__'
---
+++ packages/drf-serializer-dumps/drf_serializer_dumps-1.0.4.tar.gz/drf_serializer_dumps-1.0.4/tests/test_decorators.py
@@ -0,0 +1,52 @@
+from time import sleep
+from unittest.mock import ANY
+
+from src.drf_serializer_dumps.decorators import serializer_dumps
+
+from tests.serializers import PersonSerializer1, PersonSerializer2
+
+
+# python -m pip install .
+# pytest -s ./tests
+def test_serializer_dumps() -> None:
+    """"""
+    result = serializer_dumps(PersonSerializer1, exclude_fields=['age'])
+    assert result == {
+        'name': 'string',
+        'birthday': ANY,
+        'field_without_annotation': None,
+        'height': 1,
+        'weight': 1,
+        'cars': [{'car_name': 'string', 'car_price': 1}],
+        'house': {'address': 'string', 'cfield': 'string'},
+    }
+
+
+def test_model_serializer_dumps() -> None:
+    """"""
+    result = serializer_dumps(PersonSerializer2)
+    assert result == {'id': 1, 'name': 'string', 'phones': ['string']}
+
+
+def test_renew_type_value() -> None:
+    """"""
+    expected = {
+        'name': 'string',
+        'age': 1,
+        'birthday': ANY,
+        'field_without_annotation': None,
+        'height': 1,
+        'weight': 1,
+        'cars': [{'car_name': 'string', 'car_price': 1}],
+        'house': {'address': 'string', 'cfield': 'string'},
+    }
+
+    result1 = serializer_dumps(PersonSerializer1, renew_type_value=True)
+    assert result1 == expected
+
+    sleep(0.01)
+
+    result2 = serializer_dumps(PersonSerializer1, renew_type_value=True)
+    assert result2 == expected
+
+    assert result1['birthday'] != result2['birthday']
