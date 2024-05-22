cf3dce2f4b6803e792e61da261f086ca5b82aa8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 17:04:42 +0000

    Add package AlekSIS-Core/4.0.0.dev9/aleksis_core-4.0.0.dev9.tar.gz

---
+++ packages/AlekSIS-Core/aleksis_core-4.0.0.dev9.tar.gz/aleksis_core-4.0.0.dev9/aleksis/core/frontend/components/generic/chips/CounterChip.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-chip v-bind="$attrs" v-on="$listeners">
+    <v-avatar left>
+      {{ count }}
+    </v-avatar>
+    <slot />
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: "CounterChip",
+  extends: "v-chip",
+  props: {
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
