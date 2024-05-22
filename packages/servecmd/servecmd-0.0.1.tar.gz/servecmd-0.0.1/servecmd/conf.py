4bb40ef9bd7f0880b25c55c868109395f5e3ac7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:09 +0000

    Add package servecmd/0.0.1/servecmd-0.0.1.tar.gz

---
+++ packages/servecmd/servecmd-0.0.1.tar.gz/servecmd-0.0.1/servecmd/conf.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import yaml
+
+CONFIG = {
+
+}
+
+CONFIG_SEARCH_LOCATIONS = [
+    'servecmd.yaml',
+    'servecmd.json',
+]
+
+
+def load(config_file=None):
+    locations = []
+    if config_file:
+        locations.append(config_file)
+    else:
+        locations.extend(CONFIG_SEARCH_LOCATIONS)
+    for location in locations:
+        try:
+            with open(location) as f:
+                if location.endswith('.json'):
+                    CONFIG.update(json.load(f))
+                elif location.endswith('.yaml') or location.endswith('.yml'):
+                    CONFIG.update(yaml.safe_load(f))
+                else:
+                    raise ValueError('Unsupported config file type')
+        except FileNotFoundError:
+            pass
+    # setup logging
+    logging.basicConfig(level=CONFIG.get('log_level', 'INFO'))
