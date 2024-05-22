923ae95d0d62c8ab7cfab847e4a2c19d24ce7ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:10 +0000

    Add package loopies-scraper/0.0.3/loopies-scraper-0.0.3.tar.gz

---
+++ packages/loopies-scraper/loopies-scraper-0.0.3.tar.gz/loopies-scraper-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Web scraper for basic and/or list of pages scraping'
+LONG_DESCRIPTION = 'Wrapper for Selenium WebDriver and its configuration to turn it into web scraper to scrape list of pages or to use it as prebuilt scraper'
+
+# Setting up
+setup(
+    name="loopies-scraper",
+    version=VERSION,
+    author="tmspsk",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=['selenium', 'fake_useragent'],
+    keywords=['python', 'scraper', 'multiprocess scraper', 'list', 'selenium webdriver'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
