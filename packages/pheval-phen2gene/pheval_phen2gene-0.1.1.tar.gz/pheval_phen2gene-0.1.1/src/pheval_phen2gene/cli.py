e325f39c70cb33bc00aad84f18b17716f4c6d4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:31 +0000

    Add package pheval-phen2gene/0.1.1/pheval_phen2gene-0.1.1.tar.gz

---
+++ packages/pheval-phen2gene/pheval_phen2gene-0.1.1.tar.gz/pheval_phen2gene-0.1.1/src/pheval_phen2gene/cli.py
@@ -0,0 +1,20 @@
+import click
+
+from pheval_phen2gene.cli_phen2gene import (
+    prepare_commands_command,
+    prepare_input_command,
+    prepare_inputs_command,
+)
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(prepare_input_command)
+main.add_command(prepare_inputs_command)
+main.add_command(prepare_commands_command)
+
+if __name__ == "__main__":
+    main()
