27ae8ce97cce00c3309ad0f3fd62ddd2efb33f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:05:40 +0000

    Add package py-distance-transforms/0.1.0/py_distance_transforms-0.1.0-py3-none-any.whl

---
+++ packages/py-distance-transforms/py_distance_transforms-0.1.0-py3-none-any.whl/py_distance_transforms/julia_import.py
@@ -0,0 +1,8 @@
+from juliacall import Main as jl
+
+jl.seval("using Pkg; Pkg.status()")
+jl.seval("using DistanceTransforms")
+jl.seval("using CUDA")
+
+DistanceTransforms = jl.DistanceTransforms
+CUDA = jl.CUDA
