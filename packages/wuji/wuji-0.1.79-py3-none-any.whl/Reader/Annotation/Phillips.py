a8b5c37f1a37734c6951f17c3d2fcf0722607cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:36 +0000

    Add package wuji/0.1.79/wuji-0.1.79-py3-none-any.whl

---
+++ packages/wuji/wuji-0.1.79-py3-none-any.whl/Reader/Annotation/Phillips.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File        :   Phillip 
+@Time        :   2023/9/14 15:40
+@Author      :   Xuesong Chen
+@Description :   
+"""
---
+++ packages/wuji/wuji-0.1.79-py3-none-any.whl/Reader/EDF/Philips.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File        :   Philips 
+@Time        :   2023/9/14 15:41
+@Author      :   Xuesong Chen
+@Description :   
+"""
+
