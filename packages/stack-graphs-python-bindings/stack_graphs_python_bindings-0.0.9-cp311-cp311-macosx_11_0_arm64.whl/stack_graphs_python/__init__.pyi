93fc92e77cc8e788ab178d8909b913b204c537d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:35 +0000

    Add package stack-graphs-python-bindings/0.0.9/stack_graphs_python_bindings-0.0.9-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/stack-graphs-python-bindings/stack_graphs_python_bindings-0.0.9-cp311-cp311-macosx_11_0_arm64.whl/stack_graphs_python/__init__.pyi
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Language(Enum):
+    Python = 0
+    JavaScript = 1
+    TypeScript = 2
+    Java = 3
+
+class Position:
+    path: str
+    line: int
+    column: int
+
+    def __init__(self, path: str, line: int, column: int) -> None: ...
+    def __eq__(self, other: object) -> bool: ...
+    def __repr__(self) -> str: ...
+
+class Querier:
+    def __init__(self, db_path: str) -> None: ...
+    def definitions(self, reference: Position) -> list[Position]: ...
+    def __repr__(self) -> str: ...
+
+def index(paths: list[str], db_path: str, language: Language) -> None: ...
