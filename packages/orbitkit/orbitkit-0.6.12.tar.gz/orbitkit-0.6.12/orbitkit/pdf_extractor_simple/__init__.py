f1dd9e092b4fcf430327fd94e1c0b9b1a69a5fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:02 +0000

    Add package orbitkit/0.6.12/orbitkit-0.6.12.tar.gz

---
+++ packages/orbitkit/orbitkit-0.6.12.tar.gz/orbitkit-0.6.12/orbitkit/pdf_extractor_simple/__init__.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from orbitkit.pdf_extractor_simple.base import CloudObjectProvider, PdfExtractor
+from orbitkit.pdf_extractor_simple.cloud_provider import AwsCloudObjectProvider, OssCloudObjectProvider
+from orbitkit.pdf_extractor_simple.core import CorePdfExtract
+from orbitkit.pdf_extractor_simple.extractors import PyPdfPdfExtractor, MixedPdfPdfExtractor
+
+
+class ExtractPdfSimpleTxtByCloud:
+    def __init__(self, skip_ocr_exceed_page=0, *args, **kwargs):
+        self.core_pdf_extract = CorePdfExtract(*args, **kwargs)
+        # Add extractor
+        self.core_pdf_extract.add_pdf_extractor(MixedPdfPdfExtractor(issue_page_per=95,
+                                                                     skip_ocr_exceed_page=skip_ocr_exceed_page))
+
+    def pdf_extract(self,
+                    cloud_path: str,
+                    cloud_txt_path: Optional[str] = None,
+                    copy_path: Optional[str] = None,
+                    auto_upload: bool = True,
+                    force_txt_overwrite: bool = False):
+        return self.core_pdf_extract.pdf_extract(cloud_path=cloud_path,
+                                                 cloud_txt_path=cloud_txt_path,
+                                                 copy_path=copy_path,
+                                                 auto_upload=auto_upload,
+                                                 force_txt_overwrite=force_txt_overwrite,
+                                                 return_txt_list=True)
