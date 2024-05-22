c7f3a8b8563182b3aa6f7304a8997829044fe61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:55 +0000

    Add package pydicomsorter/0.6.1/pydicomsorter-0.6.1-py3-none-any.whl

---
+++ packages/pydicomsorter/pydicomsorter-0.6.1-py3-none-any.whl/pydicomsorter/__init__.py
@@ -0,0 +1,15 @@
+"""This is a docstring for the public package."""
+
+from .io import find_dicom_files
+from .main import main
+
+version = '0.1.0'
+
+__version__ = version
+
+
+__all__ = [
+    'find_dicom_files',
+    'main',
+    'version',
+]
