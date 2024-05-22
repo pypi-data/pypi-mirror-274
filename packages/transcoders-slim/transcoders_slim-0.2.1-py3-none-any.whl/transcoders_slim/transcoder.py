cecc501ea7e9bbb5f8688ae0f80306c8cc09c63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:34 +0000

    Add package transcoders-slim/0.2.1/transcoders_slim-0.2.1-py3-none-any.whl

---
+++ packages/transcoders-slim/transcoders_slim-0.2.1-py3-none-any.whl/transcoders_slim/transcoder.py
@@ -0,0 +1,8 @@
+from transcoders_slim import sae_training  # noqa
+from transcoders_slim.sae_training.sparse_autoencoder import SparseAutoencoder
+
+
+class Transcoder(SparseAutoencoder):
+    """Dummy wrapper around base SAE class"""
+
+    pass
