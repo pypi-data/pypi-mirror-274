b531d2c29793ed9dc4538f47cd765f0ea1d99efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:58 +0000

    Add package stockwell/1.1.1/stockwell-1.1.1-cp310-cp310-win_amd64.whl

---
+++ packages/stockwell/stockwell-1.1.1-cp310-cp310-win_amd64.whl/stockwell/__init__.py
@@ -0,0 +1,27 @@
+"""
+Time-frequency analysis through Stockwell transform.
+
+:copyright:
+    2021-2024 Claudio Satriano <satriano@ipgp.fr>
+
+:license:
+    GNU General Public License v3.0 or later.
+    (https://www.gnu.org/licenses/gpl-3.0.html)
+"""
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_6_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'stockwell.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_6_0()
+del _delvewheel_patch_1_6_0
+# end delvewheel patch
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
