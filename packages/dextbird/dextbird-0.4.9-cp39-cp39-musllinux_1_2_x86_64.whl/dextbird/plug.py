ed3e36d8b6e2f6d663b192642d654d5fa2c78eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 06:05:00 +0000

    Add package dextbird/0.4.9/dextbird-0.4.9-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/dextbird/dextbird-0.4.9-cp39-cp39-musllinux_1_2_x86_64.whl/dextbird/plug.py
@@ -0,0 +1,28 @@
+from typing import TypedDict, Optional
+import discord
+
+
+class Option(TypedDict):
+    guild_id: int
+    channel_id: Optional[int]
+    self_deaf: bool
+    self_mute: bool
+
+
+class GuildNotFoundError(Exception):
+    pass
+
+
+async def update_voice_state(client: discord.Client, option: Option) -> None:
+    guild: Optional[discord.Guild] = client.get_guild(option["guild_id"])
+    if guild is not None:
+        channel = None
+        if option["channel_id"] is not None:
+            channel = discord.Object(id=option["channel_id"])
+        await guild.change_voice_state(
+            channel=channel,
+            self_deaf=option["self_deaf"],
+            self_mute=option["self_mute"],
+        )
+    else:
+        raise GuildNotFoundError("I can't found guild'")
