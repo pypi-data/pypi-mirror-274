243ca9d1421f9f8eb268683a92f34f7036593607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:23 +0000

    Add package MilesToMuppets/0.0.14/milestomuppets-0.0.14.tar.gz

---
+++ packages/MilesToMuppets/milestomuppets-0.0.14.tar.gz/milestomuppets-0.0.14/src/milesToMuppets/data.py
@@ -0,0 +1,17 @@
+# default_mph_speed = 670_616_629
+default_mph_speed = 60.0
+data = {
+        "songs": {
+            "Muppets Most Wanted": "3Z1dw9cLeFAyhvkXdn6P5G",
+            "The Muppets": "0mahHDhPnuYMbo3sXOEW50"
+        },
+
+        "key_list": {
+            0: "Muppets Most Wanted",
+            1: "The Muppets"
+        },
+        "constants": {
+            "defMphSpeed": float(default_mph_speed),
+            "defMinPerMile": float(60 / default_mph_speed)
+        }
+    }
