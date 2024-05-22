70af5f9f5a51c49f10997cc6bab3acb68a3d468d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 06:04:36 +0000

    Add package azureml-automl-runtime/1.56.0.post1/azureml_automl_runtime-1.56.0.post1-py3-none-any.whl

---
+++ packages/azureml-automl-runtime/azureml_automl_runtime-1.56.0.post1-py3-none-any.whl/azureml/automl/runtime/featurizer/transformer/timeseries/publicholidays/holidays.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Methods and classes used to get public holidays infomation."""
+from azureml.training.tabular.featurization.timeseries._holidays import (
+    PACKAGE_NAME,
+    HOLIDAYS_DATA_PATH,
+    _holiday_data_loader,
+    Holidays
+)
