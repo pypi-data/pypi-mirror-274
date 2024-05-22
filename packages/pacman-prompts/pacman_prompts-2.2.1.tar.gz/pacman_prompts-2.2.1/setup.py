267bc82f80cf5723e50c130369a4904316e70a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:26 +0000

    Add package pacman-prompts/2.2.1/pacman_prompts-2.2.1.tar.gz

---
+++ packages/pacman-prompts/pacman_prompts-2.2.1.tar.gz/pacman_prompts-2.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'pacman-prompts',
+  packages = find_packages(exclude=['examples']),
+  version = '2.2.1',
+  license='MIT',
+  description = 'Prompts As Code, man',
+  long_description_content_type = 'text/markdown',
+  author = 'Anish Thite',
+  author_email = 'anishthite@gmail.com',
+  url = 'https://github.com/anishthite/pacman',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'image recognition'
+  ],
+  install_requires=[
+      'pyyaml',
+      'openai',
+      'posthog',
+      'groq'
+  ],
+  setup_requires=[
+  ],
+  tests_require=[
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
