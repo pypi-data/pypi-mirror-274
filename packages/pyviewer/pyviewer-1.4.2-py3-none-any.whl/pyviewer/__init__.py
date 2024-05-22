a0760160a344ef9284e9ff219c75cb8a357c4335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:05:29 +0000

    Add package pyviewer/1.4.2/pyviewer-1.4.2-py3-none-any.whl

---
+++ packages/pyviewer/pyviewer-1.4.2-py3-none-any.whl/pyviewer/__init__.py
@@ -0,0 +1,10 @@
+from . import single_image_viewer
+from . import toolbar_viewer
+from . import gl_viewer
+
+# Check imgui version
+import imgui
+plot = getattr(imgui, 'plot', None)
+assert plot, \
+    'Pyviewer requires a custom version of imgui that comes bundled with implot (github.com/harskish/pyimgui).\n' + \
+    'Please reinstall pyviewer to get the correct version.'
