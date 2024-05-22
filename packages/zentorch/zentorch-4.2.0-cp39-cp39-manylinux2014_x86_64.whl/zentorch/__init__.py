08e079929598d75ff5154c316f32aa9ebc2dad7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:35 +0000

    Add package zentorch/4.2.0/zentorch-4.2.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/zentorch/zentorch-4.2.0-cp39-cp39-manylinux2014_x86_64.whl/zentorch/__init__.py
@@ -0,0 +1,9 @@
+# ******************************************************************************
+# Copyright (c) 2023-2024 Advanced Micro Devices, Inc.
+# All rights reserved.
+# ******************************************************************************
+
+from ._optimize import optimize # noqa
+from ._info import __config__, __version__ # noqa
+from ._compile_backend import * # noqa
+from ._meta_registrations import * # noqa
