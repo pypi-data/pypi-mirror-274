dba770f4e1c814d1b3659f33c0b603ebfb092489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:28 +0000

    Add package servicenow-api/0.20.48/servicenow_api-0.20.48-py2.py3-none-any.whl

---
+++ packages/servicenow-api/servicenow_api-0.20.48-py2.py3-none-any.whl/servicenow_api/utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# coding: utf-8
+from typing import Union
+
+import requests
+
+try:
+    from servicenow_api.servicenow_models import (
+        Response,
+    )
+except ModuleNotFoundError:
+    from servicenow_models import (
+        Response,
+    )
+
+
+def process_response(response: requests.Response) -> Union[Response, requests.Response]:
+    try:
+        response.raise_for_status()
+    except Exception as response_error:
+        print(f"Response Error: {response_error}")
+    status_code = response.status_code
+    raw = response.content
+    try:
+        response = response.json()
+    except Exception as response_error:
+        print(f"JSON Conversion Error: {response_error}")
+    try:
+        response = Response(**response, status_code=status_code, raw=raw, json=response)
+    except Exception as response_error:
+        print(f"Response Model Application Error: {response_error}")
+
+    return response
