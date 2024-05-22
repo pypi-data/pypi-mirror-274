2d7658737a5a35009f06d4a58c5eb2f34c512c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:21 +0000

    Add package masina/0.0.1/masina-0.0.1-py3-none-any.whl

---
+++ packages/masina/masina-0.0.1-py3-none-any.whl/masina/control.py
@@ -0,0 +1,21 @@
+from datetime import date
+import masina
+
+
+def get_auto_vals(ini_file, selectedStartDate, selectedEndDate):
+    auto = masina.Masina(ini_file)
+    alimentari = auto.get_alimentari_for_interval_type(selectedStartDate, selectedEndDate, None)
+    for i in alimentari:
+        print(i)
+    # print(auto.table_totals)
+
+
+def main():
+    selectedStartDate = date(2021, 12, 30)
+    selectedEndDate = date(2024, 1, 29)
+
+    income_ini = r"D:\Python\MySQL\cheltuieli_db.ini"
+    get_auto_vals(income_ini, selectedStartDate, selectedEndDate)
+
+if __name__ == '__main__':
+    main()
