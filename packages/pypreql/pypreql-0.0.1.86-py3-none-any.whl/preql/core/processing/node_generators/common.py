110b70c4d71fc4ca29c8c18e92036fcbb4d2b4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 06:04:28 +0000

    Add package pypreql/0.0.1.86/pypreql-0.0.1.86-py3-none-any.whl

---
+++ packages/pypreql/pypreql-0.0.1.86-py3-none-any.whl/preql/core/processing/node_generators/common.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+
+
+from preql.core.enums import PurposeLineage
+from preql.core.models import Concept, Function, AggregateWrapper, FilterItem
+from preql.utility import unique
+
+
+def resolve_function_parent_concepts(concept: Concept) -> List[Concept]:
+    if not isinstance(concept.lineage, (Function, AggregateWrapper)):
+        raise ValueError(f"Concept {concept} lineage is not function or aggregate")
+    if concept.derivation == PurposeLineage.AGGREGATE:
+        if concept.grain:
+            return unique(
+                concept.lineage.concept_arguments + concept.grain.components_copy,
+                "address",
+            )
+        return concept.lineage.concept_arguments
+    # TODO: handle basic lineage chains?
+
+    return unique(concept.lineage.concept_arguments, "address")
+
+
+def resolve_filter_parent_concepts(concept: Concept) -> Tuple[Concept, List[Concept]]:
+    if not isinstance(concept.lineage, FilterItem):
+        raise ValueError
+    base = [concept.lineage.content]
+    base += concept.lineage.where.concept_arguments
+    return concept.lineage.content, unique(base, "address")
