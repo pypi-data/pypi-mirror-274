be1ae0ec8dbe419f664a7fb4389cc93f83ee7e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:20 +0000

    Add package jaxsim/0.2.1.dev56/jaxsim-0.2.1.dev56.tar.gz

---
+++ packages/jaxsim/jaxsim-0.2.1.dev56.tar.gz/jaxsim-0.2.1.dev56/src/jaxsim/parsers/descriptions/link.py
@@ -0,0 +1,93 @@
+import dataclasses
+from typing import List
+
+import jax.numpy as jnp
+import jax_dataclasses
+import jaxlie
+from jax_dataclasses import Static
+
+import jaxsim.typing as jtp
+from jaxsim.utils import JaxsimDataclass
+
+
+@jax_dataclasses.pytree_dataclass
+class LinkDescription(JaxsimDataclass):
+    """
+    In-memory description of a robot link.
+
+    Attributes:
+        name (str): The name of the link.
+        mass (float): The mass of the link.
+        inertia (jtp.Matrix): The inertia matrix of the link.
+        index (Optional[int]): An optional index for the link.
+        parent (Optional[LinkDescription]): The parent link of this link.
+        pose (jtp.Matrix): The pose transformation matrix of the link.
+        children (List[LinkDescription]): List of child links.
+    """
+
+    name: Static[str]
+    mass: float
+    inertia: jtp.Matrix
+    index: int | None = None
+    parent: Static["LinkDescription"] = dataclasses.field(default=None, repr=False)
+    pose: jtp.Matrix = dataclasses.field(default_factory=lambda: jnp.eye(4), repr=False)
+    children: Static[List["LinkDescription"]] = dataclasses.field(
+        default_factory=list, repr=False
+    )
+
+    def __hash__(self) -> int:
+        return hash(self.__repr__())
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.name == other.name
+            and self.mass == other.mass
+            and (self.inertia == other.inertia).all()
+            and self.index == other.index
+            and self.parent == other.parent
+            and (self.pose == other.pose).all()
+            and self.children == other.children
+        )
+
+    @property
+    def name_and_index(self) -> str:
+        """
+        Get a formatted string with the link's name and index.
+
+        Returns:
+            str: The formatted string.
+
+        """
+        return f"#{self.index}_<{self.name}>"
+
+    def lump_with(
+        self, link: "LinkDescription", lumped_H_removed: jtp.Matrix
+    ) -> "LinkDescription":
+        """
+        Combine the current link with another link, preserving mass and inertia.
+
+        Args:
+            link (LinkDescription): The link to combine with.
+            lumped_H_removed (jtp.Matrix): The transformation matrix between the two links.
+
+        Returns:
+            LinkDescription: The combined link.
+
+        """
+        # Get the 6D inertia of the link to remove
+        I_removed = link.inertia
+
+        # Create the SE3 object. Note the inverse.
+        r_H_l = jaxlie.SE3.from_matrix(lumped_H_removed).inverse()
+        r_X_l = r_H_l.adjoint()
+
+        # Move the inertia
+        I_removed_in_lumped_frame = r_X_l.transpose() @ I_removed @ r_X_l
+
+        # Create the new combined link
+        lumped_link = self.replace(
+            mass=self.mass + link.mass,
+            inertia=self.inertia + I_removed_in_lumped_frame,
+        )
+
+        return lumped_link
