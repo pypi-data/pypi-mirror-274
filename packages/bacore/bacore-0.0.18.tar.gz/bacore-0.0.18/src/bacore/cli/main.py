4e0474aa909a4a69d398105519e11ac8ae2cb834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:50 +0000

    Add package bacore/0.0.18/bacore-0.0.18.tar.gz

---
+++ packages/bacore/bacore-0.0.18.tar.gz/bacore-0.0.18/src/bacore/cli/main.py
@@ -0,0 +1,7 @@
+"""BACore CLI entrypoint module."""
+from bacore.cli import create, serve
+from typer import Typer
+
+app = Typer(rich_markup_mode="rich", add_completion=False)
+app.add_typer(create.app, name="create", help="Create a project")
+app.add_typer(serve.app, name="serve", help="Serve documentation")
