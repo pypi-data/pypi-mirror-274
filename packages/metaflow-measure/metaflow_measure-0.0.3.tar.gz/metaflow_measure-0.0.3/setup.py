7ee383fc94b5e35d622a22c6e3bb862cced260d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:56 +0000

    Add package metaflow-measure/0.0.3/metaflow_measure-0.0.3.tar.gz

---
+++ packages/metaflow-measure/metaflow_measure-0.0.3.tar.gz/metaflow_measure-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+version = "0.0.3"
+
+setup(
+    name="metaflow-measure",
+    version=version,
+    description="Measure metrics and timings in Metaflow steps and send them optionally to Datadog or other backends",
+    author="Outerbounds",
+    author_email="hello@outerbounds.co",
+    packages=find_namespace_packages(include=["metaflow_extensions.*"]),
+    py_modules=[
+        "metaflow_extensions",
+    ]
+)
