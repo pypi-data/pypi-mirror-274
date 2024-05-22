4e1953fc25fd82b5d5a34fabbb4d0c7ea4654879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:01 +0000

    Add package movingpandas/0.18.1/movingpandas-0.18.1.tar.gz

---
+++ packages/movingpandas/movingpandas-0.18.1.tar.gz/movingpandas-0.18.1/movingpandas/__init__.py
@@ -0,0 +1,37 @@
+"""
+`movingpandas`: Implementation of Trajectory classes and functions built on top
+of GeoPandas
+"""
+
+from .trajectory import Trajectory  # noqa F401
+from .trajectory_generalizer import (  # noqa F401
+    TrajectoryGeneralizer,
+    MaxDistanceGeneralizer,
+    MinDistanceGeneralizer,
+    MinTimeDeltaGeneralizer,
+    DouglasPeuckerGeneralizer,
+    TopDownTimeRatioGeneralizer,
+)
+from .trajectory_collection import TrajectoryCollection  # noqa F401
+from .io import read_mf_json  # noqa F401
+from .trajectory_aggregator import TrajectoryCollectionAggregator  # noqa F401
+from .trajectory_splitter import (  # noqa F401
+    TrajectorySplitter,
+    TemporalSplitter,
+    ObservationGapSplitter,
+    SpeedSplitter,
+    StopSplitter,
+    AngleChangeSplitter,
+)
+from .trajectory_cleaner import IqrCleaner, OutlierCleaner  # noqa F401
+from .trajectory_stop_detector import TrajectoryStopDetector  # noqa F401
+from .point_clusterer import PointClusterer  # noqa F401
+from .tools._show_versions import show_versions  # noqa F401
+
+try:
+    from .trajectory_smoother import KalmanSmootherCV  # noqa F401
+except ImportError:
+    pass
+
+name = "movingpandas"
+__version__ = "0.18.1"
---
+++ packages/movingpandas/movingpandas-0.18.1.tar.gz/movingpandas-0.18.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = (
+        "Movement trajectory classes and functions built on top of GeoPandas"
+    )
+
+# Packages that MovingPandas uses explicitly:
+INSTALL_REQUIRES = [
+    "matplotlib",
+    "geopandas",
+    "fiona",
+    "rtree",
+    "geopy",
+]
+
+setuptools.setup(
+    name="movingpandas",
+    version="0.18.1",
+    author="Anita Graser",
+    author_email="anitagraser@gmx.at",
+    description="MovingPandas implements Trajectory classes and corresponding methods "
+    "for the analysis of movement data based on GeoPandas. "
+    "Visit movingpandas.org for details.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/movingpandas/movingpandas",
+    packages=[
+        "movingpandas",
+        "movingpandas.tools",
+        "movingpandas.tests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        "smoothing": ["stonesoup"],
+        "viz": ["hvplot", "bokeh", "cartopy", "geoviews"],
+    },
+)
