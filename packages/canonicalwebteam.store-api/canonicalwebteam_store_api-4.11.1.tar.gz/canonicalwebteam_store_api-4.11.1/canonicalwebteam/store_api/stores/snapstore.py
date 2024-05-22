df9623cfa424af2102b99626628870a9c6832bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:34 +0000

    Add package canonicalwebteam.store-api/4.11.1/canonicalwebteam_store_api-4.11.1.tar.gz

---
+++ packages/canonicalwebteam.store-api/canonicalwebteam_store_api-4.11.1.tar.gz/canonicalwebteam_store_api-4.11.1/canonicalwebteam/store_api/stores/snapstore.py
@@ -0,0 +1,532 @@
+from os import getenv
+
+import requests
+
+from canonicalwebteam.store_api.store import Store
+from canonicalwebteam.store_api.publisher import Publisher
+
+SNAPSTORE_API_URL = getenv("SNAPSTORE_API_URL", "https://api.snapcraft.io/")
+SNAPSTORE_DASHBOARD_API_URL = getenv(
+    "SNAPSTORE_DASHBOARD_API_URL", "https://dashboard.snapcraft.io/"
+)
+SNAPSTORE_PUBLISHERWG_API_URL = getenv(
+    "SNAPSTORE_PUBLISHERWG_API_URL", "https://api.charmhub.io/"
+)
+
+
+class SnapStore(Store):
+    def __init__(self, session=requests.Session(), store=None):
+        super().__init__(session, store)
+
+        self.config = {
+            1: {
+                "base_url": f"{SNAPSTORE_API_URL}api/v1/snaps/",
+                "headers": {"X-Ubuntu-Series": "16"},
+            },
+            2: {
+                "base_url": f"{SNAPSTORE_API_URL}v2/snaps/",
+                "headers": {"Snap-Device-Series": "16"},
+            },
+        }
+
+        if store:
+            self.config[1]["headers"].update({"X-Ubuntu-Store": store})
+            self.config[2]["headers"].update({"Snap-Device-Store": store})
+
+    def get_item_details(
+        self,
+        name,
+        fields=[
+            "title",
+            "summary",
+            "description",
+            "license",
+            "contact",
+            "website",
+            "publisher",
+            "media",
+            "download",
+            "version",
+            "created-at",
+            "confinement",
+            "categories",
+            "trending",
+            "unlisted",
+            "links",
+        ],
+        api_version=1,
+    ):
+        return super(SnapStore, self).get_item_details(
+            name, None, fields, api_version
+        )
+
+    def find(
+        self,
+        query="",
+        category="",
+        architecture="",
+        publisher="",
+        featured="false",
+        fields=[],
+    ):
+        url = self.get_endpoint_url("find", 2)
+        headers = self.config[2].get("headers")
+        params = {
+            "q": query,
+            "category": category,
+            "architecture": architecture,
+            "publisher": publisher,
+            "featured": featured,
+        }
+        if fields:
+            params["fields"] = ",".join(fields)
+        response = self.session.get(url, params=params, headers=headers)
+        return self.process_response(response)
+
+
+class SnapPublisher(Publisher):
+    def __init__(self, session=requests.Session()):
+        super().__init__(session)
+
+        self.config = {
+            1: {"base_url": f"{SNAPSTORE_DASHBOARD_API_URL}dev/api/"},
+            2: {"base_url": f"{SNAPSTORE_DASHBOARD_API_URL}api/v2/"},
+        }
+
+    def get_macaroon(self, permissions):
+        """
+        Return a bakery v2 macaroon from the publisher API to be discharged
+        """
+        response = self.session.post(
+            url=self.get_endpoint_url("tokens", 2),
+            json={"permissions": permissions},
+        )
+
+        return self.process_response(response)["macaroon"]
+
+    def whoami(self, session):
+        response = self.session.get(
+            url=self.get_endpoint_url("tokens/whoami", 2),
+            headers=self._get_authorization_header(session),
+        )
+
+        return self.process_response(response)
+
+    def get_publisherwg_endpoint_url(self, endpoint):
+        return f"{SNAPSTORE_PUBLISHERWG_API_URL}v1/{endpoint}"
+
+    def _get_publisherwg_authorization_header(self, session):
+        return {"Authorization": f"Macaroon {session['developer_token']}"}
+
+    def exchange_dashboard_macaroons(self, session):
+        """
+        Exchange dashboard.snapcraft.io SSO discharged macaroons
+        """
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url("tokens/dashboard/exchange"),
+            headers=self._get_authorization_header(session),
+            json={},
+        )
+
+        return self.process_response(response)["macaroon"]
+
+    def get_collaborators(self, session, name):
+        """
+        Get collaborators (accepted invites) for the given package.
+        """
+        response = self.session.get(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+        )
+        return self.process_response(response)
+
+    def get_pending_invites(self, session, name):
+        """
+        Get pending collaborator invites for the given package.
+        """
+        response = self.session.get(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators/invites"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+        )
+        return self.process_response(response)
+
+    def invite_collaborators(self, session, name, emails):
+        """
+        Invite one or more collaborators for a package.
+        """
+        payload = {"invites": []}
+
+        for email in emails:
+            payload["invites"].append({"email": email})
+
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators/invites"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json=payload,
+        )
+        return self.process_response(response)
+
+    def revoke_invites(self, session, name, emails):
+        """
+        Revoke invites to the specified emails for the package.
+        """
+        payload = {"invites": []}
+
+        for email in emails:
+            payload["invites"].append({"email": email})
+
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators/invites/revoke"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json=payload,
+        )
+        return self.process_response(response)
+
+    def accept_invite(self, session, name, token):
+        """
+        Accept a collaborator invite.
+        """
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators/invites/accept"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json={"token": token},
+        )
+        return self.process_response(response)
+
+    def reject_invite(self, session, name, token):
+        """
+        Reject a collaborator invite.
+        """
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(
+                f"snap/{name}/collaborators/invites/reject"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json={"token": token},
+        )
+        return self.process_response(response)
+
+    def create_track(self, session, snap_name, track_name):
+        """
+        Create a track for a snap base on the snap's guardrail pattern.
+        """
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(f"snap/{snap_name}/tracks"),
+            headers=self._get_publisherwg_authorization_header(session),
+            json=[{"name": track_name}],
+        )
+        return response
+
+    def unregister_package_name(self, publisher_auth, snap_name):
+        """
+        Unregister a package name.
+
+        Args:
+            publisher_auth: Serialized macaroon to consume the API.
+            name: Name of the package to unregister
+        Returns:
+            The package name ID if successful
+            Otherwise, returns an error list
+        """
+        url = self.get_publisherwg_endpoint_url(f"snap/{snap_name}")
+        response = self.session.delete(
+            url=url,
+            headers=self._get_authorization_header(publisher_auth),
+        )
+        return response
+
+
+class SnapStoreAdmin(SnapPublisher):
+    def get_endpoint_url(self, endpoint, api_version=2):
+        return super().get_endpoint_url(f"stores/{endpoint}", api_version)
+
+    def get_stores(self, session, roles=["admin", "review", "view", "access"]):
+        """Return a list a stores with the given roles
+
+        :return: A list of stores
+        """
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=super().get_endpoint_url("account", 1), headers=headers
+        )
+
+        account_info = self.process_response(response)
+        stores = account_info.get("stores", [])
+        user_stores = []
+
+        for store in stores:
+            if not set(roles).isdisjoint(store["roles"]):
+                user_stores.append(store)
+
+        return user_stores
+
+    def get_store(self, session, store_id):
+        """Return a store where the user is an admin
+
+        :return: Store details
+        """
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=self.get_endpoint_url(store_id), headers=headers
+        )
+
+        return self.process_response(response)["store"]
+
+    def get_store_snaps(
+        self, session, store_id, query=None, allowed_for_inclusion=None
+    ):
+        headers = self._get_authorization_header(session)
+        params = {}
+
+        if query:
+            params["q"] = query
+
+        if allowed_for_inclusion:
+            params["allowed-for-inclusion"] = allowed_for_inclusion
+
+        response = self.session.get(
+            url=self.get_endpoint_url(f"{store_id}/snaps"),
+            params=params,
+            headers=headers,
+        )
+
+        return self.process_response(response).get("snaps", [])
+
+    def get_store_members(self, session, store_id):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=self.get_endpoint_url(f"{store_id}"),
+            headers=headers,
+        )
+
+        return self.process_response(response).get("users", [])
+
+    def update_store_members(self, session, store_id, members):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.post(
+            url=self.get_endpoint_url(f"{store_id}/users"),
+            headers=headers,
+            json=members,
+        )
+
+        return self.process_response(response)
+
+    def invite_store_members(self, session, store_id, members):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.post(
+            url=self.get_endpoint_url(f"{store_id}/invites"),
+            headers=headers,
+            json=members,
+        )
+
+        return self.process_response(response)
+
+    def change_store_settings(self, session, store_id, settings):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.put(
+            url=self.get_endpoint_url(f"{store_id}/settings"),
+            headers=headers,
+            json=settings,
+        )
+
+        return self.process_response(response)
+
+    def update_store_snaps(self, session, store_id, snaps):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.post(
+            url=self.get_endpoint_url(f"{store_id}/snaps"),
+            headers=headers,
+            json=snaps,
+        )
+
+        return self.process_response(response)
+
+    def update_store_invites(self, session, store_id, invites):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.put(
+            url=self.get_endpoint_url(f"{store_id}/invites"),
+            headers=headers,
+            json=invites,
+        )
+
+        return self.process_response(response)
+
+    def get_store_invites(self, session, store_id):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=self.get_endpoint_url(f"{store_id}"),
+            headers=headers,
+        )
+
+        return self.process_response(response).get("invites", [])
+
+    # MODEL SERVICE ADMIN
+    def get_store_models(self, session, store_id):
+        response = self.session.get(
+            url=self.get_publisherwg_endpoint_url(f"brand/{store_id}/model"),
+            headers=self._get_publisherwg_authorization_header(session),
+        )
+
+        return self.process_response(response)
+
+    def create_store_model(self, session, store_id, name, api_key=None):
+        if api_key:
+            payload = {"name": name, "api-key": api_key, "series": "16"}
+        else:
+            payload = {"name": name, "series": "16"}
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(f"brand/{store_id}/model"),
+            headers=self._get_publisherwg_authorization_header(session),
+            json=payload,
+        )
+
+        return self.process_response(response)
+
+    def update_store_model(self, session, store_id, model_name, api_key):
+        response = self.session.patch(
+            url=self.get_publisherwg_endpoint_url(
+                f"brand/{store_id}/model/{model_name}"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json={"api-key": api_key},
+        )
+
+        return self.process_response(response)
+
+    def get_store_model_policies(self, session, store_id, model_name):
+        response = self.session.get(
+            url=self.get_publisherwg_endpoint_url(
+                f"brand/{store_id}/model/{model_name}/serial_policy"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+        )
+
+        return self.process_response(response)
+
+    def create_store_model_policy(
+        self, session, store_id, model_name, signing_key
+    ):
+        response = self.session.post(
+            url=self.get_publisherwg_endpoint_url(
+                f"brand/{store_id}/model/{model_name}/serial_policy"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+            json={"signing-key-sha3-384": signing_key},
+        )
+
+        return self.process_response(response)
+
+    def delete_store_model_policy(
+        self, session, store_id, model_name, revision
+    ):
+        response = self.session.delete(
+            url=self.get_publisherwg_endpoint_url(
+                f"brand/{store_id}/model/{model_name}/serial_policy/{revision}"
+            ),
+            headers=self._get_publisherwg_authorization_header(session),
+        )
+
+        return response
+
+    def get_store_signing_keys(self, session, store_id):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url(
+            f"brand/{store_id}/signing_key"
+        )
+        response = self.session.get(
+            url=url,
+            headers=headers,
+        )
+        return self.process_response(response)
+
+    def create_store_signing_key(self, session, store_id, name):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url(
+            f"brand/{store_id}/signing_key"
+        )
+        response = self.session.post(
+            url=url,
+            headers=headers,
+            json={"name": name},
+        )
+        return self.process_response(response)
+
+    def delete_store_signing_key(
+        self, session, store_id, signing_key_sha3_384
+    ):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url(
+            f"brand/{store_id}/signing_key/{signing_key_sha3_384}"
+        )
+        response = self.session.delete(
+            url=url,
+            headers=headers,
+        )
+
+        return response
+
+    def get_brand(self, session, store_id):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url(f"brand/{store_id}")
+        response = self.session.get(
+            url=url,
+            headers=headers,
+        )
+
+        return self.process_response(response)
+
+    # FEATURED SNAPS AUTOMATION
+    def delete_featured_snaps(self, session, snaps):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url("snap/featured")
+        response = self.session.delete(
+            url=url,
+            headers=headers,
+            json=snaps,
+        )
+        return response
+
+    def update_featured_snaps(self, session, snaps):
+        headers = self._get_publisherwg_authorization_header(session)
+        url = self.get_publisherwg_endpoint_url("snap/featured")
+        response = self.session.put(
+            url=url,
+            headers=headers,
+            json=snaps,
+        )
+        return response
+
+    def get_featured_snaps(self, session, api_version=1):
+        url = "https://api.staging.snapcraft.io/api/v1/snaps/search"
+        headers = self.config[api_version].get("headers")
+
+        params = {
+            "scope": "wide",
+            "arch": "wide",
+            "confinement": "strict,classic,devmode",
+            "fields": "snap_id",
+            "section": "featured",
+        }
+
+        return self.process_response(
+            self.session.get(url, params=params, headers=headers)
+        )
