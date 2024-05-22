a3b7e92d5555ccf0b377f29269c6bd5b969fe6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:57 +0000

    Add package afl-ai-utils/0.4.9/afl_ai_utils-0.4.9-py3-none-any.whl

---
+++ packages/afl-ai-utils/afl_ai_utils-0.4.9-py3-none-any.whl/afl_ai_utils/secrets_utils.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from google.cloud import secretmanager
+
+def load_da_secrets():
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/162221169123/secrets/da-secrets/versions/latest"
+    response = client.access_secret_version(request={"name": secret_name})
+    payload = response.payload.data.decode("UTF-8")
+    with open('.env', 'w', encoding='utf-8') as f:
+        f.write(payload)
+    load_dotenv()
+    os.remove(".env")
+def load_de_secrets():
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/162221169123/secrets/de-secrets/versions/latest"
+    response = client.access_secret_version(request={"name": secret_name})
+    payload = response.payload.data.decode("UTF-8")
+    with open('.env', 'w', encoding='utf-8') as f:
+        f.write(payload)
+    load_dotenv()
+    os.remove(".env")
