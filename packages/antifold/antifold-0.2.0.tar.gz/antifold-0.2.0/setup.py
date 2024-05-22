929cd13fdb412bc459587d1edaf2ed9d0b75a8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:21 +0000

    Add package antifold/0.2.0/antifold-0.2.0.tar.gz

---
+++ packages/antifold/antifold-0.2.0.tar.gz/antifold-0.2.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    name="antifold",
+    version="0.2.0",
+    packages=find_packages(),
+    description="Inverse folding of antibodies",
+    url="https://github.com/Magnushhoie/antifold_web/",
+    author="Magnus Haraldson Høie & Alissa Hummer",
+    author_email="maghoi@dtu.dk & alissa.hummer@stcatz.ox.ac.uk",
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
