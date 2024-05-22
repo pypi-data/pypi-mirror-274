f56d7bfe0d7eae9708d58ff6d21e6ba3878e8451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:49 +0000

    Add package chemical-safety/0.0.3/chemical_safety-0.0.3.tar.gz

---
+++ packages/chemical-safety/chemical_safety-0.0.3.tar.gz/chemical_safety-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chemical_safety",
+    version="0.0.3",
+    packages=find_packages(include=['chemical_safety', 'chemical_safety.*']),
+    description="A package for retreiving chemical safety information",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Demetrios Pagonis",
+    author_email="demetriospagonis@weber.edu",
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+        'jinja2',
+        'requests',
+        'Levenshtein',
+        'natsort',
+        'flask',
+        'scipy',
+        'rdkit'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chemical-dashboard=chemical_safety.dashboard.app:dashboard',
+        ],
+    },
+)
