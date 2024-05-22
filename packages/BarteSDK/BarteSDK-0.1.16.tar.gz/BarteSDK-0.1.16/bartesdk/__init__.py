c0a5db8c6c6950f94d48a7e9b55ac82515bdf09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:50 +0000

    Add package BarteSDK/0.1.16/BarteSDK-0.1.16.tar.gz

---
+++ packages/BarteSDK/BarteSDK-0.1.16.tar.gz/BarteSDK-0.1.16/bartesdk/__init__.py
@@ -0,0 +1,10 @@
+from .charges import ChargesAPI
+from .reportsellers import ReportSellersAPI
+
+class BarteSDK:
+    def __init__(self, api_key, env="prd", api_version="v2"):
+        self.api_key = api_key
+        self.env = env
+        self.api_version = api_version
+        self.charges = ChargesAPI(api_key, env, api_version)
+        self.reportsellers = ReportSellersAPI(api_key, env, api_version)
