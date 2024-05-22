6d753e045b3f36c36d27ef242c9b8529dc892df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:26 +0000

    Add package rpa-helpers-btime/1.1.0/rpa_helpers_btime-1.1.0-py3-none-any.whl

---
+++ packages/rpa-helpers-btime/rpa_helpers_btime-1.1.0-py3-none-any.whl/rpa_helpers/src/models/entities/rpa.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer
+from rpa_helpers.src.models.configs.base import Base
+
+
+class Rpa(Base):
+
+    __tablename__ = "rpa"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+    def __repr__(self):
+        return f"Rpa [NAME='(self.name)']"
