ad8ed49dc0687ecb7d941cb92931d1723856735b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 17:04:43 +0000

    Add package django-categories/2.0.0/django_categories-2.0.0-py2.py3-none-any.whl

---
+++ packages/django-categories/django_categories-2.0.0-py2.py3-none-any.whl/categories/fields.py
@@ -0,0 +1,21 @@
+"""Custom category fields for other models."""
+
+from django.db.models import ForeignKey, ManyToManyField
+
+
+class CategoryM2MField(ManyToManyField):
+    """A many to many field to a Category model."""
+
+    def __init__(self, **kwargs):
+        if "to" in kwargs:
+            kwargs.pop("to")
+        super(CategoryM2MField, self).__init__(to="categories.Category", **kwargs)
+
+
+class CategoryFKField(ForeignKey):
+    """A foreign key to the Category model."""
+
+    def __init__(self, **kwargs):
+        if "to" in kwargs:
+            kwargs.pop("to")
+        super(CategoryFKField, self).__init__(to="categories.Category", **kwargs)
