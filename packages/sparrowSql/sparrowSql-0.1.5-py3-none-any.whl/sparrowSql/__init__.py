feeecc2406132626d23cb5e56df97c9644cad4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:05:04 +0000

    Add package sparrowSql/0.1.5/sparrowSql-0.1.5-py3-none-any.whl

---
+++ packages/sparrowSql/sparrowSql-0.1.5-py3-none-any.whl/sparrowSql/__init__.py
@@ -0,0 +1,6 @@
+from sparrowSql.mysql import MySQL
+from sparrowSql.mariaDB import MariaDB
+from sparrowSql.sparrow import Sparrow
+from sparrowSql.sqLite import SqLite
+from sparrowSql.postgresql import Postgresql
+
