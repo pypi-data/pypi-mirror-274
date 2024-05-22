52aa8718d9148016bcfd74f24d0970f83d3fb055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:22 +0000

    Add package VeriFact/0.0.1/VeriFact-0.0.1.tar.gz

---
+++ packages/VeriFact/VeriFact-0.0.1.tar.gz/VeriFact-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+   name="VeriFact",
+   version="0.0.1",
+   packages=find_packages(),
+   install_requires=[
+       #'requests', 'numpy'
+   ],
+   author="Yixio Song",
+   author_email="yixiaosong@umass.edu",
+   description="Pip package for Verifact",
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   url="https://github.com/mungg/VeriScore",
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   python_requires='>=3.6',
+)
