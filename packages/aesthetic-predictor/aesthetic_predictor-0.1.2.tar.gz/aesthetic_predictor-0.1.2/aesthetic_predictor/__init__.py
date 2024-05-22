b71aefd4454aaee2461f0a60423e1733e23724ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:07 +0000

    Add package aesthetic-predictor/0.1.2/aesthetic_predictor-0.1.2.tar.gz

---
+++ packages/aesthetic-predictor/aesthetic_predictor-0.1.2.tar.gz/aesthetic_predictor-0.1.2/aesthetic_predictor/__init__.py
@@ -0,0 +1,13 @@
+from aesthetic_predictor.libs import (
+    get_aesthetic_model,
+    get_clip_model,
+    predict_aesthetic,
+)
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "get_aesthetic_model",
+    "get_clip_model",
+    "predict_aesthetic",
+]
