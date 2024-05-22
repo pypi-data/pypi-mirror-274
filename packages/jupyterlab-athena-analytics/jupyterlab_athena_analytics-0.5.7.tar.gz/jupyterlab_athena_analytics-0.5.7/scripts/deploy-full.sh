4be052470636ab46322d2a94d79a3da2ad01fbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 06:04:30 +0000

    Add package jupyterlab-athena-analytics/0.5.7/jupyterlab_athena_analytics-0.5.7.tar.gz

---
+++ packages/jupyterlab-athena-analytics/jupyterlab_athena_analytics-0.5.7.tar.gz/jupyterlab_athena_analytics-0.5.7/scripts/deploy-full.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# pip install build twine hatch
+
+if [ -z "$ATHENA_PYPI_TOKEN" ]
+then
+  echo "Error: ATHENA_PYPI_TOKEN is not set."
+  exit 1
+fi
+
+export TWINE_PASSWORD=$ATHENA_PYPI_TOKEN
+hatch version patch && jlpm clean:all && git clean -dfX && python -m build && twine upload dist/*
