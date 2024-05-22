abf2f2d6f57e8760a19bd368439468649b2d4005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:59 +0000

    Add package pyautd3/24.0.0/pyautd3-24.0.0-py3-none-linux_armv7l.whl

---
+++ packages/pyautd3/pyautd3-24.0.0-py3-none-linux_armv7l.whl/pyautd3/driver/datagram/gain/transform.py
@@ -0,0 +1,66 @@
+from collections.abc import Callable
+from ctypes import POINTER
+from functools import reduce
+from typing import Generic, TypeVar
+
+import numpy as np
+
+from pyautd3.driver.datagram.gain.base import GainBase
+from pyautd3.driver.datagram.with_segment import IntoDatagramWithSegment
+from pyautd3.driver.firmware.fpga import Drive, Phase
+from pyautd3.driver.firmware.fpga.emit_intensity import EmitIntensity
+from pyautd3.driver.geometry import Device, Geometry, Transducer
+from pyautd3.native_methods.autd3capi import NativeMethods as Base
+from pyautd3.native_methods.autd3capi_driver import Drive as _Drive
+from pyautd3.native_methods.autd3capi_driver import GainPtr
+from pyautd3.native_methods.utils import _validate_ptr
+
+from .cache import IntoGainCache
+
+G = TypeVar("G", bound=GainBase)
+
+
+class Transform(
+    IntoDatagramWithSegment["Transform[G]"],
+    IntoGainCache["Transform[G]"],
+    GainBase,
+    Generic[G],
+):
+    _g: G
+    _f: Callable[[Device, Transducer, Drive], Drive]
+
+    def __init__(self: "Transform", g: G, f: Callable[[Device, Transducer, Drive], Drive]) -> None:
+        super().__init__()
+        self._g = g
+        self._f = f
+
+    def _gain_ptr(self: "Transform[G]", geometry: Geometry) -> GainPtr:
+        res = _validate_ptr(Base().gain_calc(self._g._gain_ptr(geometry), geometry._geometry_ptr()))
+
+        drives: dict[int, np.ndarray] = {}
+        for dev in geometry.devices:
+            d = np.zeros(dev.num_transducers, dtype=_Drive)
+
+            Base().gain_calc_get_result(res, d.ctypes.data_as(POINTER(_Drive)), dev.idx)  # type: ignore[arg-type]
+            for tr in dev:
+                drive = self._f(dev, tr, Drive(Phase(int(d[tr.idx]["phase"])), EmitIntensity(int(d[tr.idx]["intensity"]))))
+                d[tr.idx] = np.void(_Drive(drive.phase._value, drive.intensity._value))  # type: ignore[call-overload]
+            drives[dev.idx] = d
+
+        Base().gain_calc_free_result(res)
+
+        return reduce(
+            lambda acc, dev: Base().gain_raw_set(
+                acc,
+                dev.idx,
+                drives[dev.idx].ctypes.data_as(POINTER(_Drive)),  # type: ignore[arg-type]
+                len(drives[dev.idx]),
+            ),
+            geometry.devices,
+            Base().gain_raw(),
+        )
+
+
+class IntoGainTransform(GainBase, Generic[G]):
+    def with_transform(self: G, f: Callable[[Device, Transducer, Drive], Drive]) -> Transform[G]:
+        return Transform(self, f)
