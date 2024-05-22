080115e9cd20064b02e942b98b3b4676e9996bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:27 +0000

    Add package composabl-core-dev/0.8.0.dev88/composabl_core_dev-0.8.0.dev88-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/composabl-core-dev/composabl_core_dev-0.8.0.dev88-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/composabl_core/examples/tamagotchi/test-working/test_working/sim_impl.py
@@ -0,0 +1,62 @@
+# Copyright (C) Composabl, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+from typing import Any, Dict, SupportsFloat, Tuple
+
+import gymnasium as gym
+from gymnasium.envs.registration import EnvSpec
+
+import composabl_core.utils.logger as logger_util
+from composabl_core.agent.scenario import Scenario
+from composabl_core.networking.server_composabl import ServerComposabl
+from test_working.sim import TamagotchiSim
+
+logger = logger_util.get_logger(__name__)
+
+
+class SimImpl(ServerComposabl):
+    def __init__(self):
+        self.sim = TamagotchiSim()
+
+    async def make(self, env_id: str, env_init: dict) -> EnvSpec:
+        return None
+
+    async def sensor_space_info(self) -> gym.Space:
+        return self.sim.sensor_space
+
+    async def action_space_info(self) -> gym.Space:
+        return self.sim.action_space
+
+    async def action_space_sample(self) -> Any:
+        return self.sim.action_space.sample()
+
+    async def reset(self) -> Tuple[Any, Dict[str, Any]]:
+        sensors, info = await self.sim.reset()
+        return sensors, info
+
+    async def step(
+        self, action
+    ) -> Tuple[Any, SupportsFloat, bool, bool, Dict[str, Any]]:
+        return await self.sim.step(action)
+
+    async def close(self) -> None:
+        await self.sim.close()
+
+    async def set_scenario(self, scenario) -> None:
+        self.sim.scenario = scenario
+
+    async def get_scenario(self) -> Scenario:
+        if self.sim.scenario is None:
+            return Scenario({"dummy": 0})
+
+        return self.sim.scenario
+
+    async def set_render_mode(self, render_mode):
+        self.sim.render_mode = render_mode
+
+    async def get_render_mode(self) -> str:
+        return self.sim.render_mode
+
+    async def get_render(self) -> Any:
+        return await self.sim.get_render()
