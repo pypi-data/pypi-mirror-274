5b693ac0e3c229c9b692b3a50440c822e176a125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:42 +0000

    Add package formant/1.170.66/formant-1.170.66.tar.gz

---
+++ packages/formant/formant-1.170.66.tar.gz/formant-1.170.66/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import ScopeFilterTypesItem, AnnotationMetadata, SheetParameters, AnnotationTemplate, AnnotationFieldValuesResponse, AnnotationFieldValuesRequestTags, AnnotationStreamType, AnnotationTemplateListResponse, AnnotationField, AnnotationFieldValue, ScopeFilter, Annotation, TagParameters, AnnotationFieldValuesRequest, AnnotationTags, AnnotationType, AnnotationFieldValueTags, AnnotationFieldType, UserParametersRolesItem, UserParameters, AnnotationTemplateTags
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.170.66.tar.gz/formant-1.170.66/formant/sdk/cloud/v2/src/resources/queries/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import StreamDataTags, Battery, StreamAggregateDataType, StreamAggregateDataTags, Goal, Twist, Map, Transform, Location, StreamData, QueryTypesItem, StreamAggregateData, QueryAggregate, Health, ImageAnnotation, Vector3, StreamDataListResponse, Odometry, Video, Localization, Health,HealthStatus, VideoMimeType, StreamDataPointsItem, BoundingBox, StreamDataType, Image, PointCloud, Path, HealthStatus, Quaternion, Video,VideoMimeType, Bitset, File, TransformNode, Query, NumericSetEntry
+from formant.sdk.cloud.v2.formant_query_api_client.api.query import query_controller_query
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
