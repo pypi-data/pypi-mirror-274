b54bd106099de49fc3cf02bdfd777ccd5818f21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 06:05:02 +0000

    Add package tester-story-sdk/0.1.4/tester_story_sdk-0.1.4.tar.gz

---
+++ packages/tester-story-sdk/tester_story_sdk-0.1.4.tar.gz/tester_story_sdk-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tester_story_sdk',
+    version='0.1.4',
+    packages=find_packages(where='src', exclude=["tests"]),
+    package_dir={'': 'src'},
+    install_requires=[
+        'web3>=5.0.0',
+        'pytest',
+        'python-dotenv'
+    ],
+    url='https://github.com/aandrewchung/python-sdk/tree/andrew/pip-install',
+    license='MIT',
+    author='Andrew Chung',
+    author_email='andrew@storyprotocol.xyz',
+    description='A Python SDK for interacting with the Story Protocol.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
