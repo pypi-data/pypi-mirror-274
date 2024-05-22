7f02fe547d17c452c769430a71a4f5985e82a3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:05:51 +0000

    Add package dinamis-sdk/0.1.8/dinamis_sdk-0.1.8-py3-none-any.whl

---
+++ packages/dinamis-sdk/dinamis_sdk-0.1.8-py3-none-any.whl/dinamis_sdk/utils.py
@@ -0,0 +1,94 @@
+"""Some helpers."""
+import json
+import logging
+import os
+import appdirs
+from pydantic import BaseModel  # pylint: disable = no-name-in-module
+import requests
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL") or "INFO")
+log = logging.getLogger("dinamis_sdk")
+
+def _get_seconds(env_var_name: str) -> int:
+    val = os.environ.get(env_var_name)
+    if val:
+        if val.isdigit():
+            return int(val)
+    return None
+
+# Signed TTL margin default to 1800 seconds (30 minutes), or env. var.
+if (ttl_margin_from_env := _get_seconds("DINAMIS_SDK_TTL_MARGIN")):
+    log.info(
+        "Setting TTL margin from environment variable to %s seconds",
+        ttl_margin_from_env
+    )
+SIGNED_URL_TTL_MARGIN = ttl_margin_from_env or 1800
+
+if (SIGNED_URL_DURATION_SECONDS := _get_seconds("DINAMIS_SDK_DURATION_SECONDS")):
+    log.info(
+        "Setting duration seconds from environment variable to %s seconds",
+        SIGNED_URL_DURATION_SECONDS
+    )
+
+MAX_URLS = 64
+S3_STORAGE_DOMAIN = "meso.umontpellier.fr"
+S3_SIGNING_ENDPOINT = \
+    "https://s3-signing-cdos.apps.okd.crocc.meso.umontpellier.fr/"
+
+CFG_PTH = appdirs.user_config_dir(appname='dinamis_sdk_auth')
+if not os.path.exists(CFG_PTH):
+    try:
+        os.makedirs(CFG_PTH)
+        log.debug("Config path created in %s", CFG_PTH)
+    except PermissionError:
+        log.warning("Unable to create config path")
+        CFG_PTH = None
+else:
+    log.debug("Config path already exist in %s", CFG_PTH)
+
+JWT_FILE = os.path.join(CFG_PTH, ".token") if CFG_PTH else None
+log.debug("JWT file is %s", JWT_FILE)
+
+settings_file = os.path.join(CFG_PTH, ".settings") if CFG_PTH else None
+log.debug("Settings file is %s", settings_file)
+
+
+class StorageCredentials(BaseModel):  # pylint: disable = R0903
+    """Credentials model."""
+
+    access_key: str
+    secret_key: str
+
+
+CREDENTIALS = None
+if settings_file and os.path.isfile(settings_file):
+    try:
+        with open(settings_file, encoding='UTF-8') as json_file:
+            CREDENTIALS = StorageCredentials(**json.load(json_file))
+    except FileNotFoundError:
+        log.debug("Setting file %s does not exist", settings_file)
+
+
+def retrieve_token_endpoint(s3_signing_endpoint: str = S3_SIGNING_ENDPOINT):
+    """Retrieve the token endpoint from the s3 signing endpoint."""
+    openapi_url = s3_signing_endpoint + "openapi.json"
+    log.debug("Fetching OAuth2 endpoint from openapi url %s", openapi_url)
+    res = requests.get(
+        openapi_url,
+        timeout=10,
+    )
+    res.raise_for_status()
+    data = res.json()
+    oauth2_defs = data["components"]["securitySchemes"]["OAuth2PasswordBearer"]
+    return oauth2_defs["flows"]["password"]["tokenUrl"]
+
+
+# Token endpoint is typically something like: https://keycloak-dinamis.apps.okd
+# .crocc.meso.umontpellier.fr/auth/realms/dinamis/protocol/openid-connect/token
+TOKEN_ENDPOINT = retrieve_token_endpoint()
+# Auth base URL is typically something like: https://keycloak-dinamis.apps.okd.
+# crocc.meso.umontpellier.fr/auth/realms/dinamis/protocol/openid-connect
+AUTH_BASE_URL = TOKEN_ENDPOINT.rsplit('/', 1)[0]
+
+# Token server (optional)
+TOKEN_SERVER = os.environ.get("DINAMIS_SDK_TOKEN_SERVER")
