0b830b37afd18052efd67927f485a33bf694423c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:14 +0000

    Add package cortexquest/0.0.1/cortexquest-0.0.1.tar.gz

---
+++ packages/cortexquest/cortexquest-0.0.1.tar.gz/cortexquest-0.0.1/src/cortexquest/core/google_auth.py
@@ -0,0 +1,7 @@
+class GoogleAuth:
+    def __init__(self, credentials_file: str):
+        self.credentials_file = credentials_file
+
+    def authenticate(self):
+        # Implement Google API authentication logic
+        pass
