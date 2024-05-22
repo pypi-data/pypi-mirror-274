43dd260be1e34b01ba8b0f4b63d8bd38e25e1050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:43 +0000

    Add package pysonrpc/1.0.7/pysonrpc-1.0.7-py3-none-any.whl

---
+++ packages/pysonrpc/pysonrpc-1.0.7-py3-none-any.whl/pysonrpc/__init__.py
@@ -0,0 +1,9 @@
+from pysonrpc.jsonrpc import (
+    JsonRpcClient,
+    JsonRpcClientError,
+    JsonRpcEndpoint,
+    JsonRpcError,
+    JsonRpcServerError,
+    Method,
+)
+from pysonrpc.version import __version__
