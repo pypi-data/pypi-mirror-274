aa60f8ac7f3c34b08ebfa2c43ab1fa69e4f4fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:05:22 +0000

    Add package ob-metaflow-stubs/3.6/ob-metaflow-stubs-3.6.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-3.6.tar.gz/ob-metaflow-stubs-3.6/metaflow-stubs/metadata/util.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.16.1+ob(v1)                                                   #
+# Generated on 2024-05-21T17:36:50.094351                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def copy_tree(src, dst, update = False):
+    ...
+
+def sync_local_metadata_to_datastore(metadata_local_dir, task_ds):
+    ...
+
+def sync_local_metadata_from_datastore(metadata_local_dir, task_ds):
+    ...
+
