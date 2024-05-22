083f4329994088ac1d01d9211a7f93c29898a7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:59 +0000

    Add package rpa-helpers-btime/1.0.8/rpa_helpers_btime-1.0.8-py3-none-any.whl

---
+++ packages/rpa-helpers-btime/rpa_helpers_btime-1.0.8-py3-none-any.whl/rpa_helpers/src/configuration/configuration.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+TO_EMAIL = ["espacozanata1@gmail.com", "geovanne.zanata@btime.com.br"]
---
+++ packages/rpa-helpers-btime/rpa_helpers_btime-1.0.8-py3-none-any.whl/rpa_helpers/src/models/repository/repository_log.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from rpa_helpers.src.models.configs.connection import DBConnectionHandler
+from rpa_helpers.src.models.entities.log import Logs
+from sqlalchemy import text
+
+
+class LogRepository:
+    def __init__(self) -> None:
+        self.create_table()
+        self.date = datetime.now()
+
+    
+    def create_table(self):
+        with DBConnectionHandler() as db:
+            query = text(
+                """
+                CREATE TABLE IF NOT EXISTS logs
+                (id SERIAL PRIMARY KEY, message TEXT, status TEXT, description TEXT, start_date TEXT, end_date TEXT, rpa_id INTEGER, execution_id TEXT)
+                """
+            )
+
+            db.session.execute(query)
+            db.session.commit()
+
+
+    def insert(self, message, status, description, start_date, end_date, rpa_id, execution_id):
+        start_date = datetime.strptime(start_date, "%d/%m/%Y %H:%M:%S")
+        end_date = datetime.strptime(end_date, "%d/%m/%Y %H:%M:%S")
+
+        with DBConnectionHandler() as db:
+            data_insert = Logs(
+                message = message,
+                status = status,
+                description = description,
+                start_date = start_date,
+                end_date = end_date,
+                rpa_id = rpa_id,
+                execution_id = execution_id
+            )
+
+            db.session.add(data_insert)
+            db.session.commit()
