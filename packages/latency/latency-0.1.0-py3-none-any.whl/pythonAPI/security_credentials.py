9401dd1f95d4dc0cef7c8b28929dd5cb35376fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:05:49 +0000

    Add package latency/0.1.0/latency-0.1.0-py3-none-any.whl

---
+++ packages/latency/latency-0.1.0-py3-none-any.whl/pythonAPI/security_credentials.py
@@ -0,0 +1,15 @@
+# username_authentication = "kess003"
+
+# password_authentication = "pcZ1#18"
+
+# username_query = "kess003"
+
+# password_query = "pcZ1#18"
+
+username_authentication = "alex001"
+
+password_authentication = "ALexandre774$"
+
+username_query = "alex001"
+
+password_query = "ALexandre774$"
