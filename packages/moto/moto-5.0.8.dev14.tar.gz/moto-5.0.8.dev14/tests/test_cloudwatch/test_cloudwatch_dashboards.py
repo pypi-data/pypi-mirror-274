2a7b19da989ec6c2601c8df76e9e8158f7f0ac37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:27 +0000

    Add package moto/5.0.8.dev14/moto-5.0.8.dev14.tar.gz

---
+++ packages/moto/moto-5.0.8.dev14.tar.gz/moto-5.0.8.dev14/tests/test_cloudwatch/test_cloudwatch_dashboards.py
@@ -0,0 +1,79 @@
+import boto3
+import pytest
+from botocore.exceptions import ClientError
+
+from moto import mock_aws
+
+
+@mock_aws
+def test_put_list_dashboard():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+    widget = '{"widgets": [{"type": "text", "x": 0, "y": 7, "width": 3, "height": 3, "properties": {"markdown": "Hello world"}}]}'
+
+    client.put_dashboard(DashboardName="test1", DashboardBody=widget)
+    resp = client.list_dashboards()
+
+    assert len(resp["DashboardEntries"]) == 1
+
+
+@mock_aws
+def test_put_list_prefix_nomatch_dashboard():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+    widget = '{"widgets": [{"type": "text", "x": 0, "y": 7, "width": 3, "height": 3, "properties": {"markdown": "Hello world"}}]}'
+
+    client.put_dashboard(DashboardName="test1", DashboardBody=widget)
+    resp = client.list_dashboards(DashboardNamePrefix="nomatch")
+
+    assert len(resp["DashboardEntries"]) == 0
+
+
+@mock_aws
+def test_delete_dashboard():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+    widget = '{"widgets": [{"type": "text", "x": 0, "y": 7, "width": 3, "height": 3, "properties": {"markdown": "Hello world"}}]}'
+
+    client.put_dashboard(DashboardName="test1", DashboardBody=widget)
+    client.put_dashboard(DashboardName="test2", DashboardBody=widget)
+    client.put_dashboard(DashboardName="test3", DashboardBody=widget)
+    client.delete_dashboards(DashboardNames=["test2", "test1"])
+
+    resp = client.list_dashboards(DashboardNamePrefix="test3")
+    assert len(resp["DashboardEntries"]) == 1
+
+
+@mock_aws
+def test_delete_dashboard_fail():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+    widget = '{"widgets": [{"type": "text", "x": 0, "y": 7, "width": 3, "height": 3, "properties": {"markdown": "Hello world"}}]}'
+
+    client.put_dashboard(DashboardName="test1", DashboardBody=widget)
+    client.put_dashboard(DashboardName="test2", DashboardBody=widget)
+    client.put_dashboard(DashboardName="test3", DashboardBody=widget)
+    # Doesn't delete anything if some dashboards to be deleted do not exist
+    with pytest.raises(ClientError) as exc:
+        client.delete_dashboards(DashboardNames=["test2", "test1", "test_no_match"])
+    assert exc.value.response["Error"]["Code"] == "ResourceNotFound"
+
+    resp = client.list_dashboards()
+    assert len(resp["DashboardEntries"]) == 3
+
+
+@mock_aws
+def test_get_dashboard():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+    widget = '{"widgets": [{"type": "text", "x": 0, "y": 7, "width": 3, "height": 3, "properties": {"markdown": "Hello world"}}]}'
+    client.put_dashboard(DashboardName="test1", DashboardBody=widget)
+
+    resp = client.get_dashboard(DashboardName="test1")
+    assert "DashboardArn" in resp
+    assert "DashboardBody" in resp
+    assert resp["DashboardName"] == "test1"
+
+
+@mock_aws
+def test_get_dashboard_fail():
+    client = boto3.client("cloudwatch", region_name="eu-central-1")
+
+    with pytest.raises(ClientError) as exc:
+        client.get_dashboard(DashboardName="test1")
+    assert exc.value.response["Error"]["Code"] == "ResourceNotFound"
