a24eae9093c11141c74d494cbc6ba84af55d4b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:15 +0000

    Add package lsm303d/1.0.0/lsm303d-1.0.0.tar.gz

---
+++ packages/lsm303d/lsm303d-1.0.0.tar.gz/lsm303d-1.0.0/CHANGELOG.md
@@ -0,0 +1,31 @@
+1.0.0
+-----
+
+* Port to hatch/pyproject.toml
+* Require i2cdevice>=1.0.0
+* Fix typo in CLICK register "threshold" option
+
+0.0.5
+-----
+
+* Require i2cdevice>=0.0.6
+
+0.0.4
+-----
+
+* Port to i2cdevice>=0.0.6 set/get API
+
+0.0.3
+-----
+
+* BugFix: Fix for compatibility with i2cdevice>=0.0.6
+
+0.0.2
+-----
+
+* Bumped i2cdevice dependency to >=0.0.4
+
+0.0.1
+-----
+
+* Initial Release
---
+++ packages/lsm303d/lsm303d-1.0.0.tar.gz/lsm303d-1.0.0/test.py
@@ -0,0 +1,18 @@
+import time
+
+from lsm303d import LSM303D
+
+lsm = LSM303D(0x1e)
+
+while True:
+    t = lsm.temperature()
+    m = lsm.magnetometer()
+    a = lsm.accelerometer()
+    #t_raw = lsm._lsm303d.values['TEMPERATURE']
+    #print("{:04.1f} {:016b}".format(t, t_raw))
+
+    values = list(m) + list(a)
+
+    print(("{:+06.2f} : {:+06.2f} : {:+06.2f}   " * 2).format(*values))
+
+    time.sleep(1.0/25)
