b33872eae3f4cbe18f8f510db67e45675cda1e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 14:04:31 +0000

    Add package bdi-kit/0.1.0/bdi_kit-0.1.0-py3-none-any.whl

---
+++ packages/bdi-kit/bdi_kit-0.1.0-py3-none-any.whl/bdikit/api.py
@@ -0,0 +1,87 @@
+from bdikit.data_ingestion.dataset_loader import load_dataframe
+from bdikit.mapping_recommendation.scope_reducing_manager import ScopeReducingManager
+from bdikit.mapping_recommendation.value_mapping_manager import ValueMappingManager
+from bdikit.mapping_recommendation.column_mapping_manager import ColumnMappingManager
+from bdikit.visualization.mappings import plot_reduce_scope, plot_column_mappings, plot_value_mappings
+from bdikit.utils import get_gdc_data
+from os.path import join, dirname
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false" # Disable huggingface messages
+
+GDC_DATA_PATH = join(dirname(__file__), './resource/gdc_table.csv')
+
+
+class APIManager():
+
+    def __init__(self,):
+        # TODO: move into database object (in data_ingestion folder)
+        self.dataset = None
+        # TODO: move into database object (in data_ingestion folder)
+        self.global_table = None
+        # Assuming self.reduced_scope to be a list o dictionary (one dict for each column)TODO: move into database object (in data_ingestion folder)
+        self.reduced_scope = None
+        self.column_manager = None
+        self.value_manager = None
+        self.column_mappings = None # TODO move this to a property in column_manager
+        self.value_mappings = None # TODO move this to a property in value_manager
+
+    def load_global_table(self, global_table_path=None):
+        if global_table_path is None:
+            self.global_table = load_dataframe(GDC_DATA_PATH)
+        else:
+            self.global_table = load_dataframe(global_table_path)
+        return self.global_table
+
+    def load_dataset(self, dataset_path):
+        if self.global_table is None:
+            self.load_global_table()
+        self.dataset = load_dataframe(dataset_path)
+
+        return self.dataset
+
+    def reduce_scope(self, num_columns=5, num_candidates=5):
+        self.scope_manager = ScopeReducingManager(self.dataset, self.global_table)
+        self.reduced_scope = self.scope_manager.reduce()
+        plot_reduce_scope(self.reduced_scope, num_columns, num_candidates)
+
+        return self.reduced_scope
+
+    def map_columns(self, algorithm='SimFloodAlgorithm'):
+        self.column_manager = ColumnMappingManager(self.dataset, self.global_table, algorithm)
+        self.column_manager.reduced_scope = self.reduced_scope
+        self.column_mappings = self.column_manager.map()
+        plot_column_mappings(self.column_mappings)
+
+        return self.column_mappings
+
+    def map_values(self, algorithm='EditAlgorithm'):
+        self.global_table_all = get_gdc_data(self.column_mappings.values())
+        self.value_manager = ValueMappingManager(self.dataset, self.column_mappings, self.global_table_all, algorithm)
+        self.value_mappings = self.value_manager.map()
+        plot_value_mappings(self.value_mappings)
+
+        return self.value_mappings
+
+    def update_reduced_scope(self, original_column, new_candidate_name, new_candidate_sim=1.0):
+        for index in range(len(self.reduced_scope)):
+            if self.reduced_scope[index]['Candidate column'] == original_column:
+                self.reduced_scope[index]['Top k columns'].append((new_candidate_name, new_candidate_sim))
+                print('Reduced scope updated!')
+                plot_reduce_scope(self.reduced_scope)
+                break
+
+    def update_column_mappings(self, new_mappings):
+        for original_column, new_target_column in new_mappings:
+            self.column_mappings[original_column] = new_target_column
+
+        print('Column mapping updated!')
+        plot_column_mappings(self.column_mappings)
+
+    def update_value_mappings(self, original_column, original_value, new_target_value, new_similarity=1.0):
+        for index in range(len(self.value_mappings[original_column]['matches'])):
+            if self.value_mappings[original_column]['matches'][index][0] == original_value:
+                self.value_mappings[original_column]['matches'][index] = (original_value, new_target_value, new_similarity)
+                print('Value mapping updated!')
+                plot_value_mappings(self.value_mappings)
+                break
---
+++ packages/bdi-kit/bdi_kit-0.1.0-py3-none-any.whl/bdikit/mapping_algorithms/column_mapping/algorithms.py
@@ -0,0 +1,140 @@
+from valentine import valentine_match
+from valentine.algorithms import SimilarityFlooding,Coma,Cupid,DistributionBased,JaccardDistanceMatcher
+from openai import OpenAI
+
+
+class BaseColumnMappingAlgorithm():
+    
+    def __init__(self, dataset, global_table):
+        self._dataset = dataset
+        self._global_table = global_table
+
+    def map(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class SimFloodAlgorithm(BaseColumnMappingAlgorithm):
+
+    def __init__(self, dataset, global_table):
+        super().__init__(dataset, global_table)
+    
+    def map(self):
+        matcher = SimilarityFlooding()
+        matches = valentine_match(self._dataset, self._global_table, matcher)
+
+        mappings = {}
+        for match in matches.one_to_one():
+            dataset_candidate = match[0][1]
+            global_table_candidate = match[1][1]
+            mappings[dataset_candidate] = global_table_candidate        
+        return mappings
+    
+class ComaAlgorithm(BaseColumnMappingAlgorithm):
+
+    def __init__(self, dataset, global_table):
+        super().__init__(dataset, global_table)
+    
+    def map(self):
+        matcher = Coma()
+        matches = valentine_match(self._dataset, self._global_table, matcher)
+
+        mappings = {}
+        for match in matches.one_to_one():
+            dataset_candidate = match[0][1]
+            global_table_candidate = match[1][1]
+            mappings[dataset_candidate] = global_table_candidate        
+        return mappings
+    
+class CupidAlgorithm(BaseColumnMappingAlgorithm):
+    
+        def __init__(self, dataset, global_table):
+            super().__init__(dataset, global_table)
+        
+        def map(self):
+            matcher = Cupid()
+            matches = valentine_match(self._dataset, self._global_table, matcher)
+    
+            mappings = {}
+            for match in matches.one_to_one():
+                dataset_candidate = match[0][1]
+                global_table_candidate = match[1][1]
+                mappings[dataset_candidate] = global_table_candidate        
+            return mappings
+        
+class DistributionBasedAlgorithm(BaseColumnMappingAlgorithm):
+        
+            def __init__(self, dataset, global_table):
+                super().__init__(dataset, global_table)
+            
+            def map(self):
+                matcher = DistributionBased()
+                matches = valentine_match(self._dataset, self._global_table, matcher)
+        
+                mappings = {}
+                for match in matches.one_to_one():
+                    dataset_candidate = match[0][1]
+                    global_table_candidate = match[1][1]
+                    mappings[dataset_candidate] = global_table_candidate        
+                return mappings
+            
+class JaccardDistanceAlgorithm(BaseColumnMappingAlgorithm):
+                
+                def __init__(self, dataset, global_table):
+                    super().__init__(dataset, global_table)
+                
+                def map(self):
+                    matcher = JaccardDistanceMatcher()
+                    matches = valentine_match(self._dataset, self._global_table, matcher)
+            
+                    mappings = {}
+                    for match in matches.one_to_one():
+                        dataset_candidate = match[0][1]
+                        global_table_candidate = match[1][1]
+                        mappings[dataset_candidate] = global_table_candidate        
+                    return mappings
+            
+
+class GPTAlgorithm(BaseColumnMappingAlgorithm):
+    
+    def __init__(self, dataset, global_table):
+        super().__init__(dataset, global_table)
+        self.client = OpenAI()
+    
+    def map(self):
+        global_columns = self._global_table.columns
+        labels = ', '.join(global_columns)
+        candidate_columns = self._dataset.columns
+        mappings = {}
+        for column in candidate_columns:
+            col = self._dataset[column]
+            values = col.drop_duplicates().dropna()
+            if len(values) > 15:
+                rows = values.sample(15).tolist()
+            else:
+                rows = values.tolist()
+            serialized_input = f"{column}: {', '.join([str(row) for row in rows])}"
+            context = serialized_input.lower()
+            column_types = self.get_column_type(context, labels)
+            for column_type in column_types:
+                if column_type in global_columns:
+                    mappings[column] = column_type
+                    break
+        return mappings
+
+
+
+    def get_column_type(self, context, labels, m=10, model="gpt-4-turbo-preview"):
+        messages=[
+                {
+                    "role": "system", 
+                    "content": "You are an assistant for column matching."},
+                {
+                    "role": "user", 
+                    "content": """ Please select the top """ + str(m) +  """ class from """ + labels + """ which best describes the context. The context is defined by the column name followed by its respective values. Please respond only with the name of the classes separated by semicolon.
+                    \n CONTEXT: """ + context +  """ \n RESPONSE: \n"""},
+            ]
+        col_type = self.client.chat.completions.create(model=model,
+        messages=messages,
+        temperature=0.3)
+        col_type_content = col_type.choices[0].message.content
+        return col_type_content.split(";")
