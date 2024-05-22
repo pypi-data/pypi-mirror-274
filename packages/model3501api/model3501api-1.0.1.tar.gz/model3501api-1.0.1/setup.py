81249c4208fa7bf886e5a648edc4833da000ea49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:05:14 +0000

    Add package model3501api/1.0.1/model3501api-1.0.1.tar.gz

---
+++ packages/model3501api/model3501api-1.0.1.tar.gz/model3501api-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='model3501api',
+    version='v1.0.1',
+    description='API for MCCI Connection exercisers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='MCCI Corporation',
+    author_email='vinayn@mcci.com',
+    url='https://github.com/mcci-usb/cricketlib',
+    packages=find_packages(),
+    install_requires=[
+        'pyusb',  # Add other dependencies if necessary
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
