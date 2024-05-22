cd4b52c43189564dc8cbd647ea7e212199bec32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:04:57 +0000

    Add package joistpy/0.1.2/joistpy-0.1.2.tar.gz

---
+++ packages/joistpy/joistpy-0.1.2.tar.gz/joistpy-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+name = 'joistpy'
+version = '0.1.2'
+author = 'Matthew Upshaw'
+author_email = 'matthew.upshaw02@gmail.com'
+description = 'Library that acts as a database for SJI steel open web bar joist shapes for easy use in structural calculations.'
+packages = find_packages()
+classifiers = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+]
+python_requires = '>=3.8'
+
+setup(name=name, version=version, author=author, author_email=author_email, description=description, packages=packages, classifiers=classifiers, python_requires=python_requires, include_package_data=True)
