56b7b3d5200d719337fe599b260eece7b801eeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 17:04:29 +0000

    Add package mdtidy/0.1.1/mdtidy-0.1.1.tar.gz

---
+++ packages/mdtidy/mdtidy-0.1.1.tar.gz/mdtidy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mdtidy',
+    version='0.1.1',
+    description='A Python library to clean and format markdown content into Jupyter Notebooks.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='David Jeremiah',
+    author_email='flasconnect@gmail.com',
+    url='https://github.com/davidkjeremiah/mdtidy',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
