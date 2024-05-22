285770272994d76f6482c4e7f18e4b849665489a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:15 +0000

    Add package server-templates/0.3.2/server_templates-0.3.2.tar.gz

---
+++ packages/server-templates/server_templates-0.3.2.tar.gz/server_templates-0.3.2/server_templates/__init__.py
@@ -0,0 +1,5 @@
+# from server_templates.server_aiohttp.main import *
+from server_templates.server_aiohttp import *
+from server_templates.server_fastapi import *
+from server_templates.client_requests import *
+from server_templates.url import UrlCreator
