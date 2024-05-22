0c73b90b95cd352f45fb62622a411d8e30eb9f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 05:05:17 +0000

    Add package Mensajes-diegopolc/6.0/mensajes_diegopolc-6.0.tar.gz

---
+++ packages/Mensajes-diegopolc/mensajes_diegopolc-6.0.tar.gz/mensajes_diegopolc-6.0/mensajes/hola/saludos.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def saludar():
+    print("Hola, te saludo desde saludos.saludar()")
+
+def prueba():
+    print('Esto es una nueva prueba de la nueva versión 6.0')
+
+def generar_array(numeros):
+    return np.arange(numeros)
+
+
+
+class Saludo:
+    def __init__ (self):
+        print("Hola, te saludo desde saludo.__init__()")
+
+
+if __name__ == '__main__': #Esta linea es una condicion dentro del modulo para que la linea de codigo no se ejecute desde un modulo cargado.
+    print(generar_array(5))
+
+    
