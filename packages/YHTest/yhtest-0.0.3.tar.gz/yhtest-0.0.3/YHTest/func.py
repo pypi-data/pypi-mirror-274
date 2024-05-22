b4ee1ef0d462f43d17db1b412242eea619674c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 15:05:47 +0000

    Add package YHTest/0.0.3/yhtest-0.0.3.tar.gz

---
+++ packages/YHTest/yhtest-0.0.3.tar.gz/yhtest-0.0.3/YHTest/func.py
@@ -0,0 +1,8 @@
+def author_address():
+    print('shenzhen,China')
+
+def author_master():
+    print('YH')
+
+def author_hometown():
+    print('Hubei Province')
---
+++ packages/YHTest/yhtest-0.0.3.tar.gz/yhtest-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='YHTest',
+    version='0.0.3',
+    author='YHTest',
+    author_email='yihuain163@163.com',
+    description='YHTest Python Package',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
