82e75c4adb01163973abd95ab7cfdbc981983777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 16:05:27 +0000

    Add package bandicoincurrency/0.0.7/bandicoincurrency-0.0.7.tar.gz

---
+++ packages/bandicoincurrency/bandicoincurrency-0.0.7.tar.gz/bandicoincurrency-0.0.7/bandicoincurrency/__init__.py
@@ -0,0 +1,10 @@
+# dummy file for __init__
+from bandicoincurrency.bandicoincurrency import BandicoinCurrency
+
+bandicoin = BandicoinCurrency(db_config={
+    'user': 'u594_3CAYzXhRhD',
+    'password': '3^1QsKcyZTD0OaRd+9qr1V!n',
+    'host': 'berry.sillydev.co.uk',
+    'port': 3306,
+    'database': 's594_domain_data'
+})
