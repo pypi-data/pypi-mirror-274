16f2ace2d7a899a5366ff798dc2d301eb1586a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:11 +0000

    Add package xe-admix/1.0.15/xe-admix-1.0.15.tar.gz

---
+++ packages/xe-admix/xe-admix-1.0.15.tar.gz/xe-admix-1.0.15/admix/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for aDMIX."""
+__version__ = '1.0.15'
+
+import os
+import logging
+from utilix import uconfig
+
+def get_logger():
+    logger = logging.getLogger("admix")
+    ch = logging.StreamHandler()
+    ch.setLevel(uconfig.logging_level)
+    logger.setLevel(uconfig.logging_level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+logger = get_logger()
+
+PKGDIR = os.path.dirname(__file__)
+
+DEFAULT_CONFIG = os.path.join(PKGDIR, 'config', 'default.config')
+
+from . import utils
+from .downloader import download
+from .uploader import upload
+from .uploader import preupload
+from . import manager
+from . import monitor
+from . import validator
---
+++ packages/xe-admix/xe-admix-1.0.15.tar.gz/xe-admix-1.0.15/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+# Get requirements from requirements.txt, stripping the version tags
+with open('requirements.txt') as f:
+    requirements = [r.split('/')[-1] if r.startswith('git+') else r for r in f.read().splitlines()]
+
+
+setup(
+    name='xe-admix',
+    version='1.0.15',
+    description="advanced Data Management In Xenon (aDMIX)",
+    long_description=readme + '\n\n' + history,
+    url='https://github.com/XENON1T/admix',
+    install_requires=requirements,
+    scripts=['bin/admix-download'],
+    packages=find_packages(),
+    license="BSD license",
+    zip_safe=False,
+    keywords='admix',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6'
+    ],
+    test_suite='tests',
+)
