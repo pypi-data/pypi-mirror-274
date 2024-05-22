56f0da1c061d80f4e6649e380afddd03bef4fb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:14 +0000

    Add package parsee-core/0.1.4.34/parsee_core-0.1.4.34.tar.gz

---
+++ packages/parsee-core/parsee_core-0.1.4.34.tar.gz/parsee_core-0.1.4.34/parsee/extraction/models/llm_models/llm_base_model.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from typing import *
+
+from tiktoken.core import Encoding
+
+from parsee.extraction.models.llm_models.prompts import Prompt
+from parsee.extraction.models.model_dataclasses import MlModelSpecification
+
+
+def get_tokens_encoded(prompt: str, encoding: Encoding) -> List[int]:
+    return encoding.encode(prompt)
+
+
+def truncate_prompt(prompt: str, encoding: Encoding, max_tokens: int) -> Tuple[str, int]:
+    tokens = get_tokens_encoded(prompt, encoding)
+    num_tokens = len(tokens)
+    if num_tokens > max_tokens:
+        prompt = encoding.decode(tokens[0:max_tokens])
+        num_tokens = max_tokens
+    return prompt, num_tokens
+
+
+class LLMBaseModel:
+
+    spec: MlModelSpecification
+
+    def __init__(self, spec: MlModelSpecification):
+        self.spec = spec
+
+    def make_prompt_request(self, prompt: Prompt) -> Tuple[str, Decimal]:
+        raise NotImplementedError
---
+++ packages/parsee-core/parsee_core-0.1.4.34.tar.gz/parsee_core-0.1.4.34/parsee/storage/vector_stores/interfaces.py
@@ -0,0 +1,9 @@
+from typing import *
+
+from parsee.extraction.extractor_elements import StandardDocumentFormat, ExtractedEl
+
+
+class VectorStore:
+
+    def find_closest_elements(self, document: StandardDocumentFormat, search_element_title: str, keywords: str, tables_only: bool = True) -> List[ExtractedEl]:
+        raise NotImplementedError
