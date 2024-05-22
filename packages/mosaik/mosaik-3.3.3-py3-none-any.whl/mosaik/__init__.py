32ac1e2493e148d7defa2fc0552aac8d1bd01fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 03:06:31 +0000

    Add package mosaik/3.3.3/mosaik-3.3.3-py3-none-any.whl

---
+++ packages/mosaik/mosaik-3.3.3-py3-none-any.whl/mosaik/__init__.py
@@ -0,0 +1,16 @@
+"""
+This module provides convenient access to all classes and functions required
+to create scenarios and run simulations.
+
+Currently, this is only :class:`mosaik.scenario.World`.
+
+"""
+# Saying "import X as X" (with X repeated) is the standard way of
+# marking to linters and type checkers that something is re-exported.
+from mosaik.scenario import SimConfig as SimConfig
+from mosaik.scenario import World as World
+
+from mosaik._version import version
+
+__all__ = ['World']
+__version__ = version
