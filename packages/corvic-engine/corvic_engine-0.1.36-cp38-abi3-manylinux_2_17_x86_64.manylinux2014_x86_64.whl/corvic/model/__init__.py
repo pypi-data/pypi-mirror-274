d41b24078519523879592265d718973a2e332232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-22 04:04:35 +0000

    Add package corvic-engine/0.1.36/corvic_engine-0.1.36-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/corvic-engine/corvic_engine-0.1.36-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/corvic/model/__init__.py
@@ -0,0 +1,17 @@
+"""Data modeling objects for creating corvic pipelines."""
+
+from corvic.model.experiments import Experiment
+from corvic.model.sources import Source, SourceType
+from corvic.model.spaces import Column, Space, SpaceEdgeTableMetadata
+from corvic.table import FeatureType, feature_type
+
+__all__ = [
+    "Column",
+    "Experiment",
+    "FeatureType",
+    "Source",
+    "SourceType",
+    "Space",
+    "SpaceEdgeTableMetadata",
+    "feature_type",
+]
