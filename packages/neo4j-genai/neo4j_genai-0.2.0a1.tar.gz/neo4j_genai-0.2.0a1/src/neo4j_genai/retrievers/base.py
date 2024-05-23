11cb6d9682bde9920d9addfd21e47b2093b4c4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:44 +0000

    Add package neo4j-genai/0.2.0a1/neo4j_genai-0.2.0a1.tar.gz

---
+++ packages/neo4j-genai/neo4j_genai-0.2.0a1.tar.gz/neo4j_genai-0.2.0a1/src/neo4j_genai/retrievers/base.py
@@ -0,0 +1,102 @@
+#  Copyright (c) "Neo4j"
+#  Neo4j Sweden AB [https://neo4j.com]
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from abc import ABC, abstractmethod
+from typing import Optional, Any
+import neo4j
+
+
+class Retriever(ABC):
+    """
+    Abstract class for Neo4j retrievers
+    """
+
+    def __init__(self, driver: neo4j.Driver):
+        self.driver = driver
+        self._verify_version()
+
+    def _verify_version(self) -> None:
+        """
+        Check if the connected Neo4j database version supports vector indexing.
+
+        Queries the Neo4j database to retrieve its version and compares it
+        against a target version (5.18.1) that is known to support vector
+        indexing. Raises a ValueError if the connected Neo4j version is
+        not supported.
+        """
+        records, _, _ = self.driver.execute_query("CALL dbms.components()")
+        version = records[0]["versions"][0]
+
+        if "aura" in version:
+            version_tuple = (
+                *tuple(map(int, version.split("-")[0].split("."))),
+                0,
+            )
+            target_version = (5, 18, 0)
+        else:
+            version_tuple = tuple(map(int, version.split(".")))
+            target_version = (5, 18, 1)
+
+        if version_tuple < target_version:
+            raise ValueError(
+                "This package only supports Neo4j version 5.18.1 or greater"
+            )
+
+    @abstractmethod
+    def search(self, *args, **kwargs) -> Any:
+        pass
+
+    def _fetch_index_infos(self):
+        """Fetch the node label and embedding property from the index definition"""
+        query = (
+            "SHOW VECTOR INDEXES "
+            "YIELD name, labelsOrTypes, properties, options "
+            "WHERE name = $index_name "
+            "RETURN labelsOrTypes as labels, properties, "
+            "options.indexConfig.`vector.dimensions` as dimensions"
+        )
+        result = self.driver.execute_query(query, {"index_name": self.index_name})
+        try:
+            result = result.records[0]
+        except IndexError:
+            raise Exception(f"No index with name {self.index_name} found")
+        self._node_label = result["labels"][0]
+        self._embedding_node_property = result["properties"][0]
+        self._embedding_dimension = result["dimensions"]
+
+
+class ExternalRetriever(ABC):
+    """
+    Abstract class for External Vector Stores
+    """
+
+    def __init__(self, id_property_external: str, id_property_neo4j: str):
+        self.id_property_external = id_property_external
+        self.id_property_neo4j = id_property_neo4j
+
+    @abstractmethod
+    def search(
+        self,
+        query_vector: Optional[list[float]] = None,
+        query_text: Optional[str] = None,
+        top_k: int = 5,
+        **kwargs,
+    ) -> list[neo4j.Record]:
+        """
+
+        Returns:
+                list[neo4j.Record]: List of Neo4j Records
+
+        """
+        pass
---
+++ packages/neo4j-genai/neo4j_genai-0.2.0a1.tar.gz/neo4j_genai-0.2.0a1/src/neo4j_genai/types.py
@@ -0,0 +1,155 @@
+#  Copyright (c) "Neo4j"
+#  Neo4j Sweden AB [https://neo4j.com]
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from enum import Enum
+from typing import Any, Literal, Optional
+from pydantic import (
+    BaseModel,
+    PositiveInt,
+    model_validator,
+    field_validator,
+    ConfigDict,
+)
+import neo4j
+from neo4j_genai.retrievers.utils import validate_search_query_input
+
+
+class VectorSearchRecord(BaseModel):
+    node: Any
+    score: float
+
+
+class IndexModel(BaseModel):
+    driver: Any
+
+    @field_validator("driver")
+    def check_driver_is_valid(cls, v):
+        if not isinstance(v, neo4j.Driver):
+            raise ValueError("driver must be an instance of neo4j.Driver")
+        return v
+
+
+class VectorIndexModel(IndexModel):
+    name: str
+    label: str
+    property: str
+    dimensions: PositiveInt
+    similarity_fn: Literal["euclidean", "cosine"]
+
+
+class FulltextIndexModel(IndexModel):
+    name: str
+    label: str
+    node_properties: list[str]
+
+    @field_validator("node_properties")
+    def check_node_properties_not_empty(cls, v):
+        if len(v) == 0:
+            raise ValueError("node_properties cannot be an empty list")
+        return v
+
+
+class VectorSearchModel(BaseModel):
+    vector_index_name: str
+    top_k: PositiveInt = 5
+    query_vector: Optional[list[float]] = None
+    query_text: Optional[str] = None
+
+    @model_validator(mode="before")
+    def check_query(cls, values):
+        """
+        Validates that one of either query_vector or query_text is provided exclusively.
+        """
+        query_vector, query_text = values.get("query_vector"), values.get("query_text")
+        validate_search_query_input(query_text, query_vector)
+        return values
+
+
+class VectorCypherSearchModel(VectorSearchModel):
+    query_params: Optional[dict[str, Any]] = None
+
+
+class HybridSearchModel(BaseModel):
+    vector_index_name: str
+    fulltext_index_name: str
+    query_text: str
+    top_k: PositiveInt = 5
+    query_vector: Optional[list[float]] = None
+
+
+class HybridCypherSearchModel(HybridSearchModel):
+    query_params: Optional[dict[str, Any]] = None
+
+
+class SearchType(str, Enum):
+    """Enumerator of the search strategies."""
+
+    VECTOR = "vector"
+    HYBRID = "hybrid"
+
+
+class EmbedderModel(BaseModel):
+    embedder: Optional[Any]
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @field_validator("embedder")
+    def check_embedder(cls, value):
+        if not hasattr(value, "embed_query") or not callable(
+            getattr(value, "embed_query", None)
+        ):
+            raise ValueError(
+                "Provided embedder object must have an 'embed_query' callable method."
+            )
+        return value
+
+
+class Neo4jDriverModel(BaseModel):
+    driver: neo4j.Driver
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @field_validator("driver")
+    def check_driver(cls, value):
+        if not isinstance(value, neo4j.Driver):
+            raise ValueError("Provided driver needs to be of type neo4j.Driver")
+        return value
+
+
+class VectorRetrieverModel(BaseModel):
+    driver_model: Neo4jDriverModel
+    index_name: str
+    embedder_model: Optional[EmbedderModel] = None
+    return_properties: Optional[list[str]] = None
+
+
+class VectorCypherRetrieverModel(BaseModel):
+    driver_model: Neo4jDriverModel
+    index_name: str
+    retrieval_query: str
+    embedder_model: Optional[EmbedderModel] = None
+
+
+class HybridRetrieverModel(BaseModel):
+    driver_model: Neo4jDriverModel
+    vector_index_name: str
+    fulltext_index_name: str
+    embedder_model: Optional[EmbedderModel] = None
+    return_properties: Optional[list[str]] = None
+
+
+class HybridCypherRetrieverModel(BaseModel):
+    driver_model: Neo4jDriverModel
+    vector_index_name: str
+    fulltext_index_name: str
+    retrieval_query: str
+    embedder_model: Optional[EmbedderModel] = None
