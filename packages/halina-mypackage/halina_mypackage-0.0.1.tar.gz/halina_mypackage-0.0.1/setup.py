5906cc219e1c1efb224aa9e5b739a9a71374bcb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:05:11 +0000

    Add package halina-mypackage/0.0.1/halina_mypackage-0.0.1.tar.gz

---
+++ packages/halina-mypackage/halina_mypackage-0.0.1.tar.gz/halina_mypackage-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='halina_mypackage', #same name as directory
+    author='halina',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+)
