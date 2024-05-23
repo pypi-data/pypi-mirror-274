8396bc28008c2cdbe57f4110cc34402b6a48b2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:05:25 +0000

    Add package vocabulary-quiz/0.4/vocabulary_quiz-0.4.tar.gz

---
+++ packages/vocabulary-quiz/vocabulary_quiz-0.4.tar.gz/vocabulary_quiz-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vocabulary_quiz",
+    version="0.4",  # 버전을 0.3으로 업데이트
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "requests",
+        "beautifulsoup4"
+    ],
+    entry_points={
+        'console_scripts': [
+            'vocabulary_quiz=App.app:main',
+        ],
+    },
+)
