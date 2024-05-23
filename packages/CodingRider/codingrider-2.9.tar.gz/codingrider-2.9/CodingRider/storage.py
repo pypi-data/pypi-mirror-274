4439686e1bf6ee8bc7be41f1a56d69d76752df75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:06:01 +0000

    Add package CodingRider/2.9/codingrider-2.9.tar.gz

---
+++ packages/CodingRider/codingrider-2.9.tar.gz/codingrider-2.9/CodingRider/storage.py
@@ -0,0 +1,70 @@
+import time
+
+from CodingRider.protocol import *
+from CodingRider.system import *
+
+
+# EventHandler
+class EventHandler:
+
+    def __init__(self):
+        self.d = dict.fromkeys(list(DataType))
+
+
+
+# StorageHeader
+class StorageHeader:
+
+    def __init__(self):
+        self.d = dict.fromkeys(list(DataType))
+
+
+
+# Storage
+class Storage:
+
+    def __init__(self):
+        self.d = dict.fromkeys(list(DataType))
+
+
+
+# Storage Count
+class StorageCount:
+
+    def __init__(self):
+        self.d = dict.fromkeys(list(DataType))
+        
+        for key in self.d:
+            self.d[key] = 0
+
+
+
+# Storage
+class Parser:
+
+    def __init__(self):
+        self.d = dict.fromkeys(list(DataType))
+
+        self.d[DataType.Ping]               = Ping.parse
+        self.d[DataType.Ack]                = Ack.parse
+        self.d[DataType.Error]              = Error.parse
+        self.d[DataType.Information]        = Information.parse
+        self.d[DataType.VisionSensor]       = VisionSensor.parse
+        self.d[DataType.Pairing]            = Pairing.parse
+
+        self.d[DataType.RawMotion]          = RawMotion.parse
+
+        self.d[DataType.State]              = State.parse
+    
+        self.d[DataType.Altitude]           = Altitude.parse
+        self.d[DataType.Motion]             = Motion.parse
+
+        self.d[DataType.Count]              = Count.parse
+        self.d[DataType.Trim]               = Trim.parse
+
+        self.d[DataType.Button]             = Button.parse
+        self.d[DataType.Joystick]           = Joystick.parse
+
+        self.d[DataType.InformationAssembledForController]  = InformationAssembledForController.parse
+
+
