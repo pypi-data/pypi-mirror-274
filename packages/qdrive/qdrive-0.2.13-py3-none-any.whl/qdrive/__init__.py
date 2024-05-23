c4bb54222dfa1a5d500e4cacc6619a05fddba6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 04:04:59 +0000

    Add package qdrive/0.2.13/qdrive-0.2.13-py3-none-any.whl

---
+++ packages/qdrive/qdrive-0.2.13-py3-none-any.whl/qdrive/__init__.py
@@ -0,0 +1,8 @@
+from etiket_client import login, logout
+from etiket_client.GUI.app import launch_GUI as l_GUI
+from etiket_client.settings.user_settings import user_settings
+
+def launch_GUI():
+    l_GUI()
+
+from qdrive.dataset.dataset import dataset
