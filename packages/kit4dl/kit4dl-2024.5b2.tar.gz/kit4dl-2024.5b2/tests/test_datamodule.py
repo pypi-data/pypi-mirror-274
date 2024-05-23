0e27d2751db8e480ef68055ab38dd86d93b32a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 04:04:30 +0000

    Add package kit4dl/2024.5b2/kit4dl-2024.5b2.tar.gz

---
+++ packages/kit4dl/kit4dl-2024.5b2.tar.gz/kit4dl-2024.5b2/tests/test_datamodule.py
@@ -0,0 +1,17 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from kit4dl.nn.dataset import Kit4DLAbstractDataModule
+
+
+class TestDatamodule:
+    class DummyDataModule(Kit4DLAbstractDataModule):
+        pass
+
+    def test_setting_extra_arguments(self):
+        conf = MagicMock()
+        conf.arguments = {"abc": 1}
+        datamodule = TestDatamodule.DummyDataModule(conf)
+        assert hasattr(datamodule, "abc")
+        assert datamodule.abc == 1
