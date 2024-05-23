2115c0fa9833918e432660ff722238b0afdf0378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:23 +0000

    Add package pytbai/1.5.0/pytbai-1.5.0.tar.gz

---
+++ packages/pytbai/pytbai-1.5.0.tar.gz/pytbai-1.5.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="pytbai",
+    version="1.5.0",
+    description=(
+        "pytbai allows to create, manage and send TicketBai invoices to the"
+        " Basque tax authorities"
+    ),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Urtzi Odriozola",
+    author_email="uodriozola@codesyntax.com",
+    url="https://github.com/codesyntax/pytbai",
+    license=license,
+    packages=find_packages(exclude=("tests",)),
+    include_package_data = True,
+    package_data={
+        "pytbai": ["templates/*"],
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
