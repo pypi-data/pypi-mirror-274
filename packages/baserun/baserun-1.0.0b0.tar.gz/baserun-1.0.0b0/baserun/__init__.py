89f7de54dbbc670b4c0a15731fbc17f7dae22f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:06:14 +0000

    Add package baserun/1.0.0b0/baserun-1.0.0b0.tar.gz

---
+++ packages/baserun/baserun-1.0.0b0.tar.gz/baserun-1.0.0b0/baserun/__init__.py
@@ -0,0 +1,67 @@
+import os
+
+from .annotation import Annotation
+from .baserun import Baserun
+from .checks import (
+    check as check,
+    check_equals as check_equals,
+    check_includes as check_includes,
+)
+from .sessions import (
+    start_session as start_session,
+    end_session as end_session,
+    with_session as with_session,
+    astart_session as astart_session,
+    aend_session as aend_session,
+)
+from .templates import (
+    format_prompt as format_prompt,
+    register_template as register_template,
+    aregister_template as aregister_template,
+    get_templates as get_templates,
+    get_template as get_template,
+)
+from .thread_wrapper import baserun_thread_wrapper as thread_wrapper
+from .users import (
+    submit_user as submit_user,
+    asubmit_user as asubmit_user,
+)
+
+init = Baserun.init
+start_trace = Baserun.start_trace
+trace = Baserun.trace
+log = Baserun.log
+evals = Baserun.evals
+api_key = os.environ.get("BASERUN_API_KEY")
+annotate = Annotation.exported_annotate
+submit_input_variable = Baserun.submit_input_variable
+finish = Baserun.finish
+
+__all__ = [
+    "Baserun",
+    "api_key",
+    "init",
+    "start_trace",
+    "trace",
+    "annotate",
+    "log",
+    "evals",
+    "thread_wrapper",
+    "with_session",
+    "start_session",
+    "end_session",
+    "astart_session",
+    "aend_session",
+    "format_prompt",
+    "register_template",
+    "get_templates",
+    "get_template",
+    "aregister_template",
+    "submit_user",
+    "asubmit_user",
+    "check",
+    "check_equals",
+    "check_includes",
+    "submit_input_variable",
+    "finish",
+]
