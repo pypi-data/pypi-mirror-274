e77617b8db6d517bf82d69a8d58aa2e49159da8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:05:30 +0000

    Add package talking-equipment-sdk/0.0.3/talking_equipment_sdk-0.0.3.tar.gz

---
+++ packages/talking-equipment-sdk/talking_equipment_sdk-0.0.3.tar.gz/talking_equipment_sdk-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+module_directory = 'talking_equipment'
+
+setup(
+    name="talking-equipment-sdk",
+    version="0.0.3",
+    author="Nathan Johnson",
+    author_email="nathanj@stratusadv.com",
+    description="Talking Equipment Standard Development Kit",
+    long_description=open(f"{module_directory}/README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": module_directory},
+    packages=find_packages(where=module_directory, exclude=["tests"]),
+    include_package_data=True,
+    python_requires=">=3.9",
+)
