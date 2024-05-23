20b7f46cfde1000bec3ce447e40314c59373dba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:02 +0000

    Add package hikac/0.0.10/hikac-0.0.10.tar.gz

---
+++ packages/hikac/hikac-0.0.10.tar.gz/hikac-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="hikac",
+    version="0.0.10",
+    description="A hikvision access control request manager",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yusufjonc07/hikac",
+    author="YusufAxmad",
+    author_email="studentyusufaxmad@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["starlette"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.8",
+)
