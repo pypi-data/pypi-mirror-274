237e9e9a2d1527b2fac986f80f2a6b60993ba9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:06:15 +0000

    Add package pyerualjetwork/1.0.4/pyerualjetwork-1.0.4.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-1.0.4.tar.gz/pyerualjetwork-1.0.4/DPNN/__init__.py
@@ -0,0 +1,18 @@
+import pyerualjetwork
+import pyerualjetwork.DPNN
+from pyerualjetwork.DPNN import TrainDPNN
+from pyerualjetwork.DPNN import WeightIdentification
+from pyerualjetwork.DPNN import SynapticPruning
+from pyerualjetwork.DPNN import SynapticDividing
+from pyerualjetwork.DPNN import Fex
+from pyerualjetwork.DPNN import Cat
+from pyerualjetwork.DPNN import Normalization
+from pyerualjetwork.DPNN import Softmax
+from pyerualjetwork.DPNN import Sigmoid
+from pyerualjetwork.DPNN import Relu
+from pyerualjetwork.DPNN import TestDPNN
+from pyerualjetwork.DPNN import SaveDPNN
+from pyerualjetwork.DPNN import LoadDPNN
+from pyerualjetwork.DPNN import PredictFromDiscDPNN
+from pyerualjetwork.DPNN import PredictFromRamDPNN
+from pyerualjetwork.DPNN import AutoBalancer
