60962b13be9cf1b9712f057399c9df5bc08c74fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:05:01 +0000

    Add package ctuFaultDetector/0.9.2/ctuFaultDetector-0.9.2.tar.gz

---
+++ packages/ctuFaultDetector/ctuFaultDetector-0.9.2.tar.gz/ctuFaultDetector-0.9.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ctuFaultDetector",
+    version="0.9.2",
+    author="Ales Trna",
+    author_email="altrna@fel.cvut.cz",
+    description="Anomaly detection in time series model",
+    long_description="Contains only the feature and deviation classifiers to make the package more lightweight",
+    packages=find_packages(),
+    url="https://github.com/altrna/Anomaly-detection-in-timeseries",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scikit_learn',
+        'scipy',
+        'tslearn',
+    ],
+)
