578e0ecc86fdafb21b98ce20007aff842415ca7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:23 +0000

    Add package bitlinear/2.0.0/bitlinear-2.0.0.tar.gz

---
+++ packages/bitlinear/bitlinear-2.0.0.tar.gz/bitlinear-2.0.0/bitlinear/__init__.py
@@ -0,0 +1,16 @@
+from .bitlinear import (
+    BitLinear,
+    replace_modules,
+)
+from .measures import (
+    AbsMax,
+    AbsMean,
+    AbsMedian,
+)
+from .kernels import (
+    Naive,
+    NaiveListComp,
+    TernaryNaive,
+    TorchLinear,
+    TorchMulAdd,
+)
