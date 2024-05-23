244c090d3af99d72b061e62b9b7cc6e24d457451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 04:04:30 +0000

    Add package pydicomsorter/0.7.0/pydicomsorter-0.7.0-py3-none-any.whl

---
+++ packages/pydicomsorter/pydicomsorter-0.7.0-py3-none-any.whl/pydicomsorter/io.py
@@ -0,0 +1,16 @@
+"""I/O utilities."""
+
+from pathlib import Path
+
+def find_dicom_files(source_dir: Path) -> list[Path]:
+    """Find all DICOM files in the source directory."""
+    return [file.resolve() for file in source_dir.glob('**/*.dcm') if file.is_file()]
+
+
+if '__main__' == __name__:
+    files = find_dicom_files(Path('data'))
+
+    print(f'Length of files: {len(files)}')
+    print(f'First file: {files[0]}')
+
+    print(f'Permission in octal: {files[0].stat().st_mode & 0o777}')
---
+++ packages/pydicomsorter/pydicomsorter-0.7.0-py3-none-any.whl/pydicomsorter/main.py
@@ -0,0 +1,16 @@
+"""Main module of the package."""
+
+from rich import print
+
+def hello() -> tuple[str, str]:
+    """Return a greeting message and an e moji."""
+    return 'Hello from pydicomsorter, [bold magenta]World[/bold magenta]!', ':vampire:'
+
+
+def main() -> None:
+    """Print a greeting message and an emoji."""
+    print(*hello())
+
+
+if __name__ == '__main__':
+    main()
