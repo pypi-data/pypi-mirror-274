7133ec95b85b8852e6e3b470c2f5af123fa7c020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:02 +0000

    Add package aws-opentelemetry-distro/0.2.0/aws_opentelemetry_distro-0.2.0-py3-none-any.whl

---
+++ packages/aws-opentelemetry-distro/aws_opentelemetry_distro-0.2.0-py3-none-any.whl/amazon/opentelemetry/distro/_aws_attribute_keys.py
@@ -0,0 +1,18 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""Utility module holding attribute keys with special meaning to AWS components"""
+AWS_SPAN_KIND: str = "aws.span.kind"
+AWS_LOCAL_SERVICE: str = "aws.local.service"
+AWS_LOCAL_OPERATION: str = "aws.local.operation"
+AWS_REMOTE_SERVICE: str = "aws.remote.service"
+AWS_REMOTE_OPERATION: str = "aws.remote.operation"
+AWS_REMOTE_RESOURCE_TYPE: str = "aws.remote.resource.type"
+AWS_REMOTE_RESOURCE_IDENTIFIER: str = "aws.remote.resource.identifier"
+AWS_SDK_DESCENDANT: str = "aws.sdk.descendant"
+AWS_CONSUMER_PARENT_SPAN_KIND: str = "aws.consumer.parent.span.kind"
+
+# AWS_#_NAME attributes are not supported in python as they are not part of the Semantic Conventions.
+# TODO：Move to Semantic Conventions when these attributes are added.
+AWS_QUEUE_URL: str = "aws.sqs.queue_url"
+AWS_QUEUE_NAME: str = "aws.sqs.queue_name"
+AWS_STREAM_NAME: str = "aws.kinesis.stream_name"
