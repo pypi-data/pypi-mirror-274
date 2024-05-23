385bec763ad82e88740168d245e1a0b1dab12ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:06:12 +0000

    Add package pyerualjetwork/1.1.4/pyerualjetwork-1.1.4-py3-none-any.whl

---
+++ packages/pyerualjetwork/pyerualjetwork-1.1.4-py3-none-any.whl/DPNN/__init__.py
@@ -0,0 +1,20 @@
+# pyerualjetwork/DPNN/__init__.py
+
+# Bu dosya, DPNN modülünün ana giriş noktasıdır.
+
+from .AutoBalancer import AutoBalancer
+from .Normalization import Normalization
+from .Softmax import Softmax
+from .Sigmoid import Sigmoid
+from .Relu import Relu
+from .SynapticPruning import SynapticPruning
+from .SynapticDividing import SynapticDividing
+from .WeightIdentification import WeightIdentification
+from .Fex import Fex
+from .Cat import Cat
+from .TrainDPNN import TrainDPNN
+from .TestDPNN import TestDPNN
+from .SaveDPNN import SaveDPNN
+from .LoadDPNN import LoadDPNN
+from .PredictFromDiscDPNN import PredictFromDiscDPNN
+from .PredictFromRamDPNN import PredictFromRamDPNN
