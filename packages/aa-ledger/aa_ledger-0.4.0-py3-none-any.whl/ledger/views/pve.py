589c23d8e3bb70fd7ceda814a3c5eb8bff186f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 07:05:14 +0000

    Add package aa-ledger/0.4.0/aa_ledger-0.4.0-py3-none-any.whl

---
+++ packages/aa-ledger/aa_ledger-0.4.0-py3-none-any.whl/ledger/views/pve.py
@@ -0,0 +1,38 @@
+"""PvE Views"""
+
+# Django
+from django.conf import settings
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import render
+
+# Voices of War
+from ledger.hooks import get_extension_logger
+
+logger = get_extension_logger(__name__)
+
+
+@login_required
+@permission_required("ledger.basic_access")
+def ledger_index(request):
+    context = {
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/index.html", context=context)
+
+
+@login_required
+@permission_required("ledger.basic_access")
+def ratting_index(request):
+    context = {
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/corpledger/corp_ledger.html", context=context)
+
+
+@login_required
+@permission_required("ledger.basic_access")
+def ratting_char_index(request):
+    context = {
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/charledger/char_ledger.html", context=context)
