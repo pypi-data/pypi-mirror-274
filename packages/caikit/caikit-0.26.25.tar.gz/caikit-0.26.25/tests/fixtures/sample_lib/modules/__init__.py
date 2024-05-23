ef387f40f3839e82128e47d623be01a9ee119596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:06:26 +0000

    Add package caikit/0.26.25/caikit-0.26.25.tar.gz

---
+++ packages/caikit/caikit-0.26.25.tar.gz/caikit-0.26.25/tests/fixtures/sample_lib/modules/__init__.py
@@ -0,0 +1,13 @@
+# Local
+from .file_processing import BoundingBoxModule
+from .geospatial import GeoStreamingModule
+from .multi_task import FirstTask, MultiTaskModule, SecondTask
+from .other_task import OtherModule
+from .sample_task import (
+    CompositeModule,
+    InnerModule,
+    ListModule,
+    SampleModule,
+    SamplePrimitiveModule,
+    StreamingModule,
+)
