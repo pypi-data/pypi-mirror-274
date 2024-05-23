c95c7c0af3d75cd71915db221d283ef949877dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:49 +0000

    Add package hypercrawl-lite/0.1.0/hypercrawl-lite-0.1.0.tar.gz

---
+++ packages/hypercrawl-lite/hypercrawl-lite-0.1.0.tar.gz/hypercrawl-lite-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hypercrawl-lite',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hypercrawl-lite=hypercrawl_lite.scraper:main'
+        ]
+    },
+    author='Udit Akhouri',
+    description='A lightweight web scraper for extracting URLs from a webpage. This is utlrafastlite version of Hypercrawl',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uditakhourii/hypercrawl-lite',
+)
+
