6ab1a7ba66b9b5c95041d746068b28304b0394ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:58 +0000

    Add package model3501api/1.0.5/model3501api-1.0.5-py3-none-any.whl

---
+++ packages/model3501api/model3501api-1.0.5-py3-none-any.whl/model3501lib/pdcaptive_cables.py
@@ -0,0 +1,51 @@
+##############################################################################
+# 
+# Module: pdcaptive_cables.py
+#
+# Description:
+#     Switch PD to captive cable.
+#
+# Author:
+#     Vinay N, MCCI Corporation May 2024
+#
+#############################################################################
+import usb.core
+
+class PDCaptiveCablesController:
+    def __init__(self, vendor_id, product_id):
+        self.vendor_id = vendor_id
+        self.product_id = product_id
+        self.device = None
+    
+    def find_device(self):
+        self.device = usb.core.find(idVendor=self.vendor_id, idProduct=self.product_id)
+        return self.device is not None
+    
+    def pd_captive_cables(self):
+        if self.device is None:
+            print("Device not found")
+            return False
+        
+        # Control transfer parameters for PdCaptiveCable
+        bmRequestType = 0x40  # Request type: Vendor, Host-to-device, Device-to-interface
+        bRequest = 0xE9       # Request code for PdCaptiveCable command
+        wValue = 0x0000       # Value
+        wIndex = 0x0000       # Index
+        wLength = 0x0000      # Length
+
+        # Send control transfer for PdCaptiveCable command
+        result = self.device.ctrl_transfer(bmRequestType, bRequest, wValue, wIndex, wLength)
+        print("result-pd-captive-cables:", result)
+        if result == 0:
+            print("Switched PD support to captive cable successfully")
+        else:
+            print("Failed to switch PD support to captive cable")
+        return result == 0
+
+def pd_captive_cables_status():
+    VENDOR_ID = 0x045e  # Replace with your vendor ID
+    PRODUCT_ID = 0x078f  # Replace with your product ID
+
+    controller = PDCaptiveCablesController(VENDOR_ID, PRODUCT_ID)
+    if controller.find_device():
+        controller.pd_captive_cables()
