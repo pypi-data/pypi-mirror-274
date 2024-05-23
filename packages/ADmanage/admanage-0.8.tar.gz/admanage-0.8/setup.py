6b12151d0e43a4c1ecad0caf707e4cc8f22f1233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:09 +0000

    Add package ADmanage/0.8/admanage-0.8.tar.gz

---
+++ packages/ADmanage/admanage-0.8.tar.gz/admanage-0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='ADmanage',
+    version='0.8',
+    packages=find_packages(),
+    install_requires=[
+        'ldap3',
+        'impacket'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
