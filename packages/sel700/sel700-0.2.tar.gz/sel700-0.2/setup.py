0f776be1c1155c49b48639120d930fc905632895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 07:05:19 +0000

    Add package sel700/0.2/sel700-0.2.tar.gz

---
+++ packages/sel700/sel700-0.2.tar.gz/sel700-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+with open('README.MD', 'r') as arq:
+    readme = arq.read()
+
+setup(
+    name='sel700',
+    version='0.2',
+    author='Elisandro Peixoto',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    keywords='SEL relays 700',
+    description='Library to use Telnet commands in SEL relays 700 series',
+    packages=['sel700'],
+    install_requires=[]
+)
