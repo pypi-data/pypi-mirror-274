887e9a2997a09022b27b6e3e919db99021f41150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:06:28 +0000

    Add package pydicomsorter/0.9.0/pydicomsorter-0.9.0-py3-none-any.whl

---
+++ packages/pydicomsorter/pydicomsorter-0.9.0-py3-none-any.whl/pydicomsorter/io.py
@@ -0,0 +1,36 @@
+"""I/O utilities."""
+
+from pathlib import Path
+
+from pydicom import dcmread
+from pydicom.errors import InvalidDicomError
+
+async def find_dicom_files(source_dir: Path) -> list[Path]:
+    """Find all DICOM files in the source directory."""
+    return [file.resolve() for file in source_dir.glob("**/*.dcm") if file.is_file()]
+
+
+def read_all(file: Path, tags: list[str]) -> dict[str, str]:
+    """Read all tags from the DICOM file."""
+    try:
+        dicom = dcmread(file, stop_before_pixels=True)
+    except TypeError as te:
+        raise TypeError(f"Type error reading DICOM file: {file}") from te
+    except InvalidDicomError as ide:
+        raise InvalidDicomError(f"Invalid DICOM file: {file}") from ide
+    except ValueError as ve:
+        raise ValueError(f"Value error reading DICOM file: {file}") from ve
+    return {tag: str(dicom.get(tag, "")) for tag in tags}
+
+
+def read_tags(file: Path, tags: list[str]) -> dict[str, str]:
+    """Read the specified tags from the DICOM file."""
+    try:
+        dicom = dcmread(file, specific_tags=tags, stop_before_pixels=True)
+    except TypeError as te:
+        raise TypeError(f"Type error reading DICOM file: {file}") from te
+    except InvalidDicomError as ide:
+        raise InvalidDicomError(f"Invalid DICOM file: {file}") from ide
+    except ValueError as ve:
+        raise ValueError(f"Value error reading DICOM file: {file}") from ve
+    return {tag: str(dicom.get(tag, "")) for tag in tags}
