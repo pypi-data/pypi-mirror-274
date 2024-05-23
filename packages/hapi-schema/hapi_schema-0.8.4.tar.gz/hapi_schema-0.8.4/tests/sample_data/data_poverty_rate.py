00123a4ff8e149ace45c0b10a29efb9a16d3f699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:32 +0000

    Add package hapi-schema/0.8.4/hapi_schema-0.8.4.tar.gz

---
+++ packages/hapi-schema/hapi_schema-0.8.4.tar.gz/hapi_schema-0.8.4/tests/sample_data/data_poverty_rate.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+data_poverty_rate = [
+    dict(
+        resource_hdx_id="90deb235-1bf5-4bae-b231-3393222c2d01",
+        location_ref=1,
+        admin1_name="Province 01",  # not p-coded/normalised yet
+        classification="vulnerable",
+        rate=0.75,
+        reference_period_start=datetime(2024, 1, 1),
+        reference_period_end=datetime(2024, 12, 31),
+    ),
+]
