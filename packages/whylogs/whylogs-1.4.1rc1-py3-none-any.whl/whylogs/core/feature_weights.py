ac4b922cd996386670bf73afa7be1fccc2b0d423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 07:05:06 +0000

    Add package whylogs/1.4.1rc1/whylogs-1.4.1rc1-py3-none-any.whl

---
+++ packages/whylogs/whylogs-1.4.1rc1-py3-none-any.whl/whylogs/core/feature_weights.py
@@ -0,0 +1,41 @@
+import json
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from whylogs.api.writer.writer import Writable, WriterWrapper
+from whylogs.core import Segment
+
+
+class FeatureWeights(Writable):
+    def __init__(self, weights: Dict[str, float], segment: Optional[Segment] = None, metadata: Optional[Dict] = None):
+        """Feature Weights
+
+        Parameters
+        ----------
+        weights : Dict[str, float]
+            Feature weights
+        segment : str, optional
+            If segment is None, weights are considered to be for the complete unsegmented data, by default None
+        """
+        self.weights = weights
+        if segment:
+            raise NotImplementedError("Segmented Feature Weights is currently not supported.")
+        self.segment = segment
+        self.metadata = metadata
+
+    def _get_default_filename(self) -> str:
+        raise ValueError("I'm not a real Writable")
+
+    def _get_default_path(self) -> str:
+        raise ValueError("I'm not a real Writable")
+
+    def write(self, path: Optional[str] = None, **kwargs: Any) -> Tuple[bool, Union[str, List[str]]]:
+        raise ValueError("I'm not a real Writable")
+
+    def to_json(self) -> str:
+        return json.dumps({"segment": self.segment, "weights": self.weights})
+
+    def to_dict(self) -> Dict[str, Union[Optional[Segment], Optional[float]]]:
+        return {"segment": self.segment, "weights": self.weights}
+
+
+FeatureWeightWriter = WriterWrapper
