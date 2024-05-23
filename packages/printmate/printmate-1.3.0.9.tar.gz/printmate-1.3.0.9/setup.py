8bbe154a3c78901e22480ba0fc28dd491879ec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:15 +0000

    Add package printmate/1.3.0.9/printmate-1.3.0.9.tar.gz

---
+++ packages/printmate/printmate-1.3.0.9.tar.gz/printmate-1.3.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='printmate',
+    version='1.3.0.9',
+    packages=find_packages(),
+    install_requires=[
+        "colorama"],
+    entry_points={
+        'console_scripts': [
+            'printmate = printmate.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+)
