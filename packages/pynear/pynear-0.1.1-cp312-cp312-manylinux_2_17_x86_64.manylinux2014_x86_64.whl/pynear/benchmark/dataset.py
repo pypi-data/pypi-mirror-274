3309b611cf2d0246044e005d8e0d5952b6eb4ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 04:04:48 +0000

    Add package pynear/0.1.1/pynear-0.1.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pynear/pynear-0.1.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pynear/benchmark/dataset.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+import numpy as np
+
+
+def generate_gaussian_dataset(total_size: int, num_clusters: int, dim: int, data_type: Any = np.float64) -> np.ndarray:
+    """
+    Generate a set of gaussian clusters of specific size and dimention
+    """
+    # sample gaussan center points
+    cluster_size = total_size // num_clusters
+
+    centers = None
+    scalers = None
+    if data_type == np.uint8:
+        centers = np.random.uniform(0, 255, size=(cluster_size, dim))
+        scalers = np.random.uniform(1, 1, size=(cluster_size, dim))
+    else:
+        centers = np.random.uniform(-10000, 10000, size=(cluster_size, dim))
+        scalers = np.random.uniform(1, 10, size=(cluster_size, dim))
+    datas: list = []
+    for cluster in range(num_clusters):
+        center = centers[cluster, :]
+        scale = scalers[cluster, :]
+        data = np.random.normal(loc=center, scale=scale, size=(cluster_size, dim))
+        datas.append(data)
+
+    return np.concatenate(datas, axis=0).astype(data_type)
