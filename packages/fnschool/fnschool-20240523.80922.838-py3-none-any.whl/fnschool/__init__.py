597e77138c549a0f68c16503168de4da9e0989ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:06:01 +0000

    Add package fnschool/20240523.80922.838/fnschool-20240523.80922.838-py3-none-any.whl

---
+++ packages/fnschool/fnschool-20240523.80922.838-py3-none-any.whl/fnschool/__init__.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import argparse
+import random
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+from openpyxl import load_workbook
+from openpyxl.styles import numbers
+from openpyxl.styles import Font
+from fnschool.app import *
+from fnschool.language import _
+from fnschool.fnprint import *
+from fnschool.path import *
+from fnschool.entry import *
+from fnschool.external import *
+
+
+__version__ = "20240523.80922.838"
+
+
+# The end.
