a25a460ee404b0de3b4e76aeaad113d8fbeedb79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:04:44 +0000

    Add package blaze-double-bot/0.1.2/blaze_double_bot-0.1.2.tar.gz

---
+++ packages/blaze-double-bot/blaze_double_bot-0.1.2.tar.gz/blaze_double_bot-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open('README.md', 'r') as f:
+          readme =f.read()
+
+setup(
+    name='blaze_double_bot',
+    licence='MIT License',
+    version='0.1.2',
+    author='Rafael de Oliveira Ribeiro',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='ror74559@gmail.com',
+    description='This Python library automates the betting process on the Blaze Double',
+    packages=['blaze_double_bot'],
+    install_requires=[
+        'undetected_chromedriver',
+        'selenium',
+        'requests'
+    ],
+   
+)
+
