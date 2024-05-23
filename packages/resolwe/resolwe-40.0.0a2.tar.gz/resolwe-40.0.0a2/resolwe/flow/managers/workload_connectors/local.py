ffe1489d95dc864a7890ad70aca2c5e520ede1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:02 +0000

    Add package resolwe/40.0.0a2/resolwe-40.0.0a2.tar.gz

---
+++ packages/resolwe/resolwe-40.0.0a2.tar.gz/resolwe-40.0.0a2/resolwe/flow/managers/workload_connectors/local.py
@@ -0,0 +1,39 @@
+""".. Ignore pydocstyle D400.
+
+===============
+Local Connector
+===============
+
+"""
+
+import logging
+import subprocess
+
+from resolwe.flow.models import Data
+from resolwe.storage import settings as storage_settings
+from resolwe.utils import BraceMessage as __
+
+from .base import BaseConnector
+
+logger = logging.getLogger(__name__)
+
+
+class Connector(BaseConnector):
+    """Local connector for job execution."""
+
+    def submit(self, data: Data, argv):
+        """Run process locally.
+
+        For details, see
+        :meth:`~resolwe.flow.managers.workload_connectors.base.BaseConnector.submit`.
+        """
+        logger.debug(
+            __(
+                "Connector '{}' running for Data with id {} ({}).",
+                self.__class__.__module__,
+                data.id,
+                repr(argv),
+            )
+        )
+        runtime_dir = storage_settings.FLOW_VOLUMES["runtime"]["config"]["path"]
+        subprocess.Popen(argv, cwd=runtime_dir, stdin=subprocess.DEVNULL).wait()
---
+++ packages/resolwe/resolwe-40.0.0a2.tar.gz/resolwe-40.0.0a2/resolwe/flow/models/worker.py
@@ -0,0 +1,61 @@
+"""Worker data model representing the state of the executor."""
+
+import logging
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+from django.db import models
+
+logger = logging.getLogger(__name__)
+
+
+class Worker(models.Model):
+    """Stores information about worker."""
+
+    STATUS_PREPARING = "PP"
+    STATUS_FINISHED_PREPARING = "FP"
+    STATUS_PROCESSING = "PR"
+    STATUS_NONRESPONDING = "NR"
+    STATUS_COMPLETED = "CM"
+    STATUS_ERROR_PREPARING = "EP"
+
+    FINAL_STATUSES = (
+        STATUS_COMPLETED,
+        STATUS_ERROR_PREPARING,
+        STATUS_NONRESPONDING,
+    )
+
+    STATUS_CHOICES = (
+        (STATUS_PROCESSING, "Processing data"),
+        (STATUS_NONRESPONDING, "Unresponsive"),
+        (STATUS_PREPARING, "Preparing data"),
+        (STATUS_COMPLETED, "Finished"),
+    )
+
+    started = models.DateTimeField(auto_now=True)
+    finished = models.DateTimeField(null=True)
+    data = models.OneToOneField(
+        "flow.Data", on_delete=models.CASCADE, related_name="worker"
+    )
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+
+    def terminate(self):
+        """Terminate the running worker."""
+
+        # Can only terminate running processes.
+        if self.status != self.STATUS_PROCESSING:
+            logger.info(
+                "Worker has status %s, no terminate signal is sent.", self.status
+            )
+            return
+
+        packet = {"type": "terminate", "identity": str(self.data.id).encode()}
+        from resolwe.flow.managers.state import LISTENER_CONTROL_CHANNEL
+
+        logger.debug("Sending the terminate signal for data '%d'.", self.data_id)
+        async_to_sync(get_channel_layer().send)(LISTENER_CONTROL_CHANNEL, packet)
+
+    def __str__(self):
+        """Return the string representation."""
+        return f"Worker(status={self.status}, data={self.data}, status={self.status})"
