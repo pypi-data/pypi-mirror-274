af68223c45e598ebe1786166e70a8ef693d35749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:44 +0000

    Add package ventures/1.6.0/ventures-1.6.0.tar.gz

---
+++ packages/ventures/ventures-1.6.0.tar.gz/ventures-1.6.0/venues/stages/ventures/plays/turn_off.py
@@ -0,0 +1,55 @@
+
+
+''''
+	turn_off ({
+		
+	})
+	
+"'''
+
+
+#++++
+#
+from ventures.utilities.map.etch import etch_map
+from ventures.utilities.map.scan import scan_map	
+from ventures.utilities.ventures.find_venture import find_venture
+from ventures.utilities.process.check_is_on import check_is_on
+#
+from ventures.plays_venture.turn_off import turn_off_venture
+#
+#
+import rich
+#
+#
+import json
+import signal
+import os
+import time
+#
+#++++
+
+
+def turn_off (packet):
+	ventures_map_bracket = packet ["ventures_map_bracket"]
+	ventures = packet ["ventures"]
+	
+	unfinished = []
+	for name in ventures_map_bracket:
+		try:
+			turn_off_venture ({
+				"venture": find_venture (ventures, name),
+				"ventures_map_bracket": ventures_map_bracket
+			})
+		except Exception as E:
+			unfinished.append ({
+				"name": name
+			})
+			
+	return {
+		"unfinished": unfinished
+	}
+	
+	
+		
+	
+	
