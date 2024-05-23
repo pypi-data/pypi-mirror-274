bf0e7c69a50ce3de444ddc8308892b6798e7d302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:36 +0000

    Add package karp-lex-types/0.7.2/karp_lex_types-0.7.2-py3-none-any.whl

---
+++ packages/karp-lex-types/karp_lex_types-0.7.2-py3-none-any.whl/karp_lex_types/value_objects/__init__.py
@@ -0,0 +1,13 @@
+from karp_lex_types.value_objects.unique_id import (  # noqa: D104
+    UniqueId,
+    UniqueIdStr,
+    UniqueIdType,
+    make_unique_id,
+)
+
+__all__ = [
+    "UniqueId",
+    "UniqueIdStr",
+    "UniqueIdType",
+    "make_unique_id",
+]
