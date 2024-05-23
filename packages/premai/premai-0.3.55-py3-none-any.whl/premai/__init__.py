5352a3593942af24579fccfb701da3ab08243eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:18 +0000

    Add package premai/0.3.55/premai-0.3.55-py3-none-any.whl

---
+++ packages/premai/premai-0.3.55-py3-none-any.whl/premai/__init__.py
@@ -0,0 +1,32 @@
+from .client import AuthenticatedClient
+
+from .api import (
+    EmbeddingsModule,
+    ModelsModule,
+    FeedbacksModule,
+    TracesModule,
+    ChatModuleWrapper,
+    RepositoryModuleWrapper,
+)
+
+class Prem:
+    embeddings: EmbeddingsModule
+    models: ModelsModule
+    feedbacks: FeedbacksModule
+    traces: TracesModule
+    chat: ChatModuleWrapper
+    repository: RepositoryModuleWrapper
+
+    def __init__(self, api_key: str, base_url='https://app.premai.io'):
+        client = AuthenticatedClient(token=api_key, base_url=base_url)
+        # Init modules
+        self.embeddings = EmbeddingsModule(client)
+        self.models = ModelsModule(client)
+        self.feedbacks = FeedbacksModule(client)
+        self.traces = TracesModule(client)
+        self.chat = ChatModuleWrapper(client)
+        self.repository = RepositoryModuleWrapper(client)
+
+__all__ = [
+    "Prem"
+]
