755c2b92aaee329ff18d436d8b4e690fdf3099bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:43 +0000

    Add package agixt/1.5.16/agixt-1.5.16-py3-none-any.whl

---
+++ packages/agixt/agixt-1.5.16-py3-none-any.whl/agixt/ApiClient.py
@@ -0,0 +1,93 @@
+import os
+import logging
+import jwt
+from agixtsdk import AGiXTSDK
+from dotenv import load_dotenv
+from fastapi import Header, HTTPException
+
+load_dotenv()
+logging.basicConfig(
+    level=os.environ.get("LOGLEVEL", "INFO"),
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+USING_JWT = True if os.getenv("USING_JWT", "false").lower() == "true" else False
+DB_CONNECTED = True if os.getenv("DB_CONNECTED", "false").lower() == "true" else False
+WORKERS = int(os.getenv("UVICORN_WORKERS", 10))
+AGIXT_URI = os.getenv("AGIXT_URI", "http://localhost:7437")
+AGIXT_API_KEY = os.getenv("AGIXT_API_KEY", None)
+ApiClient = AGiXTSDK(base_uri="http://localhost:7437", api_key=AGIXT_API_KEY)
+
+# Defining these here to be referenced externally.
+if DB_CONNECTED:
+    from db.Agent import Agent, add_agent, delete_agent, rename_agent, get_agents
+    from db.Chain import Chain
+    from db.Prompts import Prompts
+    from db.Conversations import Conversations
+else:
+    from fb.Agent import Agent, add_agent, delete_agent, rename_agent, get_agents
+    from fb.Chain import Chain
+    from fb.Prompts import Prompts
+    from fb.Conversations import Conversations
+
+
+def verify_api_key(authorization: str = Header(None)):
+    load_dotenv()
+    USING_JWT = True if os.getenv("USING_JWT", "false").lower() == "true" else False
+    AGIXT_API_KEY = os.getenv("AGIXT_API_KEY", None)
+    DEFAULT_USER = os.getenv("DEFAULT_USER", "USER")
+    if DEFAULT_USER == "" or DEFAULT_USER is None or DEFAULT_USER == "None":
+        DEFAULT_USER = "USER"
+    if AGIXT_API_KEY:
+        if authorization is None:
+            logging.info("Authorization header is missing")
+            raise HTTPException(
+                status_code=401, detail="Authorization header is missing"
+            )
+        authorization = str(authorization).replace("Bearer ", "").replace("bearer ", "")
+        if USING_JWT:
+            try:
+                token = jwt.decode(
+                    jwt=authorization,
+                    key=AGIXT_API_KEY,
+                    algorithms=["HS256"],
+                )
+                return token["email"]
+            except Exception as e:
+                if authorization != AGIXT_API_KEY:
+                    logging.info(f"Invalid API Key: {authorization}")
+                    raise HTTPException(status_code=401, detail="Invalid API Key")
+                return DEFAULT_USER
+        if authorization != AGIXT_API_KEY:
+            logging.info(f"Invalid API Key: {authorization}")
+            raise HTTPException(status_code=401, detail="Invalid API Key")
+    else:
+        return DEFAULT_USER
+
+
+def get_api_client(authorization: str = Header(None)):
+    authorization = str(authorization).replace("Bearer ", "").replace("bearer ", "")
+    return AGiXTSDK(base_uri="http://localhost:7437", api_key=authorization)
+
+
+def is_admin(email: str = "USER", api_key: str = None):
+    load_dotenv()
+    AGIXT_API_KEY = os.getenv("AGIXT_API_KEY", "")
+    DB_CONNECTED = (
+        True if os.getenv("DB_CONNECTED", "false").lower() == "true" else False
+    )
+    if DB_CONNECTED != True:
+        return True
+    if api_key is None:
+        api_key = ""
+    api_key = api_key.replace("Bearer ", "").replace("bearer ", "")
+    if AGIXT_API_KEY == api_key:
+        return True
+    if DB_CONNECTED == True:
+        from db.User import is_agixt_admin
+
+        if email == "" or email is None or email == "None":
+            email = os.getenv("DEFAULT_USER", "USER")
+            if email == "" or email is None or email == "None":
+                email = "USER"
+        return is_agixt_admin(email=email, api_key=api_key)
+    return False
---
+++ packages/agixt/agixt-1.5.16-py3-none-any.whl/agixt/Defaults.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DEFAULT_USER = os.getenv("DEFAULT_USER", "USER")
+
+DEFAULT_SETTINGS = {
+    "provider": "gpt4free",
+    "mode": "prompt",
+    "prompt_category": "Default",
+    "prompt_name": "Chat",
+    "embeddings_provider": "default",
+    "tts_provider": "None",
+    "transcription_provider": "default",
+    "translation_provider": "default",
+    "image_provider": "None",
+    "vision_provider": "None",
+    "AI_MODEL": "mixtral-8x7b",
+    "AI_TEMPERATURE": "0.7",
+    "AI_TOP_P": "1",
+    "MAX_TOKENS": "4096",
+    "helper_agent_name": "gpt4free",
+    "websearch": False,
+    "websearch_depth": 3,
+    "WEBSEARCH_TIMEOUT": 0,
+    "WAIT_BETWEEN_REQUESTS": 1,
+    "WAIT_AFTER_FAILURE": 3,
+    "WORKING_DIRECTORY": "./WORKSPACE",
+    "WORKING_DIRECTORY_RESTRICTED": True,
+    "AUTONOMOUS_EXECUTION": True,
+    "PERSONA": "",
+}
