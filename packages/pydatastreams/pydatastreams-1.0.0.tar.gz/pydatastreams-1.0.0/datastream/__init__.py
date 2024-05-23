206c35dc57306dd5dcf21ec28c232a1c6b2487c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 07:05:15 +0000

    Add package pydatastreams/1.0.0/pydatastreams-1.0.0.tar.gz

---
+++ packages/pydatastreams/pydatastreams-1.0.0.tar.gz/pydatastreams-1.0.0/datastream/__init__.py
@@ -0,0 +1,5 @@
+from datastream.base import ByteOrder
+from datastream.deserializing import DeserializingStream
+from datastream.serializing import SerializingStream
+
+__all__ = ["ByteOrder", "DeserializingStream", "SerializingStream"]
