a12baff500ba42cc5a4b81e401955928f8888ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:06:23 +0000

    Add package onetick-latency/0.1.9/onetick_latency-0.1.9.tar.gz

---
+++ packages/onetick-latency/onetick_latency-0.1.9.tar.gz/onetick_latency-0.1.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Safely read the long description from 'README.md'
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='onetick_latency',
+    version='0.1.9',
+    description='Library for latency simulation and query for Onetick',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alexandre Levert',
+    author_email='alexandre.levert.1@bnc.ca',
+    url='https://gitlab.awsged.com/bigdata/onetick_latency.git',
+    packages=find_packages(include=['onetick_latency*', 'modules*', 'pythonAPI*']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'pytz'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
