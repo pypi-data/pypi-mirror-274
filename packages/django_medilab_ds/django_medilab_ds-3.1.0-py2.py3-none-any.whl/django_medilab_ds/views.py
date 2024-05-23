ce6c5a12366c57b804b43f4965c0f1562dbc0004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:06:06 +0000

    Add package django_medilab_ds/3.1.0/django_medilab_ds-3.1.0-py2.py3-none-any.whl

---
+++ packages/django_medilab_ds/django_medilab_ds-3.1.0-py2.py3-none-any.whl/django_medilab_ds/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django_utilsds import utils
+from .forms import AppointmentForm
+from _data import medilabds
+
+import logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s: [%(name)s] %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.ERROR)
+
+template_name = medilabds.context['template_name']
+c = medilabds.context
+
+
+def robots(request):
+    from django.shortcuts import HttpResponse
+    file_content = utils.make_robots()
+    return HttpResponse(file_content, content_type="text/plain")
+
+
+def home(request):
+    logger.info(c)
+    if request.method == 'GET':
+        c.update({'form': AppointmentForm()})
+        c['post_message'] = None
+        return render(request, template_name + '/index.html', c)
+    elif request.method == "POST":
+        c.update(make_post_context(request.POST, c['basic_info']['consult_email']))
+        return render(request, template_name + '/index.html', c)
+
+
+def make_post_context(request_post, consult_mail):
+    logger.info(request_post)
+    context = {}
+    # appointment 앱에서 post 요청을 처리함.
+    logger.info(f'request.POST : {request_post}')
+    form = AppointmentForm(request_post)
+
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        logger.info(f'Pass validation test -  {name} {email} {subject} {message}')
+        is_sendmail = utils.mail_to(title=f'{name} 고객 상담 문의',
+                                    text=f'이름: {name}\n메일: {email}\n제목: {subject}\n메시지: {message}',
+                                    mail_addr=consult_mail)
+        if is_sendmail:
+            context['post_message'] = '담당자에게 내용이 전달되었습니다. 확인 후 바로 연락 드리겠습니다. 감사합니다.'
+        else:
+            context['post_message'] = '메일 전송에서 오류가 발생하였습니다. 카카오톡이나 전화로 문의주시면 감사하겠습니다. 죄송합니다.'
+        return context
+    else:
+        logger.error('Fail form validation test')
+        context['post_message'] = '입력 항목이 유효하지 않습니다. 다시 입력해 주십시요.'
+        return context
