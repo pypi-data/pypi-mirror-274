d826658494c4ee355785754c3f0bdac63bf61447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:03 +0000

    Add package timetable-to-image/1.2.4/timetable-to-image-1.2.4.tar.gz

---
+++ packages/timetable-to-image/timetable-to-image-1.2.4.tar.gz/timetable-to-image-1.2.4/src/TimetableToImage/Timetable/__init__.py
@@ -0,0 +1,15 @@
+from .week import Week
+from .day import Day
+from .pair import Pair
+from .lesson import Lesson
+
+from .bells import Bells
+from .bell import Bell
+
+from .func import get_timetable_lesson_from_json
+from .func import get_timetable_pair_from_json
+from .func import get_timetable_day_from_json
+from .func import get_timetable_week_from_json
+
+from .func import get_timetable_bell_from_json
+from .func import get_timetable_bells_from_json
---
+++ packages/timetable-to-image/timetable-to-image-1.2.4.tar.gz/timetable-to-image-1.2.4/src/TimetableToImage/Timetable/day.py
@@ -0,0 +1,8 @@
+import datetime
+
+
+class Day:
+    def __init__(self):
+        self.number = None
+        self.date = None
+        self.pairs = None
