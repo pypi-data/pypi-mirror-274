03b16baa0ff7b7bd58306716f3639d090b775a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:04:39 +0000

    Add package lk-logger/5.7.4/lk_logger-5.7.4-py3-none-any.whl

---
+++ packages/lk-logger/lk_logger-5.7.4-py3-none-any.whl/lk_logger/__init__.py
@@ -0,0 +1,37 @@
+from . import console
+from . import printer
+from .control import disable
+from .control import disable as mute
+from .control import enable
+from .control import enable as unmute
+from .control import setup
+from .control import start_ipython
+from .control import unload
+from .control import unload as restore_builtin_print
+from .control import update
+from .frame_info import FrameInfo
+from .logger import logger
+from .pipeline import pipeline
+from .printer import bprint
+from .printer import parallel_printing
+
+
+def __init() -> None:
+    # import traceback
+    # pipeline.add(traceback, bprint)
+    # setup(quiet=True)
+    
+    # from .printer import dprint
+    # dprint(f'{__IPYTHON__=}')
+    try:
+        __IPYTHON__  # noqa
+    except NameError:
+        pass
+    else:
+        import IPython
+        pipeline.add(IPython, bprint, scope=True)
+        # pipeline.add('[ipython]', bprint)
+
+
+__init()
+__version__ = '5.7.4'
---
+++ packages/lk-logger/lk_logger-5.7.4-py3-none-any.whl/lk_logger/config.py
@@ -0,0 +1,147 @@
+import sys
+import typing as t
+from sys import excepthook as _default_excepthook
+
+
+class LoggingConfig:
+    """
+    options:
+        show_source: bool[true]
+            add source info (filepath and line number) prefix to log messages.
+            example:
+                logger.log('hello world')
+                # enabled : './main.py:10  >>  hello world'
+                # disabled: 'hello world'
+        show_varnames: bool[false]
+            show both var names and values. (magic reflection)
+            example:
+                a, b = 1, 2
+                logger.log(a, b, a + b)
+                # enabled : 'main.py:11  >>  a = 1; b = 2; a + b = 3'
+                # disabled: 'main.py:11  >>  1, 2, 3'
+        show_external_lib: bool[true]
+            if `param source` came from an external library, whether to print.
+            for example, if a third-party library 'xxx' also used `logger.log`,
+            its source path (relative to current working dir) may be very long,
+            if you don't want to see any prints except your own project, you'd
+            set this to False.
+
+        # the following options are only available if `show_external_lib` is
+        # true.
+        path_style_for_external_lib: literal
+            literal:
+                'pretty_relpath': default
+                    trunscate the source path of external lib to be shorter.
+                    example:
+                        before:
+                            '../../../../site-packages/lk_logger/sourcemap.py'
+                            # there may be a lot of '../'.
+                        after:
+                            '[lk_logger]/sourcemap.py'
+                'relpath':
+                    a relative path to current working dir. (<- `os.getcwd()`)
+                    note there may be a lot of '../../../...' if external lib
+                    is far beyond the current working dir.
+                'lib_name_only':
+                    show only the library name (surrounded by brackets).
+                    example: '[lk_logger]'
+            ps: if you don't want to show anything, you should turn to set
+            `show_external_lib` to False.
+    """
+    async_: bool
+    clear_unfinished_stream: bool
+    console_width: t.Optional[int]
+    path_style_for_external_lib: str
+    rich_traceback: bool
+    separator: str
+    show_external_lib: bool
+    show_funcname: bool
+    show_source: bool
+    show_varnames: bool
+    sourcemap_alignment: t.Literal['left', 'right']
+    v2_meaning: t.Literal['info', 'success']  # TODO: not used.
+    
+    _preset_conf = {
+        'async_'                     : False,  # TODO
+        'clear_unfinished_stream'    : False,
+        'console_width'              : None,
+        'path_style_for_external_lib': 'pretty_relpath',
+        'rich_traceback'             : True,
+        'separator'                  : ';   ',
+        'show_external_lib'          : True,
+        'show_funcname'              : True,
+        'show_source'                : True,
+        'show_varnames'              : False,
+        'sourcemap_alignment'        : 'left',
+        'v2_meaning'                 : 'info',
+    }
+    
+    def __init__(self, **kwargs) -> None:
+        for k, v in self._merge_dict(self._preset_conf, kwargs).items():
+            self._apply(k, v)
+    
+    def update(self, **kwargs) -> None:
+        for k, v in kwargs.items():
+            if k in self._preset_conf and v != getattr(self, k, None):
+                self._apply(k, v)
+    
+    def reset(self) -> None:
+        for k, v in self._preset_conf.items():
+            if v != getattr(self, k, None):
+                self._apply(k, v)
+    
+    def _apply(self, key: str, val: t.Union[bool, int, str]) -> None:
+        setattr(self, key, val)
+        if key == 'console_width':
+            if val and isinstance(val, int):
+                from .console import console
+                console.width = val
+        elif key == 'rich_traceback':
+            import sys
+            from functools import partial
+            if val:
+                # https://rich.readthedocs.io/en/stable/traceback.html
+                from rich.traceback import install
+                from .console import console
+                install(console=console, show_locals=False)
+                modified = sys.excepthook
+                sys.excepthook = partial(
+                    self._wrap_system_excepthook,
+                    callback=modified
+                )
+            else:
+                sys.excepthook = self._wrap_system_excepthook
+    
+    @staticmethod
+    def _wrap_system_excepthook(
+        type_, value, traceback, callback=_default_excepthook
+    ) -> None:
+        # print(':r', '[red dim]drain out message queue[/]')
+        # from .logger import logger
+        # if hasattr(logger, '_stop_running'):
+        #     logger._stop_running()  # noqa
+        if type_ is KeyboardInterrupt:
+            print(':r', '[red dim]KeyboardInterrupt[/]')
+            sys.exit(0)
+        else:
+            callback(type_, value, traceback)
+    
+    # -------------------------------------------------------------------------
+    
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        return {
+            k: getattr(self, k)
+            for k in self._preset_conf
+        }
+    
+    @staticmethod
+    def _merge_dict(base: dict, update: dict) -> dict:
+        return {k: update.get(k, default_v) for k, default_v in base.items()}
+    
+    @staticmethod
+    def _diff_dict(base: dict, update: dict) -> dict:
+        out = {}
+        for k, v0 in base.items():
+            if k in update and v0 != (v1 := update[k]):
+                out[k] = v1
+        return out
