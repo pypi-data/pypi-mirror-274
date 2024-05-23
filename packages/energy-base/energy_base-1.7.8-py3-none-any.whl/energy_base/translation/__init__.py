5dddd81c5bc6894e943f819956f0504cb4b76b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:36 +0000

    Add package energy-base/1.7.8/energy_base-1.7.8-py3-none-any.whl

---
+++ packages/energy-base/energy_base-1.7.8-py3-none-any.whl/energy_base/translation/__init__.py
@@ -0,0 +1,14 @@
+from typing import Callable, Any
+
+from energy_base.translation.utils import translate
+
+trans = translate
+t = translate
+_ = translate
+
+__all__ = [
+    'translate'
+    'trans',
+    't',
+    '_'
+]
---
+++ packages/energy-base/energy_base-1.7.8-py3-none-any.whl/energy_base/utils/__init__.py
@@ -0,0 +1,10 @@
+from .dict_util import get_by_dotted as dict_get_dotted
+from .date import EDate, EDateTime
+from .data import EData
+
+__all__ = [
+    'dict_get_dotted',
+    'EData',
+    'EDate',
+    'EDateTime',
+]
