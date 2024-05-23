24ef9a76fbec28b834aca4f8c07ceafd4f4bac4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:05:01 +0000

    Add package plg/0.1.0/plg-0.1.0.tar.gz

---
+++ packages/plg/plg-0.1.0.tar.gz/plg-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plg',
+    version='0.1.0',
+    author='CLK',
+    author_email='617682513@qq.com',
+    description='A short description of your project',
+    url='https://github.com/chenlike/plg-test',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+
+    ],
+    project_urls={
+        'Source': 'https://github.com/chenlike/plg-test',
+    },
+)
