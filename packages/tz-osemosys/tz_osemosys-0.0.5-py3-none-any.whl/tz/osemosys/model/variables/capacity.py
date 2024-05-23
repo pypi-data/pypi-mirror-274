831f39a28b147561da4c325df34992f655b1f3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:23 +0000

    Add package tz-osemosys/0.0.5/tz_osemosys-0.0.5-py3-none-any.whl

---
+++ packages/tz-osemosys/tz_osemosys-0.0.5-py3-none-any.whl/tz/osemosys/model/variables/capacity.py
@@ -0,0 +1,30 @@
+import xarray as xr
+from linopy import Model
+from numpy import inf
+
+
+def add_capacity_variables(ds: xr.Dataset, m: Model) -> Model:
+    """Add capacity variables to the model
+
+    Arguments
+    ---------
+    ds: xarray.Dataset
+        The parameters dataset
+    m: linopy.Model
+        A linopy model
+
+    Returns
+    -------
+    linopy.Model
+    """
+    # Create the required index
+    RTeY = [ds.coords["REGION"], ds.coords["TECHNOLOGY"], ds.coords["YEAR"]]
+
+    # masks
+    mask = ds["CapacityOfOneTechnologyUnit"].notnull()
+
+    m.add_variables(
+        lower=0, upper=inf, coords=RTeY, name="NumberOfNewTechnologyUnits", integer=True, mask=mask
+    )
+    m.add_variables(lower=0, upper=inf, coords=RTeY, name="NewCapacity", integer=False)
+    return m
