c83e21a60eee4b050ac8ba0f1d76080ac8ad670f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:52 +0000

    Add package mqt-core/2.5.0/mqt_core-2.5.0-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/mqt-core/mqt_core-2.5.0-cp312-cp312-macosx_10_15_x86_64.whl/mqt/core/include/mqt-core/algorithms/RandomCliffordCircuit.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QuantumComputation.hpp>
+#include <functional>
+#include <random>
+
+namespace qc {
+class RandomCliffordCircuit : public QuantumComputation {
+protected:
+  std::function<std::uint16_t()> cliffordGenerator;
+
+  void append1QClifford(std::uint16_t idx, Qubit target);
+  void append2QClifford(std::uint16_t, Qubit control, Qubit target);
+
+public:
+  std::size_t depth = 1;
+  std::size_t seed = 0;
+
+  explicit RandomCliffordCircuit(std::size_t nq, std::size_t depth = 1,
+                                 std::size_t seed = 0);
+
+  std::ostream& printStatistics(std::ostream& os) const override;
+};
+} // namespace qc
