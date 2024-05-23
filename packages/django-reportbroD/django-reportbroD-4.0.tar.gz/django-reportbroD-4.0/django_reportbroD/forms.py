d42a43dbb53c5f38392aaf004e9304ffc3996544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:17 +0000

    Add package django-reportbroD/4.0/django-reportbroD-4.0.tar.gz

---
+++ packages/django-reportbroD/django-reportbroD-4.0.tar.gz/django-reportbroD-4.0/django_reportbroD/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import ReportDefinition
+from django.utils.translation import gettext_lazy  as _
+
+class ReportForm(forms.ModelForm):
+
+    class Meta:
+        model=ReportDefinition
+        fields=['name', 'remark']
+        labels={
+            
+             'name':'Nombre',
+            'remark':'Descripcion'
+
+        }
+
+        widgets={
+           'name':forms.TextInput(attrs={"class":"form-control", "id":"nombre" , "placeholder": _("holdern")}),
+            'remark':forms.Textarea(attrs={"class":"form-control", "rows":"3" ,"id":"desc" ,"placeholder": _("holderd")}),
+            
+            }  
+
+
+
+class ReportImpForm(forms.Form):
+
+    template= forms.FileField(label="Plantilla",widget=forms.FileInput(attrs={"class":"form-control-file","id":"filetemplate", "placeholder": _("holderf")}))
+
+    
+
+
+      
