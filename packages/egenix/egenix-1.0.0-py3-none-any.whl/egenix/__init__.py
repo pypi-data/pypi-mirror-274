f3662c7afc31ac7835967d254ac308decdc4dcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:34 +0000

    Add package egenix/1.0.0/egenix-1.0.0-py3-none-any.whl

---
+++ packages/egenix/egenix-1.0.0-py3-none-any.whl/egenix/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    eGenix.com Namespace Package
+
+    This namespace package will host future eGenix.com library releases
+    as sub-packages.
+
+    Written by Marc-Andre Lemburg.
+    Copyright (c) 2024, eGenix.com Software GmbH; mailto:info@egenix.com
+    License: Apache-2.0
+
+"""
+# Public API
+#
+# None at the moment
+
+# Version
+__version__ = '1.0.0'
