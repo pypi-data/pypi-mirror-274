a63236ad8fdbcf2f781f6c2121428ba740d56faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 05:05:16 +0000

    Add package moonshotai/0.0.0.dev1/moonshotai-0.0.0.dev1-py3-none-any.whl

---
+++ packages/moonshotai/moonshotai-0.0.0.dev1-py3-none-any.whl/moonshotai/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is a placeholder for the moonshotai package.
+The development of the package is still in progress.
+"""
+
+from .version import __version__
+
+__all__ = [
+    "__version__",
+]
