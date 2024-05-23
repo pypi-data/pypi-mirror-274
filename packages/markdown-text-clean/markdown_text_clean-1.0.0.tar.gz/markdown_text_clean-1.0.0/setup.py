970ac47f8965dac6f99ce7760598b287edf98a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 15:05:06 +0000

    Add package markdown-text-clean/1.0.0/markdown_text_clean-1.0.0.tar.gz

---
+++ packages/markdown-text-clean/markdown_text_clean-1.0.0.tar.gz/markdown_text_clean-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="markdown_text_clean",
+    version="1.0.0",
+    LICENSE = 'MIT',
+    description="A package to clean markdown text",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    packages=find_packages(),
+    CLASSIFIERS=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6'
+    ],
+)
