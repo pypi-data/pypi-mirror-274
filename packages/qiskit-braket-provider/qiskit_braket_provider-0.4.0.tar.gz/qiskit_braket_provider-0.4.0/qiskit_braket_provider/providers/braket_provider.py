a7b8acf5123eb76e4a6d574d191c99799ffcabe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 07:05:20 +0000

    Add package qiskit-braket-provider/0.4.0/qiskit_braket_provider-0.4.0.tar.gz

---
+++ packages/qiskit-braket-provider/qiskit_braket_provider-0.4.0.tar.gz/qiskit_braket_provider-0.4.0/qiskit_braket_provider/providers/braket_provider.py
@@ -0,0 +1,84 @@
+"""Amazon Braket provider."""
+
+import warnings
+
+from braket.aws import AwsDevice
+from braket.device_schema.dwave import DwaveDeviceCapabilities
+from braket.device_schema.quera import QueraDeviceCapabilities
+from braket.device_schema.xanadu import XanaduDeviceCapabilities
+from qiskit.providers import ProviderV1
+
+from .braket_backend import BraketAwsBackend, BraketLocalBackend
+
+
+class BraketProvider(ProviderV1):
+    """BraketProvider class for accessing Amazon Braket backends.
+
+    Example:
+        >>> provider = BraketProvider()
+        >>> backends = provider.backends()
+        >>> backends
+        [BraketBackend[Aria 1],
+         BraketBackend[Aria 2],
+         BraketBackend[Aspen-M-3],
+         BraketBackend[Forte 1],
+         BraketBackend[Harmony],
+         BraketBackend[Lucy],
+         BraketBackend[SV1],
+         BraketBackend[TN1],
+         BraketBackend[dm1]]
+    """
+
+    def backends(self, name=None, **kwargs):
+        if kwargs.get("local"):
+            return [
+                BraketLocalBackend(name="braket_sv"),
+                BraketLocalBackend(name="braket_dm"),
+            ]
+        names = [name] if name else None
+        devices = AwsDevice.get_devices(names=names, **kwargs)
+        # filter by supported devices
+        # gate models are only supported
+        supported_devices = [
+            d
+            for d in devices
+            if not isinstance(
+                d.properties,
+                (
+                    DwaveDeviceCapabilities,
+                    XanaduDeviceCapabilities,
+                    QueraDeviceCapabilities,
+                ),
+            )
+        ]
+        return [
+            BraketAwsBackend(
+                device=device,
+                provider=self,
+                name=device.name,
+                description=f"AWS Device: {device.provider_name} {device.name}.",
+                online_date=device.properties.service.updatedAt,
+                backend_version="2",
+            )
+            for device in supported_devices
+        ]
+
+
+class AWSBraketProvider(BraketProvider):
+    """AWSBraketProvider class for accessing Amazon Braket backends."""
+
+    def __init_subclass__(cls, **kwargs):
+        """This throws a deprecation warning on subclassing."""
+        warnings.warn(
+            f"{cls.__name__} is deprecated.", DeprecationWarning, stacklevel=2
+        )
+        super().__init_subclass__(**kwargs)
+
+    def __init__(self):
+        """This throws a deprecation warning on initialization."""
+        warnings.warn(
+            f"{self.__class__.__name__} is deprecated. Use BraketProvider instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        super().__init__()
---
+++ packages/qiskit-braket-provider/qiskit_braket_provider-0.4.0.tar.gz/qiskit_braket_provider-0.4.0/tests/providers/test_braket_quantum_task.py
@@ -0,0 +1,162 @@
+"""Tests for AWS Braket job."""
+
+from unittest import TestCase
+from unittest.mock import Mock
+
+import pytest
+from braket.aws.aws_quantum_task import AwsQuantumTask
+from qiskit.providers import JobStatus
+
+from qiskit_braket_provider.providers import (
+    AmazonBraketTask,
+    AWSBraketJob,
+    BraketLocalBackend,
+    BraketQuantumTask,
+)
+from tests.providers.mocks import MOCK_LOCAL_QUANTUM_TASK
+
+
+class TestBraketTask(TestCase):
+    """Tests BraketTask."""
+
+    def _get_task(self):
+        return BraketQuantumTask(
+            backend=BraketLocalBackend(name="default"),
+            task_id="AwesomeId",
+            tasks=[MOCK_LOCAL_QUANTUM_TASK],
+            shots=10,
+        )
+
+    def test_task(self):
+        """Tests task."""
+        task = self._get_task()
+
+        self.assertTrue(isinstance(task, BraketQuantumTask))
+        self.assertEqual(task.shots, 10)
+
+        self.assertEqual(task.status(), JobStatus.DONE)
+
+    def test_result(self):
+        """Tests result."""
+        task = self._get_task()
+
+        self.assertEqual(task.result().job_id, "AwesomeId")
+        self.assertEqual(task.result().results[0].data.counts, {"01": 1, "10": 2})
+        self.assertEqual(task.result().results[0].data.memory, ["10", "10", "01"])
+        self.assertEqual(task.result().results[0].status, "COMPLETED")
+        self.assertEqual(task.result().results[0].shots, 3)
+        self.assertEqual(task.result().get_memory(), ["10", "10", "01"])
+
+    def test_queue_position_for_local_quantum_task(self):
+        """Tests job status when multiple task status is present."""
+        task = BraketQuantumTask(
+            backend=BraketLocalBackend(name="default"),
+            task_id="MockId",
+            tasks=[MOCK_LOCAL_QUANTUM_TASK],
+            shots=100,
+        )
+        message = "We don't provide queue information for the LocalQuantumTask."
+        with pytest.raises(NotImplementedError, match=message):
+            task.queue_position()
+
+
+class TestAmazonBraketTask(TestCase):
+    """Tests AmazonBraketTask."""
+
+    def _get_task(self):
+        return AmazonBraketTask(
+            backend=BraketLocalBackend(name="default"),
+            task_id="AwesomeId",
+            tasks=[MOCK_LOCAL_QUANTUM_TASK],
+            shots=10,
+        )
+
+    def test_task(self):
+        """Tests task."""
+        task = self._get_task()
+
+        self.assertTrue(isinstance(task, AmazonBraketTask))
+        self.assertEqual(task.shots, 10)
+
+        self.assertEqual(task.status(), JobStatus.DONE)
+
+    def test_result(self):
+        """Tests result."""
+        task = self._get_task()
+
+        self.assertEqual(task.result().job_id, "AwesomeId")
+        self.assertEqual(task.result().results[0].data.counts, {"01": 1, "10": 2})
+        self.assertEqual(task.result().results[0].data.memory, ["10", "10", "01"])
+        self.assertEqual(task.result().results[0].status, "COMPLETED")
+        self.assertEqual(task.result().results[0].shots, 3)
+        self.assertEqual(task.result().get_memory(), ["10", "10", "01"])
+
+
+class TestAWSBraketJob(TestCase):
+    """Tests AWSBraketJob"""
+
+    def _get_job(self):
+        return AWSBraketJob(
+            backend=BraketLocalBackend(name="default"),
+            job_id="AwesomeId",
+            tasks=[MOCK_LOCAL_QUANTUM_TASK],
+            shots=10,
+        )
+
+    def test_AWS_job(self):
+        """Tests job."""
+        job = self._get_job()
+
+        self.assertTrue(isinstance(job, AWSBraketJob))
+        self.assertEqual(job.shots, 10)
+
+        self.assertEqual(job.result().job_id, "AwesomeId")
+        self.assertEqual(job.status(), JobStatus.DONE)
+
+    def test_AWS_result(self):
+        """Tests result."""
+        job = self._get_job()
+
+        self.assertEqual(job.result().job_id, "AwesomeId")
+        self.assertEqual(job.result().results[0].data.counts, {"01": 1, "10": 2})
+        self.assertEqual(job.result().results[0].data.memory, ["10", "10", "01"])
+        self.assertEqual(job.result().results[0].status, "COMPLETED")
+        self.assertEqual(job.result().results[0].shots, 3)
+        self.assertEqual(job.result().get_memory(), ["10", "10", "01"])
+
+
+class TestBraketJobStatus:
+    """Tests for Amazon Braket job status."""
+
+    def _get_mock_aws_quantum_task(self, status: str) -> AwsQuantumTask:
+        """
+        Creates a mock AwsQuantumTask with the given status.
+        Status can be one of "CREATED", "QUEUED", "RUNNING", "COMPLETED",
+        "FAILED", "CANCELLING", "CANCELLED"
+        """
+        task = Mock(spec=AwsQuantumTask)
+        task.state.return_value = status
+        return task
+
+    @pytest.mark.parametrize(
+        "task_states, expected_status",
+        [
+            (["COMPLETED", "FAILED"], JobStatus.ERROR),
+            (["COMPLETED", "CANCELLED"], JobStatus.CANCELLED),
+            (["COMPLETED", "COMPLETED"], JobStatus.DONE),
+            (["RUNNING", "RUNNING"], JobStatus.RUNNING),
+            (["QUEUED", "QUEUED"], JobStatus.QUEUED),
+        ],
+    )
+    def test_status(self, task_states, expected_status):
+        """Tests job status when multiple task status is present."""
+        job = AWSBraketJob(
+            backend=BraketLocalBackend(name="default"),
+            job_id="MockId",
+            tasks=[MOCK_LOCAL_QUANTUM_TASK],
+            shots=100,
+        )
+        job._tasks = Mock(spec=BraketQuantumTask)
+        job._tasks = [self._get_mock_aws_quantum_task(state) for state in task_states]
+
+        assert job.status() == expected_status
