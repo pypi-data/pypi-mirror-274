e46afb0105e5e2aab9cdafca2b314f6330518b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:32 +0000

    Add package whaox-wapi/1.1.4/whaox-wapi-1.1.4.tar.gz

---
+++ packages/whaox-wapi/whaox-wapi-1.1.4.tar.gz/whaox-wapi-1.1.4/wapi/features/DELETE.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+from requests import Response
+
+from wapi.features.request import make_request
+from wapi.static.T import T
+from wapi.static.methods import HTTPMethod
+from wapi.utils.get_path import get_path
+
+
+def DELETE(
+        path: str = "",
+        _T: T = None
+) -> T | Response:
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            p = get_path(self, func, path)
+
+            return make_request(
+                url=p,
+                method=HTTPMethod.DELETE,
+                _T=_T,
+                data=kwargs
+            )
+
+        return wrapper
+
+    return decorator
