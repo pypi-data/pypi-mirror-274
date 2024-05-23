56e810fa85ad84f57506c87847becd1d58fd489d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 14:06:10 +0000

    Add package pyerualjetwork/1.1.5/pyerualjetwork-1.1.5-py3-none-any.whl

---
+++ packages/pyerualjetwork/pyerualjetwork-1.1.5-py3-none-any.whl/plan/__init__.py
@@ -0,0 +1,5 @@
+# pyerualjetwork/DPNN/__init__.py
+
+# Bu dosya, plan modülünün ana giriş noktasıdır.
+
+from .plan import AutoBalancer, Normalization, Softmax, Sigmoid, Relu, SynapticPruning, SynapticDividing, WeightIdentification, Fex, Cat, TrainDPNN, TestDPNN, SaveDPNN, LoadDPNN, PredictFromDiscDPNN, PredictFromRamDPNN
