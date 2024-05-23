6f4e04c34dc31588c7a27dc58ff21aab626f85c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:05:32 +0000

    Add package terratorch/0.99.0/terratorch-0.99.0.tar.gz

---
+++ packages/terratorch/terratorch-0.99.0.tar.gz/terratorch-0.99.0/src/terratorch/models/backbones/__init__.py
@@ -0,0 +1,6 @@
+# import so they get registered
+from terratorch.models.backbones.prithvi_vit import TemporalViTEncoder
+
+__all__ = ["TemporalViTEncoder"]
+__all__ = ["TemporalViTEncoder"]
+__all__ = ["TemporalViTEncoder"]
