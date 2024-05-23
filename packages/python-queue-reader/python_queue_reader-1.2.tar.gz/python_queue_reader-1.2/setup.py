b1135b417c1e1d6c12bc18a83c9ba75cbe81eed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 06:06:11 +0000

    Add package python-queue-reader/1.2/python_queue_reader-1.2.tar.gz

---
+++ packages/python-queue-reader/python_queue_reader-1.2.tar.gz/python_queue_reader-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+   name='python_queue_reader',
+   version='1.2',
+   description='Queue reader for python abstracting message brokers such as RabbitMQ', 
+   author='uug.ai',
+   author_email='', #TODO: Add email
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   packages=find_packages(),
+   install_requires=requirements
+)
