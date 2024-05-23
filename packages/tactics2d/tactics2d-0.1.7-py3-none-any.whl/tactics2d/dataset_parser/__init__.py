85b05df21d6e2f8c0fdcc3276906dc97bddc71b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 03:04:52 +0000

    Add package tactics2d/0.1.7/tactics2d-0.1.7-py3-none-any.whl

---
+++ packages/tactics2d/tactics2d-0.1.7-py3-none-any.whl/tactics2d/dataset_parser/__init__.py
@@ -0,0 +1,22 @@
+# #! python3
+# Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
+# @File: __init__.py
+# @Description: Initialize the dataset parser module.
+# @Author: Yueyuan Li
+# @Version: 1.0.0
+
+from .parse_argoverse import ArgoverseParser
+from .parse_dlp import DLPParser
+from .parse_interaction import InteractionParser
+from .parse_levelx import LevelXParser
+from .parse_nuplan import NuPlanParser
+from .parse_womd import WOMDParser
+
+__all__ = [
+    "ArgoverseParser",
+    "DLPParser",
+    "InteractionParser",
+    "LevelXParser",
+    "NuPlanParser",
+    "WOMDParser",
+]
---
+++ packages/tactics2d/tactics2d-0.1.7-py3-none-any.whl/tactics2d/dataset_parser/parse_ngsim.py
@@ -0,0 +1,15 @@
+##! python3
+# Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
+# @File: parse_ngsim.py
+# @Description: This file implements a parser for NGSIM dataset.
+# @Author: Yueyuan Li
+# @Version: 1.0.0
+
+
+class NGSimParser:
+    """
+    TODO: The support of NGSIM dataset is planned to be added before version 1.1.0.
+    """
+
+    def parse_trajectory(self):
+        return
