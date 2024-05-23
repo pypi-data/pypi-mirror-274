2b256e6adc5d70badaee4dfd33f5e070ec8ebae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-23 04:04:59 +0000

    Add package etiket-client/0.2.13/etiket_client-0.2.13-py3-none-any.whl

---
+++ packages/etiket-client/etiket_client-0.2.13-py3-none-any.whl/etiket_client/__init__.py
@@ -0,0 +1,7 @@
+from etiket_client.settings.logging import set_up_logging
+set_up_logging(__name__)
+
+from etiket_client.sync.proc import start_sync_agent
+from etiket_client.remote.authenticate import login, logout
+
+start_sync_agent()
